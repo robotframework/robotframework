267366495a09644b3d5ff9a726abbc37fa8ed8eb
Pekka Klärck <peke@iki.fi>
2008-05-31 16:59:56 +0000

    tests for xmlrpc connection proto (moved from atest)

---
+++ proto/xmlrpc/test/running/XmlRpcServerHandler.py
@@ -0,0 +1,40 @@
+import sys
+import xmlrpclib
+import time
+import socket
+import os.path
+
+        
+def stop_xmlrpc_server(port):
+    url = 'http://localhost:%s' % port
+    server = xmlrpclib.ServerProxy(url)
+    server.robot.stop()
+    print "Server at %s stopped" % url
+    
+def test_xmlrpc_server(port, attempts=1):
+    url = 'http://localhost:%s' % port
+    for i in range(int(attempts)):
+        try:
+            server = xmlrpclib.ServerProxy(url)
+            if not server.robot.get_keyword_names():
+                raise AssertionError("Server at %s returned no keyword names" % url)
+        except socket.error:
+            if i+1 >= attempts:
+                break
+            print "Attempt %d failed. Will try again shortly." % (i+1)
+            time.sleep(1)
+        else:
+            print "Server at %s seems to work ok" % url
+            return
+    raise AssertionError("Failed to connect to server at %s" % url)
+    
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print "usage:  %s stop|test port" % os.path.basename(sys.argv[0])
+        sys.exit(1)
+    mode, port = sys.argv[1:3]
+    if mode.upper() == 'STOP':
+        stop_xmlrpc_server(port)
+    else:
+        test_xmlrpc_server(port)
