60b5e38ad3831fa23e21916a6544b5394f2b6636
Janne Härkönen <janne.harkonen@iki.fi>
2008-06-24 11:57:16 +0000

    fixes to outputs

--- doc/userguide/ug2html.py
+++ doc/userguide/ug2html.py
@@ -2,17 +2,16 @@
 
 """ug2html.py -- Creates HTML version of Robot Framework User Guide
 
-Synopsis:
-ug2html.py [ cr(eate) | dist ]
+Usage:  ug2html.py [ cr(eate) | dist <version> ]
 
-Description:
 With 'create' as argument, user guide will be generated. This userguide will 
 have relative links that work only from version control or with full source 
 distribution.
 
-With 'dist' as argument, 'rfug' directory is created and all images and link 
-targets are copied under it. This way the created output directory may be 
-compressed and the user guide distributed independently.
+With 'dist' as argument, 'robotframework-userguide-<version>' directory is 
+created and all images and link targets are copied under it. This way the 
+created output directory may be compressed and the user guide distributed 
+independently. Version number to use must be given as second argument.
 """
 
 
@@ -108,19 +107,18 @@ pygments_directive.options = dict([(key, directives.flag) for key in VARIANTS])
 
 directives.register_directive('sourcecode', pygments_directive)
 
-#
-# Create outputdir and move images and library documentation there
-#
 
-
-def distribute_userguide():
+#
+# Create user guide distribution
+#
+def distribute_userguide(version):
     import shutil
     import re
     from urlparse import urlparse
 
     create_userguide()
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    outdir = "rfug"
+    outdir = "robotframework-userguide-%s" % version 
     toolsdir = os.path.join(outdir, 'tools')
     librariesdir = os.path.join(outdir, 'libraries')
     imagesdir = os.path.join(outdir, 'images')
@@ -173,9 +171,11 @@ def distribute_userguide():
 
     content = open('RobotFrameworkUserGuide.html').read()
     content = link_regexp.sub(replace_links, content)
-    outfile = open(os.path.join(outdir, 'RobotFrameworkUserGuide.html'), 'wb')
+    outfile = open(os.path.join(outdir, 'RobotFrameworkUserGuide-%s.html' % version), 'wb')
     outfile.write(content)
     outfile.close()
+    print outfile.name
+
 
 #
 # Creating the documentation
@@ -200,15 +200,15 @@ RobotFrameworkUserGuide.html
 '''.split('\n')[1:-1] 
 
     publish_cmdline(writer_name='html', description=description, argv=arguments)
-
     print os.path.abspath(arguments[-1])
+
 
 if __name__ == '__main__':
     import sys
     actions = { 'create': create_userguide, 'cr': create_userguide,
                 'dist': distribute_userguide }
     try:
-        actions[sys.argv[1]]()
+        actions[sys.argv[1]](*sys.argv[2:])
     except (KeyError, IndexError):
         print __doc__
 
