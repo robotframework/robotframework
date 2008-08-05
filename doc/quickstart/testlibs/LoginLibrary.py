5316e237f718df8921818c49d29e4b72a0455115
Pekka Klärck <peke@iki.fi>
2008-08-05 23:34:03 +0000

    refactor

--- doc/quickstart/testlibs/LoginLibrary.py
+++ doc/quickstart/testlibs/LoginLibrary.py
@@ -1,22 +1,30 @@
 import os
+import sys
+
 
 class LoginLibrary:
 
     def __init__(self):
         sut_path = os.path.join(os.path.dirname(__file__),
                                 '..', 'sut', 'src', 'auth.py')
-        self.cmd = 'python %s' % sut_path
-        self.status = ''
+        self._command_prefix = '%s %s ' % (sys.executable, sut_path)
+        self._status = ''
 
-    def create_user(self, username="", password=""):
-        command_string = '%s create %s %s' % (self.cmd, username, password)
-        self.status = os.popen(command_string).read()
+    def create_user(self, username='', password=''):
+        command = 'create %s %s' % (username, password)
+        self._run_command(command)
 
-    def attempt_to_login_with_credentials(self, username="", password=""):
-        command_string = '%s login %s %s' % (self.cmd, username, password)
-        self.status = os.popen(command_string).read()
+    def attempt_to_login_with_credentials(self, username='', password=''):
+        command = 'login %s %s' % (username, password)
+        self._run_command(command)
 
     def status_should_be(self, expected_status):
-        if expected_status != self.status.strip():
+        if expected_status != self._status:
             raise AssertionError("Expected status to be '%s' but was '%s'"
-                                  % (expected_status, self.status))
+                                  % (expected_status, self._status))
+
+    def _run_command(self, command):
+        command = '%s %s' % (self._command_prefix, command)
+        process = os.popen(command)
+        self._status = process.read().strip()
+        process.close()
