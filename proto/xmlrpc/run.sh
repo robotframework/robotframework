38bf7d5ba230591bbf2fd9de20221c916c431037
Pekka Klärck <peke@iki.fi>
2008-08-14 00:43:43 +0000

    all outputs to 'logs' directory

--- proto/xmlrpc/run.sh
+++ proto/xmlrpc/run.sh
@@ -4,9 +4,9 @@ cd ruby
 ruby examplelibrary.rb 1234 2> /dev/null &
 cd ..
 sleep 1
-pybot --variable PORT:1234 --log none --report none test/xmlrpc.html
-../../tools/statuschecker/statuschecker.py output.xml
-rebot output.xml
+pybot --variable PORT:1234 --log none --report none --output logs/output.xml test/xmlrpc.html
+../../tools/statuschecker/statuschecker.py logs/output.xml
+rebot --outputdir logs logs/output.xml
 rc=$?
 python test/XmlRpcServerHandler.py stop 1234
 echo $rc tests failed
