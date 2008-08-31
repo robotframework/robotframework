7a1e33585835cb9dc79bbef412de80ff2ba46d81
Pekka Klärck <peke@iki.fi>
2008-08-31 10:11:52 +0000

    use robot.utils.asserts, don't use global instance in tests (it should have been initialized/cleaned up in setup/teardown but creating instances when needed is easier), some other cleanup

--- utest/running/test_runkwregister.py
+++ utest/running/test_runkwregister.py
@@ -1,6 +1,8 @@
 import unittest
 
-from robot.running.runkwregister import RUN_KW_REGISTER
+from robot.utils.asserts import *
+
+from robot.running.runkwregister import _RunKeywordRegister as Register
 
 
 class Lib:
@@ -28,59 +30,47 @@ def function_with_three(one, two, three, *args):
 class TestRunKeywordRegister(unittest.TestCase):
     
     def test_register_run_keyword_method_with_kw_name_and_arg_count(self):
-        self._test_registering('My Lib', 'myKeyword', 
-                               'My Keyword', 3, 3)
+        self._verify_reg('My Lib', 'myKeyword', 'My Keyword', 3, 3)
 
     def test_register_run_keyword_method_with_kw_name_without_arg_count(self):
-        self.assertRaises(ValueError, RUN_KW_REGISTER.register_run_keyword, 
-                          'My Lib', 'my_keyword')
+        assert_raises(ValueError, Register().register_run_keyword, 'My Lib', 'my_keyword')
         
     def test_register_run_keyword_method_with_function_without_arg(self):
-        self._test_registering('My Lib', function_without_arg, 
-                               'Function Without Arg', 0)
+        self._verify_reg('My Lib', function_without_arg, 'Function Without Arg', 0)
     
     def test_register_run_keyword_method_with_function_with_one_arg(self):
-        self._test_registering('My Lib', function_with_one, 
-                               'Function With One', 1)
-
+        self._verify_reg('My Lib', function_with_one, 'Function With One', 1)
 
     def test_register_run_keyword_method_with_function_with_three_arg(self):
-        self._test_registering('My Lib', function_with_three, 
-                               'Function With Three', 3)
+        self._verify_reg('My Lib', function_with_three, 'Function With Three', 3)
 
     def test_register_run_keyword_method_with_method_without_arg(self):
-        self._test_registering('My Lib', Lib().method_without_arg, 
-                               'Method Without Arg', 0)
+        self._verify_reg('My Lib', Lib().method_without_arg, 'Method Without Arg', 0)
 
     def test_register_run_keyword_method_with_method_with_one_arg(self):
-        self._test_registering('My Lib', Lib().method_with_one, 
-                               'Method With One', 1)
-
+        self._verify_reg('My Lib', Lib().method_with_one, 'Method With One', 1)
 
     def test_register_run_keyword_method_with_method_with_default_arg(self):
-        self._test_registering('My Lib', Lib().method_with_default, 
-                               'Method With Default', 3)
+        self._verify_reg('My Lib', Lib().method_with_default, 'Method With Default', 3)
 
     def test_register_run_keyword_method_with_invalid_keyword_type(self):
-        self.assertRaises(ValueError, RUN_KW_REGISTER.register_run_keyword, 
-                          'My Lib', 1)
+        assert_raises(ValueError, Register().register_run_keyword, 'My Lib', 1)
     
     def test_get_arg_count_with_non_existing_keyword(self):
-        self.assertEqual(RUN_KW_REGISTER.get_args_to_process('My Lib', 'No Keyword'), -1)
+        assert_equal(Register().get_args_to_process('My Lib', 'No Keyword'), -1)
 
     def test_get_arg_count_with_non_existing_library(self):
-        self._test_registering('My Lib', 'get_arg', 
-                               'Get Arg', 3, 3)
-        self.assertEqual(RUN_KW_REGISTER.get_args_to_process('No Lib', 'Get Arg'), -1)
+        self._verify_reg('My Lib', 'get_arg', 'Get Arg', 3, 3)
+        assert_equal(Register().get_args_to_process('No Lib', 'Get Arg'), -1)
 
-    
-    def _test_registering(self, lib_name, keyword, keyword_name, arg_count, 
-                          given_arg_count=None):
-        if given_arg_count is None:
-            RUN_KW_REGISTER.register_run_keyword(lib_name, keyword)
+    def _verify_reg(self, lib_name, keyword, keyword_name, arg_count, given_count=None):
+        reg = Register()
+        if given_count is None:
+            reg.register_run_keyword(lib_name, keyword)
         else:
-            RUN_KW_REGISTER.register_run_keyword(lib_name, keyword, given_arg_count)
-        self.assertEqual(RUN_KW_REGISTER.get_args_to_process(lib_name, keyword_name), arg_count)
+            reg.register_run_keyword(lib_name, keyword, given_count)
+        assert_equal(reg.get_args_to_process(lib_name, keyword_name), arg_count)
+
 
 if __name__ == '__main__':
     unittest.main()
