c5176435eced8d28c8cbdfefd486a51a15dee1fe
Pekka Klärck <peke@iki.fi>
2008-09-02 17:42:08 +0000

    New tool for generating std library docs. Can create all docs or just one doc, and also handles Screenshot on Python. See help or source for more details.

---
+++ doc/libraries/lib2html.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import sys
+import tempfile
+import os
+
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+LIBRARIES = { 'builtin': 'BuiltIn', 'bu': 'BuiltIn',
+              'operatingsystem': 'OperatingSystem', 'op': 'OperatingSystem',
+              'telnet': 'Telnet', 'te': 'Telnet',
+              'collections': 'Collections', 'co': 'Collections',
+              'screenshot': 'Screenshot', 'sc': 'Screenshot' }
+
+sys.path.insert(0, os.path.join(BASEDIR,'..','..','tools','libdoc'))
+sys.path.insert(0, os.path.join(BASEDIR,'..','..','src'))
+
+from libdoc import LibraryDoc, create_html_doc
+
+
+def create_libdoc(name):
+    if name == 'Screenshot':
+       javamods = _FakeJavaModules() 
+    lib = LibraryDoc(name)
+    output = os.path.abspath(os.path.join(BASEDIR, name+'.html'))
+    create_html_doc(lib, output)
+    if name == 'Screenshot':
+        javamods.cleanup()
+    print output
+
+
+class _FakeJavaModules:
+
+    __path__ = [tempfile.gettempdir()]
+
+    def __init__(self):
+        self._fake_module_paths = []
+        self.create_module('awt', ['Toolkit', 'Robot', 'Rectangle'])
+        self.create_module('io', ['File'])
+        self.create_module('imageio', ['ImageIO'])
+        sys.modules['java'] = self
+        sys.modules['javax'] = self
+
+    def create_module(self, name, attrs):
+        f = open(os.path.join(self.__path__[0], name+'.py'), 'w')
+        self._fake_module_paths.append(f.name)
+        for name in attrs:
+            f.write('%s = 42\n' % name)
+        f.close()
+
+    def cleanup(self):
+        for path in self._fake_module_paths:
+            os.unlink(path)
+        del sys.modules['java']
+        del sys.modules['javax']
+    
+
+if __name__ == '__main__':
+    try:
+        name = sys.argv[1].lower()
+        if name == 'all':
+            for name in sorted(set(LIBRARIES.values())):
+                create_libdoc(name)
+        else:
+            create_libdoc(LIBRARIES[name])
+    except (IndexError, KeyError):
+        print 'Usage:  %s library | all' % (os.path.basename(sys.argv[0]))
+        print 
+        print 'Libraries:'
+        for name in sorted(set(LIBRARIES.values())):
+            print '  %s (%s)' % (name, name[:2].lower())
+
+
