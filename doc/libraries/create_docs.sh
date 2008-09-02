57e2e08c8d67a6fbd5c4f109e640ad538f05f212
Pekka Klärck <peke@iki.fi>
2008-09-02 17:42:30 +0000

    not needed anymore

--- doc/libraries/create_docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dir=$(dirname $0)
-libdoc=$dir/../../tools/libdoc/libdoc.py
-
-for lib in BuiltIn OperatingSystem Telnet Collections; do
-    python $libdoc --output $dir/$lib.html $lib
-done
