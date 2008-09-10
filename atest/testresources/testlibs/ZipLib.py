dd9ef6232235ff9c2ed07f083e22b061c1251e29
Janne Härkönen <janne.harkonen@iki.fi>
2008-09-10 12:34:35 +0000

    new test libraries for testing importing test libraries from archives(.jar, .zip)

---
+++ atest/testresources/testlibs/ZipLib.py
@@ -0,0 +1,7 @@
+class ZipLib:
+
+	def kw_from_zip(self, arg):
+		print '*INFO*', arg
+		return arg * 2
+
+
---
+++ atest/testresources/testlibs/org/robotframework/JarLib.java
@@ -0,0 +1,8 @@
+package org.robotframework;
+
+public class JarLib {
+	
+	public void kwFromJar(String arg) {
+		System.out.println("*INFO*" + arg);
+	}
+}
