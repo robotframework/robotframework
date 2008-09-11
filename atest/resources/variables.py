cdda7e0576f601f2d507be9d8e2d13aef01dbb06
Pekka Klärck <peke@iki.fi>
2008-09-11 22:25:08 +0000

    fixes for jython tests on mac. interestingly java.io.tmpdir on this platform is dirrerent from python tempfile.tempdir

--- atest/resources/variables.py
+++ atest/resources/variables.py
@@ -1,9 +1,12 @@
 import time
 import os.path
 import robot
+import sys
+import tempfile
 
-__all__ = ['timestamp', 'robotpath']
+__all__ = ['timestamp', 'robotpath', 'javatempdir']
 
 timetuple = time.localtime(time.time())[:6]  # from year to secs
 timestamp = "%d%02d%02d-%02d%02d%02d" % timetuple
 robotpath = os.path.abspath(os.path.dirname(robot.__file__))
+javatempdir = sys.platform == 'darwin' and '/tmp' or tempfile.gettempdir()
