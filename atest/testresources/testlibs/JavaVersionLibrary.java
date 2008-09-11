6aab0e21bede8878fdc90ab6222a803c32eb894f
Janne Härkönen <janne.harkonen@iki.fi>
2008-09-11 12:17:32 +0000

    version must be final for libdoc

--- atest/testresources/testlibs/JavaVersionLibrary.java
+++ atest/testresources/testlibs/JavaVersionLibrary.java
@@ -1,6 +1,6 @@
 public class JavaVersionLibrary {
 	
-	public static String ROBOT_LIBRARY_VERSION = "1.0";
+	public static final String ROBOT_LIBRARY_VERSION = "1.0";
 	
 	public Object kw() {
 		return null;
