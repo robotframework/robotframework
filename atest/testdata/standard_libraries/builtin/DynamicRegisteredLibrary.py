653452d4f121d14e0e1f652033f295cd796bfcd6
Juha Rantanen <juha.rantanen@reaktor.fi>
2008-09-11 09:45:58 +0000

    Added tests for situation when there are multiple @{list} variables in the arguments run keyword variant. See r671.

---
+++ atest/testdata/standard_libraries/builtin/DynamicRegisteredLibrary.py
@@ -0,0 +1,14 @@
+from robot.libraries.BuiltIn import BuiltIn, register_run_keyword
+
+class DynamicRegisteredLibrary:
+    
+    def get_keyword_names(self):
+        return ['dynamic_run_keyword']
+    
+    def run_keyword(self, name, args):
+        dynamic_run_keyword(*args)
+
+def dynamic_run_keyword(name, *args):
+    return BuiltIn().run_keyword(name, *args)
+
+register_run_keyword('DynamicRegisteredLibrary', dynamic_run_keyword)
