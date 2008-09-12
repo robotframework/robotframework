3609dfd947972eb1a796ff3bf0eae2ebc8f538e6
Juha Rantanen <juha.rantanen@reaktor.fi>
2008-09-12 13:16:54 +0000

    Fixed test as the OperatingSystem library has now version. Added new variable ${ROBOT VERSION} that was used to fix this.

--- atest/resources/variables.py
+++ atest/resources/variables.py
@@ -4,9 +4,10 @@ import robot
 import sys
 import tempfile
 
-__all__ = ['timestamp', 'robotpath', 'javatempdir']
+__all__ = ['timestamp', 'robotpath', 'javatempdir', 'robotversion']
 
 timetuple = time.localtime(time.time())[:6]  # from year to secs
 timestamp = "%d%02d%02d-%02d%02d%02d" % timetuple
 robotpath = os.path.abspath(os.path.dirname(robot.__file__))
 javatempdir = sys.platform == 'darwin' and '/tmp' or tempfile.gettempdir()
+robotversion = robot.utils.get_version()
