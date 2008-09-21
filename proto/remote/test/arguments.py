3d2ca84b6b580a06b3e3dfacc348b1b3098632e9
Pekka Klärck <peke@iki.fi>
2008-09-21 21:40:50 +0000

    more/better tests

---
+++ proto/remote/test/arguments.py
@@ -0,0 +1,14 @@
+# Can be used in the test data like ${MyObject()} or ${MyObject(1)}
+class MyObject:
+    def __init__(self, index=''):
+        self.index = index
+    def __str__(self):
+        return '<MyObject%s>' % self.index
+
+LIST_WITH_OBJECTS = [MyObject(1), MyObject(2)]
+NESTED_LIST = [ [True, False], [[1, None, MyObject(), {}]] ]
+NESTED_TUPLE = ( (True, False), [(1, None, MyObject(), {})] )
+DICT_WITH_OBJECTS = {'As value': MyObject(1), MyObject(2): 'As key'}
+NESTED_DICT = { 1: {True: False},
+                2: {'A': {1: None},
+                    'B': {MyObject(): {}}} }
