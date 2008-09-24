e742c054d3da79e3ad1eb156b3268fca6171883a
Pekka Klärck <peke@iki.fi>
2008-09-24 09:18:41 +0000

    support for cli options to pybot

--- proto/remote/run_tests.py
+++ proto/remote/run_tests.py
@@ -4,6 +4,7 @@ import time
 import socket
 import os
 
+
 class Library:
 
     def __init__(self, lang):
@@ -11,26 +12,27 @@ class Library:
         cmd = '%s %s/examplelibrary.%s' % (lang, lang, ext)
         stdin, self._stdout, self._stderr = os.popen3(cmd)
         stdin.close()
-        time.sleep(1)  # TODO: test condition
+        time.sleep(1)  # TODO: Test server is available instead of sleeping
 
     def stop(self, port=8270):
-        url = 'http://localhost:%s' % port
-        server = xmlrpclib.ServerProxy(url)
+        server = xmlrpclib.ServerProxy('http://localhost:%s' % port)
         server.stop_remote_server()
         self._stdout.close()
         self._stderr.close()
 
 
-    
-
 if __name__ == '__main__':
-    lang, path = sys.argv[1:3]
+    if len(sys.argv) < 2:
+        raise "Will get an usage here..."
+    lang = sys.argv[1]
     lib = Library(lang)
-    os.system('pybot --name %s --log none --report none --output logs/output.xml --include %s %s'
-              % (lang, lang, path))
+    args = 'test' if len(sys.argv) == 2 else ' '.join(sys.argv[2:])
+    os.system('pybot --log none --report none --output logs/output.xml '
+              '--name %s --include %s %s' % (lang, lang, args))
     lib.stop()
     os.system('../../tools/statuschecker/statuschecker.py logs/output.xml')
     rc = os.system('rebot --outputdir logs logs/output.xml')
-    print '%d tests failed' % (rc >> 8)
-
-
+    if rc == 0:
+        print 'All tests passed'
+    else:
+        print '%d test(s) failed' % (rc >> 8)
