a35c38d9c5343563d1ab91b4d148afc52dd6d088
Pekka Klärck <peke@iki.fi>
2008-10-07 16:00:24 +0000

    use 'get_variables'

--- atest/testdata/core/resources_and_variables/vars_from_cli2.py
+++ atest/testdata/core/resources_and_variables/vars_from_cli2.py
@@ -1,3 +1,11 @@
-scalar_from_cli_varfile = 'This value is not taken into use because this ' \
-                        + 'variable already exists in vars_from_cli.py'
-scalar_from_cli_varfile_2 = 'Variable from second variable file from cli'
+def get_variables():
+    return {
+        'scalar_from_cli_varfile'  : ('This variable is not taken into use '
+                                      'because it already exists in '
+                                      'vars_from_cli.py'),
+        'scalar_from_cli_varfile_2': ('Variable from second variable file '
+                                      'from cli')
+        }
+
+
+
