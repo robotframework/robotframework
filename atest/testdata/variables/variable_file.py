8ffc00f53683e5c192acc2fc56118a11030bd3c5
Pekka Klärck <peke@iki.fi>
2008-10-11 14:05:39 +0000

    slitting variable tests to separate suites...

---
+++ atest/testdata/variables/variable_file.py
@@ -0,0 +1,20 @@
+STRING = 'Hello world!'
+INTEGER = 42
+FLOAT = -1.2
+BOOLEAN = True
+NONE_VALUE =None
+ESCAPES = 'one \\ two \\\\ ${non_existing}'
+NO_VALUE = ''
+LIST = ['Hello', 'world', '!']
+LIST_WITH_NON_STRINGS = [42, -1.2, True, None]
+LIST_WITH_ESCAPES = ['one \\', 'two \\\\', 'three \\\\\\', '${non_existing}']
+
+LIST__ONE_ITEM = ['Hello again?']
+LIST__LIST = ['Hello', 'again', '?']
+LIST__LIST_WITH_ESCAPES = LIST_WITH_ESCAPES[:]
+LIST__EMPTY = []
+
+lowercase = 'Variable name in lower case'
+LIST__lowercase = [lowercase]
+Und_er__scores_____ = 'Variable name with under scores'
+LIST________UN__der__SCO__r_e_s__ = [Und_er__scores_____]
