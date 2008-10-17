2fc1d60895f475d03ec4d4c1b5559bb0da2dfddd
Pekka Klärck <peke@iki.fi>
2008-10-17 09:29:51 +0000

    test for __all__

--- atest/testdata/core/resources_and_variables/variables.py
+++ atest/testdata/core/resources_and_variables/variables.py
@@ -1,3 +1,6 @@
-variables = 'Variable from variables.py'
+__all__ = ['variables', 'LIST__valid_list']
 
+variables = 'Variable from variables.py'
 LIST__valid_list = 'This is a list'.split()
+not_included = 'Non in __all__ and thus not incuded'
+
