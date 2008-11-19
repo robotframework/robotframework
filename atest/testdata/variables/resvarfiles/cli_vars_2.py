7871ddb0fcbb6ad68947a2e1c677ef20d4b7dd05
Pekka Klärck <peke@iki.fi>
2008-11-19 22:59:45 +0000

    updated cli var tests to use new cli_vars*.py files under variables/resvarfiles instead of using files from core/resources_and_variables

--- atest/testdata/variables/resvarfiles/cli_vars_2.py
+++ atest/testdata/variables/resvarfiles/cli_vars_2.py
@@ -1,1 +1,7 @@
-PRIORITIES_1 = PRIORITIES_2 = PRIORITIES_2B = 'Second Variable File from CLI'
+def get_variables():
+    varz = { 'ANOTHER_SCALAR': 'Variable from CLI var file with get_variables',
+             'LIST__ANOTHER_LIST': ['List variable from CLI var file',
+                                    'with get_variables'] }
+    for name in 'PRIORITIES_1', 'PRIORITIES_2', 'PRIORITIES_2B':
+        varz[name] = 'Second Variable File from CLI'
+    return varz
