08fa589e41a00808d2a3794ca2c028732bbe39dd
Pekka Klärck <peke@iki.fi>
2008-11-24 13:02:12 +0000

    tests for 1) args for cli var files, 2) handling failures gracefully when cli var files are taken into use

--- atest/testdata/variables/resvarfiles/cli_vars_2.py
+++ atest/testdata/variables/resvarfiles/cli_vars_2.py
@@ -1,5 +1,8 @@
-def get_variables():
-    varz = { 'ANOTHER_SCALAR': 'Variable from CLI var file with get_variables',
+def get_variables(name, value='default value'):
+    if name == 'FAIL':
+        1/0
+    varz = { name: value,
+             'ANOTHER_SCALAR': 'Variable from CLI var file with get_variables',
              'LIST__ANOTHER_LIST': ['List variable from CLI var file',
                                     'with get_variables'] }
     for name in 'PRIORITIES_1', 'PRIORITIES_2', 'PRIORITIES_2B':
