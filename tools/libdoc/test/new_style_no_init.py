119ea9bd2670582665acba1fff714a273197c1b9
Pekka Klärck <peke@iki.fi>
2008-11-25 22:27:43 +0000

    new test, cleanup

---
+++ tools/libdoc/test/new_style_no_init.py
@@ -0,0 +1,6 @@
+class new_style_no_init(object):
+
+    """No __init__ on this one."""
+
+    def kw(self):
+        """The only lonely keyword."""
--- tools/libdoc/test/no_arg_init.py
+++ tools/libdoc/test/no_arg_init.py
@@ -1,19 +1,20 @@
 class no_arg_init:
 
     def __init__(self):
-        """This doc not shown because there are no arguments"""
-        import os
+        """This doc not shown because there are no arguments."""
 
     def keyword(self):
-        """A keyword
+        """A keyword.
 
-        See `get hello` for details"""
-        pass
+        See `get hello` for details and *never* run this keyword.
+        """
+        1/0
 
-    def get_hello(self):
-        """Get the intialization variables
+    def get_hello(self, arg):
+        """Returns 'Hello `arg`!'.
 
-        See `initialization` for explanation of arguments
-        and `introduction` for introduction"""
-        return self.arg1, self.arg2
+        See `initialization` for explanation of arguments and `introduction`
+        for introduction. Neither of them really exist, though.
+        """
+        return 'Hello %s' % arg
 
