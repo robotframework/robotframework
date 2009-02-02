80910cfc3770da8b3db906d2b2310e763a1f9d15
Pekka Klärck <peke@iki.fi>
2009-02-02 18:16:10 +0000

    tests for non-ascii problems in issue 224

--- tools/libdoc/test/module.py
+++ tools/libdoc/test/module.py
@@ -1,4 +1,10 @@
-"""Module test library"""
+u"""Module test library.
+
+With some non-ascii stuff:
+
+Hyv\u00E4\u00E4 y\u00F6t\u00E4.
+\u0421\u043F\u0430\u0441\u0438\u0431\u043E!
+"""
 
 ROBOT_LIBRARY_VERSION = '0.1-alpha'
 
