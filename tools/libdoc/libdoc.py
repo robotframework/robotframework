b8fd82f776ebf29386ac310d08698e2202720706
Janne Härkönen <janne.harkonen@iki.fi>
2009-03-30 08:47:11 +0000

    Took command line logger into use.

--- tools/libdoc/libdoc.py
+++ tools/libdoc/libdoc.py
@@ -52,12 +52,14 @@ import re
 
 from robot.running import TestLibrary, UserLibrary
 from robot.serializing import Template, Namespace
-from robot import utils
 from robot.errors import DataError, Information
 from robot.parsing import rawdata
+from robot.output import LOGGER
+from robot import utils
 
 
 rawdata.PROCESS_CURDIR = False
+LOGGER.register_console_logger()
 
 
 def main(args):
