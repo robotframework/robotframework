005070270f10d2449e2418d3c5a01263681dee19
Pekka Klärck <peke@iki.fi>
2009-04-05 11:03:12 +0000

    renamed whole/start/end_element also in unittests (some files also got newlines to the end by sed)

--- utest/utils/test_htmlwriter.py
+++ utest/utils/test_htmlwriter.py
@@ -34,78 +34,78 @@ class TestHtmlWriter(unittest.TestCase):
         self._verify(expected, actual)
         
     def test_init(self):
-        self.writer.start_element('r')
+        self.writer.start('r')
         self._verify('<r>\n')
 
-    def test_start_element(self):
-        self.writer.start_element('robot', newline=False)
+    def test_start(self):
+        self.writer.start('robot', newline=False)
         self._verify('<robot>')
 
-    def test_start_element_with_attribute(self):
-        self.writer.start_element('robot', newline=False)
-        self.writer.start_element('suite', {'name': 'Suite1'}, False)
+    def test_start_with_attribute(self):
+        self.writer.start('robot', newline=False)
+        self.writer.start('suite', {'name': 'Suite1'}, False)
         self._verify('<robot><suite name="Suite1">')
 
-    def test_start_element_with_attribute2(self):
-        self.writer.start_element('test case', {'class': '123'})
+    def test_start_with_attribute2(self):
+        self.writer.start('test case', {'class': '123'})
         self._verify('<test case class="123">\n')
         
-    def test_start_element_with_attributes(self):
-        self.writer.start_element('test case', {'class': '123', 'x': 'y'})
+    def test_start_with_attributes(self):
+        self.writer.start('test case', {'class': '123', 'x': 'y'})
         self._verify('<test case class="123" x="y">\n')
         
-    def test_start_element_with_non_ascii_attributes(self):
-        self.writer.start_element('test', {'name': u'\u00A7', u'\u00E4': u'\u00A7'})
+    def test_start_with_non_ascii_attributes(self):
+        self.writer.start('test', {'name': u'\u00A7', u'\u00E4': u'\u00A7'})
         self._verify(u'<test name="\u00A7" \u00E4="\u00A7">\n')
         
-    def test_start_element_with_quotes_in_attribute_value(self):
-        self.writer.start_element('x', {'q':'"', 'qs': '""""', 'a': "'"}, False)
+    def test_start_with_quotes_in_attribute_value(self):
+        self.writer.start('x', {'q':'"', 'qs': '""""', 'a': "'"}, False)
         self._verify('<x a="\'" q="&quot;" qs="&quot;&quot;&quot;&quot;">')
         
-    def test_start_element_with_html_in_attribute_values(self):
-        self.writer.start_element('x', {'1':'<', '2': '&', '3': '</html>'}, False)
+    def test_start_with_html_in_attribute_values(self):
+        self.writer.start('x', {'1':'<', '2': '&', '3': '</html>'}, False)
         self._verify('<x 1="&lt;" 2="&amp;" 3="&lt;/html&gt;">')
 
-    def test_start_element_with_newlines_and_tabs_in_attribute_values(self):
-        self.writer.start_element('x', {'1':'\n', '3': 'A\nB\tC', '2': '\t'}, False)
+    def test_start_with_newlines_and_tabs_in_attribute_values(self):
+        self.writer.start('x', {'1':'\n', '3': 'A\nB\tC', '2': '\t'}, False)
         self._verify('<x 1=" " 2=" " 3="A B C">')
 
-    def test_end_element(self):
-        self.writer.start_element('robot', newline=False)
-        self.writer.end_element('robot')
+    def test_end(self):
+        self.writer.start('robot', newline=False)
+        self.writer.end('robot')
         self._verify('<robot></robot>\n')
 
     def test_content(self):
-        self.writer.start_element('robot')
+        self.writer.start('robot')
         self.writer.content('Hello world!')
         self._verify('<robot>\nHello world!')
 
     def test_content_with_non_ascii_data(self):
-        self.writer.start_element('robot', newline=False)
+        self.writer.start('robot', newline=False)
         self.writer.content(u'Circle is 360\u00B0 ')
         self.writer.content(u'Hyv\u00E4\u00E4 \u00FC\u00F6t\u00E4')
-        self.writer.end_element('robot', newline=False)
+        self.writer.end('robot', newline=False)
         expected = u'<robot>Circle is 360\u00B0 Hyv\u00E4\u00E4 \u00FC\u00F6t\u00E4</robot>'
         self._verify(expected)
                    
     def test_multiple_content(self):
-        self.writer.start_element('robot')
+        self.writer.start('robot')
         self.writer.content('Hello world!')
         self.writer.content('Hi again!')
         self._verify('<robot>\nHello world!Hi again!')
 
     def test_content_with_entities(self):
-        self.writer.start_element('robot')
+        self.writer.start('robot')
         self.writer.content('Me, Myself & I')
         self._verify('<robot>\nMe, Myself &amp; I')
 
     def test_none_content(self):
-        self.writer.start_element('robot')
+        self.writer.start('robot')
         self.writer.content(None)
         self._verify('<robot>\n')
     
     def test_close_empty(self):
-        self.writer.end_element('suite', False)
+        self.writer.end('suite', False)
         self._verify('</suite>')
 
     def _verify(self, expected, actual=None):
--- utest/utils/test_xmlwriter.py
+++ utest/utils/test_xmlwriter.py
@@ -17,37 +17,37 @@ class TestXmlWriter(unittest.TestCase):
         os.remove(PATH)
     
     def test_write_element_in_pieces(self): 
-        self.writer.start_element('name', {'attr': 'value'}, True)
+        self.writer.start('name', {'attr': 'value'}, True)
         self.writer.content('Some content here!!')
-        self.writer.end_element('name', True)
+        self.writer.end('name', True)
         self.writer.close()
         self._verify_node(None, 'name', '\nSome content here!!',
                           {'attr': 'value'})
 
     def test_calling_content_multiple_times(self):
-        self.writer.start_element(u'robot-log', newline=False)
+        self.writer.start(u'robot-log', newline=False)
         self.writer.content(u'Hello world!\n')
         self.writer.content(u'Hi again!')
         self.writer.content('\tMy name is John')
-        self.writer.end_element('robot-log')
+        self.writer.end('robot-log')
         self.writer.close()
         self._verify_node(None, 'robot-log', 
                           'Hello world!\nHi again!\tMy name is John')
     
-    def test_write_whole_element(self):
-        self.writer.whole_element('foo', 'Node\n content', {'a1':'attr1', 'a2':'attr2'})
+    def test_write_element(self):
+        self.writer.element('foo', 'Node\n content', {'a1':'attr1', 'a2':'attr2'})
         self.writer.close()
         self._verify_node(None, 'foo', 'Node\n content', 
                           {'a1': 'attr1', 'a2': 'attr2'})
 
     def test_write_many_elements(self):
-        self.writer.start_element('root', {'version': 'test'})
-        self.writer.start_element('child1', {'my-attr': 'my value'})
-        self.writer.whole_element('leaf1.1', 'leaf content', {'type': 'kw'})
-        self.writer.whole_element('leaf1.2')
-        self.writer.end_element('child1')
-        self.writer.whole_element('child2', attributes={'class': 'foo'})
-        self.writer.end_element('root')
+        self.writer.start('root', {'version': 'test'})
+        self.writer.start('child1', {'my-attr': 'my value'})
+        self.writer.element('leaf1.1', 'leaf content', {'type': 'kw'})
+        self.writer.element('leaf1.2')
+        self.writer.end('child1')
+        self.writer.element('child2', attributes={'class': 'foo'})
+        self.writer.end('root')
         self.writer.close()
         root = utils.DomWrapper(PATH)
         self._verify_node(root, 'root', attrs={'version': 'test'})
@@ -58,17 +58,17 @@ class TestXmlWriter(unittest.TestCase):
         self._verify_node(root.get_node('child2'), 'child2', attrs={'class': 'foo'})
 
     def test_newline_insertion(self):
-        self.writer.start_element('root')
-        self.writer.start_element('suite', {'type': 'directory_suite'})
-        self.writer.whole_element('test', attributes={'name': 'my_test'}, 
+        self.writer.start('root')
+        self.writer.start('suite', {'type': 'directory_suite'})
+        self.writer.element('test', attributes={'name': 'my_test'}, 
                                   newline=False)
-        self.writer.whole_element('test', attributes={'name': 'my_2nd_test'})
-        self.writer.end_element('suite', False)
-        self.writer.start_element('suite', {'name': 'another suite'}, 
+        self.writer.element('test', attributes={'name': 'my_2nd_test'})
+        self.writer.end('suite', False)
+        self.writer.start('suite', {'name': 'another suite'}, 
                                   newline=False)
         self.writer.content('Suite 2 content')
-        self.writer.end_element('suite')
-        self.writer.end_element('root')
+        self.writer.end('suite')
+        self.writer.end('root')
         self.writer.close()
         f = open(PATH)
         lines = [ line for line in f.readlines() if line != '\n' ]
@@ -76,25 +76,25 @@ class TestXmlWriter(unittest.TestCase):
         assert_equal(len(lines), 6)
 
     def test_none_content(self):
-        self.writer.whole_element(u'robot-log', None)
+        self.writer.element(u'robot-log', None)
         self.writer.close()
         self._verify_node(None, 'robot-log')
         
     def test_content_with_invalid_command_char(self):
-        self.writer.whole_element('robot-log', '\033[31m\033[32m\033[33m\033[m')
+        self.writer.element('robot-log', '\033[31m\033[32m\033[33m\033[m')
         self.writer.close()
         self._verify_node(None, 'robot-log', '[31m[32m[33m[m')
 
     def test_content_with_invalid_command_char_unicode(self):
-        self.writer.whole_element('robot-log', u'\x1b[31m\x1b[32m\x1b[33m\x1b[m')
+        self.writer.element('robot-log', u'\x1b[31m\x1b[32m\x1b[33m\x1b[m')
         self.writer.close()
         self._verify_node(None, 'robot-log', '[31m[32m[33m[m')
 
     def test_content_with_unicode(self):
-        self.writer.start_element('root')
-        self.writer.whole_element(u'e', u'Circle is 360\u00B0')
-        self.writer.whole_element(u'f', u'Hyv\u00E4\u00E4 \u00FC\u00F6t\u00E4')
-        self.writer.end_element('root')
+        self.writer.start('root')
+        self.writer.element(u'e', u'Circle is 360\u00B0')
+        self.writer.element(u'f', u'Hyv\u00E4\u00E4 \u00FC\u00F6t\u00E4')
+        self.writer.end('root')
         self.writer.close()
         root = utils.DomWrapper(PATH)
         self._verify_node(root.get_node('e'), 'e', u'Circle is 360\u00B0')
@@ -103,7 +103,7 @@ class TestXmlWriter(unittest.TestCase):
         
 
     def test_content_with_entities(self):
-        self.writer.whole_element(u'robot-log', 'Me, Myself & I > you')
+        self.writer.element(u'robot-log', 'Me, Myself & I > you')
         self.writer.close()
         f = open(PATH)
         content = f.read()
