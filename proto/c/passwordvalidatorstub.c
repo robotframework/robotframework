04fcdcfd7bd7f6db9f3829756a2cd959d30ebc4f
Pekka Klärck <peke@iki.fi>
2009-09-02 23:08:28 +0000

    debug print to investigate problem

--- proto/c/passwordvalidatorstub.c
+++ proto/c/passwordvalidatorstub.c
@@ -1,7 +1,9 @@
+#include <stdio.h>
 #include "passwordvalidatorstub.h"
 #include "password-validator.h"
 
 int check_password(const char* name, const char* user) {
+	printf("check_password: %s/%s\n", name, user);
 	return validate_user(name, user);
 }
 
