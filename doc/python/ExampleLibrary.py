fed6c8b6510a3a2449233bbaf570d42375dfd4a7
Pekka Klärck <peke@iki.fi>
2009-10-07 13:06:32 +0000

    1) Simplified example library (no need to use class) and fixed a bug in logging arguments to numbers_should_be_equal, 2) Slightly better example tests

--- doc/python/ExampleLibrary.py
+++ doc/python/ExampleLibrary.py
@@ -1,22 +1,20 @@
-class ExampleLibrary:
+def simple_keyword():
+    """Log a message"""
+    print 'You have used the simplest keyword.'
 
-    def simple_keyword(self):
-        """Log a message"""
-        print 'You have used the simplest keyword.'
+def greet(name):
+    """Logs a friendly greeting to person given as argument"""
+    print 'Hello %s!' % name
 
-    def greet(self, name):
-        """Logs a friendly greeting to person given as argument"""
-        print 'Hello %s!' % name
+def multiply_by_two(number):
+    """Returns the given number multiplied by two
+    
+    The result is always a floating point number.
+    This keyword fails if the given `number` cannot be converted to number.
+    """
+    return float(number) * 2
 
-    def multiply_by_two(self, number):
-        """Returns the given number multiplied by two
-        
-        The result is always a floating point number.
-        This keyword fails if the given `number` cannot be converted to number.
-        """
-        return float(number) * 2
-
-    def numbers_should_be_equal(self, first, second):
-        print '*DEBUG* Got %s and %s'
-        if float(first) != float(second):
-            raise AssertionError('Given numbers are unequal!')
+def numbers_should_be_equal(first, second):
+    print '*DEBUG* Got arguments %s and %s' % (first, second)
+    if float(first) != float(second):
+        raise AssertionError('Given numbers are unequal!')
