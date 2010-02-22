013109df10f3d3a55819b316a1732d20c917d7fe
Janne Härkönen <janne.harkonen@iki.fi>
2010-02-22 12:46:36 +0000

    whitespace

--- src/robot/utils/abstractxmlwriter.py
+++ src/robot/utils/abstractxmlwriter.py
@@ -17,24 +17,24 @@ from robottypes import unic
 
 
 # See http://www.spamagogo.com/wiki/index.php/Illegal_XML_characters
-_ILLEGAL_CHARS_IN_XML = [ u'\x00', u'\x01', u'\x02', u'\x03', u'\x04', u'\x05', 
-                          u'\x06', u'\x07', u'\x08', u'\x0b', u'\x0c', u'\x0e', 
-                          u'\x0f', u'\x10', u'\x11', u'\x12', u'\x13', u'\x14', 
-                          u'\x15', u'\x16', u'\x17', u'\x18', u'\x19', u'\x1a', 
+_ILLEGAL_CHARS_IN_XML = [ u'\x00', u'\x01', u'\x02', u'\x03', u'\x04', u'\x05',
+                          u'\x06', u'\x07', u'\x08', u'\x0b', u'\x0c', u'\x0e',
+                          u'\x0f', u'\x10', u'\x11', u'\x12', u'\x13', u'\x14',
+                          u'\x15', u'\x16', u'\x17', u'\x18', u'\x19', u'\x1a',
                           u'\x1b', u'\x1c', u'\x1d', u'\x1e', u'\x1f' ]
 
 
 class AbstractXmlWriter:
-        
+
     def start(self, name, attributes={}, newline=True):
         raise NotImplementedError
-        
+
     def content(self, content):
         raise NotImplementedError
 
     def end(self, name, newline=True):
         raise NotImplementedError
-    
+
     def element(self, name, content=None, attributes={}, newline=True):
         self.start(name, attributes, newline=False)
         self.content(content)
@@ -42,7 +42,7 @@ class AbstractXmlWriter:
 
     def close(self):
         raise NotImplementedError
-    
+
     def _encode(self, message):
         message = unic(message)
         for char in _ILLEGAL_CHARS_IN_XML:
