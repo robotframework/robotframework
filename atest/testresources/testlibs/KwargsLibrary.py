d5709caca0fd9b74ce30a03962c9d3c0d5d6754c
Kari Husa <kari.husa@reaktor.fi>
2010-04-07 12:19:08 +0000

    more tests

--- atest/testresources/testlibs/KwargsLibrary.py
+++ atest/testresources/testlibs/KwargsLibrary.py
@@ -1,14 +1,17 @@
 class KwargsLibrary(object):
 
-    def onekwarg(self, foo=None):
+    def one_kwarg(self, foo=None):
         return 'foo=%s' % foo
 
-    def twokwargs(self, fst=None, snd=None):
+    def two_kwargs(self, fst=None, snd=None):
         return 'fst=%s, snd=%s' % (fst, snd)
 
-    def fourkwargs(self, a=None, b=None, c=None, d=None):
+    def four_kwargs(self, a=None, b=None, c=None, d=None):
         return 'a=%s, b=%s, c=%s, d=%s' % (a, b, c, d)
 
-    def mandatoryandkwargs(self, a, b, c=None):
+    def mandatory_and_kwargs(self, a, b, c=None):
         return 'a=%s, b=%s, c=%s' % (a, b, c)
 
+    def kwargs_and_mandatory_args(self, mandatory, d1=None, d2=None, *varargs):
+        return 'mandatory=%s, d1=%s, d2=%s, varargs=%s' \
+                % (mandatory, d1, d2, '[%s]' % ','.join(varargs))
