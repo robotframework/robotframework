65f5bb888cd7e377e12d32411a4cf5efd3ced54c
Janne Härkönen <janne.harkonen@iki.fi>
2010-04-09 19:09:49 +0000

    Cleanup

--- atest/testresources/testlibs/KwargsLibrary.py
+++ atest/testresources/testlibs/KwargsLibrary.py
@@ -9,18 +9,17 @@ class KwargsLibrary(object):
             raise AssertionError('Wrong initialization values: %s, %s'
                                  % (self.arg1, self.arg2))
 
-    def one_kwarg(self, foo=None):
-        return 'foo=%s' % foo
+    def one_kwarg(self, kwarg=None):
+        return kwarg
 
     def two_kwargs(self, fst=None, snd=None):
-        return 'fst=%s, snd=%s' % (fst, snd)
+        return '%s, %s' % (fst, snd)
 
     def four_kwargs(self, a=None, b=None, c=None, d=None):
-        return 'a=%s, b=%s, c=%s, d=%s' % (a, b, c, d)
+        return '%s, %s, %s, %s' % (a, b, c, d)
 
     def mandatory_and_kwargs(self, a, b, c=None):
-        return 'a=%s, b=%s, c=%s' % (a, b, c)
+        return '%s, %s, %s' % (a, b, c)
 
     def kwargs_and_mandatory_args(self, mandatory, d1=None, d2=None, *varargs):
-        return 'mandatory=%s, d1=%s, d2=%s, varargs=%s' \
-                % (mandatory, d1, d2, '[%s]' % ','.join(varargs))
+        return '%s, %s, %s, %s' % (mandatory, d1, d2, '[%s]' % ', '.join(varargs))
