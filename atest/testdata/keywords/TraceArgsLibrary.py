01433f861b93808e05c502b5181bcbc9d0d7e66f
Juha Rantanen <juha.rantanen@reaktor.fi>
2010-04-14 17:54:52 +0000

    Added tests for logging arguments from library keywords. Issue 531.

---
+++ atest/testdata/keywords/TraceArgsLibrary.py
@@ -0,0 +1,13 @@
+class TraceArgsLibrary(object):
+
+    def only_mandatory(self, mand1, mand2):
+        pass
+
+    def mandatory_and_default(self, mand, default="default value"):
+        pass
+
+    def multiple_default_values(self, a=1, a2=2, a3=3, a4=4):
+        pass
+
+    def mandatory_and_varargs(self, mand, *varargs):
+        pass
