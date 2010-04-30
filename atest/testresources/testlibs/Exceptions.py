8df472d6d4d0fd8f11b3f8d510bb1c085ef7d69c
Kari Husa <kari.husa@reaktor.fi>
2010-04-30 11:09:12 +0000

    Initial tests for continue on failure, issue 137

--- atest/testresources/testlibs/Exceptions.py
+++ atest/testresources/testlibs/Exceptions.py
@@ -1,5 +1,11 @@
 class FatalCatastrophyException(RuntimeError):
     ROBOT_EXIT_ON_FAILURE = True
 
+class ContinuableApocalypseException(RuntimeError):
+    ROBOT_CONTINUE_ON_FAILURE = True
+
 def exit_on_failure():
     raise FatalCatastrophyException()
+
+def raise_continuable_failure(msg='Can be continued'):
+    raise ContinuableApocalypseException(msg)
