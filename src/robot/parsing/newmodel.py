545818207dd5d3c622e0e656555544e5b92f706c
Pekka Klärck <peke@iki.fi>
2010-05-12 08:09:28 +0000

    move

---
+++ src/robot/parsing/newmodel.py
@@ -0,0 +1,100 @@
+#  Copyright 2008-2009 Nokia Siemens Networks Oyj
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+class TestCaseFile(object):
+
+    def __init__(self, source=None):
+        self.source = source
+        self.setting_table = SettingTable()
+        self.variable_table = VariableTable()
+        self.testcase_table = TestCaseTable()
+        self.keyword_table = KeywordTable()
+
+    def __iter__(self):
+        for table in [self.setting_table, self.variable_table,
+                      self.testcase_table, self.keyword_table]:
+            yield table
+
+    def edited(self):
+        return any(table.edited() for table in self)
+
+
+class DataTable(object):
+
+    def edited(self):
+        return False
+
+class SettingTable(DataTable):
+
+    def __init__(self):
+        self.doc = Documentation()
+        self.suite_setup = Fixture()
+        self.suite_teardown = Fixture()
+        self.metadata = Metadata()
+        self.test_setup = Fixture()
+        self.test_teardown = Fixture()
+        self.test_timeout = Timeout()
+        self.force_tags = Tags()
+        self.default_tags = Tags()
+        self.imports = []
+
+    def __iter__(self):
+        for setting in [self.doc, self.suite_setup, self.suite_teardown,
+                        self.metadata, self.test_setup, self.test_teardown,
+                        self.test_timeout, self.force_tags, self.default_tags] \
+                        + self.imports:
+            yield setting
+
+    def edited(self):
+        return any(setting.edited() for setting in self)
+
+class VariableTable(DataTable):
+    pass
+
+class TestCaseTable(DataTable):
+    pass
+
+class KeywordTable(DataTable):
+    pass
+
+
+class Setting(object):
+
+    def __init__(self):
+        self.value = []
+
+    def set(self, value):
+        self.value = value
+
+    def edited(self):
+        return bool(self.value)
+
+class Documentation(Setting):
+
+    def set(self, value):
+        if not isinstance(value, basestring):
+            value = ' '.join(value)
+        self.value = value
+
+class Fixture(Setting):
+    pass
+
+class Metadata(Setting):
+    pass
+
+class Timeout(Setting):
+    pass
+
+class Tags(Setting):
+    pass
--- utest/parsing/test_model.py
+++ utest/parsing/test_model.py
@@ -1,7 +1,7 @@
-
 import unittest
 
 from robot.utils.asserts import *
+from robot.parsing.newmodel import *
 
 
 class TestTestCaseFile(unittest.TestCase):
@@ -55,106 +55,18 @@ class TestVariableTable(unittest.TestCase):
     def setUp(self):
         self.table = TestCaseFile().variable_table
 
-    def test_variable_table(self):
+    def _test_variable_table(self):
         assert_equal(self.table.variables, [])
 
-    def test_create_scalar(self):
+    def _test_create_scalar(self):
         self.table.create('${VAR}', 'hello')
         assert_true(len(self.table.variables), 1)
         assert_equal(self.table.variables[0].name, '${VAR}')
         assert_equal(self.table.variables[0].value, ['hello'])
 
-    def test_create_list(self):
+    def _test_create_list(self):
         self.table.create('@{VAR}', ['hello', 'world'])
 
 
-class TestCaseFile(object):
-
-    def __init__(self, source=None):
-        self.source = source
-        self.setting_table = SettingTable()
-        self.variable_table = VariableTable()
-        self.testcase_table = TestCaseTable()
-        self.keyword_table = KeywordTable()
-
-    def __iter__(self):
-        for table in [self.setting_table, self.variable_table,
-                      self.testcase_table, self.keyword_table]:
-            yield table
-
-    def edited(self):
-        return any(table.edited() for table in self)
-
-
-class DataTable(object):
-
-    def edited(self):
-        return False
-
-class SettingTable(DataTable):
-
-    def __init__(self):
-        self.doc = Documentation()
-        self.suite_setup = Fixture()
-        self.suite_teardown = Fixture()
-        self.metadata = Metadata()
-        self.test_setup = Fixture()
-        self.test_teardown = Fixture()
-        self.test_timeout = Timeout()
-        self.force_tags = Tags()
-        self.default_tags = Tags()
-        self.imports = []
-
-    def __iter__(self):
-        for setting in [self.doc, self.suite_setup, self.suite_teardown,
-                        self.metadata, self.test_setup, self.test_teardown,
-                        self.test_timeout, self.force_tags, self.default_tags] \
-                        + self.imports:
-            yield setting
-
-    def edited(self):
-        return any(setting.edited() for setting in self)
-
-class VariableTable(DataTable):
-    pass
-
-class TestCaseTable(DataTable):
-    pass
-
-class KeywordTable(DataTable):
-    pass
-
-
-class Setting(object):
-
-    def __init__(self):
-        self.value = []
-
-    def set(self, value):
-        self.value = value
-
-    def edited(self):
-        return bool(self.value)
-
-class Documentation(Setting):
-
-    def set(self, value):
-        if not isinstance(value, basestring):
-            value = ' '.join(value)
-        self.value = value
-
-class Fixture(Setting):
-    pass
-
-class Metadata(Setting):
-    pass
-
-class Timeout(Setting):
-    pass
-
-class Tags(Setting):
-    pass
-
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.test_create_test_case_file']
     unittest.main()
