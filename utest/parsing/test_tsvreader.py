8bf90a6a73904ea8d7abb0c24b159e5e32cbab8a
Kari Husa <kari.husa@reaktor.fi>
2010-05-20 13:50:33 +0000

    Fixed imports & API

--- utest/parsing/test_tsvreader.py
+++ utest/parsing/test_tsvreader.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
 
 from robot.parsing.tsvreader import TsvReader
 from robot.parsing.newmodel import TestCaseFile
+from robot.parsing.datareader import FromFilePopulator
 from robot.utils.asserts import *
 import robot.parsing.populator
 
@@ -15,11 +16,11 @@ class TestTsvParser(unittest.TestCase):
 
     def setUp(self):
         self.tcf = TestCaseFile()
-        self._orig_curdir = robot.parsing.populator.PROCESS_CURDIR
-        robot.parsing.populator.PROCESS_CURDIR = False
+        self._orig_curdir = robot.parsing.datareader.PROCESS_CURDIR
+        robot.parsing.datareader.PROCESS_CURDIR = False
 
     def tearDown(self):
-        robot.parsing.populator.PROCESS_CURDIR = self._orig_curdir
+        robot.parsing.datareader.PROCESS_CURDIR = self._orig_curdir
 
     def test_start_table(self):
         tsv = StringIO('''*Setting*\t*Value*\t*V*
@@ -31,7 +32,7 @@ Keyword*\tNot a table because doesn't start with '*'
 
 *******************T*e*s*t*********C*a*s*e************\t***********\t******\t*
 ''')
-        TsvReader().read(tsv, self.tcf)
+        TsvReader().read(tsv, FromFilePopulator(self.tcf))
         assert_false(True, "Need to add checks after new model supports table names")
 
     def test_rows(self):
@@ -45,7 +46,7 @@ Default Tags\tt1\tt2\tt3\t\t
 \\ \\ 2 escaped spaces before and after \\ \\\t\\ \\ value \\ \\
     
 ''')
-        TsvReader().read(tsv, self.tcf)
+        TsvReader().read(tsv, FromFilePopulator(self.tcf))
         assert_equal(self.tcf.setting_table.doc.value, 'Whatever  \\')
         assert_equal(self.tcf.setting_table.default_tags.value, ['t1','t2','t3'])
         assert_equal(self.tcf.variable_table.variables[0].name, '\\ \\ 2 escaped spaces before and after \\ \\')
@@ -63,7 +64,7 @@ ${v}\t"""Hel "" """" lo"""""""
 ${v}\t"Hello
 ${v}\tHello"
 ''')
-        TsvReader().read(tsv, self.tcf)
+        TsvReader().read(tsv, FromFilePopulator(self.tcf))
         actual = [ variable for variable in self.tcf.variable_table.variables ]
         expected = ['Hello','Hello','"Hello"','""Hello""','Hel"lo',
                     '"Hel " "" lo"""','"Hello','Hello"']
