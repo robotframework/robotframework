e72dafb57cd3bb7744069291bf1ac23cd6370c85
Janne Härkönen <janne.harkonen@iki.fi>
2010-05-24 05:12:15 +0000

    own module for execution context

--- src/robot/running/model.py
+++ src/robot/running/model.py
@@ -29,6 +29,7 @@ from keywords import Keywords
 from namespace import Namespace
 from runerrors import SuiteRunErrors, TestRunErrors
 from userkeyword import UserLibrary
+from context import ExecutionContext
 
 
 def TestSuite(datasources, settings):
@@ -55,78 +56,6 @@ def _get_directory_or_file_suite(path, suite_names):
     if os.path.isdir(path):
         return TestDataDirectory(source=path) #FIXME: filter suites: , suite_names)
     return TestCaseFile(source=path)
-
-
-class ExecutionContext(object):
-
-    def __init__(self, namespace, output, dry_run=False):
-        self.namespace = namespace
-        self.output = output
-        self.dry_run = dry_run
-
-    def get_current_vars(self):
-        return self.namespace.variables
-
-    def end_test(self, test):
-        self.output.end_test(test)
-        self.namespace.end_test()
-
-    def end_suite(self, suite):
-        self.output.end_suite(suite)
-        self.namespace.end_suite()
-
-    def output_file_changed(self, filename):
-        self._set_global_variable('${OUTPUT_FILE}', filename)
-
-    def replace_vars_from_setting(self, name, value, errors):
-        return self.namespace.variables.replace_meta(name, value, errors)
-
-    def log_file_changed(self, filename):
-        self._set_global_variable('${LOG_FILE}', filename)
-
-    def set_prev_test_variables(self, test):
-        self._set_prev_test_variables(self.get_current_vars(), test.name,
-                                      test.status, test.message)
-
-    def copy_prev_test_vars_to_global(self):
-        varz = self.get_current_vars()
-        name, status, message = varz['${PREV_TEST_NAME}'], \
-                    varz['${PREV_TEST_STATUS}'], varz['${PREV_TEST_MESSAGE}']
-        self._set_prev_test_variables(GLOBAL_VARIABLES, name, status, message)
-
-    def _set_prev_test_variables(self, destination, name, status, message):
-        destination['${PREV_TEST_NAME}'] = name
-        destination['${PREV_TEST_STATUS}'] = status
-        destination['${PREV_TEST_MESSAGE}'] = message
-
-    def _set_global_variable(self, name, value):
-        self.namespace.variables.set_global(name, value)
-
-    def report_suite_status(self, status, message):
-        self.get_current_vars()['${SUITE_STATUS}'] = status
-        self.get_current_vars()['${SUITE_MESSAGE}'] = message
-
-    def start_test(self, test):
-        self.namespace.start_test(test)
-        self.output.start_test(test)
-
-    def set_test_status_before_teardown(self, message, status):
-        self.namespace.set_test_status_before_teardown(message, status)
-
-    def get_handler(self, name):
-        return self.namespace.get_handler(name)
-
-    def start_keyword(self, keyword):
-        self.output.start_keyword(keyword)
-
-    def end_keyword(self, keyword):
-        self.output.end_keyword(keyword)
-
-    def warn(self, message):
-        self.output.warn(message)
-
-    def trace(self, message):
-        self.output.trace(message)
 
 
 class RunnableTestSuite(BaseTestSuite):
