0f1c189daabaa28f800b5f5ae5779c3d3a75f184
Janne Härkönen <janne.harkonen@iki.fi>
2010-05-28 11:47:46 +0000

    fix import path

--- atest/resources/variables.py
+++ atest/resources/variables.py
@@ -1,10 +1,9 @@
 import os.path
 import robot
-import sys
 import tempfile
 
 __all__ = ['robotpath', 'javatempdir', 'robotversion']
 
 robotpath = os.path.abspath(os.path.dirname(robot.__file__))
 javatempdir = tempfile.gettempdir() # Used to be different on OSX and elsewhere
-robotversion = robot.utils.get_version()
+robotversion = robot.version.get_version()
