7192663268b78b6037096f850705e33434b110aa
Pekka Klärck <peke@iki.fi>
2010-05-28 11:44:00 +0000

    cleanup

--- src/robot/utils/__init__.py
+++ src/robot/utils/__init__.py
@@ -12,32 +12,29 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-
-import sys
-import os
-
 from argumentparser import ArgumentParser
+from connectioncache import ConnectionCache
 from domwrapper import DomWrapper
+from encoding import decode_output, encode_output
+from error import (get_error_message, get_error_details, ErrorDetails,
+                   RERAISED_EXCEPTIONS)
 from escaping import escape, unescape
-from error import get_error_message, get_error_details, RERAISED_EXCEPTIONS, \
-    ErrorDetails
 from htmlutils import html_escape, html_attr_escape
 from htmlwriter import HtmlWriter
+from idgenerator import IdGenerator, FileNameGenerator
 from importing import simple_import, import_
-from normalizing import normalize, normalize_tags, normpath, NormalizedDict
 from match import eq, eq_any, matches, matches_any
 from misc import plural_or_not, get_link_path, printable_name, seq2str, seq2str2
+from normalizing import normalize, normalize_tags, normpath, NormalizedDict
 from outputcapture import capture_output, release_output
-from robottime import get_timestamp, get_start_timestamp, format_time, \
-        get_time, get_elapsed_time, elapsed_time_to_string, \
-        timestr_to_secs, secs_to_timestr, secs_to_timestamp, \
-        timestamp_to_secs, parse_time
-from unic import unic, safe_repr
-from encoding import decode_output, encode_output
+from robottime import (get_timestamp, get_start_timestamp, format_time,
+                       get_time, get_elapsed_time, elapsed_time_to_string,
+                       timestr_to_secs, secs_to_timestr, secs_to_timestamp,
+                       timestamp_to_secs, parse_time)
 from text import cut_long_message, format_assign_message
+from unic import unic, safe_repr
 from xmlwriter import XmlWriter
-from connectioncache import ConnectionCache
-from idgenerator import IdGenerator, FileNameGenerator
 
-
+import sys
 is_jython  = sys.platform.startswith('java')
+del sys
