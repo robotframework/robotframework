64d49ac182176c53674f8656bb3d9d2fa5246c26
Janne Härkönen <janne.harkonen@iki.fi>
2010-06-01 10:02:36 +0000

    these are needed by RIDE

--- src/robot/parsing/__init__.py
+++ src/robot/parsing/__init__.py
@@ -12,4 +12,5 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from model import TestCaseFile, TestDataDirectory, ResourceFile
+from model import (TestCaseFile, TestDataDirectory, ResourceFile,
+                   TestCase, UserKeyword)
