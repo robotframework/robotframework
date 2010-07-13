22d25d0bf1ebcbaaaa4c889752da26e4daf45f8b
Pekka Klärck <peke@iki.fi>
2010-07-13 13:55:07 +0000

    tabs to spaces

--- atest/testresources/testlibs/ExampleJavaLibrary.java
+++ atest/testresources/testlibs/ExampleJavaLibrary.java
@@ -10,17 +10,16 @@ import java.util.Iterator;
 
 
 public class ExampleJavaLibrary {
-
-	public static final String ROBOT_LIBRARY_SCOPE = "GLOBAL";
-	private int counter = 0;
+    public static final String ROBOT_LIBRARY_SCOPE = "GLOBAL";
+    private int counter = 0;
 
     public void print(Object msg) {
-    	print(msg, "INFO");
+	print(msg, "INFO");
     }
 
     public void print(Object msg, String level) {
-    	System.out.println("*" + level.toUpperCase() + "*" + msg);
-	}    
+	System.out.println("*" + level.toUpperCase() + "*" + msg);
+    }    
     
     public void divByZero() {
         int i = 1/0;
@@ -33,13 +32,13 @@ public class ExampleJavaLibrary {
     public void javaException(String msg) {
         exception(msg);
     }
-	
-	// Jython creates properties from getters, which should not be accessed
-	// at library creation time. See issue 188.
-	public int getCount() {
-		counter++;
-		return counter;
-	}
+
+    // Jython creates properties from getters, which should not be accessed
+    // at library creation time. See issue 188.
+    public int getCount() {
+	counter++;
+	return counter;
+    }
     
     private void exception(String msg) {
         throw new ArrayStoreException(msg); 
@@ -50,29 +49,29 @@ public class ExampleJavaLibrary {
     }
     
     public void javaSleep(String secs) throws InterruptedException {
-    	javaSleep(Double.parseDouble(secs));
+	javaSleep(Double.parseDouble(secs));
     }
     
     public void javaSleep(double secs) throws InterruptedException {
-    	int millis = (new Double(secs * 1000)).intValue(); 
-    	Thread.sleep(millis);
-    	System.out.println("Slept " + secs + " seconds in Java");
+	int millis = (new Double(secs * 1000)).intValue(); 
+	Thread.sleep(millis);
+	System.out.println("Slept " + secs + " seconds in Java");
     }
     
     public void printMultipleMessagesAndSleepBetween() throws InterruptedException {
-    	System.out.println("First message.");
-    	Thread.sleep(42);
-    	System.out.println("Secong line of the first message.");
-    	System.out.println("*WARN* Looping and sleeping next..");
-    	System.out.print("*INFO*");
-    	for (int i=0; i < 100; i++) {
-    		System.out.print(i);
-    		Thread.sleep(2);
-    	}
+	System.out.println("First message.");
+	Thread.sleep(42);
+	System.out.println("Secong line of the first message.");
+	System.out.println("*WARN* Looping and sleeping next..");
+	System.out.print("*INFO*");
+	for (int i=0; i < 100; i++) {
+	    System.out.print(i);
+	    Thread.sleep(2);
+	}
     }
 
     public String returnStringFromLibrary(String s) {
-    	return s;
+	return s;
     }
 
     public JavaObject getJavaObject(String name) {
@@ -80,7 +79,7 @@ public class ExampleJavaLibrary {
     }
 
     public String[] getStringArray(String[] args) {
-    	return args;
+	return args;
     }
 
     public Vector getStringVector(String[] args) {
@@ -104,32 +103,32 @@ public class ExampleJavaLibrary {
     }
 
     public int[] getArrayOfThreeInts() {
-    	int[] ret = { 1, 2, 42 }; 
-    	return ret;
+	int[] ret = { 1, 2, 42 }; 
+	return ret;
     }
     
     public Hashtable getHashtable() {
-    	return new Hashtable();
+	return new Hashtable();
     }
     
     public void setToHashtable(Hashtable ht, String key, String value) {
-    	ht.put(key, value);
+	ht.put(key, value);
     }
     
     public String getFromHashtable(Hashtable ht, String key) {
-    	return (String)ht.get(key);
+	return (String)ht.get(key);
     }
 
     public void checkInHashtable(Hashtable ht, String key, String expected) {
-    	String actual = (String)ht.get(key);
-    	if (!actual.equals(expected)) {
-    		throw new AssertionError(actual + " != " + expected);
-    	}
+	String actual = (String)ht.get(key);
+	if (!actual.equals(expected)) {
+	    throw new AssertionError(actual + " != " + expected);
+	}
     }
 
     public LinkedList getLinkedList(Object[] values) {
         LinkedList list = new LinkedList();
-    	for (int i=0; i < values.length; i++) {
+	for (int i=0; i < values.length; i++) {
             list.add(values[i]);
         }
         return list;
