a7ed4197e8b3519e208c7bc09ae3900a2e1bc6fe
Pekka Klärck <peke@iki.fi>
2010-07-13 11:29:39 +0000

    1) Improved tests for importing Python libs with arguments, 2) Separated test data for importing Java libs with arguments into a separate suite (and will continue improving Java tests to fix issue 584)

--- atest/testresources/testlibs/PythonVarArgsConstructor.py
+++ atest/testresources/testlibs/PythonVarArgsConstructor.py
@@ -1,4 +1,9 @@
 class PythonVarArgsConstructor:
     
     def __init__(self, mandatory, *varargs):
-        pass
+        self.mandatory = mandatory
+        self.varargs = varargs
+
+    def get_args(self):
+        return self.mandatory, ' '.join(self.varargs)
+
---
+++ atest/testresources/testlibs/libswithargs.py
@@ -0,0 +1,32 @@
+class Normal:
+
+    def __init__(self, mandatory1, mandatory2):
+        self.mandatory1 = mandatory1
+        self.mandatory2 = mandatory2
+
+    def get_args(self):
+        return self.mandatory1, self.mandatory2
+
+
+class Defaults(object):
+
+    def __init__(self, mandatory, default1='value', default2=None):
+        self.mandatory = mandatory
+        self.default1 = default1
+        self.default2 = default2
+
+    def get_args(self):
+        return self.mandatory, self.default1, self.default2
+
+
+class Varargs(Normal):
+
+    def __init__(self, mandatory, *varargs):
+        Normal.__init__(self, mandatory, ' '.join(str(a) for a in varargs))
+
+
+class Mixed(Defaults):
+
+    def __init__(self, mandatory, default=42, *extra):
+        Defaults.__init__(self, mandatory, default, 
+                          ' '.join(str(a) for a in extra))
