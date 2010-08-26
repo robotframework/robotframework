150b89df7b2391fbb236a47f151826bca4c67aab
Janne Härkönen <janne.harkonen@iki.fi>
2010-08-26 12:52:12 +0000

    renamed class

--- src/java/org/robotframework/RobotFramework.java
+++ src/java/org/robotframework/RobotFramework.java
@@ -1,15 +1,29 @@
+/* Copyright 2008-2010 Nokia Siemens Networks Oyj
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.robotframework;
 
 import org.robotframework.RunnerFactory;
 import org.robotframework.RobotRunner;
 
 /**
- *
+ * 
  * Entry point for using Robot Framework from Java programs.
- *
+ * 
  */
-
-public class JarRobot {
+public class RobotFramework {
 
     public static void main(String[] args) {
         int rc = run(args);
@@ -18,7 +32,7 @@ public class JarRobot {
 
     /**
      * Runs Robot Framework tests.
-     *
+     * 
      * @param args
      *            The command line options to Robot Framework, for example
      *            ['--outputdir', '/tmp', 'mytestdir']. At least one datasource
