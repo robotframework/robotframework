ec0901a87ed3cf9a5a4b17909939bda22d89cc35
Pekka Klärck <peke@iki.fi>
2010-09-16 22:33:50 +0000

    test for stopping execution gracefully even if keyword swallows exception (issue 657) and some cleanup

--- atest/testdata/running/stopping_with_signal/Library.py
+++ atest/testdata/running/stopping_with_signal/Library.py
@@ -1,7 +1,14 @@
 import time
 
-class Library:
-    def busy_sleep(self, seconds):
-        max_time = time.time() + int(seconds)
-        while time.time() < max_time:
+
+def busy_sleep(seconds):
+    max_time = time.time() + int(seconds)
+    while time.time() < max_time:
+        pass
+
+def swallow_exception():
+    try:
+        while True:
             pass
+    except:
+        pass
