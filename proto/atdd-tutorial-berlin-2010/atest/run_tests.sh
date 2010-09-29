b044da9dfacd83011dd3dc568cfe2dc0176d1fc4
Juha Rantanen <juha.rantanen@reaktor.fi>
2010-09-29 21:12:35 +0000

    No more datasource required.

--- proto/atdd-tutorial-berlin-2010/atest/run_tests.sh
+++ proto/atdd-tutorial-berlin-2010/atest/run_tests.sh
@@ -1,5 +1,6 @@
 ROOTDIR=`dirname $0`/..
 LIBPATH=$ROOTDIR/lib
 SRCPATH=$ROOTDIR/src
+TESTDATA=$ROOTDIR/atest/vacalc
 CP=$LIBPATH/robotframework-2.5.4.jar:$LIBPATH/swinglibrary-1.1.1.jar:$ROOTDIR/bin/
-CLASSPATH=$CP java org.robotframework.RobotFramework -P $SRCPATH $*
+CLASSPATH=$CP java org.robotframework.RobotFramework -P $SRCPATH $* $TESTDATA
