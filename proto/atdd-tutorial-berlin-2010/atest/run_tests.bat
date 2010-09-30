6d15df26dec6c9e058760c110a60a938cc11eccd
Janne Härkönen <janne.harkonen@iki.fi>
2010-09-30 19:08:43 +0000

    Add test lib dir to PYTHONPATH for tests

--- proto/atdd-tutorial-berlin-2010/atest/run_tests.bat
+++ proto/atdd-tutorial-berlin-2010/atest/run_tests.bat
@@ -3,6 +3,6 @@ set LIBPATH=%ROOTDIR%\lib
 set SRCPATH=%ROOTDIR%\src
 set TESTDATA=%ROOTDIR%\atest\vacalc
 set CLASSPATH=%LIBPATH%\robotframework-2.5.4.1.jar;%LIBPATH%\swinglibrary-1.1.1.jar;%ROOTDIR%\bin
-java org.robotframework.RobotFramework -P %SRCPATH% %* --outputdir %ROOTDIR%\results --critical regression %TESTDATA%
+java org.robotframework.RobotFramework -P %SRCPATH% -P %ROOTDIR%\atest\libraries %* --outputdir %ROOTDIR%\results --critical regression %TESTDATA%
 
 
--- proto/atdd-tutorial-berlin-2010/atest/run_tests.sh
+++ proto/atdd-tutorial-berlin-2010/atest/run_tests.sh
@@ -3,4 +3,4 @@ LIBPATH=$ROOTDIR/lib
 SRCPATH=$ROOTDIR/src
 TESTDATA=$ROOTDIR/atest/vacalc
 CP=$LIBPATH/robotframework-2.5.4.1.jar:$LIBPATH/swinglibrary-1.1.1.jar:$ROOTDIR/bin/
-CLASSPATH=$CP java org.robotframework.RobotFramework -P $SRCPATH $* --outputdir $ROOTDIR/results --critical regression $TESTDATA
+CLASSPATH=$CP java org.robotframework.RobotFramework -P $SRCPATH -P $ROOTDIR/atest/libraries $* --outputdir $ROOTDIR/results --critical regression $TESTDATA
