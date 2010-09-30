c2de1abaddd7998c6fbc6ffa10a57e659184336e
Janne Härkönen <janne.harkonen@iki.fi>
2010-09-30 08:03:04 +0000

    calculate vacation

--- proto/atdd-tutorial-berlin-2010/src/vacalc/employeestore.py
+++ proto/atdd-tutorial-berlin-2010/src/vacalc/employeestore.py
@@ -65,7 +65,56 @@ class EmployeeStore(object):
 
 
 class Employee(object):
+    max_vacation = int(12 * 2.5)
+    no_vacation = 0
+    vacation_per_month = 2
+    credit_start_month = 4
+    work_days_required= 14
 
     def __init__(self, name, startdate):
         self.name = name
         self.startdate = startdate
+
+    def count_vacation(self, year):
+        return '%s days ' % self._count_vacation(self.startdate, year)
+
+    def _count_vacation(self, startdate, year):
+        if self._has_worked_longer_than_year(startdate, year):
+            return self.max_vacation
+        if self._started_after_holiday_credit_year_ended(startdate, year):
+            return self.no_vacation
+        return self._count_working_months(startdate) * self.vacation_per_month
+
+    def _has_worked_longer_than_year(self, start, year):
+        return year-start.year > 1 or \
+                (year-start.year == 1 and start.month < self.credit_start_month)
+
+    def _started_after_holiday_credit_year_ended(self, start, year):
+        return start.year-year > 0 or \
+                (year == start.year and start.month >= self.credit_start_month)
+
+    def _count_working_months(self, start):
+        months = self.credit_start_month - start.month
+        if months <= 0:
+            months += 12
+        if self._first_month_has_too_few_working_days(start):
+            months -= 1
+        return months
+
+    def _first_month_has_too_few_working_days(self, start):
+        days = 0
+        date = start
+        while date:
+            if self._is_working_day(date):
+                days += 1
+            date = self._next_date(date)
+        return days < self.work_days_required
+
+    def _is_working_day(self, date):
+        return date.weekday() < 5
+
+    def _next_date(self, date):
+        try:
+            return date.replace(day=date.day+1)
+        except ValueError:
+            return None
