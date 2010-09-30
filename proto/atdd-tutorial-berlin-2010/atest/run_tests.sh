3175cda661e41fe6ec783fe24b24d2d99924c7c8
Juha Rantanen <juha.rantanen@reaktor.fi>
2010-09-30 10:21:31 +0000

    Finetuning tests.

--- proto/atdd-tutorial-berlin-2010/atest/run_tests.sh
+++ proto/atdd-tutorial-berlin-2010/atest/run_tests.sh
@@ -3,4 +3,4 @@ LIBPATH=$ROOTDIR/lib
 SRCPATH=$ROOTDIR/src
 TESTDATA=$ROOTDIR/atest/vacalc
 CP=$LIBPATH/robotframework-2.5.4.jar:$LIBPATH/swinglibrary-1.1.1.jar:$ROOTDIR/bin/
-CLASSPATH=$CP java org.robotframework.RobotFramework -P $SRCPATH $* --outputdir $ROOTDIR/results $TESTDATA
+CLASSPATH=$CP java org.robotframework.RobotFramework -P $SRCPATH $* --outputdir $ROOTDIR/results --critical regression $TESTDATA
