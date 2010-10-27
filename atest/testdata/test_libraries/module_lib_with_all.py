eb758b75e77907179c3ab63c0b6966d4b0b91c64
Pekka Klärck <peke@iki.fi>
2010-10-27 14:06:13 +0000

    ooops, this lib is needed by tests added in prev commit

---
+++ atest/testdata/test_libraries/module_lib_with_all.py
@@ -0,0 +1,15 @@
+from os.path import join, abspath
+
+__all__ = ['join_with_execdir', 'abspath', 'attr_is_not_kw',
+           '_not_kw_even_if_listed_in_all', 'extra stuff', None]
+
+def join_with_execdir(arg):
+    return join(abspath('.'), arg)
+
+def not_in_all():
+    pass
+
+attr_is_not_kw = 'Listed in __all__ but not a fuction'
+
+def _not_kw_even_if_listed_in_all():
+    print 'Listed in __all__ but starts with an underscore'
