6410cb1ef75deb8e79a131189b9a24e89a002e78
Pekka Klärck <peke@iki.fi>
2010-12-07 21:14:22 +0000

    no need for fake java modules now that Screenshot works also on Python

--- doc/libraries/lib2html.py
+++ doc/libraries/lib2html.py
@@ -32,43 +32,11 @@ from libdoc import LibraryDoc, create_html_doc
 
 
 def create_libdoc(name):
-    if name == 'Screenshot':
-       javamods = _FakeJavaModules()
     ipath = os.path.join(ROOT,'src','robot','libraries',name+'.py')
     opath = os.path.join(ROOT,'doc','libraries',name+'.html')
     create_html_doc(LibraryDoc(ipath), opath)
-    if name == 'Screenshot':
-        javamods.cleanup()
     print opath
 
-
-class _FakeJavaModules:
-
-    """Adds fake Java modules to sys.modules to enable importing Screenshot"""
-
-    __path__ = [tempfile.gettempdir()]
-
-    def __init__(self):
-        self._fake_module_paths = []
-        self.create_module('awt', ['Toolkit', 'Robot', 'Rectangle'])
-        self.create_module('io', ['File'])
-        self.create_module('imageio', ['ImageIO'])
-        sys.modules['java'] = self
-        sys.modules['javax'] = self
-
-    def create_module(self, name, attrs):
-        f = open(os.path.join(self.__path__[0], name+'.py'), 'w')
-        self._fake_module_paths.append(f.name)
-        for name in attrs:
-            f.write('%s = 42\n' % name)
-        f.close()
-
-    def cleanup(self):
-        for path in self._fake_module_paths:
-            os.unlink(path)
-        del sys.modules['java']
-        del sys.modules['javax']
-    
 
 if __name__ == '__main__':
     try:
