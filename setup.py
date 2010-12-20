fee3ec313398288a55b53ab7865674d11cf9eca4
Pekka Klärck <peke@iki.fi>
2010-12-20 13:20:20 +0000

    cleanup and metadata update

--- setup.py
+++ setup.py
@@ -1,36 +1,21 @@
 #!/usr/bin/env python
 
-# Copyright 2008-2010 Nokia Siemens Networks Oyj
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import sys
 import os
 from distutils.core import setup
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src','robot'))
-from version import get_version
 import robot_postinstall
+execfile(os.path.join(os.path.dirname(__file__),'src','robot','version.py'))
 
 
-# Maximum width in Windows installer seems to be 70 characters
+# Maximum width in Windows installer seems to be 70 characters -------|
 DESCRIPTION = """
-Robot Framework is a Python-based keyword-driven test automation
-framework for acceptance level testing and acceptance test-driven
-development (ATDD). It has an easy-to-use tabular syntax for creating
-test cases and its testing capabilities can be extended by test
-libraries implemented either with Python or Java. Users can also
-create new keywords from existing ones using the same simple syntax
+Robot Framework is a generic Python based test automation framework
+for acceptance testing and acceptance test-driven development (ATDD).
+It has easy-to-use tabular test data syntax and utilizes the
+keyword-driven testing approach. Its testing capabilities can be
+extended by test libraries implemented either with Python or Java, and
+users can create new keywords from existing ones using the same syntax
 that is used for creating test cases.
 """[1:-1]
 CLASSIFIERS = """
@@ -68,9 +53,9 @@ def main():
           author_email = 'robotframework-devel@googlegroups.com',
           url          = 'http://robotframework.org',
           license      = 'Apache License 2.0',
-          description  = 'A keyword-driven acceptance test automation framework',
+          description  = 'A generic test automation framework',
           long_description = DESCRIPTION,
-          keywords     = 'acceptance test automation atdd',
+          keywords     = 'robotframework testing testautomation atdd',
           platforms    = 'any',
           classifiers  = CLASSIFIERS.splitlines(),
           package_dir  = {'': 'src'},
