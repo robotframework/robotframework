35534f067769c52cc886d2c568a338f091910211
Janne Härkönen <janne.harkonen@iki.fi>
2011-02-01 11:54:21 +0000

    memory profiler: print memory usages with awk

--- proto/memprofile.sh
+++ proto/memprofile.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-python ../src/robot/rebot.py $1 &
+python src/robot/rebot.py $1 >/dev/null &
+#/usr/lib/jvm/java-6-sun/jre/bin/java -Xmx1024m -Xss1024k -classpath /home/jth/opt/jython2.5.1/jython.jar: -Dpython.home=/home/jth/opt/jython2.5.1 -Dpython.executable=/home/jth/opt/jython2.5.1/jython org.python.util.jython src/robot/rebot.py $1 >/dev/null &
 rebotpid=$!
-echo $rebotpid
-while ps -ef | grep $rebotpid; do
-	top -b -p $rebotpid -n 1
+while ps -ef | grep -v grep| grep $rebotpid > /dev/null; do
+	top -b -p $rebotpid -n 1 | awk "/$rebotpid/ {print \$5,\$6,\$11}"
 	sleep 1
 done
 
+exit 0
