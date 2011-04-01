3fe218765df9f565e0422a56c1571938e01c5328
Mikko Korpela <mikko.korpela@gmail.com>
2011-04-01 12:27:24 +0300

    Decided to do this with another library.
    Backed out changeset b8609b1c7a60

--- proto/parallel/Parallel.py
+++ proto/parallel/Parallel.py
@@ -24,124 +24,28 @@ from robot.libraries import BuiltIn
 from robot.utils import html_escape, ArgumentParser
 from robot.version import get_version
 
-from Queue import Queue, Empty
-from threading import Event
-import socket
-
-try:
-    from multiprocessing.managers import BaseManager
-except ImportError:
-    class Communicate(object):
-
-        def __init__(self, address=None, port=None, authkey=None):
-           pass
-
-        def connect(self):
-           pass
-
-        def stop_server(self):
-           pass
-
-        def __getattr__(self, name):
-           if not name.startswith('__'):
-               raise RuntimeError('Use of messaging is not possible when '+\
-                                  'multiprocessing python module is not available.\n'+\
-                                  'Multiprocessing module is available in Python >= 2.6')
-           return object.__getattr__(self, name)
-else:
-    def _create_caching_getter(clazz):
-        objects = {}
-        def get_object(key):
-            if key not in objects:
-                objects[key] = clazz()
-            return objects[key]
-        return get_object
-
-    class Communicate(object):
-
-        def __init__(self, address='127.0.0.1', port=2187, authkey='live long and prosper'):
-            self._address = address
-            self._port = port
-            self._authkey = authkey
-            self._queue = None
-            self._connected = False
-            self._server = False
-
-        def set_address(self, address):
-            if self._connected:
-                raise RuntimeError('Address can not be set as there is already a connection')
-            self._address = address
-
-        def set_port(self, port):
-            if self._connected:
-                raise RuntimeError('Port can not be set as there is already a connection')
-            self._port = port
-
-        def connect(self):
-            if self._connected:
-               return
-            try:
-               socket.create_connection((self._address, self._port), 0.1).close()
-               self._create_manager().connect()
-               self._server = False
-            except socket.error:
-               self._create_manager(_create_caching_getter(Queue),
-                                    _create_caching_getter(Event)).start()
-               self._server = True
-            self._connected = True
-
-        def stop_server(self):
-            if self._server:
-                self._manager.shutdown()
-                self._server = False
-            self._connected = False
-
-        def _create_manager(self, queue_getter=None, event_getter=None):
-            BaseManager.register('get_queue', queue_getter)
-            BaseManager.register('get_event', event_getter)
-            self._manager = BaseManager((self._address, self._port), self._authkey)
-            return self._manager
-
-        def send_message(self, queue_id, value):
-            self._get_queue(queue_id).put(value)
-
-        def receive_message(self, queue_id, timeout):
-            return self._get_queue(queue_id).get(timeout=timeout)
-
-        def _get_queue(self, queue_id):
-            return self._manager.get_queue(queue_id)
-
-        def wait_for_event(self, event_id):
-            return self._get_event(event_id).wait()
-
-        def signal_event(self, event_id):
-            return self._get_event(event_id).set()
-
-        def _get_event(self, event_id):
-            return self._manager.get_event(event_id)
-
 
 class Parallel(object):
     """
     Library for executing tests in parallel from inside of a robot test case.
     Tests are executed in subprocesses. 
-
+    
     You can add arguments to all parallel test runs from `library importing`,
     for a set of parallel tests with `Add Arguments For Parallel Tests` and
     for an individual parallel test by passing the arguments in `Start Parallel Test`.
-
+     
     The following command line arguments (also from argument files) are automatically 
     passed to parallel tests:
     --loglevel, --runmode, --pythonpath, --variable, --variablefile
-
+    
     Example:
     | *Settings* |
     | Library | Parallel | pybot |
-
+    
+    
     | *Test Cases* |
     | Runner |
-    |        | Run Parallel Tests | Hello | World |
-    |        | [Teardown] | Teardown Parallel |
+    |        | Run Parallel Tests | Hello | World |  
     | Hello |
     |       | [Tags] | parallel |
     |       | Log    | Hello ${WORLD} |
@@ -164,7 +68,6 @@ class Parallel(object):
         self._script = runner_script
         self._arguments = self._get_arguments(arguments)
         self._processes = []
-        self._communicate = Communicate()
         self._data_source = None
 
     def _get_arguments(self, additional_arguments):
@@ -225,7 +128,7 @@ class Parallel(object):
         `Add Arguments For Parallel Tests` keyword.
 
         Returns a process object that represents this execution.
-
+        
         Example:
         | Set Data Source For Parallel Tests | MySuite.txt |
         | Start Parallel Test | Test From My Suite |
@@ -237,8 +140,8 @@ class Parallel(object):
             self._data_source = BuiltIn.BuiltIn().replace_variables('${SUITE_SOURCE}')
         process = _ParaRobo(test_name, self._data_source, 
                             self._arguments+list(arguments))
-        self._add_process(process)
         process.run(self._script)
+        self._processes.append(process)
         return process
 
     def run_parallel_tests(self, *test_names):
@@ -246,7 +149,7 @@ class Parallel(object):
 
         Arguments can be set with keyword `Add Arguments For Parallel Tests`
         and data source with keyword `Set Data Source For Parallel Tests`. 
-
+        
         Example:
         | Add Arguments For Parallel Tests | --variable | SOME_VARIABLE:someValue |
         | Set Data Source For Parallel Tests | MySuite.txt |
@@ -277,7 +180,7 @@ class Parallel(object):
             if process.wait() != 0:
                 failed += [process.test]
             process.report()
-            self._remove_process(process)
+            self._processes.remove(process)
         if failed:
             raise AssertionError("Following tests failed:\n%s" % "\n".join(failed))
 
@@ -292,139 +195,8 @@ class Parallel(object):
         """
         for process in self._processes:
             process.stop_test_execution()
-        self._clear_processes()
-
-    def wait_for_event(self, event):
-        """Waits until an event has happened.
-
-        `event` is the identifier for the waited event.
-
-        NOTE: Requires Python 2.6 or later.
-
-        Example:
-        In one process
-        | Wait For Event | my event |
-        ...
-        In another process
-        | Signal Event | my event |
-        """
-        self._communicate.connect()
-        self._communicate.wait_for_event(event)
-
-    def signal_event(self, event):
-        """Signals an event.
-        If a process is waiting for this event it will stop waiting after the signal.
-
-        `event` is the identifier for the event.
-
-        NOTE: Requires Python 2.6 or later.
-
-        Example:
-        In one process
-        | Wait For Event | my event |
-        ...
-        In another process
-        | Signal Event | my event |
-        """
-        self._communicate.connect()
-        self._communicate.signal_event(event)
-
-    def send_message_to(self, queue_id, value):
-        """Send a message to a message queue.
-
-        `queue_id` is the identifier for the queue.
-
-        `value` is the message. This can be a string, a number or any serializable object.
-
-        NOTE: Requires Python 2.6 or later.
-
-        Example:
-        In one process
-        | Send Message To | my queue | hello world! |
-        ...
-        In another process
-        | ${message}= | Receive Message From | my queue |
-        | Should Be Equal | ${message} | hello world! |
-        """
-        self._communicate.connect()
-        self._communicate.send_message(queue_id, value)
-
-    def receive_message_from(self, queue_id, timeout=None):
-        """Receive and consume a message from a message queue.
-        By default this keyword will block until there is a message in the queue.
-
-        `queue_id` is the identifier for the queue.
-
-        `timeout` is the time out in seconds to wait.
-
-        Returns the value from the message queue. Throws an exception if timeout expires.
-
-        NOTE: Requires Python 2.6 or later.
-
-        Example:
-        In one process
-        | Send Message To | my queue | hello world! |
-        ...
-        In another process
-        | ${message}= | Receive Message From | my queue |
-        | Should Be Equal | ${message} | hello world! |
-        """
-        self._communicate.connect()
-        timeout = float(timeout) if timeout is not None else None
-        return self._communicate.receive_message(queue_id, timeout)
-
-    def set_parallel_communication_server_address(self, address):
-        """
-        Set a new address for communication server (default is localhost).
-        Use this in case the parallel communication is done between remote computers.
-
-        NOTE!
-        The same address has to be set in every process that uses the communication server.
-        NOTE!
-        Requires Python 2.6 or later.
-        """
-        self._communicate.set_address(address)
-
-    def set_parallel_communication_server_port(self, port):
-        """
-        Set a new port for communication server (default is 2187).
-        Use this in case the default port does not work.
-
-        NOTE!
-        The same port has to be set in every process that uses the communication server.
-        NOTE!
-        Requires Python 2.6 or later.
-        """
-        self._communicate.set_port(int(port))
-
-    def teardown_parallel(self):
-        """
-        Use this to ensure that communication server and subprocesses have been stopped and
-        that they don't live to the next test.
-        A proper place for this is in the teardown section of the main test.
-
-        This will terminate all subprocesses that still are running and
-        stop parallel communication server if this process owns
-        the communication server and it has not been stopped yet.
-
-        NOTE: Requires Python 2.6 or later.
-        """
-        for process in self._processes:
-            process.kill()
-        self._clear_processes()
-
-    def _add_process(self, process):
-        self._communicate.connect()
-        self._processes += [process]
-
-    def _remove_process(self, process):
-        self._processes.remove(process)
-        if self._processes == []:
-            self._communicate.stop_server()
-
-    def _clear_processes(self):
         self._processes = []
-        self._communicate.stop_server()
+
 
 class _ParaRobo(object):
 
@@ -512,9 +284,6 @@ class _ParaRobo(object):
             pass
         self.report()
 
-    def kill(self):
-        self._process.kill()
-
 
 def _get_cmd_arguments():
     import robot
@@ -523,3 +292,4 @@ def _get_cmd_arguments():
     with open(runner_path, 'r') as runner_file:
         runner_content = runner_file.read()
     return re.search('"""(.+)"""', runner_content, re.DOTALL).groups()[0]
+
