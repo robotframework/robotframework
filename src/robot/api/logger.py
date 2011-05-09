615f65149075a2d4cc8902620fb7d040a331d66b
Pekka Klärck <peke@iki.fi>
2011-05-10 01:12:33 +0300

    API and UG docs for robot.api.logger
    Update issue 339
    Status: Review
    Cc: janne.t.harkonen
    First of all, we have agreed to keep the new API in `robot.api.logger`. We'll also use `robot.api` for other public APIs in the future.
    
    API docs written and a short section about the new logging API added to the User Guide. Janne, could you review the docs?
    
    The API docs still needs to be linked from the User Guide but that's covered by issue 482 (and a FIXME I added to the UG).

--- src/robot/api/logger.py
+++ src/robot/api/logger.py
@@ -12,29 +12,87 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+"""Public logging API for test libraries.
+
+This module provides a public API for writing messages to the log file
+and the console. Test libraries can use this API like `logger.info('My
+message')` instead of logging through the standard output like `print
+'*INFO* My message'`. In addition to a programmatic interface being
+cleaner to use, this API has a benefit that the log messages have
+accurate timestamps.
+
+Log levels
+----------
+
+It is possible to log messages using levels `TRACE`, `DEBUG`, `INFO`
+and `WARN` either using the `write` method or, more commonly, with the
+log level specific `trace`, `debug`, `info` and `warn` methods.
+
+By default the trace and debug messages are not logged but that can be
+changed with the `--loglevel` command line option. Warnings are
+automatically written also to the `Test Execution Errors` section in
+the log file and to the console.
+
+Logging HTML
+------------
+
+All methods that are used for writing messages to the log file have an
+optional `html` argument. If a message to be logged is supposed to be
+shown as HTML, this argument should be set to `True`.
+
+Example
+-------
+
+from robot.api import logger
+
+def my_keyword(arg):
+    logger.debug('Got argument %s' % arg)
+    do_something()
+    logger.info('<i>This</i> is a boring example', html=True)
+"""
+
 import sys
 
 from robot.output import LOGGER, Message
 
 
 def write(msg, level, html=False):
+    """Writes the message to the log file using the given level.
+
+    Valid log levels are `TRACE`, `DEBUG`, `INFO` and `WARN`. Instead
+    of using this method, it is generally better to use the level
+    specific methods such as `info` and `debug`.
+    """
     LOGGER.log_message(Message(msg, level, html))
 
 def trace(msg, html=False):
+    """Writes the message to the log file with the TRACE level."""
     write(msg, 'TRACE', html)
 
 def debug(msg, html=False):
+    """Writes the message to the log file with the DEBUG level."""
     write(msg, 'DEBUG', html)
 
 def info(msg, html=False, also_console=False):
+    """Writes the message to the log file with the INFO level.
+
+    If `also_console` argument is set to `True`, the message is written
+    both to the log file and to the console.
+    """
     write(msg, 'INFO', html)
     if also_console:
         console(msg)
 
 def warn(msg, html=False):
+    """Writes the message to the log file with the WARN level."""
     write(msg, 'WARN', html)
 
 def console(msg, newline=True):
+    """Writes the message to the console.
+
+    If the `newline` argument is `True`, a newline character is automatically
+    added to the message.
+    """
     if newline:
         msg += '\n'
     sys.__stdout__.write(msg)
