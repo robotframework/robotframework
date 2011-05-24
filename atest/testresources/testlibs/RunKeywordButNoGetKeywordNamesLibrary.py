f994445b815ec46e1cdaec5e3a283b83821293c4
Pekka Klärck <peke@iki.fi>
2011-05-24 11:24:56 +0300

    combined libs, cleanup

--- atest/testresources/testlibs/RunKeywordButNoGetKeywordNamesLibrary.py
+++
@@ -1,7 +0,0 @@
-class RunKeywordButNoGetKeywordNamesLibrary:
-    
-    def run_keyword(self, *args):
-        return ' '.join(args)
-        
-    def some_other_keyword(self, *args):
-        return ' '.join(args)
--- atest/testresources/testlibs/RunKeywordLibrary.py
+++ atest/testresources/testlibs/RunKeywordLibrary.py
@@ -3,28 +3,39 @@ from robot import utils
 
 class RunKeywordLibrary:
     ROBOT_LIBRARY_SCOPE = 'TESTCASE'
-    
+
     def __init__(self):
         self.kw_names = ['Run Keyword That Passes', 'Run Keyword That Fails']
-    
+
     def get_keyword_names(self):
         return self.kw_names
-    
-    def run_keyword(self, name, args):
-        if not utils.eq_any(name, self.kw_names):
-            raise AttributeError, "Non-existing keyword '%s'" % name
 
-        if utils.eq(name, 'Run Keyword That Passes'):
-            for arg in args:
-                print arg,
-            return ', '.join(args)
-        else:
-            if len(args) == 0:
-                msg = 'Failure'
-            else:
-                msg = 'Failure: %s' % ' '.join(args)
-            raise AssertionError, msg
+    def run_keyword(self, name, args):
+        try:
+            method = dict(zip(self.kw_names, [self._passes, self._fails]))[name]
+        except KeyError:
+            raise AttributeError
+        return method(args)
+
+    def _passes(self, args):
+        for arg in args:
+            print arg,
+        return ', '.join(args)
+
+    def _fails(self, args):
+        if not args:
+            raise AssertionError('Failure')
+        raise AssertionError('Failure: %s' % ' '.join(args))
 
 
 class GlobalRunKeywordLibrary(RunKeywordLibrary):
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
+
+
+class RunKeywordButNoGetKeywordNamesLibrary:
+
+    def run_keyword(self, *args):
+        return ' '.join(args)
+
+    def some_other_keyword(self, *args):
+        return ' '.join(args)
