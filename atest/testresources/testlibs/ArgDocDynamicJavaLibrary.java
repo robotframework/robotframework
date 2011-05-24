33f28a7dc65e5178932b40fd795d4dd7f6bb4072
Pekka Klärck <peke@iki.fi>
2011-05-24 16:01:12 +0300

    Tests for reporting failures when dynamic methods get_keyword_documentation and get_keyword_arguments fail.
    Update issue 827
    Status: Done
    And here come tests. This is now done according to option 4) above.

--- atest/testresources/testlibs/ArgDocDynamicJavaLibrary.java
+++ atest/testresources/testlibs/ArgDocDynamicJavaLibrary.java
@@ -1,7 +1,12 @@
 public class ArgDocDynamicJavaLibrary {
 
     public String[] getKeywordNames() {
-        return new String[] {"Java No Arg", "Java One Arg", "Java One or Two Args", "Java Many Args"};
+        return new String[] {"Java No Arg",
+                             "Java One Arg",
+                             "Java One or Two Args",
+                             "Java Many Args",
+                             "Invalid Java Args",
+                             "Invalid Java Doc"};
     }
 
     public Object runKeyword(String name, Object[] args) {
@@ -10,14 +15,22 @@ public class ArgDocDynamicJavaLibrary {
     }
 
     public String getKeywordDocumentation(String name) {
+        if (name.equals("Invalid Java Doc"))
+            throw new RuntimeException("Get doc failure");
         return "Keyword documentation for " + name;
     }
 
     public String[] getKeywordArguments(String name) {
-        if (name.equals("Java No Arg")) return new String[0];
-        if (name.equals("Java One Arg")) return new String[] {"arg"};
-        if (name.equals("Java One or Two Args")) return new String[] {"arg", "default=default"};
-        if (name.equals("Java Many Args")) return new String[] {"*args"};
+        if (name.equals("Java No Arg"))
+            return new String[0];
+        if (name.equals("Java One Arg"))
+            return new String[] {"arg"};
+        if (name.equals("Java One or Two Args"))
+            return new String[] {"arg", "default=default"};
+        if (name.equals("Java Many Args"))
+            return new String[] {"*args"};
+        if (name.equals("Invalid Java Args"))
+            throw new RuntimeException("Get args failure");
         return null;
     }
 }
