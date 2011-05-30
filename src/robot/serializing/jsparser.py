12796e4ea1c1163050f47cfd8eb8ec2a1e19de89
Mikko Korpela <mikko.korpela@gmail.com>
2011-05-30 09:03:24 +0300

    jsparser and serialize_log

---
+++ src/robot/serializing/jsparser.py
@@ -0,0 +1,309 @@
+from __future__ import with_statement
+from lxml import etree
+import json
+import zlib
+import base64
+from datetime import datetime
+from time import mktime
+from robot.utils import html_escape
+
+class Context(object):
+
+    def __init__(self):
+        self._texts = TextCache()
+        self._basemillis = None
+        self._stats = Stats()
+        self._current_place = []
+        self._kw_index = []
+        self._links = {}
+
+    @property
+    def basemillis(self):
+        return self._basemillis
+
+    def collect_stats(self):
+        self._stats = self._stats.new_child()
+        return self
+
+    def dump_stats(self):
+        try:
+            return self._stats.dump()
+        finally:
+            self._stats = self._stats.parent
+
+    def get_text_id(self, text):
+        return self._texts.add(text)
+
+    def dump_texts(self):
+        return self._texts.dump()
+
+    def timestamp(self, time):
+        dt = datetime.strptime(time+"000", "%Y%m%d %H:%M:%S.%f")
+        millis = int(mktime(dt.timetuple())*1000+dt.microsecond/1000)
+        if self.basemillis is None:
+            self._basemillis = millis
+        return millis - self.basemillis
+
+    def start_suite(self, name):
+        self._current_place += [('suite', name)]
+        self._kw_index += [0]
+
+    def end_suite(self):
+        self._current_place.pop()
+        self._kw_index.pop()
+
+    def start_test(self, name):
+        self._current_place += [('test', name)]
+        self._kw_index += [0]
+
+    def end_test(self):
+        self._current_place.pop()
+        self._kw_index.pop()
+
+    def start_keyword(self):
+        self._current_place += [('keyword', self._kw_index[-1])]
+        self._kw_index[-1] += 1
+        self._kw_index += [0]
+
+    def end_keyword(self):
+        kw = self._current_place.pop()
+        self._kw_index.pop()
+
+    def create_link_to_current_location(self, key):
+        self._links[str(key)] = self._create_link()
+
+    def _create_link(self):
+        return "keyword_"+".".join(str(v) for _, v in self._current_place)
+
+    def link_to(self, key):
+        return self._links[str(key)]
+
+    def add_test(self, critical, passed):
+        self._stats.add_test(critical, passed)
+
+    def teardown_failed(self):
+        self._stats.fail_all()
+
+
+class Stats(object):
+    TOTAL = 0
+    TOTAL_PASSED = 1
+    CRITICAL = 2
+    CRITICAL_PASSED = 3
+
+    def __init__(self, parent=None):
+        self.parent = parent
+        self._stats = [0,0,0,0]
+        self._children = []
+
+    def new_child(self):
+        self._children += [Stats(self)]
+        return self._children[-1]
+
+    def add_test(self, critical, passed):
+        self._stats[Stats.TOTAL] += 1
+        if passed:
+            self._stats[Stats.TOTAL_PASSED] +=1
+        if critical:
+            self._stats[Stats.CRITICAL] += 1
+            if passed:
+                self._stats[Stats.CRITICAL_PASSED] += 1
+
+    def dump(self):
+        if self.parent:
+            for i in range(4):
+                self.parent._stats[i] += self._stats[i]
+        return self._stats
+
+    def fail_all(self):
+        self._stats[1] = 0
+        self._stats[3] = 0
+        for child in self._children:
+            child.fail_all()
+
+
+class TextCache(object):
+
+    def __init__(self):
+        self.texts = {}
+        self.index = 1
+
+    def add(self, text):
+        if not text:
+            return 0
+        text = self._encode(text)
+        if text not in self.texts:
+            self.texts[text] = self.index
+            self.index +=1
+        return self.texts[text]
+
+    def _encode(self, text):
+        encoded = base64.b64encode(zlib.compress(text, 9))
+        raw = '*'+text
+        return encoded if len(encoded) < len(raw) else raw
+
+    def dump(self):
+        l = range(len(self.texts)+1)
+        l[0] = '*'
+        for k, v in self.texts.items():
+            l[v] = k
+        return l
+
+
+def _keyword_node_parser(node, context):
+    context.start_keyword()
+    try:
+        result = [node.get('type'),
+                  context.get_text_id(node.get('name')),
+                  context.get_text_id(node.get('timeout'))]+_children(node, context)
+        if result[0] == 'teardown' and result[-1][0] == 'F':
+            context.teardown_failed()
+        return result
+    finally:
+        context.end_keyword()
+
+def _test_node_parser(node, context):
+    name = node.get('name')
+    context.start_test(name)
+    try:
+        critical = 'Y' if node.find('status').get('critical') == 'yes' else 'N'
+        result = ['test',
+                  context.get_text_id(name),
+                  context.get_text_id(node.get('timeout')),
+                  critical]+_children(node, context)
+        context.add_test(critical == 'Y', result[-1][0] == 'P')
+        return result
+    finally:
+        context.end_suite()
+
+def _errors_node_parser(node, context):
+    return _children(node, context)
+
+def _children(node, context):
+    return [_create_from_node(child, context) for child in node.getchildren()]
+
+def _doc_node_parser(node, context):
+    return context.get_text_id(node.text)
+
+levels = {'TRACE':'T',
+          'DEBUG':'D',
+          'INFO':'I',
+          'WARN':'W',
+          'ERROR':'E',
+          'FAIL':'F'}
+
+def _get_level(node):
+    return levels[node.get('level')]
+
+def _get_msg_text_id(node, context):
+    if node.get('html'):
+        return context.get_text_id(node.text)
+    else:
+        return context.get_text_id(html_escape(node.text, replace_whitespace=False))
+
+def _message_node_parser(node, context):
+    msg = [context.timestamp(node.get('timestamp')),
+            _get_level(node),
+            _get_msg_text_id(node, context)]
+    if node.get("linkable") == "yes":
+        msg += [context.link_to(msg)]
+    elif msg[1] == 'W':
+        context.create_link_to_current_location(msg)
+    return msg
+
+def _metadata_node_parser(node, context):
+    items = {}
+    for item in node.getchildren():
+        items[item.get('name')] = context.get_text_id(item.text)
+    return items
+
+def _status_node_parser(node, context):
+    status = node.get('status')[0]
+    starttime = context.timestamp(node.get('starttime'))
+    endtime = context.timestamp(node.get('endtime'))
+    return [status,
+            starttime,
+            endtime-starttime]
+
+def _tags_node_parser(node, context):
+    return [context.get_text_id(c.text) for c in node.getchildren()]
+
+def _arguments_node_parser(node, context):
+    return context.get_text_id(', '.join(c.text for c in node.getchildren() if c.text is not None))
+
+def _suite_node_parser(node, context):
+    name = node.get('name')
+    context.start_suite(name)
+    try:
+        return ['suite',
+                node.get('source'),
+                name]+\
+                _children(node, context.collect_stats())+\
+                [context.dump_stats()]
+    finally:
+        context.end_suite()
+
+
+def _robot_node_parser(node, context):
+    generator = node.get('generator')
+    children = _children(node, context)
+    generated = node.get('generated')
+    if generated:
+        generated = context.timestamp(generated)
+    return [generated, generator]+children
+
+def _statistics_parser(node, context):
+    return [_stat_from(c) for c in node.getchildren()]
+
+def _stat_from(node):
+    return [[stat.text, int(stat.get('pass')), int(stat.get('fail')),
+             stat.get('doc', ''), stat.get('info', ''), stat.get('links', '')]
+             for stat in node.getchildren()]
+
+
+_node_parsers = {
+'robot':_robot_node_parser,
+'suite':_suite_node_parser,
+'doc':_doc_node_parser,
+'metadata':_metadata_node_parser,
+'status':_status_node_parser,
+'errors':_errors_node_parser,
+'test':_test_node_parser,
+'kw':_keyword_node_parser,
+'tags':_tags_node_parser,
+'arguments':_arguments_node_parser,
+'statistics':_statistics_parser,
+'msg':_message_node_parser
+}
+
+def _create_from_node(node, context):
+    return _node_parsers[node.tag](node, context)
+
+def create_datamodel(xml):
+    context = Context()
+    robot_data = _create_from_node(xml.getroot(), context)
+    return context.basemillis, robot_data, context.dump_texts()
+
+def parse_js(file, output):
+    xml = etree.parse(file)
+    basemillis, robot_data, texts = create_datamodel(xml)
+    _write_js(basemillis, robot_data, texts, output)
+
+def _write_js(basemillis, robot_data, texts, output):
+    output.write('window.basemillis = '+str(basemillis)+';\n')
+    output.write('window.data = ')
+    json.dump(robot_data, output, separators=(',', ':'))
+    output.write(';\n')
+    output.write('window.strings =')
+    json.dump(texts, output, separators=(',', ':'))
+    output.write(';\n')
+
+def parse(input_filename, output_filename):
+    with open(input_filename, 'r') as file:
+        with open(output_filename, 'w') as output:
+            parse_js(file, output)
+
+if __name__ == '__main__':
+    import sys
+    filename = sys.argv[1]
+    parse(filename, filename+'.js')
---
+++ src/robot/serializing/serialize_log.py
@@ -0,0 +1,90 @@
+from __future__ import with_statement
+
+import re
+import os
+from robot import webcontent
+
+PATH = os.path.dirname(webcontent.__file__)+'/'
+LOG_TEMPLATE = PATH + 'log.html'
+JS_FILE_REGEXP = re.compile('src=\"([^\"]+)\"')
+
+def serialize_log(test_output_js_file, log_path, title=None):
+    if log_path is None:
+        return
+    _build_log_file(log_path, test_output_js_file, title)
+
+def _build_log_file(log_path, js_file, title):
+    with open(log_path, 'w') as log:
+        populator = _Populator(log, js_file, title)
+        with open(LOG_TEMPLATE, 'r') as template:
+            for line in template:
+                populator.line(line)
+
+class _Populator(object):
+
+    def __init__(self, log, js_file, title):
+        self._log = log
+        self._js_file = js_file
+        self._title = title
+        self._parsing = self._normal_parsing
+
+    def line(self, line):
+        self._parsing(line)
+
+    def _normal_parsing(self, line):
+        if self._is_begin_scripts(line):
+            self._start_script()
+        elif self._is_title_line_to_handle(line):
+            self._write_title()
+        else:
+            self._log.write(line)
+
+    def _is_begin_scripts(self, line):
+        return line == '<!-- BEGIN SCRIPTS -->\n'
+
+    def _is_title_line_to_handle(self, line):
+        return self._title is not None and line.startswith('<title>')
+
+    def _write_title(self):
+        self._log.write('<title>%s</title>\n' % self._title)
+
+    def _in_script(self, line):
+        if self._is_end_scripts(line):
+            self._end_script()
+        elif self._is_output_js(line):
+            self._write_output_js()
+        else:
+            self._write_lines_js(line)
+
+    def _is_end_scripts(self, line):
+        return line == '<!-- END SCRIPTS -->\n'
+
+    def _end_script(self):
+        self._log.write('</script>\n')
+        self._parsing = self._normal_parsing
+
+    def _is_output_js(self, line):
+        return line.startswith('<!-- OUTPUT JS -->')
+
+    def _write_output_js(self):
+        self._write_js(self._js_file)
+
+    def _write_lines_js(self, line):
+        self._write_js(self._parse_js_file_name(line))
+
+    def _start_script(self):
+        self._log.write('<script type="text/javascript">\n')
+        self._parsing = self._in_script
+
+    def _parse_js_file_name(self, line):
+        return PATH + JS_FILE_REGEXP.search(line).group(1)
+
+    def _write_js(self, js_file):
+        with open(js_file, 'r') as js:
+            for jsline in js:
+                self._log.write(jsline)
+
+if __name__ == '__main__':
+    import jsparser
+    jsparser.parse('output.xml', 'output.js')
+    serialize_log('output.js', 'logjsx.html')
