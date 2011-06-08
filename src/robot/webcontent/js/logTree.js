91dd0320981c79a4afb6ec27ae494836325144e1
Pekka Klärck <peke@iki.fi>
2011-06-08 13:28:50 +0300

    view suite, test and keyword using larger templates instead of creting them from small templates using javascript

--- src/robot/webcontent/js/logTree.js
+++ src/robot/webcontent/js/logTree.js
@@ -1,116 +1,66 @@
-
 function openSuite(suiteId) {
-    function populator(suite, childrenElement){
-        populateMetadata(suite, childrenElement);
-        addElements(suite.keyword, window.templates.keyword, childrenElement);
-        addElements(suite.suite, window.templates.suite, childrenElement);
-        addElements(suite.test, window.templates.test, childrenElement);
+    function populator(suite, childElement){
+        addElements(suite.keyword, window.templates.keyword, childElement);
+        addElements(suite.suite, window.templates.suite, childElement);
+        addElements(suite.test, window.templates.test, childElement);
     }
     openElement(suiteId, populator);
 }
 
-function populateMetadata(element, childrenElement) {
-    var metadata = $.tmpl(window.templates.metadata, element);
-    metadata.appendTo(childrenElement);
-    meta = $('#'+element.id+'_metadata');
-    addMetadata(meta, 'Full Name', element.fullname, window.templates.metadataElement);
-    addMetadata(meta, 'Documentation', element.documentation, window.templates.documentationElement);
-    addMetadata(meta, 'Timeout', element.timeout, window.templates.metadataElement);
-    if (element.tags)
-        addMetadata(meta, 'Tags', element.tags.join(', '), window.templates.metadataElement);
-    for(var key in element.metadata){
-        addMetadata(meta, key, element.metadata[key], window.templates.metadataElement);
-    }
-    addMetadata(meta, 'Source', element.source, window.templates.sourceElement);
-    addMetadata(meta, 'Start / End',
-                [element.times.startTime(), element.times.endTime()].join('  /  '),
-                window.templates.metadataElement);
-    addMetadata(meta, 'Elapsed',
-            element.times.elapsedTime(),
-            window.templates.metadataElement);
-    if (element.statusText) {
-        var status = $.tmpl(window.templates.statusElement, element);
-        status.appendTo(meta);
-    }
-    addMessage(meta, element);
-}
-
-function addMessage(parent, element){
-    var failureMessage = element.getFailureMessage();
-    if(element.total == undefined){
-        addMetadata(meta, 'Message', failureMessage, window.templates.metadataElement);
-        return;
-    }
-    var template = (failureMessage !== undefined ? window.templates.failureAndStatsMessageElement : window.templates.statsMessageElement);
-    var elem = $.tmpl(template, element);
-    elem.appendTo(parent);
-}
-
-function addMetadata(parent, key, value, template) {
-    if (value === undefined || value === "") return;
-    var data = {key:key, value:value};
-    var metadataElement = $.tmpl(template, data);
-    metadataElement.appendTo(parent);
-}
-
-function addElements(elems, template, target){
-    for(var i = 0;elems(i); i++){
-        $.tmpl(template, elems(i)).appendTo(target);
-    }
-}
-
-function openElement(elementId, populator){
-    var childrenElement = $("#"+elementId+"_children");
-    childrenElement.show();
-    $('#'+elementId+'_foldlink').show();
-    $('#'+elementId+'_unfoldlink').hide();
-    if(childrenElement.hasClass("populated"))
-        return;
-    childrenElement.addClass("populated");
-    element = window.testdata.find(elementId);
-    populator(element, childrenElement);
-}
-
-function closeElement(elementId) {
-    var childrenElement = $("#"+elementId+"_children")
-    childrenElement.hide();
-    $('#'+elementId+'_foldlink').hide();
-    $('#'+elementId+'_unfoldlink').show();
-}
-
 function openTest(testId) {
-    function populator(test, childrenElement){
-        populateMetadata(test, childrenElement);
-        addElements(test.keyword, window.templates.keyword, childrenElement);
+    function populator(test, childElement){
+        addElements(test.keyword, window.templates.keyword, childElement);
     }
     openElement(testId, populator);
 }
 
 function openKeyword(kwId) {
-    function populator(keyword, childrenElement){
-        populateMetadata(keyword, childrenElement);
-        addElements(keyword.keyword, window.templates.keyword, childrenElement);
-        addElements(keyword.message, window.templates.message, childrenElement);
+    function populator(keyword, childElement){
+        addElements(keyword.keyword, window.templates.keyword, childElement);
+        addElements(keyword.message, window.templates.message, childElement);
     }
     openElement(kwId, populator);
 }
 
+function addElements(elems, template, target){
+    for (var i = 0; elems(i); i++) {
+        $.tmpl(template, elems(i)).appendTo(target);
+    }
+}
+
+function openElement(elementId, populator){
+    var childElement = $("#"+elementId+"_children");
+    childElement.show();
+    if (!childElement.hasClass("populated")) {
+        element = window.testdata.find(elementId);
+        populator(element, childElement);
+        childElement.addClass("populated");
+    }
+    $('#'+elementId+'_foldlink').show();
+    $('#'+elementId+'_unfoldlink').hide();
+}
+
+function closeElement(elementId) {
+    $("#"+elementId+"_children").hide();
+    $('#'+elementId+'_foldlink').hide();
+    $('#'+elementId+'_unfoldlink').show();
+}
+
 function iterateTasks(){
-    if (window.tasks.length == 0)
+    if (!window.tasks.length)
         return;
     var element = window.tasks.pop();
-    var unfoldLink = $("#"+element.id+"_unfoldlink");
-    if(element == undefined || elementHiddenByUser(element.id)){
+    if (element == undefined || elementHiddenByUser(element.id)) {
         window.tasks = []
         return;
     }
     $("#"+element.id+"_unfoldlink").click();
     var children = element.children()
-    for(var i = children.length-1; i >= 0; i--){
-        if(window.tasksMatcher(children[i]))
+    for (var i = children.length-1; i >= 0; i--) {
+        if (window.tasksMatcher(children[i]))
             window.tasks.push(children[i]);
     }
-    if(window.tasks.length > 0)
+    if (window.tasks.length)
         setTimeout("iterateTasks()", 0);
 }
 
@@ -126,7 +76,7 @@ function expandAllChildren(elementId) {
 }
 
 function expandFailed(element) {
-    if(element.status == "fail"){
+    if (element.status == "fail") {
         window.tasks = [element];
         window.tasksMatcher = function(e) {return e.status == "fail"};
         iterateTasks();
