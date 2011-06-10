5aa026733eef190418099e9837c929f75d20b20b
Pekka Klärck <peke@iki.fi>
2011-06-11 01:56:43 +0300

    updated to match current situation

--- atest/run_atests.py
+++ atest/run_atests.py
@@ -4,20 +4,20 @@
 
 Usage:  run_atests.py interpreter [options] datasource(s)
 
-Data sources are paths to directories or files under 'robot' folder.
+Data sources are paths to directories or files under `robot` folder.
 
 Available options are the same that can be used with Robot Framework.
-See its help (e.g. 'pybot --help') for more information.
+See its help (e.g. `pybot --help`) for more information.
 
-The specified interpreter is used by acceptance tests under 'robot' to
-run test cases under 'testdata'. It can be simply 'python' or 'jython'
+The specified interpreter is used by acceptance tests under `robot` to
+run test cases under `testdata`. It can be simply `python` or `jython`
 (if they are in PATH) or to a path a selected interpreter (e.g.
-'/usr/bin/python23'). Note that this script itself must always be
+`/usr/bin/python26`). Note that this script itself must always be
 executed with Python.
 
 Examples:
 $ atest/run_atests.py python --test example atest/robot
-$ atest/run_atests.py /usr/bin/jython22 atest/robot/core/variables.html
+$ atest/run_atests.py /usr/bin/jython25 atest/robot/tags/tag_doc.txt
 """
 
 import signal
