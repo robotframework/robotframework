c39bb9ee3d24e3d876fce287ce6f281958f9ffde
Janne Härkönen <janne.harkonen@iki.fi>
2011-06-15 18:53:17 +0300

    fixed importer tests after cleanup

--- utest/running/test_importer.py
+++ utest/running/test_importer.py
@@ -1,24 +1,24 @@
 import unittest
 
-from robot.running.importer import _LibraryCache
+from robot.running.importer import ImportCache
 from robot.utils.asserts import *
 
 
 class TestLibraryCache(unittest.TestCase):
 
     def setUp(self):
-        self.cache = _LibraryCache()
+        self.cache = ImportCache()
         self.cache[('lib', ['a1', 'a2'])] = 'Library'
         self.cache['res'] = 'Resource'
-        
+
     def test_add_library(self):
-        assert_equals(self.cache._keys, [('lib', ['a1', 'a2']), 'res'])
-        assert_equals(self.cache._libs, ['Library', 'Resource'])
+        assert_equals(self.cache._keys, [['lib', ['a1', 'a2']], 'res'])
+        assert_equals(self.cache._items, ['Library', 'Resource'])
 
     def test_get_existing_library(self):
         assert_equals(self.cache['res'], 'Resource')
-        assert_equals(self.cache[('lib', ['a1', 'a2'])], 'Library')        
-        assert_equals(self.cache[('lib', ['a1', 'a2'])], 'Library')        
+        assert_equals(self.cache[('lib', ['a1', 'a2'])], 'Library')
+        assert_equals(self.cache[('lib', ['a1', 'a2'])], 'Library')
         assert_equals(self.cache['res'], 'Resource')
 
     def test_get_non_existing_library(self):
@@ -26,10 +26,10 @@ class TestLibraryCache(unittest.TestCase):
         assert_raises(KeyError, self.cache.__getitem__, ('lib1', ['wrong']))
 
     def test_has_library(self):
-        assert_true(self.cache.has_key(('lib', ['a1', 'a2'])))
-        assert_false(self.cache.has_key(('lib', ['a1', 'a2', 'wrong'])))
-        assert_false(self.cache.has_key('nonex'))
-        
+        assert_true(('lib', ['a1', 'a2']) in self.cache)
+        assert_false(('lib', ['a1', 'a2', 'wrong']) in self.cache)
+        assert_false('nonex' in self.cache)
+
 
 if __name__ == '__main__':
     unittest.main()
