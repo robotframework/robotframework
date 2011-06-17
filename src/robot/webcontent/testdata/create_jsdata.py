77739d4a6eea93da29713e6ef97648ac2b4791a1
Janne Härkönen <janne.harkonen@iki.fi>
2011-06-17 10:40:10 +0300

    Added testdata for creating data.js

--- src/robot/webcontent/testdata/create_jsdata.py
+++ src/robot/webcontent/testdata/create_jsdata.py
@@ -1,12 +1,21 @@
 import os
+import robot
 from robot.result.jsparser import create_datamodel_from
 
+BASEDIR = os.path.dirname(__file__)
+TESTDATA = os.path.join(BASEDIR, 'dir.suite')
+OUTPUT = os.path.join(BASEDIR, 'output.xml')
+TARGET = os.path.join(BASEDIR, 'data.js')
+
 if __name__ == '__main__':
-    base = os.path.dirname(__file__)
-    model = create_datamodel_from(os.path.join(base, 'output.xml'))
+    robot.run(TESTDATA, tagstatlink=['force:http://google.com:kuukkeli'],
+              tagdoc=['test:this_is_my_test'], tagstatcombine=['fooANDi*:zap'],
+              critical=['i?'], noncritical=['*kekkone*'], outputdir=BASEDIR,
+              log='NONE', report='NONE')
+    model = create_datamodel_from(OUTPUT)
     model.set_settings({'logURL': 'log.html',
                         'reportURL': 'report.html',
                         'background': {'fail': 'DeepPink'}})
-    with open(os.path.join(base, 'data.js'), 'w') as output:
+    with open(TARGET, 'w') as output:
         model.write_to(output)
 
