1c789b818b1346e658d59030fe9f66a158162a38
Pekka Klärck <peke@iki.fi>
2011-06-18 21:05:14 +0300

    whitespace

--- utest/output/test_output_splitter.py
+++ utest/output/test_output_splitter.py
@@ -4,18 +4,18 @@ from robot.utils.asserts import *
 
 from robot.output.output import _OutputSplitter
 
-        
+
 class TestOutputSplitter(unittest.TestCase):
-    
+
     def test_empty_output_should_result_in_empty_messages_list(self):
         splitter = _OutputSplitter('')
         assert_equals([], list(splitter))
-        
+
     def test_plain_output_should_have_info_level(self):
         splitter = _OutputSplitter('this is an output message\nin many\nlines.')
         assert_equals(1, len(list(splitter)))
         self._verify_message(splitter, 'this is an output message\nin many\nlines.')
-        
+
     def test_leading_and_trailing_space_should_be_stripped(self):
         splitter = _OutputSplitter('\t  \n My message    \t\r\n')
         self._verify_message(splitter, 'My message')
@@ -23,7 +23,7 @@ class TestOutputSplitter(unittest.TestCase):
     def test_legal_level_is_correctly_read(self):
         splitter = _OutputSplitter('*DEBUG* My message details')
         self._verify_message(splitter, 'My message details', 'DEBUG')
-        
+
     def test_it_is_possible_to_define_multiple_levels(self):
         splitter = _OutputSplitter('*WARN*WARNING!\n*DEBUG*msg')
         assert_equals(2, len(list(splitter)))
@@ -33,19 +33,18 @@ class TestOutputSplitter(unittest.TestCase):
     def test_html_flag_should_be_parsed_correctly_and_uses_info_level(self):
         splitter = _OutputSplitter('*HTML* <div><a href="">link</a></div>')
         self._verify_message(splitter, '<div><a href="">link</a></div>', 'INFO', True)
-                        
+
     def test_default_level_for_first_message_in_info(self):
         splitter = _OutputSplitter('<img src="foo bar">\n*DEBUG*bar foo')
         self._verify_message(splitter, '<img src="foo bar">')
         self._verify_message(splitter, 'bar foo', 'DEBUG', index=1)
-        
-        
+
     def _verify_message(self, splitter, msg, level='INFO', html=False, index=0):
         message = list(splitter)[index]
         assert_equals(message.message, msg)
         assert_equals(message.level, level)
         assert_equals(message.html, html)
 
-        
+
 if __name__ == '__main__':
     unittest.main()
