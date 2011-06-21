e9f49e49aaee31e47804d6540694fc9cdaf83f07
Robot Framework Developers <robotframework@gmail.com>
2011-06-21 13:01:03 +0300

    Add test seed logging to tests using randomness

--- utest/result/test_context.py
+++ utest/result/test_context.py
@@ -1,8 +1,7 @@
-import base64
+import time
 import random
 import string
 import unittest
-import zlib
 from robot.result.elementhandlers import TextCache
 from robot.utils.asserts import assert_equals, assert_true
 
@@ -50,6 +49,9 @@ class TestIntegerContext(_ContextTesting):
 class TestTextCache(unittest.TestCase):
 
     def setUp(self):
+        # To make test reproducable log the random seed if test fails
+        self._seed = long(time.time() * 256)
+        random.seed(self._seed)
         self._text_cache = TextCache()
 
     def _verify_text(self, string, expected):
@@ -70,13 +72,15 @@ class TestTextCache(unittest.TestCase):
         for i in range(300):
             id = self._text_cache.add(self._generate_random_string(i))
             assert_true(i+1 >= len(self._text_cache.dump()[id]),
-                        msg='len(self._text_cache.dump()[id]) (%s) > i+1 (%s)'  % (len(self._text_cache.dump()[id]), i+1))
+                        msg='len(self._text_cache.dump()[id]) (%s) > i+1 (%s) [test seed = %s]'  % \
+                            (len(self._text_cache.dump()[id]), i+1, self._seed))
 
     def test_long_random_strings_are_compressed(self):
         for i in range(30):
             value = self._generate_random_string(300)
             id = self._text_cache.add(value)
-            assert_equals(self._compress(value), self._text_cache.dump()[id])
+            assert_equals(self._compress(value), self._text_cache.dump()[id],\
+                          msg='Did not compress [test seed = %s]' % self._seed)
 
     def _generate_random_string(self, length):
         return ''.join(random.choice(string.digits) for _ in range(length))
