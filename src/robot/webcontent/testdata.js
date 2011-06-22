e1dd36757ec9eab6c5f59a4c24361293b557c83e
Jussi Malinen <jussi.ao.malinen@gmail.com>
2011-06-22 16:23:26 +0300

    uuid generation was very slow, new faster implementation.

--- src/robot/webcontent/testdata.js
+++ src/robot/webcontent/testdata.js
@@ -20,11 +20,12 @@ window.testdata = function () {
         return elem;
     }
 
+    var idCounter=0;
+
+    // TODO: Rename this function. 
     function uuid() {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-            return v.toString(16);
-        });
+        idCounter++;
+        return "elementId_"+idCounter;
     }
 
     function timestamp(millis) {
