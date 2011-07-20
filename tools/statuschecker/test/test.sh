cae3817ea3d6e923e02444a9d709b1b6528ceffc
Pekka Klärck <peke@iki.fi>
2011-07-20 12:55:50 +0300

    made tests easier to understand

--- tools/statuschecker/test/test.sh
+++ tools/statuschecker/test/test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-basedir=$PWD/test
-pybot --loglevel DEBUG --log none --report none --outputdir $basedir $basedir/example.html
-python $basedir/../statuschecker.py $basedir/output.xml
-rebot $basedir/output.xml
+testdir=`dirname $0`
+pybot --loglevel DEBUG --log none --report none --outputdir $testdir $testdir/example.txt
+python $testdir/../statuschecker.py $testdir/output.xml
+rebot $testdir/output.xml
+echo "$? tests failed, 5 should have failed."
+echo "Check that tests starting with 'FAILURE:' have failed and others passed."
 
 
