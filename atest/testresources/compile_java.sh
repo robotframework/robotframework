bf774dd94619f5a1c357e3e569a40ce7b896fa9e
Janne Härkönen <janne.harkonen@iki.fi>
2011-09-16 10:26:54 +0300

    compile_java: resolve dirs and JYTHON_HOME properly

--- atest/testresources/compile_java.sh
+++ atest/testresources/compile_java.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-javac -target 1.5 -source 1.5 testlibs/*.java
-javac -cp $JYTHON_HOME/jython.jar -target 1.5 -source 1.5 listeners/*.java
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+javac -target 1.5 -source 1.5 $DIR/testlibs/*.java
+if [ -n "$JYTHON_HOME" ]
+then
+    javac -cp $JYTHON_HOME/jython.jar -target 1.5 -source 1.5 $DIR/listeners/*.java
+else
+    echo set JYTHON_HOME to compile listeners
+fi
