a4901b614f483700b71046918ca63be5311ff98b
Pekka Klärck <peke@iki.fi>
2011-09-23 11:44:35 +0300

    Tests for all listener methods logging messages using INFO and WARN level.
    Update issue 548
    Tests are done.

---
+++ atest/testdata/output/listeners/logging_listener.py
@@ -0,0 +1,20 @@
+import logging
+from robot.api import logger
+
+ROBOT_LISTENER_API_VERSION = 2
+
+def get_logging_listener_method(name):
+    def listener_method(*args):
+        if name in ['message', 'log_message']:
+            msg = args[0]
+            message = '%s: %s %s' % (name, msg['level'], msg['message'])
+        else:
+            message = name
+        logging.info(message)
+        logger.warn(message)
+    return listener_method
+
+for name in ['start_suite', 'end_suite', 'start_test', 'end_test',
+             'start_keyword', 'end_keyword', 'log_message', 'message',
+             'output_file', 'log_file', 'report_file', 'debug_file', 'close']:
+    globals()[name] = get_logging_listener_method(name)
