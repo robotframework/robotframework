24e5e9ac963de72c3e0a6878cecb28d2f181199d
Pekka Klärck <peke@iki.fi>
2011-10-25 12:53:24 +0300

    TestCase.tags and TestSuite.metadata are now objects

--- src/robot/result/model.py
+++ src/robot/result/model.py
@@ -12,15 +12,18 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+from robot import utils
+
 
 class TestSuite(object):
 
-    def __init__(self, parent=None, source='', name='', doc='', status='PASS'):
+    def __init__(self, parent=None, source='', name='', doc='', metadata=None,
+                 status='PASS'):
         self.parent = parent
         self.source = source
         self.name = name
         self.doc = doc
-        self.metadata = {}
+        self.metadata = metadata
         self.status = status
         self.message = ''
         self.keywords = []
@@ -30,14 +33,20 @@ class TestSuite(object):
         self.endtime = ''
         self.elapsedtime = ''
 
+    def _get_metadata(self):
+        return self._metadata
+    def _set_metadata(self, metadata):
+        self._metadata = utils.NormalizedDict(hmetadata, ignore=['_'])
+    metadata = property(_get_metadata, _set_metadata)
+
 
 class TestCase(object):
 
-    def __init__(self, parent=None, name='', doc='', status='PASS'):
+    def __init__(self, parent=None, name='', doc='', tags=None, status='PASS'):
         self.parent = parent
         self.name = name
         self.doc = doc
-        self.tags = []
+        self.tags = tags
         self.status = status
         self.message = ''
         self.timeout = ''
@@ -46,6 +55,39 @@ class TestCase(object):
         self.starttime = ''
         self.endtime = ''
         self.elapsedtime = ''
+
+    tags = property(lambda self: self._tags,
+                    lambda self, tags: setattr(self, '_tags', Tags(tags)))
+
+
+class Tags(object):
+
+    def __init__(self, tags=None):
+        if isinstance(tags, basestring):
+            tags = [tags]
+        self._tags = utils.normalize_tags(tags or [])
+
+    def add(self, tags):
+        self._tags = utils.normalize_tags(list(self) + list(Tags(tags)))
+
+    def remove(self, tags):
+        tags = Tags(tags)
+        self._tags = [t for t in self if t not in tags]
+
+    def __contains__(self, tag):
+        return utils.eq_any(tag, list(self), ignore=['_'])
+
+    def __len__(self):
+        return len(self._tags)
+
+    def __iter__(self):
+        return iter(self._tags)
+
+    def __unicode__(self):
+        return u'[%s]' % ', '.join(self)
+
+    def __str__(self):
+        return unicode(self).encode('UTF-8')
 
 
 class Keyword(object):
---
+++ utest/result/test_resultmodel.py
@@ -0,0 +1,108 @@
+import unittest
+from robot.utils.asserts import assert_equal, assert_true, assert_raises
+
+from robot.result.model import TestSuite, TestCase, Tags
+
+
+class TestTestSuite(unittest.TestCase):
+
+    def setUp(self):
+        self.suite = TestSuite(metadata={'M': 'V'})
+
+    def test_modify_medatata(self):
+        self.suite.metadata['m'] = 'v'
+        self.suite.metadata['n'] = 'w'
+        assert_equal(dict(self.suite.metadata), {'M': 'v', 'n': 'w'})
+
+    def test_set_metadata(self):
+        self.suite.metadata = {'a': '1', 'b': '1'}
+        self.suite.metadata['A'] = '2'
+        assert_equal(dict(self.suite.metadata), {'a': '2', 'b': '1'})
+
+
+class TestTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.test = TestCase(tags=['t1', 't2'])
+
+    def test_modify_tags(self):
+        self.test.tags.add(['t0', 't3'])
+        self.test.tags.remove('T2')
+        assert_equal(list(self.test.tags), ['t0', 't1', 't3'])
+
+    def test_set_tags(self):
+        self.test.tags = ['s2', 's1']
+        self.test.tags.add('s3')
+        assert_equal(list(self.test.tags), ['s1', 's2', 's3'])
+
+
+class TestTags(unittest.TestCase):
+
+    def test_empty_init(self):
+        assert_equal(list(Tags()), [])
+
+    def test_init_with_string(self):
+        assert_equal(list(Tags('string')), ['string'])
+
+    def test_init_with_iterable_and_normalization_and_sorting(self):
+        for inp in [['T 1', 't2'],
+                    ('t2', 'T 1'),
+                    ('t2', 'T 2', '__T__2__', 'T 1', 't1', 't_1'),
+                    ('', 'T 1', '', 't2'),
+                    ('NONE', 'T 1', 't2')]:
+            assert_equal(list(Tags(inp)), ['T 1', 't2'])
+
+    def test_add_string(self):
+        tags = Tags(['Y'])
+        tags.add('x')
+        assert_equal(list(tags), ['x', 'Y'])
+
+    def test_add_iterable(self):
+        tags = Tags(['A'])
+        tags.add(('b b', '', 'a', 'NONE'))
+        tags.add(Tags(['BB', 'C']))
+        assert_equal(list(tags), ['A', 'b b', 'C'])
+
+    def test_remove_string(self):
+        tags = Tags(['a', 'B B'])
+        tags.remove('a')
+        assert_equal(list(tags), ['B B'])
+        tags.remove('bb')
+        assert_equal(list(tags), [])
+
+    def test_remove_non_existing(self):
+        tags = Tags(['a'])
+        tags.remove('nonex')
+        assert_equal(list(tags), ['a'])
+
+    def test_remove_iterable(self):
+        tags = Tags(['a', 'B B'])
+        tags.remove(['nonex', '', 'A', '_B_B_'])
+        assert_equal(list(tags), [])
+
+    def test_contains(self):
+        assert_true('a' in Tags(['a', 'b']))
+        assert_true('c' not in Tags(['a', 'b']))
+        assert_true('AA' in Tags(['a_a', 'b']))
+
+    def test_length(self):
+        assert_equal(len(Tags()), 0)
+        assert_equal(len(Tags(['a', 'b'])), 2)
+
+    def test_truth(self):
+        assert_true(not Tags())
+        assert_true(Tags(['a']))
+
+    def test_unicode(self):
+        assert_equal(unicode(Tags()), '[]')
+        assert_equal(unicode(Tags(['y', "X'X"])), "[X'X, y]")
+        assert_equal(unicode(Tags([u'\xe4', 'a'])), u'[a, \xe4]')
+
+    def test_str(self):
+        assert_equal(str(Tags()), '[]')
+        assert_equal(str(Tags(['y', "X'X"])), "[X'X, y]")
+        assert_equal(str(Tags([u'\xe4', 'a'])), '[a, \xc3\xa4]')
+
+
+if __name__ == '__main__':
+    unittest.main()
