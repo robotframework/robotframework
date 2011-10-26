4e59fb74b4f59095214d2928d0c3b87143f9afac
Mikko Korpela <mikko.korpela@gmail.com>
2011-10-26 15:32:37 +0300

    robot.result: XML writing draft

--- src/robot/result/serializer.py
+++ src/robot/result/serializer.py
@@ -1,12 +1,16 @@
+from robot.output.xmllogger import XmlLogger
+
+class ResultWriter(XmlLogger):
+
+    def close(self):
+        self._writer.end('robot')
+
 class ResultSerializer(object):
 
     def __init__(self, output):
         self._output = output
 
     def to_xml(self, suite):
-        self._output.write('''<?xml version="1.0" encoding="UTF-8"?>
-<robot>
-<suite name="name">
-</suite>
-</robot>
-''')
+        logger = ResultWriter(self._output)
+        suite.visit(logger)
+        logger.close()
--- utest/result/test_resultserializer.py
+++ utest/result/test_resultserializer.py
@@ -5,18 +5,16 @@ from robot.result.serializer import ResultSerializer
 from robot.result.model import TestSuite
 from robot.utils.asserts import assert_equals
 
+from test_resultbuilder import XML, ExecutionResultBuilder
 
 class TestResultSerializer(unittest.TestCase):
 
     def test_suite_serialization(self):
-        suite = TestSuite(name='name')
+        result = ExecutionResultBuilder(StringIO(XML)).build()
         output = StringIO()
-        ResultSerializer(output).to_xml(suite)
-        assert_equals(output.getvalue(), '''<?xml version="1.0" encoding="UTF-8"?>
-<robot>
-<suite name="name">
-</suite>
-</robot>
-''')
-
-
+        ResultSerializer(output).to_xml(result)
+        act_lines = output.getvalue().splitlines()
+        exp_lines = XML.splitlines()
+        assert_equals(len(act_lines), len(exp_lines))
+        for act, exp in zip(act_lines, exp_lines)[2:]:
+            assert_equals(act, exp.strip())
