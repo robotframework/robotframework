2174fd9c3d808618df51a571f2e256fcc1757218
Janne Härkönen <janne.harkonen@iki.fi>
2011-11-28 15:24:47 +0200

    fixed tests after API changes for RebotSettings

--- utest/reporting/test_reporting.py
+++ utest/reporting/test_reporting.py
@@ -1,10 +1,12 @@
 import unittest
+from os.path import abspath
 
+from robot.conf import RebotSettings
 from robot.reporting.outputparser import OutputParser
 from robot.reporting.builders import LogBuilder, ReportBuilder, XUnitBuilder, OutputBuilder
+from robot.reporting.resultwriter import RebotResultWriter, RobotResultWriter
 
 import resources
-from robot.reporting.resultwriter import RebotResultWriter, RobotResultWriter
 
 
 def set_write_log_mock():
@@ -55,7 +57,8 @@ def set_datamodel_generation_spy():
 class _TestReporting(object):
 
     def setUp(self):
-        self._settings = {
+        self._settings = RebotSettings()
+        self._settings._opts = {
             'Report': 'NONE',
             'Log': 'NONE',
             'XUnitFile': 'NONE',
@@ -85,7 +88,9 @@ class _TestReporting(object):
             'RunEmptySuite': False,
             'EndTime': 0,
             'LogLevel': 'INFO',
-            'RemoveKeywords': None
+            'RemoveKeywords': None,
+            'TimestampOutputs': None,
+            'OutputDir': '.',
         }
         self._log_results = set_write_log_mock()
         self._report_results = set_write_report_mock()
@@ -94,8 +99,8 @@ class _TestReporting(object):
         self._datamodel_generations = set_datamodel_generation_spy()
 
     def test_generate_report_and_log(self):
-        self._settings['Log'] = 'log.html'
-        self._settings['Report'] = 'report.html'
+        self._settings._opts['Log'] = 'log.html'
+        self._settings._opts['Report'] = 'report.html'
         self._reporter.write_results(resources.GOLDEN_OUTPUT)
         self._assert_expected_log('log.html')
         self._assert_expected_report('report.html')
@@ -107,42 +112,47 @@ class _TestReporting(object):
         self._assert_no_data_model_generation()
 
     def test_only_log(self):
-        self._settings['Log'] = 'only-log.html'
+        self._settings._opts['Log'] = 'only-log.html'
         self._reporter.write_results(resources.GOLDEN_OUTPUT)
         self._assert_expected_log('only-log.html')
         self._assert_no_report()
 
     def test_only_report(self):
-        self._settings['Report'] = 'reports-only.html'
+        self._settings._opts['Report'] = 'reports-only.html'
         self._reporter.write_results(resources.GOLDEN_OUTPUT)
         self._assert_no_log()
         self._assert_expected_report('reports-only.html')
 
     def test_only_xunit(self):
-        self._settings['XUnitFile'] = 'xunitfile-only.xml'
+        self._settings._opts['XUnitFile'] = 'xunitfile-only.xml'
         self._reporter.write_results(resources.GOLDEN_OUTPUT)
         self._assert_no_log()
         self._assert_no_report()
         self._assert_expected_xunit('xunitfile-only.xml')
 
     def test_split_tests(self):
-        self._settings['SplitLog'] = True
-        self._settings['Log'] = '/tmp/foo/log.bar.html'
+        self._settings._opts['SplitLog'] = True
+        self._settings._opts['Log'] = '/tmp/foo/log.bar.html'
         self._write_results(resources.GOLDEN_OUTPUT)
         expected = ('/tmp/foo/log.bar-%d.js' % i for i in range(1, 5))
         self._assert_expected_split_tests(*expected)
 
     def _assert_expected_log(self, expected_file_name):
+        if expected_file_name:
+            expected_file_name = abspath(expected_file_name)
         self.assertEquals(self._log_results['log_path'], expected_file_name)
 
     def _assert_expected_split_tests(self, *expected_names):
         self.assertEquals(self._split_test_names, list(expected_names))
 
     def _assert_expected_report(self, expected_file_name):
+        if expected_file_name:
+            expected_file_name = abspath(expected_file_name)
         self.assertEquals(self._report_results['report_path'], expected_file_name)
 
     def _assert_expected_xunit(self, expected_file_name):
-        self.assertEquals(self._xunit_results['xunit_path'], expected_file_name)
+        self.assertEquals(self._xunit_results['xunit_path'],
+                          abspath(expected_file_name))
 
     def _assert_no_log(self):
         self._assert_expected_log(None)
@@ -180,7 +190,8 @@ class TestRebotReporting(_TestReporting, unittest.TestCase):
         self._assert_expected_output('ouz.xml')
 
     def _assert_expected_output(self, expected_file_name):
-        self.assertEquals(self._output_results['output_path'], expected_file_name)
+        self.assertEquals(self._output_results['output_path'],
+                          abspath(expected_file_name))
 
 
 class TestRobotReporting(_TestReporting, unittest.TestCase):
