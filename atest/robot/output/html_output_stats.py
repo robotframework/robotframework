2d1fc182a3ecb2a8460df946f6ecd35b48ec38d6
Pekka Klärck <peke@iki.fi>
2011-12-01 00:45:46 +0200

    Tests for writing statistics into log/report correctly.

---
+++ atest/robot/output/html_output_stats.py
@@ -0,0 +1,37 @@
+from robot.api import logger
+
+
+def get_total_stats(path):
+    return get_all_stats(path)[0]
+
+def get_tag_stats(path):
+    return get_all_stats(path)[1]
+
+def get_suite_stats(path):
+    return get_all_stats(path)[2]
+
+def get_all_stats(path):
+    logger.info('Getting stats from <a href="file://%s">%s</a>' % (path, path),
+                html=True)
+    stats_line = _get_stats_line(path)
+    logger.debug('Stats line: %s' % stats_line)
+    total, tags, suite = eval(stats_line)
+    return total, tags, suite
+
+def _get_stats_line(path):
+    prefix = 'window.output["stats"] = '
+    with open(path) as file:
+        for line in file:
+            if line.startswith(prefix):
+                return line[len(prefix):-2]
+
+def verify_stat(stat, *attrs):
+    expected = dict(_get_expected_stat(attrs))
+    if stat != expected:
+        raise AssertionError('Wrong stat!\nGot:       %s\nExpected:  %s'
+                             % (stat, expected))
+
+def _get_expected_stat(attrs):
+    for key, value in (a.split(':', 1) for a in attrs):
+        value = int(value) if value.isdigit() else str(value)
+        yield str(key), value
