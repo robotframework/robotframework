139745a7781041bee0917fd41d0d103dafa26bec
Pekka Klärck <peke@iki.fi>
2011-12-01 22:48:02 +0200

    oops, forgot to move __future__ import

--- src/robot/result/executionresult.py
+++ src/robot/result/executionresult.py
@@ -12,6 +12,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+from __future__ import with_statement
+
 from robot.errors import DataError
 from robot.model.statistics import Statistics
 from robot.utils import ET, XmlSource
