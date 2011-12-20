0a54896cd3774c88b297b6b6a6354db89211d172
Pekka Klärck <peke@iki.fi>
2011-12-21 00:14:00 +0200

    atest/genrunner.py: 1) four space separation is better than two space separation, 2) cleanup is better than messup

--- atest/genrunner.py
+++ atest/genrunner.py
@@ -6,22 +6,22 @@ Usage:  %s path/to/data.file
 """
 
 from __future__ import with_statement
-import sys, os
+from os.path import abspath, basename, dirname, exists, join
+import os
+import sys
 
 if len(sys.argv) != 2:
-    print __doc__ % os.path.basename(sys.argv[0])
+    print __doc__ % basename(sys.argv[0])
     sys.exit(1)
 
-inpath = os.path.abspath(sys.argv[1])
-outpath = inpath.replace(os.path.join('atest', 'testdata'), 
-                         os.path.join('atest', 'robot'))
+INPATH = abspath(sys.argv[1])
+OUTPATH = INPATH.replace(join('atest', 'testdata'), join('atest', 'robot'))
 
-dirname = os.path.dirname(outpath)
-if not os.path.exists(dirname):
-    os.mkdir(dirname)
+if not exists(dirname(OUTPATH)):
+    os.mkdir(dirname(OUTPATH))
 
-with open(inpath) as input:
-    tests = []
+with open(INPATH) as input:
+    TESTS = []
     process = False
     for line in input.readlines():
         line = line.rstrip()
@@ -29,19 +29,20 @@ with open(inpath) as input:
             name = line.replace('*', '').replace(' ', '').upper()
             process = name in ('TESTCASE', 'TESTCASES')
         elif process and line and line[0] != ' ':
-            tests.append(line.split('  ')[0])
+            TESTS.append(line.split('  ')[0])
 
-with open(outpath, 'w') as output:
-    path = inpath.split(os.path.join('atest', 'testdata'))[1][1:]
-    output.write("""*** Settings ***
-Suite Setup     Run Tests  ${EMPTY}  %s
-Force Tags      regression  pybot  jybot
-Resource        atest_resource.txt
+with open(OUTPATH, 'wb') as output:
+    path = INPATH.split(join('atest', 'testdata'))[1][1:].replace(os.sep, '/')
+    output.write("""\
+*** Settings ***
+Suite Setup      Run Tests    ${EMPTY}    %(path)s
+Force Tags       regression    pybot    jybot
+Resource         atest_resource.txt
 
 *** Test Cases ***
 
-""" % path.replace(os.sep, '/'))
-    for test in tests:
-        output.write(test + '\n    Check Test Case  ${TESTNAME}\n\n')
+""" % locals())
+    for test in TESTS:
+        output.write(test + '\n    Check Test Case    ${TESTNAME}\n\n')
 
-print outpath
+print OUTPATH
