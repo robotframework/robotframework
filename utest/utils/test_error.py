92c7d51cba130f5347463e7726e1d4a6d84084e1
Pekka Klärck <peke@iki.fi>
2012-01-02 15:49:37 +0200

    cleaned up filtering non-robot entries from tracebacks -- this works also with imports

--- utest/utils/test_error.py
+++ utest/utils/test_error.py
@@ -1,24 +1,26 @@
 import unittest
+import sys
+import re
 
-from robot.utils.asserts import *
+from robot.utils.asserts import assert_equals, assert_true, assert_raises
 from robot import utils
 if utils.is_jython:
     import JavaExceptions
     java_exceptions = JavaExceptions()
 
-from robot.utils.error import get_error_details, get_error_message
+from robot.utils.error import get_error_details, get_error_message, PythonErrorDetails
 
 
 
-class TestError(unittest.TestCase):
-    
+class TestGetErrorDetails(unittest.TestCase):
+
     def test_get_error_details_python(self):
         for exception, msg, exp_msg in [
                     (AssertionError, 'My Error', 'My Error'),
                     (AssertionError, None, 'AssertionError'),
                     (Exception, 'Another Error', 'Another Error'),
                     (Exception, None, 'Exception'),
-                    (ValueError, 'Something', 'ValueError: Something'), 
+                    (ValueError, 'Something', 'ValueError: Something'),
                     (ValueError, None, 'ValueError'),
                     (AssertionError, 'Msg\nin 3\nlines', 'Msg\nin 3\nlines'),
                     (ValueError, '2\nlines', 'ValueError: 2\nlines')]:
@@ -27,31 +29,31 @@ class TestError(unittest.TestCase):
             except:
                 message, details = get_error_details()
                 assert_equals(message, get_error_message())
-            assert_equal(message, exp_msg)
+            assert_equals(message, exp_msg)
             assert_true(details.startswith('Traceback'))
-            assert_false(exp_msg in details)
-        
+            assert_true(exp_msg not in details)
+
     if utils.is_jython:
-        
+
         def test_get_error_details_java(self):
             for exception, msg, expected in [
                     ('AssertionError', 'My Error', 'My Error'),
                     ('AssertionError', None, 'AssertionError'),
                     ('RuntimeException', 'Another Error', 'Another Error'),
                     ('RuntimeException', None, 'RuntimeException'),
-                    ('ArithmeticException', 'foo', 'ArithmeticException: foo'), 
+                    ('ArithmeticException', 'foo', 'ArithmeticException: foo'),
                     ('ArithmeticException', None, 'ArithmeticException'),
                     ('AssertionError', 'Msg\nin 3\nlines', 'Msg\nin 3\nlines'),
                     ('IOException', '1\n2', 'IOException: 1\n2'),
                     ('RuntimeException', 'embedded', 'embedded'),
-                    ('IOException', 'IOException: emb', 'IOException: emb'), ]: 
+                    ('IOException', 'IOException: emb', 'IOException: emb')]:
                 try:
                     throw_method = getattr(java_exceptions, 'throw'+exception)
                     throw_method(msg)
                 except:
                     message, details = get_error_details()
                     assert_equals(message, get_error_message())
-                assert_equal(message, expected)
+                assert_equals(message, expected)
                 lines = details.splitlines()
                 assert_true(exception in lines[0])
                 for line in lines[1:]:
@@ -67,7 +69,47 @@ class TestError(unittest.TestCase):
                 line1, line2 = details.splitlines()[0:2]
                 assert_equals('java.lang.RuntimeException: ', line1)
                 assert_true(line2.strip().startswith('at '))
-             
+
+
+class TestRemoveRobotEntriesFromTraceback(unittest.TestCase):
+
+    def test_both_robot_and_non_robot_entries(self):
+        def raises():
+            raise Exception
+        self._verify_traceback('Traceback (most recent call last):\n'
+                               '  File "PATH", line X, in raises\n'
+                               '    raise Exception',
+                               assert_raises, AssertionError, raises)
+
+    def test_remove_entries_with_lambda_and_multiple_entries(self):
+        def raises():
+            1/0
+        raising_lambda = lambda: raises()
+        self._verify_traceback('Traceback (most recent call last):\n'
+                               '  File "PATH", line X, in <lambda>\n'
+                               '    raising_lambda = lambda: raises()\n'
+                               '  File "PATH", line X, in raises\n'
+                               '    1/0',
+                               assert_raises, AssertionError, raising_lambda)
+
+    def test_only_robot_entries(self):
+        self._verify_traceback('Traceback (most recent call last):\n'
+                               '  None',
+                               assert_equals, 1, 2)
+
+    def _verify_traceback(self, expected, method, *args):
+        try:
+            method(*args)
+        except Exception:
+            type, value, tb = sys.exc_info()
+            # first tb entry originates from this file and must be excluded
+            details = PythonErrorDetails(type, value, tb.tb_next)
+        else:
+            raise AssertionError
+        traceback = re.sub('File ".*", line \d+', 'File "PATH", line X',
+                           details.traceback)
+        assert_equals(traceback, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
