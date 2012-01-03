8bd6415a726abeceb4b8929ef9c21b39340df704
Pekka Klärck <peke@iki.fi>
2012-01-03 16:05:18 +0200

    test fix

--- utest/utils/test_importer_util.py
+++ utest/utils/test_importer_util.py
@@ -49,7 +49,7 @@ class LoggerStub(object):
                 msg = msg.replace(ext, '')
         self.messages.append(msg)
 
-    def assert_message(self, msg, index=0):
+    def assert_message(self, msg, index=-1):
         assert_equals(self.messages[index], msg)
 
 
@@ -75,16 +75,16 @@ class TestImportByPath(unittest.TestCase):
         imported = "Imported module 'test' from '%s'."
         path1 = create_temp_file('test.py', attr=1)
         self._import_and_verify(path1, attr=1)
-        self.logger.assert_message(imported % path1, index=-1)
+        self.logger.assert_message(imported % path1)
         path2 = join(TESTDIR, 'test')
         os.mkdir(path2)
         create_temp_file(join(path2, '__init__.py'), attr=2)
         self._import_and_verify(path2, attr=2, directory=path2)
-        self.logger.assert_message(removed)
+        self.logger.assert_message(removed, index=0)
         self.logger.assert_message(imported % path2, index=1)
         path3 = create_temp_file(join(path2, 'test.py'), attr=3)
         self._import_and_verify(path3, attr=3, directory=path2)
-        self.logger.assert_message(removed)
+        self.logger.assert_message(removed, index=0)
         self.logger.assert_message(imported % path3, index=1)
 
     def test_import_class_from_file(self):
