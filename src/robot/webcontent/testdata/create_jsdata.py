f6b06c853c35ca6f603f0cb2d0dfc906c64faf57
Pekka Klärck <peke@iki.fi>
2012-01-05 01:44:02 +0200

    Fixed create_jsdata.py to work with the current js model and writer. Notice that a bug in JsonWriter currently makes it impossible to use this script (unless you workaround the bug). I'll fix the bug tomorrow. Needing to extend JsResultWriter in order to set block separators is annoying. Should move them back to optional args to __init__.

--- src/robot/webcontent/testdata/create_jsdata.py
+++ src/robot/webcontent/testdata/create_jsdata.py
@@ -1,39 +1,52 @@
 #!/usr/bin/env python
 
-from os.path import abspath, dirname, join
+from os.path import abspath, dirname, normpath, join
 import os
 import sys
+import codecs
 
 BASEDIR = dirname(abspath(__file__))
+LOG = normpath(join(BASEDIR, '..', 'log.html'))
 TESTDATA = join(BASEDIR, 'dir.suite')
 OUTPUT = join(BASEDIR, 'output.xml')
 TARGET = join(BASEDIR, 'data.js')
+SRC = normpath(join(BASEDIR, '..', '..', '..'))
 
-sys.path.insert(0, join(BASEDIR, '..', '..', '..'))
+sys.path.insert(0, SRC)
 
 import robot
-from robot.reporting.outputparser import OutputParser
+from robot.conf.settings import RebotSettings
+from robot.reporting.resultwriter import Results
+from robot.reporting.jswriter import JsResultWriter
 
 
-def run_robot(outputdirectory, testdata):
-    robot.run(testdata, log='NONE', report='NONE',
-              tagstatlink=['force:http://google.com:<kuukkeli&gt;',
-                           'i*:http://%1/:Title of i%1'],
-              tagdoc=['test:this_is_*my_bold*_test',
-                      'IX:*Combined* & escaped <&lt; tag doc'],
-              tagstatcombine=['fooANDi*:zap', 'i?:IX'],
-              critical=['i?'], noncritical=['*kek*kone*'], outputdir=outputdirectory)
+class ResultWriter(JsResultWriter):
+    start_block = ''
+    end_block = ''
+
+
+def run_robot(testdata, outxml):
+    robot.run(testdata, log='NONE', report='NONE', output=outxml)
 
 
 def create_jsdata(outxml, target):
-    model = OutputParser(log_path=join(BASEDIR,'..','log.html')).parse(outxml)
-    model.set_settings({'logURL': 'log.html',
-                        'reportURL': 'report.html',
-                        'background': {'fail': 'DeepPink'}})
-    with open(target, 'w') as output:
-        model.write_to(output)
+    print outxml
+    settings = RebotSettings(
+        dict(tagstatlink=['force:http://google.com:<kuukkeli&gt;',
+                          'i*:http://%1/:Title of i%1'],
+             tagdoc=['test:this_is_*my_bold*_test',
+                     'IX:*Combined* & escaped <&lt; tag doc'],
+             tagstatcombine=['fooANDi*:No Match', 'i?:IX'],
+             critical=['i?'], noncritical=['*kek*kone*']))
+    result = Results([outxml], settings).js_result
+    config = dict(logURL='log.html',
+                  reportURL='report.html',
+                  background={'fail': 'DeepPink'})
+    with codecs.open(target, 'w', 'UTF-8') as output:
+        ResultWriter(output).write(result, config)
+
 
 if __name__ == '__main__':
-    run_robot(BASEDIR, TESTDATA)
+    run_robot(TESTDATA, OUTPUT)
     create_jsdata(OUTPUT, TARGET)
     os.remove(OUTPUT)
