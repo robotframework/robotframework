b963e556fe774dfc14f591bd44e4e42f857d23fd
Janne Härkönen <janne.harkonen@iki.fi>
2012-01-12 08:45:28 +0200

    proto: removed deprecated CI configurations and helpers

--- proto/buildbot/generate_reports.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd $1
-rm *.xml *html
-unzip outputs.zip
-command="rebot --name Robot_Framework_Acceptance_Tests --splitoutputs 1 output.xml"
-echo "Executing command: " $command
-$command
-exit $?
-
