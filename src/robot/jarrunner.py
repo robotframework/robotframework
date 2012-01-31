bb4a23e8ed6766e243bb6ca619307a5a6471308d
Janne Härkönen <janne.harkonen@iki.fi>
2012-01-31 08:23:14 +0200

    jarrunner: entry point for tidy.
    Update issue 801
    
    Added also jar entry point

--- src/robot/jarrunner.py
+++ src/robot/jarrunner.py
@@ -14,17 +14,29 @@
 
 from org.robotframework import RobotRunner
 from robot import run_cli, rebot_cli
+from robot.tidy import tidy_cli
 
 
 class JarRunner(RobotRunner):
     """Used for Java-Jython interop when RF is executed from .jar file"""
+    _progs = {
+        'run': run_cli,
+        'rebot': rebot_cli,
+        'tidy': tidy_cli
+    }
 
     def run(self, args):
         try:
-            if args and args[0] == 'rebot':
-                return rebot_cli(args[1:])
-            else:
-                return run_cli(args)
+            prog, args = self._parse_prog_and_args(args)
+            return prog(args)
         except SystemExit, err:
             return err.code
+
+    def _parse_prog_and_args(self, args):
+        try:
+            return self._progs[args[0]], args[1:]
+        except (KeyError, IndexError):
+            pass
+        return run_cli, args
+
 
