ebe9cc3c8b0a698f82fdaa62ad696065bc4200d5
Mikko Korpela <mikko.korpela@gmail.com>
2012-02-08 14:28:21 +0200

    log.html: Expand all - stop click event propagation so that partly expanded elements do not get collapsed

--- src/robot/webcontent/log.js
+++ src/robot/webcontent/log.js
@@ -102,3 +102,11 @@ function expandSuite(suite) {
     else
         expandCriticalFailed(suite);
 }
+
+// For a complete cross-browser experience..
+// http://www.quirksmode.org/js/events_order.html
+function stopPropagation(e) {
+    var e = e || window.event;
+    e.cancelBubble = true;
+    if (e.stopPropagation) e.stopPropagation();
+}
