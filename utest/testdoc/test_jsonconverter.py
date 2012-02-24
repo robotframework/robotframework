58229efd10e484bd1cb55db319ecb17a05516659
Pekka Klärck <peke@iki.fi>
2012-02-24 23:09:41 +0200

    testdoc: 1) test data for testing formatting (actual tests and fixes will follow), 2) adapted old tests accordingly after adding new test case file under misc

--- utest/testdoc/test_jsonconverter.py
+++ utest/testdoc/test_jsonconverter.py
@@ -8,31 +8,36 @@ from robot.testdoc import JsonConverter, TestSuiteFactory
 DATADIR = join(dirname(abspath(__file__)), '..', '..', 'atest', 'testdata', 'misc')
 
 
+def test_convert(item, **expected):
+    for name in expected:
+        assert_equals(item[name], expected[name])
+
+
 class TestJsonConverter(unittest.TestCase):
     suite = None
 
     def setUp(self):
         if not self.suite:
-            suite = TestSuiteFactory(DATADIR, doc='My doc', metadata=['a:b'])
+            suite = TestSuiteFactory(DATADIR, doc='My doc', metadata=['abc:123'])
             output = join(DATADIR, '..', 'output.html')
-            TestJsonConverter.suite = JsonConverter(output).convert(suite)
+            self.__class__.suite = JsonConverter(output).convert(suite)
 
     def test_suite(self):
-        self._verify(self.suite,
+        test_convert(self.suite,
                      source=normpath(DATADIR),
                      relativeSource='misc',
                      id='s1',
                      name='Misc',
                      fullName='Misc',
                      doc='My doc',
-                     metadata={'a': 'b'},
-                     numberOfTests=161,
+                     metadata={'abc': '123'},
+                     numberOfTests=162,
                      tests=[],
                      keywords=[])
-        self._verify(self.suite['suites'][0],
+        test_convert(self.suite['suites'][1],
                      source=join(normpath(DATADIR), 'dummy_lib_test.html'),
                      relativeSource=join('misc', 'dummy_lib_test.html'),
-                     id='s1-s1',
+                     id='s1-s2',
                      name='Dummy Lib Test',
                      fullName='Misc.Dummy Lib Test',
                      doc='',
@@ -40,12 +45,12 @@ class TestJsonConverter(unittest.TestCase):
                      numberOfTests=1,
                      suites=[],
                      keywords=[])
-        self._verify(self.suite['suites'][3]['suites'][1]['suites'][-1],
+        test_convert(self.suite['suites'][4]['suites'][1]['suites'][-1],
                      source=join(normpath(DATADIR), 'multiple_suites',
                                  '02__sub.suite.1', 'second__.Sui.te.2..html'),
                      relativeSource=join('misc', 'multiple_suites',
                                          '02__sub.suite.1', 'second__.Sui.te.2..html'),
-                     id='s1-s4-s2-s2',
+                     id='s1-s5-s2-s2',
                      name='.Sui.te.2.',
                      fullName='Misc.Multiple Suites.Sub.Suite.1..Sui.te.2.',
                      doc='',
@@ -58,7 +63,7 @@ class TestJsonConverter(unittest.TestCase):
         data = TestSuiteFactory([join(DATADIR, 'normal.html'),
                                  join(DATADIR, 'pass_and_fail.html')])
         suite = JsonConverter().convert(data)
-        self._verify(suite,
+        test_convert(suite,
                      source='',
                      relativeSource='',
                      id='s1',
@@ -69,7 +74,7 @@ class TestJsonConverter(unittest.TestCase):
                      numberOfTests=4,
                      keywords=[],
                      tests=[])
-        self._verify(suite['suites'][0],
+        test_convert(suite['suites'][0],
                      source=normpath(join(DATADIR, 'normal.html')),
                      relativeSource='',
                      id='s1-s1',
@@ -78,7 +83,7 @@ class TestJsonConverter(unittest.TestCase):
                      doc='Normal test cases',
                      metadata={'Something': 'My Value'},
                      numberOfTests=2)
-        self._verify(suite['suites'][1],
+        test_convert(suite['suites'][1],
                      source=normpath(join(DATADIR, 'pass_and_fail.html')),
                      relativeSource='',
                      id='s1-s2',
@@ -89,22 +94,22 @@ class TestJsonConverter(unittest.TestCase):
                      numberOfTests=2)
 
     def test_test(self):
-        self._verify(self.suite['suites'][0]['tests'][0],
-                     id='s1-s1-t1',
+        test_convert(self.suite['suites'][1]['tests'][0],
+                     id='s1-s2-t1',
                      name='Dummy Test',
                      fullName='Misc.Dummy Lib Test.Dummy Test',
                      doc='',
                      tags=[],
                      timeout='')
-        self._verify(self.suite['suites'][2]['tests'][-1],
-                     id='s1-s3-t5',
+        test_convert(self.suite['suites'][3]['tests'][-1],
+                     id='s1-s4-t5',
                      name='Fifth',
                      fullName='Misc.Many Tests.Fifth',
                      doc='',
                      tags=['d1', 'd2', 'f1'],
                      timeout='')
-        self._verify(self.suite['suites'][-3]['tests'][0],
-                     id='s1-s9-t1',
+        test_convert(self.suite['suites'][-3]['tests'][0],
+                     id='s1-s10-t1',
                      name='Default Test Timeout',
                      fullName='Misc.Timeouts.Default Test Timeout',
                      doc='I have a timeout',
@@ -112,65 +117,61 @@ class TestJsonConverter(unittest.TestCase):
                      timeout='1 minute 42 seconds')
 
     def test_timeout(self):
-        self._verify(self.suite['suites'][-3]['tests'][0],
+        test_convert(self.suite['suites'][-3]['tests'][0],
                      name='Default Test Timeout',
                      timeout='1 minute 42 seconds')
-        self._verify(self.suite['suites'][-3]['tests'][1],
+        test_convert(self.suite['suites'][-3]['tests'][1],
                      name='Test Timeout With Message',
                      timeout='1 day 2 hours :: The message')
-        self._verify(self.suite['suites'][-3]['tests'][2],
+        test_convert(self.suite['suites'][-3]['tests'][2],
                      name='Test Timeout With Variable',
                      timeout='${100}')
 
     def test_keyword(self):
-        self._verify(self.suite['suites'][0]['tests'][0]['keywords'][0],
+        test_convert(self.suite['suites'][1]['tests'][0]['keywords'][0],
                      name='dummykw',
                      arguments='',
                      type='KEYWORD')
-        self._verify(self.suite['suites'][2]['tests'][-1]['keywords'][0],
+        test_convert(self.suite['suites'][3]['tests'][-1]['keywords'][0],
                      name='Log',
                      arguments='Test 5',
                      type='KEYWORD')
 
     def test_suite_setup_and_teardown(self):
-        self._verify(self.suite['suites'][2]['keywords'][0],
+        test_convert(self.suite['suites'][3]['keywords'][0],
                      name='Log',
                      arguments='Setup',
                      type='SETUP')
-        self._verify(self.suite['suites'][2]['keywords'][1],
+        test_convert(self.suite['suites'][3]['keywords'][1],
                      name='Noop',
                      arguments='',
                      type='TEARDOWN')
 
     def test_test_setup_and_teardown(self):
-        self._verify(self.suite['suites'][6]['tests'][0]['keywords'][0],
+        test_convert(self.suite['suites'][7]['tests'][0]['keywords'][0],
                      name='Test Setup',
                      arguments='',
                      type='SETUP')
-        self._verify(self.suite['suites'][6]['tests'][0]['keywords'][2],
+        test_convert(self.suite['suites'][7]['tests'][0]['keywords'][2],
                      name='Test Teardown',
                      arguments='',
                      type='TEARDOWN')
 
     def test_for_loops(self):
-        self._verify(self.suite['suites'][1]['tests'][0]['keywords'][0],
+        test_convert(self.suite['suites'][2]['tests'][0]['keywords'][0],
                      name='${pet} IN [ cat | dog | horse ]',
                      arguments='',
                      type='FOR')
-        self._verify(self.suite['suites'][1]['tests'][1]['keywords'][0],
+        test_convert(self.suite['suites'][2]['tests'][1]['keywords'][0],
                      name='${i} IN RANGE [ 10 ]',
                      arguments='',
                      type='FOR')
 
     def test_assign(self):
-        self._verify(self.suite['suites'][-2]['tests'][0]['keywords'][1],
+        test_convert(self.suite['suites'][-2]['tests'][0]['keywords'][1],
                      name='${msg} = Evaluate',
                      arguments="u'Fran\\\\xe7ais'",
                      type='KEYWORD')
-
-    def _verify(self, item, **expected):
-        for name in expected:
-            assert_equals(item[name], expected[name])
 
 
 if __name__ == '__main__':
