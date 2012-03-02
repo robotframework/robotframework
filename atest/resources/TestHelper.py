a721f62ea5418deb8c5baf7addf343f5a67478c6
Pekka Klärck <peke@iki.fi>
2012-03-02 14:31:39 +0200

    atests: removed helpers that should not be needed anymore

--- atest/resources/TestHelper.py
+++ atest/resources/TestHelper.py
@@ -4,7 +4,7 @@ from stat import S_IREAD, S_IWRITE
 
 
 class TestHelper:
-    
+
     def set_read_only(self, path):
         os.chmod(path, S_IREAD)
 
@@ -17,21 +17,9 @@ class TestHelper:
         if len(datasources) == 1:
             return self._get_name(datasources[0])
         return '_'.join(self._get_name(source) for source in datasources)
-        
+
     def _get_name(self, path):
         return os.path.splitext(os.path.basename(path))[0]
-    
-    def should_contain_item_x_times(self, string, item, count):
-        if string.count(item) != int(count):
-            raise AssertionError("'%s' does not contain '%s' '%s' "
-                                 "times!" % (string, item, count))
-
-    def get_splitted_full_name(self, full_name, splitlevel):
-        splitlevel = int(splitlevel)
-        parts = full_name.split('.')
-        if splitlevel > 0 and splitlevel <= len(parts):
-            parts = parts[splitlevel:]
-        return '.'.join(parts)
 
     def running_on_jython(self, interpreter):
         return 'jython' in interpreter
