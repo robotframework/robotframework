cc7186f281a46d07f202cdf001165d243d04bf95
Janne Härkönen <janne.harkonen@iki.fi>
2012-03-05 15:25:27 +0200

    output: removed old, broken TestSuite API

--- src/robot/output/__init__.py
+++ src/robot/output/__init__.py
@@ -12,33 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from output import Output
-from logger import LOGGER
-from monitor import CommandLineMonitor
-from xmllogger import XmlLogger
-from loggerhelper import LEVELS, Message
-
-
-#TODO: Fix Public API
-def TestSuite(outpath):
-    """Factory method for getting test suite from an xml output file.
-
-    If you want statistics get suite first and say Statistics(suite).
-    """
-    suite, errors = process_output(outpath)
-
-    def write_to_file(path=None):
-        """Write processed suite (incl. statistics and errors) back to xml.
-
-        If path is not given the suite is written into the same file as it
-        originally was read from.
-        """
-        if path is None:
-            path = outpath
-        suite.set_status()
-        testoutput = ResultFromXml(suite, errors)
-        testoutput.serialize_output(path)
-
-    suite.write_to_file = write_to_file
-    return suite
-
+from .output import Output
+from .logger import LOGGER
+from .xmllogger import XmlLogger
+from .loggerhelper import LEVELS, Message
