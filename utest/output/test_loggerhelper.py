7798eb7ab8082c2cc6b3fe06d2072034f1f39584
Pekka Klärck <peke@iki.fi>
2012-03-13 13:46:53 +0200

    loggerhelper.Message: Support for lazy messages

--- utest/output/test_loggerhelper.py
+++ utest/output/test_loggerhelper.py
@@ -1,14 +1,24 @@
 import unittest
 
+from robot.utils.asserts import assert_raises, assert_equals
 from robot.errors import DataError
-from robot.output.loggerhelper import AbstractLogger
+from robot.output.loggerhelper import AbstractLogger, Message
 
 
 class TestAbstractLogger(unittest.TestCase):
 
     def test_set_invalid_threshold(self):
         logger = AbstractLogger('trace')
-        self.assertRaises(DataError, logger.set_level,'INVALID THRESHOLD')
+        assert_raises(DataError, logger.set_level, 'INVALID THRESHOLD')
+
+
+class TestMessage(unittest.TestCase):
+
+    def test_string_message(self):
+        assert_equals(Message('my message').message, 'my message')
+
+    def test_callable_message(self):
+        assert_equals(Message(lambda: 'my message').message, 'my message')
 
 
 if __name__ == '__main__':
