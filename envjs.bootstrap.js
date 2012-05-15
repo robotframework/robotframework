969cef68fcc9d81c1481b6cc63b88a4470d66f8c
Jussi Malinen <jussi.ao.malinen@gmail.com>
2012-05-15 15:53:15 +0300

    add script for running jasmine tests without browser

---
+++ envjs.bootstrap.js
@@ -0,0 +1,6 @@
+load('ext-lib/jasmine-reporters/ext/env.rhino.1.2.js');
+
+Envjs.scriptTypes['text/javascript'] = true;
+var specFile = arguments[0];
+console.log("Loading: " + specFile);
+window.location = specFile
