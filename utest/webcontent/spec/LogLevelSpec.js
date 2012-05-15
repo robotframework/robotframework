c537d8affc9ba130cdd341fde4455b3df1c131b5
Jussi Malinen <jussi.ao.malinen@gmail.com>
2012-05-15 17:30:32 +0300

    fix log level combinator tests. (had not been updated with api changes)

--- utest/webcontent/spec/LogLevelSpec.js
+++ utest/webcontent/spec/LogLevelSpec.js
@@ -1,7 +1,7 @@
 describe("LogLevelController", function() {
     function checkCombination(min_level, default_level, should_show, expected_default, show_trace) {
         var controller = LogLevelController(min_level, default_level);
-        expect(controller.shouldShowLogLevelChooser()).toEqual(should_show);
+        expect(controller.showLogLevelSelector()).toEqual(should_show);
         if(should_show){
           expect(controller.defaultLogLevel()).toEqual(expected_default);
           expect(controller.showTrace()).toEqual(show_trace);
@@ -9,11 +9,11 @@ describe("LogLevelController", function() {
     }
 
     it("Should select correct log level", function () {
-        checkCombination("TRACE", "TRACE", true, "TRACE", true);
-        checkCombination("DEBUG", "INFO", true, "INFO", false);
-        checkCombination("TRACE", "DEBUG", true, "DEBUG", true);
+        checkCombination("TRACE", "TRACE", true, 0, true);
+        checkCombination("DEBUG", "INFO", true, 2, false);
+        checkCombination("TRACE", "DEBUG", true, 1, true);
         checkCombination("INFO", "INFO", false);
         checkCombination("WARN", "INFO", false);
-        checkCombination("DEBUG", "TRACE", true, "DEBUG", false);
+        checkCombination("DEBUG", "TRACE", true, 1, false);
     });
 });
