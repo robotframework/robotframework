c2e9072df136736de45f80ef6bfd00e635441294
Jussi Malinen <jussi.ao.malinen@gmail.com>
2012-05-18 14:48:34 +0300

    fix timezone handling on timestamp tests. Patch from issue 1127 by Jan Zdunek/Codecentric AG

--- atest/testdata/test_libraries/JavaLibUsingTimestamps.java
+++ atest/testdata/test_libraries/JavaLibUsingTimestamps.java
@@ -1,7 +1,13 @@
+import java.util.GregorianCalendar;
+
 public class  JavaLibUsingTimestamps {
 
     public void javaTimestamp() throws InterruptedException {
-        System.out.println("*INFO:1308419034931* Known timestamp");
+        long timestamp = 1308419034931L;
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.setTimeInMillis(timestamp);
+        timestamp += 10800000 - calendar.getTimeZone().getOffset(calendar.getTimeInMillis());
+        System.out.println("*INFO:" + timestamp +"* Known timestamp");
         System.out.println("*HTML:" +
                            System.currentTimeMillis() +
                            "*<b>Current</b>");
--- atest/testdata/test_libraries/PythonLibUsingTimestamps.py
+++ atest/testdata/test_libraries/PythonLibUsingTimestamps.py
@@ -1,12 +1,19 @@
 import time
 
 
+def timezone_correction():
+    dst = 3600 if time.daylight == 0 else 0
+    tz = 7200 + time.timezone
+    return (tz + dst) * 1000
+
 def timestamp_as_integer():
-    print '*INFO:1308419034931* Known timestamp'
+    t = 1308419034931 + timezone_correction()
+    print '*INFO:%d* Known timestamp' % t
     print '*HTML:%d* <b>Current</b>' % int(time.time() * 1000)
     time.sleep(0.1)
 
 def timestamp_as_float():
-    print '*INFO:1308419034930.502342313* Known timestamp'
+    t = 1308419034930.502342313 + timezone_correction()
+    print '*INFO:%f* Known timestamp' % t
     print '*HTML:%f* <b>Current</b>' % float(time.time() * 1000)
     time.sleep(0.1)
