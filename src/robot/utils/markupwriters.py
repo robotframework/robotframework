9617853b7c24c54aed74174a0ddbffcae31d174a
Pekka Klärck <peke@iki.fi>
2012-06-07 00:15:01 +0300

    markupwriter: cleanup

--- src/robot/utils/markupwriters.py
+++ src/robot/utils/markupwriters.py
@@ -20,10 +20,10 @@ from .markuputils import html_escape, xml_escape, attribute_escape
 class _MarkupWriter(object):
 
     def __init__(self, output, line_separator=os.linesep, encoding=None):
-        """Creates new _MarkupWriter.
-
+        """
         :param output: Either an opened, file like object, or a path to the
-            desired output file. In the latter case, the file is created.
+            desired output file. In the latter case, the file is created
+            and clients should use :py:meth:`close` method to close it.
         :param line_separator: Defines the used line separator.
         :param encoding: Encoding to be used to encode all text written to the
             output file. If `None`, text will not be encoded.
@@ -33,6 +33,7 @@ class _MarkupWriter(object):
         self.output = output
         self._line_separator = line_separator
         self._encode = self._create_encoder(encoding)
+        self._preamble()
 
     def _create_encoder(self, encoding):
         if encoding is None:
@@ -40,15 +41,19 @@ class _MarkupWriter(object):
         return lambda text: text.encode(encoding)
 
     def start(self, name, attrs=None, newline=True):
-        self._write('<%s%s>%s' % (name, self._get_attrs(attrs),
-                                  self._line_separator if newline else ''))
+        self._write('<%s %s>' % (name, self._format_attrs(attrs))
+                    if attrs else '<%s>' % name, newline)
+
+    def _format_attrs(self, attrs):
+        return ' '.join('%s="%s"' % (name, attribute_escape(attrs[name]))
+                        for name in self._order_attrs(attrs))
 
     def content(self, content=None, escape=True):
         if content:
             self._write(self._escape(content) if escape else content)
 
     def end(self, name, newline=True):
-        self._write('</%s>%s' % (name, self._line_separator if newline else ''))
+        self._write('</%s>' % name, newline)
 
     def element(self, name, content=None, attrs=None, escape=True,
                 newline=True):
@@ -57,44 +62,39 @@ class _MarkupWriter(object):
         self.end(name, newline)
 
     def close(self):
+        """Closes the underlying output file."""
         self.output.close()
 
-    def _write(self, text):
+    def _write(self, text, newline=False):
         self.output.write(self._encode(text))
-
-    def _get_attrs(self, attrs):
-        if not attrs:
-            return ''
-        return ' ' + ' '.join(self._format_attributes(attrs))
+        if newline:
+            self.output.write(self._line_separator)
 
 
 class HtmlWriter(_MarkupWriter):
 
+    def _preamble(self):
+        pass
+
     def _escape(self, content):
         return html_escape(content)
 
-    def _format_attributes(self, attrs):
-        return ('%s="%s"' % (name, attribute_escape(attrs[name]))
-                             for name in sorted(attrs))
+    def _order_attrs(self, attrs):
+        return sorted(attrs)  # eases testing
 
 
 class XmlWriter(_MarkupWriter):
 
-    def __init__(self, output, line_separator=os.linesep, encoding=None):
-        _MarkupWriter.__init__(self, output, line_separator, encoding)
-        self._preamble()
-
     def _preamble(self):
-        self.content('<?xml version="1.0" encoding="UTF-8"?>\n', escape=False)
+        self._write('<?xml version="1.0" encoding="UTF-8"?>', newline=True)
 
     def _escape(self, text):
         return xml_escape(text)
 
-    def _format_attributes(self, attrs):
-        return ('%s="%s"' % (name, attribute_escape(attrs[name]))
-                             for name in attrs)
+    def _order_attrs(self, attrs):
+        return attrs
 
 
 class NullMarkupWriter(object):
-    """Mock implementation of _MarkupWriter interface"""
-    start = content = element = end = close = lambda *args: None
+    """Null implementation of _MarkupWriter interface"""
+    __init__ = start = content = element = end = close = lambda *args: None
