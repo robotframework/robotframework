154892dab1e383533279b17922efea45cc475ecb
Pekka Klärck <peke@iki.fi>
2012-06-27 19:31:03 +0300

    Continuing XML library development.
    1) Workaround for ET < 1.3 not handling '.' path.
    
    2) Changed `Get Element(s) Text(s)` to return all text of the element, including the texts of possible child elements. This is useful, at least, when testing HTML.
    
    3) Added `normalize_whitespace` argument to `Get Element(s) Text(s)` to allow sanitizing text. Useful, again, when testing HTML. This argument probably should be added also to text related asserts.

--- src/robot/libraries/XML.py
+++ src/robot/libraries/XML.py
@@ -14,15 +14,17 @@
 
 from __future__ import with_statement
 
+import re
+from functools import partial
+
 from robot.utils import ET, ETSource
 from robot.libraries.BuiltIn import BuiltIn
 
 
-should_be_equal = BuiltIn().should_be_equal
-should_match = BuiltIn().should_match
-
-
 class XML(object):
+    _should_be_equal = BuiltIn().should_be_equal
+    _should_match = BuiltIn().should_match
+    _normalize_whitespace = partial(re.compile('\s+').sub, ' ')
 
     def parse_xml(self, source):
         with ETSource(source) as source:
@@ -34,33 +36,50 @@ class XML(object):
         return source
 
     def get_element(self, source, match):
+        if match == '.':  # TODO: Is this good workaround for ET 1.2 not supporting '.'?
+            return self._get_parent(source)
         elements = self.get_elements(source, match)
         if not elements:
             raise RuntimeError("No element matching '%s' found." % match)
         if len(elements) > 1:
-            raise RuntimeError("Multiple elements (%d) matching '%s' found." % (len(elements), match))
+            raise RuntimeError("Multiple elements (%d) matching '%s' found."
+                               % (len(elements), match))
         return elements[0]
 
     def get_elements(self, source, match):
         return self._get_parent(source).findall(match)
 
-    def get_element_text(self, source, match='.'):
-        return self.get_element(source, match).text or ''
+    def get_element_text(self, source, match='.', normalize_whitespace=False):
+        element = self.get_element(source, match)
+        text = ''.join(self._yield_texts(element))
+        if normalize_whitespace:
+            text = self._normalize_whitespace(text).strip()
+        return text
 
-    def get_elements_texts(self, source, match):
-        return [elem.text or '' for elem in self.get_elements(source, match)]
+    def _yield_texts(self, element, top=True):
+        if element.text:
+            yield element.text
+        for child in element:
+            for text in self._yield_texts(child, top=False):
+                yield text
+        if element.tail and not top:
+            yield element.tail
+
+    def get_elements_texts(self, source, match, normalize_whitespace=False):
+        return [self.get_element_text(elem, normalize_whitespace=normalize_whitespace)
+                for elem in self.get_elements(source, match)]
 
     def element_text_should_be(self, source, expected, match='.'):
-        should_be_equal(self.get_element_text(source, match), expected)
+        self._should_be_equal(self.get_element_text(source, match), expected)
 
     def element_text_should_match(self, source, pattern, match='.'):
-        should_match(self.get_element_text(source, match), pattern)
+        self._should_match(self.get_element_text(source, match), pattern)
 
     def get_element_attribute(self, source, name, match=None):
         return self.get_element(source, match).get(name)
 
     def element_attribute_should_match(self, source, name, pattern, match=None):
-        should_match(self.get_attribute(source, name, match), pattern)
+        self._should_match(self.get_attribute(source, name, match), pattern)
 
     def element_attribute_should_be(self, source, name, expected, match=None):
-        should_be_equal(self.get_attribute(source, name, match), expected)
+        self._should_be_equal(self.get_attribute(source, name, match), expected)
