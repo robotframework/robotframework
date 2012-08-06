2850f2cf039b93fbf9e88339ea485c492e8f77c0
Pekka Klärck <peke@iki.fi>
2012-08-06 15:31:31 +0300

    XML lib: test and doc using / as path separator on Windows

--- src/robot/libraries/XML.py
+++ src/robot/libraries/XML.py
@@ -60,6 +60,12 @@ class XML(object):
     structure, other keywords also accept paths to XML files and strings
     containing XML similarly as `Parse XML`.
 
+    When the source is given as a path to a file, the forward slash character
+    (`/`) can be used as the path separator regardless the operating system.
+    On Windows also the backslash works, but it the test data it needs to be
+    escaped by doubling it (`\\\\`). Using the built-in variable `${/}`
+    naturally works too.
+
     *Example*
 
     The following simple example demonstrates parsing XML and verifying its
