42a3c513a0b594f7f00309f7d0bbc918e50fc5f4
Pekka Klärck <peke@iki.fi>
2012-09-15 00:09:02 +0300

    hopefully fixed non-ASCII variable tests with ipy

--- atest/testdata/variables/non_string_variables.py
+++ atest/testdata/variables/non_string_variables.py
@@ -3,21 +3,27 @@ from os.path import basename
 
 
 def get_variables(interpreter=None):
-    return {'integer': 42,
-            'float': 3.14,
-            'byte_string': 'hyv\xe4',
-            'byte_string_str': _byte_string_str(interpreter),
-            'boolean': True,
-            'none': None,
-            'module': sys,
-            'module_str': str(sys),
-            'list': [1, '\xe4', u'\xe4'],
-            'list_str': "[1, '\\xe4', u'\\xe4']",
-            'dict': {'\xe4': u'\xe4'},
-            'dict_str': "{'\\xe4': u'\\xe4'}"}
+    variables = {'integer': 42,
+                 'float': 3.14,
+                 'byte_string': 'hyv\xe4',
+                 'boolean': True,
+                 'none': None,
+                 'module': sys,
+                 'module_str': str(sys),
+                 'list': [1, '\xe4', u'\xe4'],
+                 'dict': {'\xe4': u'\xe4'}}
+    variables.update(_get_interpreter_specific_strs(interpreter))
+    return variables
 
-def _byte_string_str(interpreter):
-    return 'hyv\\xe4' if not _running_on_iron_python(interpreter) else 'hyv\xe4'
+def _get_interpreter_specific_strs(interpreter):
+    if not _running_on_iron_python(interpreter):
+        return {'byte_string_str': 'hyv\\xe4',
+                'list_str': "[1, '\\xe4', u'\\xe4']",
+                'dict_str': "{'\\xe4': u'\\xe4'}"}
+    else:
+        return {'byte_string_str': u'hyv\xe4',
+                'list_str': "[1, u'\\xe4', u'\\xe4']",
+                'dict_str': "{u'\\xe4': u'\\xe4'}"}
 
 def _running_on_iron_python(interpreter):
     if interpreter:
