7f798c8281034a72fe5cd45bf8f52961c24bcc2b
Pekka Klärck <peke@iki.fi>
2012-09-14 12:17:52 +0300

    byte string atests: Use different expected values with IronPython than on other interpreters because it doesn't differentiate byte strings and Unicode strings.

--- atest/testdata/core/expbytevalues.py
+++ atest/testdata/core/expbytevalues.py
@@ -1,4 +1,20 @@
-exp_return_value = 'ty\xf6paikka'
-exp_return_msg = 'ty\\xf6paikka'
-exp_error_msg = 'hyv\\xe4'
-exp_log_msg = '\\xe4ity'
+import sys
+from os.path import basename
+
+
+def get_variables(interpreter=None):
+    if not _running_on_iron_python(interpreter):
+        messages = {'exp_return_msg': 'ty\\xf6paikka',
+                    'exp_error_msg': 'hyv\\xe4',
+                    'exp_log_msg': '\\xe4ity'}
+    else:
+        messages = {'exp_return_msg': 'ty\xf6paikka',
+                    'exp_error_msg': 'hyv\xe4',
+                    'exp_log_msg': '\xe4ity'}
+    return dict(messages, exp_return_value='ty\xf6paikka')
+
+
+def _running_on_iron_python(interpreter):
+    if interpreter:
+        return 'ipy' in basename(interpreter)
+    return sys.platform == 'cli'
