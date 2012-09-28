b108bcc5f9eceebfac23323bb71158f3b42fc66d
Pekka Klärck <peke@iki.fi>
2012-09-28 15:23:26 +0300

    htmlformatters: renamed baseclass and one attribute

--- src/robot/utils/htmlformatters.py
+++ src/robot/utils/htmlformatters.py
@@ -144,7 +144,7 @@ class HtmlFormatter(object):
                 return formatter
 
 
-class _BlockFormatter(object):
+class _Formatter(object):
     _strip_lines = True
 
     def __init__(self):
@@ -168,21 +168,21 @@ class _BlockFormatter(object):
         raise NotImplementedError
 
 
-class RulerFormatter(_BlockFormatter):
-    _hr_matcher = re.compile('^-{3,}$').match
+class RulerFormatter(_Formatter):
+    _matcher = re.compile('^-{3,}$').match
 
     def _handles(self, line):
-        return not self._lines and self._hr_matcher(line)
+        return not self._lines and self._matcher(line)
 
     def format(self, lines):
         return '<hr>'
 
 
-class ParagraphFormatter(_BlockFormatter):
+class ParagraphFormatter(_Formatter):
     _format_line = LineFormatter().format
 
     def __init__(self, other_formatters):
-        _BlockFormatter.__init__(self)
+        _Formatter.__init__(self)
         self._other_formatters = other_formatters
 
     def _handles(self, line):
@@ -193,7 +193,7 @@ class ParagraphFormatter(_BlockFormatter):
         return '<p>%s</p>' % self._format_line(' '.join(lines))
 
 
-class TableFormatter(_BlockFormatter):
+class TableFormatter(_Formatter):
     _table_line = re.compile('^\| (.* |)\|$')
     _line_splitter = re.compile(' \|(?= )')
     _format_cell = LineFormatter().format
@@ -219,7 +219,7 @@ class TableFormatter(_BlockFormatter):
         return '\n'.join(table)
 
 
-class PreformattedFormatter(_BlockFormatter):
+class PreformattedFormatter(_Formatter):
     _format_line = LineFormatter().format
 
     def _handles(self, line):
@@ -230,7 +230,7 @@ class PreformattedFormatter(_BlockFormatter):
         return '\n'.join(['<pre>'] + lines + ['</pre>'])
 
 
-class ListFormatter(_BlockFormatter):
+class ListFormatter(_Formatter):
     _strip_lines = False
     _format_item = LineFormatter().format
 
