75d3342ac7e83c01b5a6e43e4b9fed84ad9abd39
Robot Framework Developers <robotframework@gmail.com>
2012-10-10 13:55:00 +0300

    libdoc tests: DocFormat library is now a class and not a module

--- atest/testdata/libdoc/DocFormat.py
+++ atest/testdata/libdoc/DocFormat.py
@@ -1,32 +1,33 @@
-"""Library to test documentation formatting.
+class DocFormat:
+    """Library to test documentation formatting.
 
-*bold* or <b>bold</b> http://example.com
-"""
-
-def keyword():
-    """*bold* or <b>bold</b> http://example.com"""
-
-def link():
-    """Link to `Keyword`."""
-
-def rest():
-    """Let's see *how well* reST__ works.
-
-    This documentation is mainly used for manually verifying reST output.
-    This link to \\`Keyword\\` is also automatically tested.
-
-    ====  =====
-    My    table
-    two   rows
-    ====  =====
-
-    - list
-    - here
-
-    Preformatted::
-
-        def example():
-            pass
-
-    __ http://docutils.sourceforge.net
+    *bold* or <b>bold</b> http://example.com
     """
+
+    def keyword(self):
+        """*bold* or <b>bold</b> http://example.com"""
+
+    def link(self):
+        """Link to `Keyword`."""
+
+    def rest(self):
+        """Let's see *how well* reST__ works.
+
+        This documentation is mainly used for manually verifying reST output.
+        This link to \\`Keyword\\` is also automatically tested.
+
+        ====  =====
+        My    table
+        two   rows
+        ====  =====
+
+        - list
+        - here
+
+        Preformatted::
+
+            def example():
+                pass
+
+        __ http://docutils.sourceforge.net
+        """
