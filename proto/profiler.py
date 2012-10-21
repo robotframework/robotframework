caf2d9ab93dcb69a7b7a4b6452b12c765fb85ee5
Pekka Klärck <peke@iki.fi>
2012-10-21 23:30:38 +0300

    proto/profiler.py: added help text

--- proto/profiler.py
+++ proto/profiler.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+
+"""Profiler for Robot Framework `run` and `rebot`.
+
+Usage: profiler.py run|rebot [options] arguments
+"""
+
 import cProfile
 import pstats
 import os
@@ -11,14 +18,20 @@ sys.path.insert(0, join(rootdir, 'src'))
 from robot.run import run_cli
 from robot.rebot import rebot_cli
 
-if sys.argv[1] != 'rebot':
-    profiled = 'run_cli(sys.argv[1:])'
-else:
-    profiled = 'rebot_cli(sys.argv[2:])'
 
-results = tempfile.mktemp(suffix='.out', prefix='pybot-profile',
+def profile(profiled):
+    results = tempfile.mktemp(suffix='.out', prefix='pybot-profile',
                           dir=join(rootdir, 'tmp'))
-cProfile.run(profiled, results)
-stats = pstats.Stats(results)
-stats.sort_stats('cumulative').print_stats(50)
-os.remove(results)
+    cProfile.run(profiled, results)
+    stats = pstats.Stats(results)
+    stats.sort_stats('cumulative').print_stats(50)
+    os.remove(results)
+
+
+if __name__ == '__main__':
+    try:
+        profiled = {'run': 'run_cli(sys.argv[2:])',
+                    'rebot': 'rebot_cli(sys.argv[2:])'}[sys.argv[1]]
+    except (IndexError, KeyError):
+        sys.exit(__doc__)
+    profile(profiled)
