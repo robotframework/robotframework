59e8b2346ce444a859af74f91260cf9503744f06
Pekka Klärck <peke@iki.fi>
2012-12-18 01:46:07 +0200

    cleaned up telnet tests

--- atest/robot/standard_libraries/telnet/telnet_variables.py
+++ atest/robot/standard_libraries/telnet/telnet_variables.py
@@ -4,9 +4,10 @@ HOST = 'localhost'
 USERNAME = 'test'
 PASSWORD = 'test'
 MACHINE = os.uname()[1]
-FULL_PROMPT = '[%s@%s ~]$' % (USERNAME, MACHINE)
+FULL_PROMPT = '%s@%s ~ $' % (USERNAME, MACHINE)
+PROMPT_START = '%s@' % USERNAME
 
 pwd = u'pwd'
 pwd_amp = u'pwd &'
-unic_string = u'Hyv\u00E4\u00E4 \u00FC\u00F6t\u00E4'
+unic_string = u'Hyv\xE4\xE4 \xFC\xF6t\xE4'
 
