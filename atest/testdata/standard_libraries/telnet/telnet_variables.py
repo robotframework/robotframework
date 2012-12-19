c785a2a47698acd54b2b58187801211a93bb6c9d
Pekka Klärck <peke@iki.fi>
2012-12-20 00:22:09 +0200

    Telnet: Split tests to smaller suites and enhanced them otherwise too. Some tests fail due bugs that hopefully can be fixed later.

--- atest/testdata/standard_libraries/telnet/telnet_variables.py
+++ atest/testdata/standard_libraries/telnet/telnet_variables.py
@@ -3,6 +3,7 @@ import os
 HOST = 'localhost'
 USERNAME = 'test'
 PASSWORD = 'test'
+PROMPT = '$ '
 FULL_PROMPT = '%s@%s ~ $ ' % (USERNAME, os.uname()[1])
 PROMPT_START = '%s@' % USERNAME
 HOME = '/home/%s' % USERNAME
