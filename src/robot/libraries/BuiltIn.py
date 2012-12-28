375c7c05c98faf01c254e79ec6859161ceb28f23
Ilmari Kontulainen <kontulai@gmail.com>
2012-12-28 05:24:12 -0600

    New documentation to Run Keywords
    Update issue 1303

--- src/robot/libraries/BuiltIn.py
+++ src/robot/libraries/BuiltIn.py
@@ -1093,25 +1093,32 @@ class _RunKeyword:
         take care of multiple actions and creating a new higher level user
         keyword is overkill.
 
-        Keywords are, by default, called without arguments eg. every argument to
-        `Run Keywords` is considered as separate keyword. Keywords can be run
-        with arguments using upper case `AND` as a separator argument. If `AND`
-        is used, the keywords are called with arguments in a way that the first
-        argument given is the keyword being called and arguments preceding `AND`
-        are arguments to that keyword.
+        Example, a simple use of `Run Keywords`:
+        | Run Keywords | Initialize database | Start servers |
+        | Run Keywords | ${KW 1} | ${KW 2} |
+        | Run Keywords | @{KEYWORDS} |
 
-        Keyword names and arguments can come from variables, but `AND` can only
-        come as first hand argument.
+        In this example, we call `Run Keywords` with three different combination
+        of arguments. Keyword names and arguments can come from variables, as
+        demonstrated in the second and third row.
 
-        Example:
-        |  *Setting*  |   *Value*    |      *Value*        |    *Value*    |
-        | Suite Setup | Run Keywords | Initialize database | Start servers |
-        | Suite Setup | Run Keywords | @{KEYWORDS} |
-        | Suite Setup | Run Keywords | Initialize database | ${DB NAME} | AND | Start servers | server1 | server2 | server3 |
-        | Suite Setup | Run Keywords | Initialize database | AND | Start servers | @{SERVERS} |
-        | Suite Setup | Run Keywords | @{INIT DB} | AND | @{START SERVERS} |
+        Starting from Robot version 2.7.6, keywords can also be run with
+        arguments using upper case `AND` as control argument.
+        The keywords are then called in a way that the first argument is the
+        keyword and proceeding arguments until `AND` are arguments to that
+        keyword. First argument after `AND` is the keyword and proceeding
+        arguments are its arguments. And so on.
 
-        The AND New in Robot Framework 2.7.6.
+        Example, some ways to use `Run Keywords` using arguments:
+        | Run Keywords | Initialize database | db1 | AND | Start servers | server1 | server2 | server3 |
+        | Run Keywords | Initialize database | ${DB NAME} | AND | Start servers | @{SERVERS} |
+        | Run Keywords | ${INIT DB} | AND | @{START SERVERS} |
+
+        Notice that `AND` control argument must be used explicitly and thus
+        cannot come from variables. If you need to use literal `AND` string as
+        argument, you can either use variables or escape them with a backslash
+        like \`AND`.
+
         """
         errors = []
         for kw, args in self._split_run_keywords(list(names)):
@@ -1142,7 +1149,7 @@ class _RunKeyword:
     def _resolve_run_keywords_name_and_args(self, names):
         needed = self._variables.replace_from_beginning(names, 1, extra_escapes=('AND',))
         if not needed:
-            raise DataError('AND requires keyword')
+            raise DataError('Incorrect use of AND')
         return needed[0], needed[1:]
 
     def run_keyword_if(self, condition, name, *args):
