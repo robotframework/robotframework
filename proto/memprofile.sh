19b0706bcf3190ae788e775fcba1d919698a13b7
Pekka Klärck <peke@iki.fi>
2013-01-02 20:45:32 +0200

    tuning memory profiler script

--- proto/memprofile.sh
+++ proto/memprofile.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-python src/robot/rebot.py $1 >/dev/null &
-#/usr/lib/jvm/java-6-sun/jre/bin/java -Xmx1024m -Xss1024k -classpath /home/jth/opt/jython2.5.1/jython.jar: -Dpython.home=/home/jth/opt/jython2.5.1 -Dpython.executable=/home/jth/opt/jython2.5.1/jython org.python.util.jython src/robot/rebot.py $1 >/dev/null &
+
+#python src/robot/rebot.py $* &> /dev/null &
+python src/robot/run.py $* &> /dev/null &
+
+#/usr/lib/jvm/java-6-sun/jre/bin/java -Xmx1024m -Xss1024k -classpath /home/jth/opt/jython2.5.1/jython.jar: -Dpython.home=/home/jth/opt/jython2.5.1 -Dpython.executable=/home/jth/opt/jython2.5.1/jython org.python.util.jython src/robot/rebot.py $* &> /dev/null &
+
 rebotpid=$!
+delay=10
+
 while ps -ef | grep -v grep| grep $rebotpid > /dev/null; do
 	top -b -p $rebotpid -n 1 | awk "/$rebotpid/ {print \$5,\$6,\$11}"
-	sleep 1
+	sleep $delay
 done
 
 exit 0
