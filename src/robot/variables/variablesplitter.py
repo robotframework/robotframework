6d57185323905a5eb1ff33000da774cf2a2626db
Pekka Klärck <peke@iki.fi>
2013-01-11 09:51:49 +0200

    Fixed `return ValueError` -> `raise ValueError` x2.
    The returned exception was assigned to two variables which caused ValueError masking this problem.
    
    This interesting snafu was actually found in Pygments:
    https://bitbucket.org/birkenfeld/pygments-main/commits/6a48b74c726f4cb8eb85877da7d1b1979ef53464

--- src/robot/variables/variablesplitter.py
+++ src/robot/variables/variablesplitter.py
@@ -72,12 +72,12 @@ class VariableSplitter:
     def _find_variable(self, string):
         max_end_index = string.rfind('}')
         if max_end_index == -1:
-            return ValueError('No variable end found')
+            raise ValueError('No variable end found')
         if self._is_escaped(string, max_end_index):
             return self._find_variable(string[:max_end_index])
         start_index = self._find_start_index(string, 1, max_end_index)
         if start_index == -1:
-            return ValueError('No variable start found')
+            raise ValueError('No variable start found')
         return start_index, max_end_index
 
     def _find_start_index(self, string, start, end):
