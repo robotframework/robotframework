3cc0d30489e3d47aabf48622569204d4a5655c72
Pekka Klärck <peke@iki.fi>
2013-01-29 02:31:28 +0200

    small test cleanup

--- atest/testdata/standard_libraries/builtin/times.py
+++ atest/testdata/standard_libraries/builtin/times.py
@@ -18,5 +18,5 @@ import datetime
 def get_timestamp_from_date(*args):
     return int(time.mktime(datetime.datetime(*(int(arg) for arg in args)).timetuple()))
 
-def get_correct_time_zone():
+def get_current_time_zone():
     return time.altzone if time.localtime().tm_isdst else time.timezone
