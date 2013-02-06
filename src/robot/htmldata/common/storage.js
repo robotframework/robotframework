b32ffe3ef19747926cb0efebfb2a9a0f7d62d799
Pekka Klärck <peke@iki.fi>
2013-02-07 01:56:37 +0200

    Report: Safer and easier-to-reuse storage API.
    Update issue 1362
    Better storage API.

--- src/robot/htmldata/common/storage.js
+++ src/robot/htmldata/common/storage.js
@@ -1,13 +1,24 @@
-function getFromLocalStorage(name, defaultValue) {
-    if (!localStorage)
-        return defaultValue;
-    var value = localStorage[name];
-    if (typeof value === 'undefined')
-        value = defaultValue;
-    return value;
-}
+storage = function () {
 
-function setToLocalStorage(name, value) {
-    if (localStorage)
-        localStorage[name] = value;
-}
+    var prefix = 'robot-framework-';
+
+    function init(user) {
+        prefix += user + '-';
+    }
+
+    function get(name, defaultValue) {
+        if (!localStorage)
+            return defaultValue;
+        var value = localStorage[prefix + name];
+        if (typeof value === 'undefined')
+            return defaultValue;
+        return value;
+    }
+
+    function set(name, value) {
+        if (localStorage)
+            localStorage[prefix + name] = value;
+    }
+
+    return {init: init, get: get, set: set};
+}();
