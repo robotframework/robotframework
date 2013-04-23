7d4cd0ea02bfb929ca2ac0ca036ea09979929372
Tatu Kairi <tatu.kairi@gmail.com>
2013-04-23 11:00:08 +0300

    Separated randomizer tests to it's own file. Cleaned up.
    Update issue 826
    
    Separated randomizer tests to it's own file. Cleaned up imports and such.

--- utest/new_running/test_model.py
+++ utest/new_running/test_model.py
@@ -2,7 +2,7 @@ import unittest
 
 from robot import model
 from robot.new_running import TestSuite, TestCase, Keyword
-from robot.utils.asserts import assert_false, assert_equals, assert_not_equals
+from robot.utils.asserts import assert_equals, assert_not_equals
 
 
 class TestModelTypes(unittest.TestCase):
@@ -21,65 +21,6 @@ class TestModelTypes(unittest.TestCase):
         kw = TestCase().keywords.create()
         assert_equals(type(kw), Keyword)
         assert_not_equals(type(kw), model.Keyword)
-
-
-class TestRandomizing(unittest.TestCase):
-    names = [str(i) for i in range(100)]
-
-    def setUp(self):
-        self.suite = TestSuite()
-        self.suite.suites = self._generate_suites()
-        self.suite.tests = self._generate_tests()
-
-    def _generate_suites(self):
-        return [TestSuite(name=n) for n in self.names]
-
-    def _generate_tests(self):
-        return [TestCase(name=n) for n in self.names]
-
-    def _assert_randomized(self, items):
-        assert_not_equals([i.name for i in items], self.names)
-
-    def _assert_not_randomized(self, items):
-        assert_equals([i.name for i in items], self.names)
-
-    def test_randomize_nothing(self):
-        self.suite.randomize(suites=False, tests=False)
-        self._assert_not_randomized(self.suite.suites)
-        self._assert_not_randomized(self.suite.tests)
-
-    def test_randomize_only_suites(self):
-        self.suite.randomize(suites=True, tests=False)
-        self._assert_randomized(self.suite.suites)
-        self._assert_not_randomized(self.suite.tests)
-
-    def test_randomize_only_tests(self):
-        self.suite.randomize(suites=False, tests=True)
-        self._assert_not_randomized(self.suite.suites)
-        self._assert_randomized(self.suite.tests)
-
-    def test_randomize_both(self):
-        self.suite.randomize(suites=True, tests=True)
-        self._assert_randomized(self.suite.suites)
-        self._assert_randomized(self.suite.tests)
-
-    def test_randomize_recursively(self):
-        self.suite.suites[0].suites = self._generate_suites()
-        self.suite.suites[1].tests = self._generate_tests()
-        self.suite.randomize(suites=True, tests=True)
-        self._assert_randomized(self.suite.suites[0].suites)
-        self._assert_randomized(self.suite.suites[1].tests)
-
-    def test_randomizing_changes_ids(self):
-        assert_equals([s.id for s in self.suite.suites],
-                      ['s1-s%d' % i for i in range(1, 101)])
-        assert_equals([t.id for t in self.suite.tests],
-                      ['s1-t%d' % i for i in range(1, 101)])
-        self.suite.randomize(suites=True, tests=True)
-        assert_equals([s.id for s in self.suite.suites],
-                      ['s1-s%d' % i for i in range(1, 101)])
-        assert_equals([t.id for t in self.suite.tests],
-                      ['s1-t%d' % i for i in range(1, 101)])
 
 
 if __name__ == '__main__':
---
+++ utest/new_running/test_randomizer.py
@@ -0,0 +1,66 @@
+import unittest
+
+from robot.new_running import TestSuite, TestCase
+from robot.utils.asserts import assert_equals, assert_not_equals
+
+class TestRandomizing(unittest.TestCase):
+    names = [str(i) for i in range(100)]
+
+    def setUp(self):
+        self.suite = TestSuite()
+        self.suite.suites = self._generate_suites()
+        self.suite.tests = self._generate_tests()
+
+    def _generate_suites(self):
+        return [TestSuite(name=n) for n in self.names]
+
+    def _generate_tests(self):
+        return [TestCase(name=n) for n in self.names]
+
+    def _assert_randomized(self, items):
+        assert_not_equals([i.name for i in items], self.names)
+
+    def _assert_not_randomized(self, items):
+        assert_equals([i.name for i in items], self.names)
+
+    def test_randomize_nothing(self):
+        self.suite.randomize(suites=False, tests=False)
+        self._assert_not_randomized(self.suite.suites)
+        self._assert_not_randomized(self.suite.tests)
+
+    def test_randomize_only_suites(self):
+        self.suite.randomize(suites=True, tests=False)
+        self._assert_randomized(self.suite.suites)
+        self._assert_not_randomized(self.suite.tests)
+
+    def test_randomize_only_tests(self):
+        self.suite.randomize(suites=False, tests=True)
+        self._assert_not_randomized(self.suite.suites)
+        self._assert_randomized(self.suite.tests)
+
+    def test_randomize_both(self):
+        self.suite.randomize(suites=True, tests=True)
+        self._assert_randomized(self.suite.suites)
+        self._assert_randomized(self.suite.tests)
+
+    def test_randomize_recursively(self):
+        self.suite.suites[0].suites = self._generate_suites()
+        self.suite.suites[1].tests = self._generate_tests()
+        self.suite.randomize(suites=True, tests=True)
+        self._assert_randomized(self.suite.suites[0].suites)
+        self._assert_randomized(self.suite.suites[1].tests)
+
+    def test_randomizing_changes_ids(self):
+        assert_equals([s.id for s in self.suite.suites],
+                      ['s1-s%d' % i for i in range(1, 101)])
+        assert_equals([t.id for t in self.suite.tests],
+                      ['s1-t%d' % i for i in range(1, 101)])
+        self.suite.randomize(suites=True, tests=True)
+        assert_equals([s.id for s in self.suite.suites],
+                      ['s1-s%d' % i for i in range(1, 101)])
+        assert_equals([t.id for t in self.suite.tests],
+                      ['s1-t%d' % i for i in range(1, 101)])
+
+
+if __name__ == '__main__':
+    unittest.main()
