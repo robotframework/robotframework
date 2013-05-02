324fac857e4d8cadd2171c1bdc5634b473107241
Jussi Malinen <jussi.ao.malinen@gmail.com>
2013-05-02 16:08:16 +0300

    move named arguments in library init tests to own use dedicated library and move kwargs library to same folder with tests

--- atest/testdata/keywords/named_args/KwargsLibrary.py
+++ atest/testdata/keywords/named_args/KwargsLibrary.py
@@ -1,15 +1,5 @@
 class KwargsLibrary(object):
 
-    def __init__(self, arg1=None, arg2=None, **kws):
-        self.arg1 = arg1
-        self.arg2 = arg2
-        self.kws = kws
-
-    def check_init_arguments(self, exp_arg1, exp_arg2, **kws):
-        if self.arg1 != exp_arg1 or self.arg2 != exp_arg2 or kws != self.kws:
-            raise AssertionError('Wrong initialization values. Got (%s, %s, %r), expected (%s, %s, %r)'
-                                 % (self.arg1, self.arg2, self.kws, exp_arg1, exp_arg2, kws))
-
     def one_named(self, named=None):
         return named
 
