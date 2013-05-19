0c0fe629c971b59862557c0f4dea3b6dd8e6c153
Jussi Malinen <jussi.ao.malinen@gmail.com>
2013-05-19 23:42:32 +0300

    new run: cleanup building

--- src/robot/new_running/builder.py
+++ src/robot/new_running/builder.py
@@ -25,54 +25,57 @@ class TestSuiteBuilder(object):
                                  name=imp.name,
                                  args=tuple(imp.args),
                                  alias=imp.alias)
-        suite_setup = data.setting_table.suite_setup
-        if suite_setup:
-            suite.keywords.create(type='setup',
-                                  name=suite_setup.name,
-                                  args=tuple(suite_setup.args))
-        suite_teardown = data.setting_table.suite_teardown
-        if suite_teardown:
-            suite.keywords.create(type='teardown',
-                                  name=suite_teardown.name,
-                                  args=tuple(suite_teardown.args))
+        self._create_fixture(suite, data.setting_table.suite_setup, 'setup')
+        self._create_fixture(suite, data.setting_table.suite_teardown, 'teardown')
         for var_data in data.variable_table.variables:
-            if var_data.name.startswith('$'):
-                value = var_data.value[0]
-            else:
-                value = var_data.value
-            suite.variables.create(name=var_data.name,
-                                   value=value)
+            self._create_variable(suite, var_data)
         for uk_data in data.keyword_table.keywords:
-            uk = suite.user_keywords.create(name=uk_data.name,
-                                            args=tuple(uk_data.args))
-            for kw_data in uk_data.steps:
-                uk.keywords.create(name=kw_data.keyword,
-                                   args=tuple(kw_data.args),
-                                   assign=tuple(kw_data.assign))
+            self._create_uk(suite, uk_data)
         for test_data in data.testcase_table.tests:
-            test = suite.tests.create(name=test_data.name,
-                                      doc=test_data.doc.value,
-                                      tags=self._get_tags(test_data,
-                                                          data.setting_table))
-            test_setup = test_data.setup or test_data.parent.parent.setting_table.test_setup
-            if test_setup:
-                test.keywords.create(type='setup',
-                                     name=test_setup.name,
-                                     args=tuple(test_setup.args))
-            for kw_data in test_data.steps:
-                test.keywords.create(name=kw_data.keyword,
-                                     args=tuple(kw_data.args),
-                                     assign=tuple(kw_data.assign))
-            test_teardown = test_data.teardown or test_data.parent.parent.setting_table.test_teardown
-            if test_teardown:
-                test.keywords.create(type='teardown',
-                                     name=test_teardown.name,
-                                     args=tuple(test_teardown.args))
+            self._create_test(suite, test_data)
         for child in data.children:
             suite.suites.append(self._build(child))
         return suite
 
-    def _get_tags(self, test, settings):
+    def _create_test(self, suite, test_data):
+        test = suite.tests.create(name=test_data.name,
+                                  doc=test_data.doc.value,
+                                  tags=self._get_tags(test_data))
+        test_setup = test_data.setup or test_data.parent.parent.setting_table.test_setup
+        self._create_fixture(test, test_setup, 'setup')
+        for step_data in test_data.steps:
+            self._create_step(test, step_data)
+        test_teardown = test_data.teardown or test_data.parent.parent.setting_table.test_teardown
+        self._create_fixture(test, test_teardown, 'teardown')
+
+    def _create_uk(self, suite, uk_data):
+        uk = suite.user_keywords.create(name=uk_data.name,
+                                        args=tuple(uk_data.args))
+        for step_data in uk_data.steps:
+            self._create_step(uk, step_data)
+
+    def _create_variable(self, suite, var_data):
+        if var_data.name.startswith('$'):
+            value = var_data.value[0]
+        else:
+            value = var_data.value
+        suite.variables.create(name=var_data.name,
+                               value=value)
+
+    def _create_fixture(self, target, element, fixture_type):
+        if element:
+            target.keywords.create(type=fixture_type,
+                                   name=element.name,
+                                   args=tuple(element.args))
+
+    def _create_step(self, parent, step_data):
+        parent.keywords.create(name=step_data.keyword,
+                               args=tuple(step_data.args),
+                               assign=tuple(step_data.assign))
+
+    def _get_tags(self, test):
+        # FIXME: get tags from a context
+        settings = test.parent.parent.setting_table
         tags = test.tags.value
         defaults = settings.default_tags.value or []
         forced = settings.force_tags.value or []
