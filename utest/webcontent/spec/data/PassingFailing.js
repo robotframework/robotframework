4cdf4d7292c06bc58554e68702bdc82476a4c8b4
Mika Hänninen <mika.hanninen@gmail.com>
2013-05-29 14:42:53 +0300

    Regen Jasmine

--- utest/webcontent/spec/data/PassingFailing.js
+++ utest/webcontent/spec/data/PassingFailing.js
@@ -1,20 +1,20 @@
 window.passingFailingOutput = {};
 
-window.passingFailingOutput["suite"] = [1,2,3,0,[],[0,0,25],[],[[4,0,1,0,[],[1,23,1],[[0,5,0,6,7,[1,23,1],[],[[24,2,7]]]]],[8,0,1,0,[],[0,24,1,9],[[0,10,0,11,9,[0,24,1],[],[[25,4,9]]]]]],[],[2,1,2,1]];
+window.passingFailingOutput["suite"] = [1,2,3,0,[],[0,0,3],[],[[4,0,1,0,[],[1,0,1],[[0,5,0,6,7,[1,1,0],[],[[1,2,7]]]]],[8,0,1,0,[],[0,1,1,9],[[0,10,0,11,9,[0,2,0],[],[[2,4,9]]]]]],[],[2,1,2,1]];
 
 window.passingFailingOutput["strings"] = [];
 
-window.passingFailingOutput["strings"] = window.passingFailingOutput["strings"].concat(["*","*PassingFailing","*/home/mkorpela/workspace/robot/utest/webcontent/spec/data/teardownFailure/PassingFailing.txt","*teardownFailure/PassingFailing.txt","*Passing","*BuiltIn.Log","*<p>Logs the given message with the given level.</p>","*passing","*Failing","*In test","*BuiltIn.Fail","*<p>Fails the test with the given message and optionally alters its tags.</p>"]);
+window.passingFailingOutput["strings"] = window.passingFailingOutput["strings"].concat(["*","*PassingFailing","*/Users/mikhanni/koodi/robotframework/utest/webcontent/spec/data/teardownFailure/PassingFailing.txt","*utest/webcontent/spec/data/teardownFailure/PassingFailing.txt","*Passing","*BuiltIn.Log","*<p>Logs the given message with the given level.</p>","*passing","*Failing","*In test","*BuiltIn.Fail","*<p>Fails the test with the given message and optionally alters its tags.</p>"]);
 
-window.passingFailingOutput["generatedTimestamp"] = "20130404 15:36:19 GMT +03:00";
+window.passingFailingOutput["generatedTimestamp"] = "20130529 14:41:30 GMT +03:00";
 
 window.passingFailingOutput["errors"] = [];
 
 window.passingFailingOutput["stats"] = [[{"elapsed":"00:00:00","fail":1,"label":"Critical Tests","pass":1},{"elapsed":"00:00:00","fail":1,"label":"All Tests","pass":1}],[],[{"elapsed":"00:00:00","fail":1,"id":"s1","label":"PassingFailing","name":"PassingFailing","pass":1}]];
 
-window.passingFailingOutput["generatedMillis"] = -892;
+window.passingFailingOutput["generatedMillis"] = -159;
 
-window.passingFailingOutput["baseMillis"] = 1365078979892;
+window.passingFailingOutput["baseMillis"] = 1369827690159;
 
 window.settings = {"background":{"fail":"DeepPink"},"logURL":"log.html","reportURL":"report.html"};
 
--- utest/webcontent/spec/data/Suite.js
+++ utest/webcontent/spec/data/Suite.js
@@ -1,20 +1,20 @@
 window.suiteOutput = {};
 
-window.suiteOutput["suite"] = [1,2,3,4,[5,6],[1,0,138],[],[[7,8,1,9,[10,11],[1,32,106],[[0,12,0,13,14,[1,32,101],[],[[133,2,15]]],[3,16,0,0,0,[1,133,5],[[4,17,0,0,0,[1,133,2],[[0,18,0,0,19,[1,133,2],[[0,20,0,21,22,[1,134,1],[],[[135,2,23]]]],[]]],[]],[4,24,0,0,0,[1,135,3],[[0,18,0,0,19,[1,136,1],[[0,20,0,21,22,[1,136,1],[],[[137,2,25]]]],[]]],[]]],[]]]]],[],[1,1,1,1]];
+window.suiteOutput["suite"] = [1,2,3,4,[5,6],[1,0,104],[],[[7,8,1,9,[10,11],[1,0,104],[[0,12,0,13,14,[1,0,101],[],[[101,2,15]]],[3,16,0,0,0,[1,102,2],[[4,17,0,0,0,[1,102,1],[[0,18,0,0,19,[1,102,1],[[0,20,0,21,22,[1,102,1],[],[[103,2,23]]]],[]]],[]],[4,24,0,0,0,[1,103,1],[[0,18,0,0,19,[1,103,1],[[0,20,0,21,22,[1,103,1],[],[[104,2,25]]]],[]]],[]]],[]]]]],[],[1,1,1,1]];
 
 window.suiteOutput["strings"] = [];
 
-window.suiteOutput["strings"] = window.suiteOutput["strings"].concat(["*","*Suite","*/home/mkorpela/workspace/robot/utest/webcontent/spec/data/Suite.txt","*Suite.txt","*<p>suite doc</p>","*meta","*<p>data</p>","*Test","*1 second","*<p>test doc</p>","*tag1","*tag2","*BuiltIn.Sleep","*<p>Pauses the test executed for the given time.</p>","*0.1 seconds","*Slept 100 milliseconds","*${i} IN RANGE [ 2 ]","*${i} = 0","*my keyword","*${i}","*BuiltIn.Log","*<p>Logs the given message with the given level.</p>","*index is ${index}","*index is 0","*${i} = 1","*index is 1"]);
+window.suiteOutput["strings"] = window.suiteOutput["strings"].concat(["*","*Suite","*/Users/mikhanni/koodi/robotframework/utest/webcontent/spec/data/Suite.txt","*utest/webcontent/spec/data/Suite.txt","*<p>suite doc</p>","*meta","*<p>data</p>","*Test","*1 second","*<p>test doc</p>","*tag1","*tag2","*BuiltIn.Sleep","*<p>Pauses the test executed for the given time.</p>","*0.1 seconds","*Slept 100 milliseconds","*${i} IN RANGE [ 2 ]","*${i} = 0","*my keyword","*${i}","*BuiltIn.Log","*<p>Logs the given message with the given level.</p>","*index is ${index}","*index is 0","*${i} = 1","*index is 1"]);
 
-window.suiteOutput["generatedTimestamp"] = "20130404 15:36:19 GMT +03:00";
+window.suiteOutput["generatedTimestamp"] = "20130529 14:41:30 GMT +03:00";
 
 window.suiteOutput["errors"] = [];
 
 window.suiteOutput["stats"] = [[{"elapsed":"00:00:00","fail":0,"label":"Critical Tests","pass":1},{"elapsed":"00:00:00","fail":0,"label":"All Tests","pass":1}],[{"elapsed":"00:00:00","fail":0,"label":"tag1","pass":1},{"elapsed":"00:00:00","fail":0,"label":"tag2","pass":1}],[{"elapsed":"00:00:00","fail":0,"id":"s1","label":"Suite","name":"Suite","pass":1}]];
 
-window.suiteOutput["generatedMillis"] = -631;
+window.suiteOutput["generatedMillis"] = 87;
 
-window.suiteOutput["baseMillis"] = 1365078979631;
+window.suiteOutput["baseMillis"] = 1369827689913;
 
 window.settings = {"background":{"fail":"DeepPink"},"logURL":"log.html","reportURL":"report.html"};
 
