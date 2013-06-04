b730b2c7b4476050756d02da0b95eba24bdaa623
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2013-06-04 12:39:18 +0300

    Updated copyright year

--- doc/api/conf.py
+++ doc/api/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Robot Framework'
-copyright = u'2012, Nokia Siemens Networks'
+copyright = u'2013, Nokia Siemens Networks'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
