d41f4f9dabe0fac5a92e9cfe53b95711fd3b7aaa
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2013-06-06 15:10:19 +0300

    API docs: Creating a test suite example

--- src/robot/running/__init__.py
+++ src/robot/running/__init__.py
@@ -14,26 +14,64 @@
 
 """Implements the core test execution logic.
 
+This package was rewritten for Robot Framework 2.8.
+
 Currently, the main entry point is the :func:`~.model.TestSuite` factory
 method.
 
-Examples
---------
+Example: Creating a test suite programmatically
+-----------------------------------------------
 
-1) Creating test cases programmatically
+Starting from Robot Framework 2.8, it is possible to create runnable test suites
+programmatically using :py:mod:`Robot Framework Public APIs <robot.api>`.
 
-[explanation]
+In this example, we create a basic test suite having one test case.
 
 .. literalinclude:: /../../doc/api/code_examples/test_suite.py
+   :linenos:
 
-2) Creating test cases from source files
+On line 1, we start by importing :py:class:`robot.running.model.TestSuite`,
+exposed via :py:mod:`robot.api`. Please note, that no other classes
+are imported via API. This is due to fact that everything related to tests
+(test cases, keywords, variables, etc.) are always created via
+:py:class:`robot.running.model.TestSuite`.
+
+On line 3, we instantiate a new test suite from
+:py:class:`robot.running.model.TestSuite`. When creating new test suites,
+test cases or keywords, the first parameter is always the name.
+
+On line 4, the `OperatingSystem` library is brought into use by
+using :py:func:`robot.model.imports.Imports.library`. Other possible import
+types are :py:func:`resource file <robot.model.imports.Imports.resource>` and
+:py:func:`variable file <robot.model.imports.Imports.variables>`.
+
+On line 5, the actual test case is created into the suite. Besides the name,
+the test case is also given an additional tag as a named argument.
+Multiple values can be given as a list, for example, multiple tags could be
+defined by using `tags=['regression', 'slow']`.
+
+On line 6, the test case setup is created by adding keyword
+`Set Environment Variable` (from `Operating System` library) into the test case.
+This keyword takes multiple arguments, hence the arguments are given as a list.
+Parameter `type` with value `setup` specifies, that this keyword is defined as
+the test case setup. Similarly, `type` could be `teardown` for defining the test
+case teardown. If no `type` is given as parameter, the keyword is assumed to be
+a normal, i.e. non-setup and non-teardown, keyword.
+
+On line 7, another keyword `Environment Variable Should Be Set`, is added into
+the test case.
+
+On line 8, the actual test suite is ran by issuing method
+:py:func:`robot.running.model.TestSuite.run`. This method returns test results
+as an object which is type of :py:class:`robot.result.executionresult.Result`.
+
+
+Example: Creating a test suite from source
+------------------------------------------
 
 [explanation]
 
 .. literalinclude:: /../../doc/api/code_examples/test_suite_builder.py
-
-
-This package was rewritten for Robot Framework 2.8.
 """
 
 from .builder import TestSuiteBuilder
