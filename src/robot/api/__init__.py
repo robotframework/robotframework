6535516e359e5a27514279c1d27f397a03efac3b
Pekka Klärck <peke@iki.fi>
2013-06-07 16:18:40 +0300

    apidocs: enhanced robot.api and robot.api.logger docs

--- src/robot/api/__init__.py
+++ src/robot/api/__init__.py
@@ -19,38 +19,46 @@ and thus safe to use when building external tools on top of Robot Framework.
 
 Currently exposed APIs are:
 
-  * :py:mod:`.logger` for test libraries' logging purposes.
+* :mod:`.logger` module for test libraries' logging purposes.
 
-  * :py:func:`~robot.result.resultbuilder.ExecutionResult` for reading
-    execution results from XML output files and
-    :py:class:`~robot.result.visitor.ResultVisitor` to ease processing
-    of the execution results.
+* :class:`~robot.parsing.model.TestCaseFile`,
+  :class:`~robot.parsing.model.TestDataDirectory`, and
+  :class:`~robot.parsing.model.ResourceFile` classes for parsing test
+  data files and directories.
+  In addition, a convenience factory method
+  :func:`~robot.parsing.model.TestData` creates either
+  :class:`~robot.parsing.model.TestCaseFile` or
+  :class:`~robot.parsing.model.TestDataDirectory` objects based on the input.
+  **Note:** Test data parsing may be rewritten in Robot Framework 2.9 or later.
 
-  * :py:class:`~robot.parsing.model.TestCaseFile`,
-    :py:class:`~robot.parsing.model.TestDataDirectory`, and
-    :py:class:`~robot.parsing.model.ResourceFile` for parsing data files.
-    In addition, a convenience factory function
-    :py:func:`~robot.parsing.model.TestData` creates either
-    :py:class:`~robot.parsing.model.TestCaseFile` or
-    :py:class:`~robot.parsing.model.TestDataDirectory` based on the input.
+* :class:`~robot.running.model.TestSuite` class for creating executable
+  test suites programmatically and
+  :class:`~robot.running.builder.TestSuiteBuilder` class
+  for creating such suites based on existing test data on the file system.
 
-  * :py:class:`~robot.running.model.TestSuite` for creating a test suites
-    programmatically and :py:class:`~robot.running.builder.TestSuiteBuilder`
-    for creating test suites from source files.
+* :func:`~robot.result.resultbuilder.ExecutionResult` factory method
+  for reading execution results from XML output files and
+  :class:`~robot.result.visitor.ResultVisitor` abstract class to ease
+  further processing the results.
 
-  * :py:class:`~robot.reporting.resultwriter.ResultWriter` for writing test
-    results, logs, reports and XUnit files.
+* :class:`~robot.reporting.resultwriter.ResultWriter` class for writing
+  reports, logs, XML outputs, and XUnit files. Can write results based on
+  XML outputs on the file system as well as based on result objects
+  returned by :func:`~robot.result.resultbuilder.ExecutionResult` or
+  executed :class:`~robot.running.model.TestSuite`.
 
-These names can be imported like this:
 
-.. code-block:: python
+All of the above names can be imported like::
 
-    from robot.api import <name>
+    from robot.api import ApiName
 
 See documentations of the individual APIs for more details.
+
+.. tip:: APIs related to the command line entry points are exposed directly
+        via the :mod:`robot` root package.
 """
 
 from robot.parsing import TestCaseFile, TestDataDirectory, ResourceFile, TestData
-from robot.result import ExecutionResult, ResultVisitor
 from robot.reporting import ResultWriter
+from robot.result import ExecutionResult, ResultVisitor
 from robot.running import TestSuite, TestSuiteBuilder
--- src/robot/api/logger.py
+++ src/robot/api/logger.py
@@ -15,21 +15,26 @@
 """Public logging API for test libraries.
 
 This module provides a public API for writing messages to the log file
-and the console. Test libraries can use this API like `logger.info('My
-message')` instead of logging through the standard output like `print
-'*INFO* My message'`. In addition to a programmatic interface being
-cleaner to use, this API has a benefit that the log messages have
-accurate timestamps.
+and the console. Test libraries can use this API like::
+
+    logger.info('My message')
+
+instead of logging through the standard output like::
+
+    print '*INFO* My message'
+
+In addition to a programmatic interface being cleaner to use, this API
+has a benefit that the log messages have accurate timestamps.
 
 Log levels
 ----------
 
-It is possible to log messages using levels `TRACE`, `DEBUG`, `INFO`
-and `WARN` either using the `write` method or, more commonly, with the
-log level specific `trace`, `debug`, `info` and `warn` methods.
+It is possible to log messages using levels ``TRACE``, ``DEBUG``, ``INFO``
+and ``WARN`` either using the ``write`` method or, more commonly, with the
+log level specific ``trace``, ``debug``, ``info`` and ``warn`` methods.
 
 By default the trace and debug messages are not logged but that can be
-changed with the `--loglevel` command line option. Warnings are
+changed with the ``--loglevel`` command line option. Warnings are
 automatically written also to the `Test Execution Errors` section in
 the log file and to the console.
 
@@ -37,8 +42,8 @@ Logging HTML
 ------------
 
 All methods that are used for writing messages to the log file have an
-optional `html` argument. If a message to be logged is supposed to be
-shown as HTML, this argument should be set to `True`.
+optional ``html`` argument. If a message to be logged is supposed to be
+shown as HTML, this argument should be set to ``True``.
 
 Example
 -------
@@ -48,9 +53,9 @@ Example
     from robot.api import logger
 
     def my_keyword(arg):
-        logger.debug('Got argument %s' % arg)
+        logger.debug('Got argument %s.' % arg)
         do_something()
-        logger.info('<i>This</i> is a boring example', html=True)
+        logger.info('<i>This</i> is a boring example.', html=True)
 """
 
 from robot.output import librarylogger
@@ -59,41 +64,41 @@ from robot.output import librarylogger
 def write(msg, level, html=False):
     """Writes the message to the log file using the given level.
 
-    Valid log levels are `TRACE`, `DEBUG`, `INFO` and `WARN`. Instead
-    of using this method, it is generally better to use the level
-    specific methods such as `info` and `debug`.
+    Valid log levels are ``TRACE``, ``DEBUG``, ``INFO`` and ``WARN``.
+    Instead of using this method, it is generally better to use the level
+    specific methods such as ``info`` and ``debug``.
     """
     librarylogger.write(msg, level, html)
 
 
 def trace(msg, html=False):
-    """Writes the message to the log file with the TRACE level."""
+    """Writes the message to the log file using the ``TRACE`` level."""
     librarylogger.trace(msg, html)
 
 
 def debug(msg, html=False):
-    """Writes the message to the log file with the DEBUG level."""
+    """Writes the message to the log file using the ``DEBUG`` level."""
     librarylogger.debug(msg, html)
 
 
 def info(msg, html=False, also_console=False):
-    """Writes the message to the log file with the INFO level.
+    """Writes the message to the log file using the ``INFO`` level.
 
-    If `also_console` argument is set to `True`, the message is written
-    both to the log file and to the console.
+    If ``also_console`` argument is set to ``True``, the message is
+    written both to the log file and to the console.
     """
     librarylogger.info(msg, html, also_console)
 
 
 def warn(msg, html=False):
-    """Writes the message to the log file with the WARN level."""
+    """Writes the message to the log file using the ``WARN`` level."""
     librarylogger.warn(msg, html)
 
 
 def console(msg, newline=True):
     """Writes the message to the console.
 
-    If the `newline` argument is `True`, a newline character is automatically
-    added to the message.
+    If the ``newline`` argument is ``True``, a newline character is
+    automatically added to the message.
     """
     librarylogger.console(msg, newline)
