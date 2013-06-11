a6fb784c408356b1cd8010c1981cf05cced59f34
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2013-06-11 13:29:28 +0300

    API docs: Class attributes/methods/properties sorted alphabetically
    Update issue 1468
    
    Class attributes/methods/properties are now sorted alphabetically.
    
    Inherited attribute docstrings (from parent class) cannot be displayed. This is a known bug in Sphinx, see: https://bitbucket.org/birkenfeld/sphinx/issue/741/autodoc-inherited-members-wont-work-for

--- doc/api/conf.py
+++ doc/api/conf.py
@@ -28,9 +28,8 @@ from robot.version import VERSION, get_version
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
-autoclass_content = "both"
-autodoc_member_order = "bysource"
-autodoc_default_flags = ["inherited-members"]
+autoclass_content = 'both'
+autodoc_default_flags = ['members', 'undoc-members', 'inherited-members']
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
