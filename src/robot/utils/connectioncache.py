d818a87f7d9d197adf174e30c815656fe897be3f
Pekka Klärck <peke@iki.fi>
2013-06-14 01:44:34 +0300

    ConnectionCache: minor cleanup and doc enhancements

--- src/robot/utils/connectioncache.py
+++ src/robot/utils/connectioncache.py
@@ -51,30 +51,33 @@ class ConnectionCache(object):
         If alias is given, it must be a string. Aliases are case and space
         insensitive.
 
-        The index of the first connection after initialization, or
+        The index of the first connection after initialization, and after
         :meth:`close_all` or :meth:`empty_cache`, is 1, second is 2, etc.
         """
         self.current = connection
         self._connections.append(connection)
+        index = len(self._connections)
         if isinstance(alias, basestring):
-            self._aliases[alias] = self.current_index
-        return self.current_index
+            self._aliases[alias] = index
+        return index
 
-    def switch(self, index_or_alias):
-        """Switches to the connection specified by given index or alias.
+    def switch(self, alias_or_index):
+        """Switches to the connection specified by the given alias or index.
+
+        Updates :attr:`current` and also returns its new value.
 
         Alias is whatever was given to :meth:`register` method and indices
         are returned by it. Index can be given either as an integer or
         as a string that can be converted to an integer. Raises an error
         if no connection with the given index or alias found.
         """
-        self.current = self.get_connection(index_or_alias)
+        self.current = self.get_connection(alias_or_index)
         return self.current
 
-    def get_connection(self, index_or_alias=None):
-        """Get the connection specified by given index or alias.
+    def get_connection(self, alias_or_index=None):
+        """Get the connection specified by the given alias or index..
 
-        If ``index_or_alias`` is ``None``, returns the current connection
+        If ``alias_or_index`` is ``None``, returns the current connection
         if it is active, or raises an error if it is not.
 
         Alias is whatever was given to :meth:`register` method and indices
@@ -82,15 +85,15 @@ class ConnectionCache(object):
         as a string that can be converted to an integer. Raises an error
         if no connection with the given index or alias found.
         """
-        if index_or_alias is None:
+        if alias_or_index is None:
             if not self:
                 self.current.raise_error()
             return self.current
         try:
-            index = self._resolve_index_or_alias(index_or_alias)
+            index = self._resolve_alias_or_index(alias_or_index)
         except ValueError:
             raise RuntimeError("Non-existing index or alias '%s'."
-                               % index_or_alias)
+                               % alias_or_index)
         return self._connections[index-1]
 
     __getitem__ = get_connection
@@ -100,7 +103,7 @@ class ConnectionCache(object):
 
         If simply calling the closer method is not adequate for closing
         connections, clients should close connections themselves and use
-        empty_cache afterwards.
+        :meth:`empty_cache` afterwards.
         """
         for conn in self._connections:
             getattr(conn, closer_method)()
@@ -125,11 +128,11 @@ class ConnectionCache(object):
     def __nonzero__(self):
         return self.current is not self._no_current
 
-    def _resolve_index_or_alias(self, index_or_alias):
+    def _resolve_alias_or_index(self, alias_or_index):
         try:
-            return self._resolve_alias(index_or_alias)
+            return self._resolve_alias(alias_or_index)
         except ValueError:
-            return self._resolve_index(index_or_alias)
+            return self._resolve_index(alias_or_index)
 
     def _resolve_alias(self, alias):
         if isinstance(alias, basestring):
