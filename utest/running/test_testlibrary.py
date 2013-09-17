09ca0b0b8bc7281352067c4b6c122f18aa0a1bf0
Jussi Malinen <jussi.ao.malinen@gmail.com>
2013-09-17 17:01:05 +0300

    fix jython unit tests expcetations after extra keyword in example library

--- utest/running/test_testlibrary.py
+++ utest/running/test_testlibrary.py
@@ -440,7 +440,7 @@ class TestHandlers(unittest.TestCase):
 
         def test_extending_java_lib_in_python(self):
             handlers = TestLibrary('extendingjava.ExtendJavaLib').handlers
-            assert_equals(len(handlers), 24)
+            assert_equals(len(handlers), 25)
             for handler in 'kw_in_java_extender', 'javaSleep', 'divByZero':
                 assert_true(handler in handlers)
 
