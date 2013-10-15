1388c06b5bf768ce150b0ef0e907c3b478325032
Pekka Klärck <peke@iki.fi>
2013-10-15 14:46:40 +0300

    More argument validation tests for --dryrun
    Update issue 1511
    The fix for this issue turned out to fix argument validation w/ dryrun in general. Added tests to prove it.

--- atest/testdata/cli/dryrun/Library.py
+++ atest/testdata/cli/dryrun/Library.py
@@ -1,4 +1,16 @@
-def positional_and_kwargs(positional, **kwargs):
+def normal_args(a, b=None):
+    pass
+
+
+def normal_and_varargs(a, b=None, *varargs):
+    pass
+
+
+def normal_and_varargs_and_kwargs(a, b=None, *varargs, **kwargs):
+    pass
+
+
+def normal_and_kwargs(positional, **kwargs):
     pass
 
 
