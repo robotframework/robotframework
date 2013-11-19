31aa80efb914a19c05be9563f0e2ffb5515d2d70
Pekka Klärck <peke@iki.fi>
2013-11-19 10:31:47 +0200

    various IronPython test fixes

--- atest/testresources/testlibs/FailUntilSucceeds.py
+++ atest/testresources/testlibs/FailUntilSucceeds.py
@@ -1,12 +1,14 @@
 class FailUntilSucceeds:
-    
     ROBOT_LIBRARY_SCOPE = 'TESTCASE'
-    
+
     def __init__(self, times_to_fail=0):
         self.times_to_fail = int(times_to_fail)
-    
+
+    def set_times_to_fail(self, times_to_fail):
+        self.__init__(times_to_fail)
+
     def fail_until_retried_often_enough(self, message="Hello"):
         self.times_to_fail -= 1
         if self.times_to_fail >= 0:
-            raise Exception('Still %d times to fail!' % (self.times_to_fail))
+            raise Exception('Still %d times to fail!' % self.times_to_fail)
         return message
