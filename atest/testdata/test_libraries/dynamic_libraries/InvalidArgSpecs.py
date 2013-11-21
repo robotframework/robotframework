cec6c679556d0ab3420999540d99c6d1037b48e3
Pekka Klärck <peke@iki.fi>
2013-11-21 11:41:12 +0200

    atests: Combined dynamic library invalid argpspec tests and cleaned them up in general.
    Update issue 1500
    Combined invalid argspec tests.

---
+++ atest/testdata/test_libraries/dynamic_libraries/InvalidArgSpecs.py
@@ -0,0 +1,20 @@
+KEYWORDS = [('argspec with other than strings', [1, 2]),
+            ('named args before positional', ['a=1', 'b']),
+            ('varargs before positional args', ['*varargs', 'a']),
+            ('varargs before named args', ['*varargs', 'a=1']),
+            ('kwargs before positional args', ['**kwargs', 'a']),
+            ('kwargs before named args', ['**kwargs', 'a=1']),
+            ('kwargs before varargs', ['**kwargs', '*varargs']),
+            ('valid argspec', ['a'])]
+
+
+class InvalidArgSpecs(object):
+
+    def get_keyword_names(self):
+        return [name for name, _ in KEYWORDS]
+
+    def run_keyword(self, name, args, kwargs):
+        return ''.join(args + tuple(kwargs)).upper()
+
+    def get_keyword_arguments(self, name):
+        return dict(KEYWORDS)[name]
