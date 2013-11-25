c79672d86ed0d151f8c4617322135f9c4f7654f7
Stefan Zimmermann <zimmermann.code@gmail.com>
2013-11-25 18:08:06 +0000

    JavaArgumentParser: *varargs with java.util.List

--- src/robot/running/arguments/argumentparser.py
+++ src/robot/running/arguments/argumentparser.py
@@ -13,6 +13,7 @@ import sys
 import inspect
 if sys.platform.startswith('java'):
     from java.lang import Class
+    from java.util import List
 
 from robot.errors import DataError
 from robot.variables import is_list_var, is_scalar_var
@@ -56,9 +57,13 @@ class JavaArgumentParser(_ArgumentParser):
         # Happens when a class has no public constructors
         return self._format_arg_spec()
 
+    def _is_varargs_type(self, arg):
+        return isinstance(arg, Class) and (
+          arg.isArray() or issubclass(arg, List))
+
     def _single_signature_arg_spec(self, signature):
         args = signature.args
-        if args and isinstance(args[-1], Class) and args[-1].isArray():
+        if args and self._is_varargs_type(args[-1]):
             return self._format_arg_spec(len(args)-1, varargs=True)
         return self._format_arg_spec(len(args))
 
