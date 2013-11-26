8ccf5fda8d93f83d2554d52048edf3eb47b9ff7f
Jussi Malinen <jussi.ao.malinen@gmail.com>
2013-11-26 16:15:29 +0200

    added comment about os.uname missing on jython

--- atest/testdata/standard_libraries/telnet/telnet_variables.py
+++ atest/testdata/standard_libraries/telnet/telnet_variables.py
@@ -5,6 +5,7 @@ HOST = 'localhost'
 USERNAME = 'test'
 PASSWORD = 'test'
 PROMPT = '$ '
+# TODO: os.uname does not exist on jython. Tests should pass otherways.
 FULL_PROMPT = '%s@%s ~ $ ' % (USERNAME, os.uname()[1])
 PROMPT_START = '%s@' % USERNAME
 HOME = '/home/%s' % USERNAME
