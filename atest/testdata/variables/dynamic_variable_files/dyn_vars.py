2e411d112c634c181c71d29da726607b110955a5
Pekka Klärck <peke@iki.fi>
2013-11-29 23:05:29 +0200

    test data clean-up

--- atest/testdata/variables/dynamic_variable_files/dyn_vars.py
+++ atest/testdata/variables/dynamic_variable_files/dyn_vars.py
@@ -1,33 +1,33 @@
-import UserDict
+from UserDict import UserDict
+
 
 def get_variables(type):
     return {'dict': get_dict,
             'mydict': MyDict,
             'UserDict': get_UserDict,
-            'MyUserDict': get_MyUserDict,
+            'MyUserDict': MyUserDict,
             'JavaMap': get_JavaMap}[type]()
 
 
 def get_dict():
     return {'from dict': 'This From Dict', 'from dict2': 2}
 
+
 class MyDict(dict):
+
     def __init__(self):
         dict.__init__(self, from_my_dict='This From My Dict', from_my_dict2=2)
 
 def get_UserDict():
-    userdict = UserDict.UserDict()
-    userdict.update({'from UserDict': 'This From UserDict', 'from UserDict2': 2})
-    return userdict
+    return UserDict({'from UserDict': 'This From UserDict', 'from UserDict2': 2})
 
-class MyUserDict(UserDict.UserDict):
-    def __init__(self, dict):
-        self.data = {}
-        self.update(dict)
 
-def get_MyUserDict(*args):
-    return MyUserDict({'from MyUserDict': 'This From MyUserDict', 
-                       'from MyUserDict2': 2})
+class MyUserDict(UserDict):
+
+    def __init__(self):
+        UserDict.__init__(self, {'from MyUserDict': 'This From MyUserDict',
+                                 'from MyUserDict2': 2})
+
 
 def get_JavaMap():
     from java.util import HashMap
