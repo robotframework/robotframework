be80ccac3c4ce34fdd7f1cb4b83627ab9b558f0f
Stefan Zimmermann <zimmermann.code@gmail.com>
2013-12-16 14:09:58 +0000

    [python3] run_utests: 2to3
    --HG--
    extra : transplant_source : isD%CEo%93%1F%09%EFs%2A0%DB%E2%1Dgr%18%B6%26

--- utest/run_utests.py
+++ utest/run_utests.py
@@ -17,9 +17,64 @@ import os
 import sys
 import re
 import getopt
+import shutil
+import subprocess
+from os.path import join, abspath, dirname
 
 
-base = os.path.abspath(os.path.normpath(os.path.split(sys.argv[0])[0]))
+# Check for new working dir after 2to3:
+if not 'UTESTDIR' in globals():
+    # ==> still the original script before 2to3.
+    UTESTDIR = dirname(abspath(__file__))
+ROBOTDIR = join(UTESTDIR, '..', 'src', 'robot')
+ATESTDIR = join(UTESTDIR, '..', 'atest')
+
+# If run with Python 3:
+# - Copy src/robot/ and atest/ to atest/python3/
+# - Run 2to3
+# - Exec this file's copy in-place for actual testing
+
+# Is this script already the Python 3 copy?
+if not 'do2to3' in globals():
+    # ==> still the original.
+    do2to3 = True
+if sys.version_info[0] == 3 and do2to3:
+    PY3DIR = join(UTESTDIR, 'python3')
+    PY3UTESTDIR = join(PY3DIR, 'utest')
+    PY3ATESTDIR = join(PY3DIR, 'atest')
+
+    shutil.rmtree((PY3DIR), ignore_errors=True)
+    os.makedirs(join(PY3DIR, 'src'))
+    shutil.copytree(ROBOTDIR, join(PY3DIR, 'src', 'robot'), symlinks=True)
+    shutil.copytree(
+      UTESTDIR, PY3UTESTDIR, symlinks=True,
+      ignore=lambda src, names: names if src == PY3DIR else []
+      )
+    shutil.copytree( # ATESTDIR, PY3ATESTDIR, symlinks=True)
+      ATESTDIR, PY3ATESTDIR, symlinks=True,
+      ignore=lambda src, names: names if src == join(ATESTDIR, 'python3') else []
+      )
+    status = subprocess.call(
+      ['2to3', '--no-diffs', '-n', '-w',
+       '-x', 'dict',
+       '-x', 'filter',
+       PY3DIR
+       ])
+    if status:
+        sys.exit(status)
+
+    print("Hey Ho!")
+    do2to3 = False
+    UTESTDIR = PY3UTESTDIR
+
+    # Exec this file's Python 3 copy:
+    TESTRUNNER = join(UTESTDIR, 'run_utests.py')
+    exec(open(TESTRUNNER).read())
+    sys.exit(0)
+
+
+base = UTESTDIR
+## base = os.path.abspath(os.path.normpath(os.path.split(sys.argv[0])[0]))
 for path in ["../src", "../src/robot/libraries", "../src/robot",
              "../atest/testresources/testlibs" ]:
     path = os.path.join(base, path.replace('/', os.sep))
@@ -60,9 +115,9 @@ def parse_args(argv):
         options, args = getopt.getopt(argv, 'hH?vqd',
                                       ['help','verbose','quiet','doc'])
         if len(args) != 0:
-            raise getopt.error, 'no arguments accepted, got %s' % (args)
-    except getopt.error, err:
-        usage_exit(err)
+            raise getopt.error('no arguments accepted, got %s' % (args))
+    except getopt.error:
+        usage_exit(sys.exc_info()[1])
     for opt, value in options:
         if opt in ('-h','-H','-?','--help'):
             usage_exit()
@@ -77,15 +132,16 @@ def parse_args(argv):
 
 
 def usage_exit(msg=None):
-    print __doc__
+    print(__doc__)
     if msg is None:
         rc = 251
     else:
-        print '\nError:', msg
+        print('\nError: %s' % msg)
         rc = 252
     sys.exit(rc)
 
 
+print(__name__)
 if __name__ == '__main__':
     docs, vrbst = parse_args(sys.argv[1:])
     tests = get_tests()
