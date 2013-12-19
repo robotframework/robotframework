7d687b2383ccd45fed0ea3f097f1b2b1edbfe662
Stefan Zimmermann <zimmermann.code@gmail.com>
2013-12-19 23:44:25 +0000

    [python3] _MarkupWriter: Better compatibility workarounds.
    --HG--
    extra : transplant_source : %CF%FF%FA%27%5D%AE%F8%D6%2B6%83%C8%C4%7E%F5U%3F%9E%8D%DC

--- src/robot/utils/markupwriters.py
+++ src/robot/utils/markupwriters.py
@@ -12,7 +12,12 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+import sys
+
 from .markuputils import html_escape, xml_escape, attribute_escape
+
+
+PY3 = sys.version_info[0] == 3
 
 
 class _MarkupWriter(object):
@@ -27,10 +32,14 @@ class _MarkupWriter(object):
             output file. If `None`, text will not be encoded.
         """
         if isinstance(output, basestring):
-            output = open(output, 'w')
+            if PY3:
+                output = open(output, 'w', encoding=encoding)
+            else:
+                output = open(output, 'w')
+        self._encode_output = not hasattr(output, 'encoding')
         self.output = output
-        self._line_separator = line_separator
         self._encoding = encoding
+        self._line_separator = self._encode(line_separator)
         self._preamble()
 
     def _preamble(self):
@@ -71,16 +80,12 @@ class _MarkupWriter(object):
         self.output.close()
 
     def _write(self, text, newline=False):
-        encoded_text = self._encode(text)
-        try:
-            self.output.write(encoded_text)
-        except TypeError: # Python 3
-            self.output.write(text)
+        self.output.write(self._encode(text))
         if newline:
             self.output.write(self._line_separator)
 
     def _encode(self, text):
-        return text.encode(self._encoding) if self._encoding else text
+        return text.encode(self._encoding) if self._encode_output else text
 
 
 class HtmlWriter(_MarkupWriter):
