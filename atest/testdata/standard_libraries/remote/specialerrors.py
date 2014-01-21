a4499b8ec3461127f5f6a9706df58c94349e5c23
Robot Framework Developers <robotframework@gmail.com>
2014-01-21 15:16:05 +0200

    Remote: Support continuable and fatal errors.
    Update issue 1628
    Owner: pekka.klarck
    Status: Started
    Implemented Remote library side of the new functionality.
    
    Final decision was to add new optional 'continuable' and 'fatal' keys to the
    result dictionary. If these keys are present, and the value is True, the
    situation will be reported to Robot accordingly. These values are only used if
    the keyword fails, and if both values are set then 'fatal' has precedence.

---
+++ atest/testdata/standard_libraries/remote/specialerrors.py
@@ -0,0 +1,20 @@
+import sys
+from remoteserver import DirectResultRemoteServer
+
+
+class SpecialErrors(object):
+
+    def continuable(self, message, traceback):
+        return self._special_error(message, traceback, continuable=True)
+
+    def fatal(self, message, traceback):
+        return self._special_error(message, traceback,
+                                   fatal='this wins', continuable=42)
+
+    def _special_error(self, message, traceback, continuable=False, fatal=False):
+        return {'status': 'FAIL', 'error': message, 'traceback': traceback,
+                'continuable': continuable, 'fatal': fatal}
+
+
+if __name__ == '__main__':
+    DirectResultRemoteServer(SpecialErrors(), *sys.argv[1:])
