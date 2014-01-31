6fd560d15fe1571e5a391f637503c07fc2235f18
Mikko Korpela <mikko.korpela@gmail.com>
2014-01-31 14:34:45 +0200

    Application: Public API for parsing command line arguments
    Update issue 1618
    Status: Started

--- src/robot/utils/application.py
+++ src/robot/utils/application.py
@@ -59,7 +59,7 @@ class Application(object):
 
     def _parse_arguments(self, cli_args):
         try:
-            options, arguments = self._ap.parse_args(cli_args)
+            options, arguments = self.parse_arguments(cli_args)
         except Information, msg:
             self._report_info(unicode(msg))
         except DataError, err:
@@ -67,6 +67,9 @@ class Application(object):
         else:
             self._logger.info('Arguments: %s' % ','.join(arguments))
             return options, arguments
+
+    def parse_arguments(self, cli_args):
+        return self._ap.parse_args(cli_args)
 
     def execute(self, *arguments, **options):
         with self._logging():
