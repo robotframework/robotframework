d226ae5d640c2788a81333af9e66314e241a20da
Pekka Klärck <peke@iki.fi>
2014-03-06 23:34:37 +0200

    Process: Explicit test sfor terminating/killing also child processes.
    This was already indectly tested by testing that processes running on a shell are terminated/killed.
    
    Also renamed and reorganized tests a little.

--- atest/testdata/standard_libraries/process/files/countdown.py
+++ atest/testdata/standard_libraries/process/files/countdown.py
@@ -1,11 +1,23 @@
 from __future__ import with_statement
+import subprocess
 import sys
 import time
 
-path = sys.argv[1]
-with open(path, 'w') as f:
-    for i in range(10, 0, -1):
-        f.write('%d\n' % i)
-        f.flush()
-        time.sleep(0.1)
-    f.write('BLASTOFF')
+
+def countdown(path):
+    with open(path, 'w') as f:
+        for i in range(10, 0, -1):
+            f.write('%d\n' % i)
+            f.flush()
+            time.sleep(0.1)
+        f.write('BLASTOFF')
+
+
+if __name__ == '__main__':
+    path = sys.argv[1]
+    children = int(sys.argv[2]) if len(sys.argv) == 3 else 0
+
+    if children:
+        subprocess.Popen([sys.executable, __file__, path, str(children-1)]).wait()
+    else:
+        countdown(path)
