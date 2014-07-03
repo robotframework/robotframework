8743d8d922c2b7e3bb67d64672d2fe69a761f6a0
Tatu Aalto <aalto.tatu@gmail.com>
2014-07-03 21:23:31 +0300

    Combined test suites, should hava done that in the first place

--- src/robot/libraries/String.py
+++ src/robot/libraries/String.py
@@ -55,7 +55,7 @@ class String(object):
         | Should Be Equal | ${str1} | abc |
         | Should Be Equal | ${str2} | 1a2c3d |
 
-        New in Robot Framework 2.8.6
+        New in Robot Framework 2.8.6.
         """
         return string.lower()
 
@@ -68,7 +68,7 @@ class String(object):
         | Should Be Equal | ${str1} | ABC |
         | Should Be Equal | ${str2} | 1A2C3D |
         
-        New in Robot Framework 2.8.6
+        New in Robot Framework 2.8.6.
         """
         return string.upper()   
 
