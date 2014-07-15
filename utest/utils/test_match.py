c67a56fdeadc0c355dd8ac8254c61ad416189fda
Guy Kisel <gkisel@truecar.com>
2014-07-15 11:30:46 -0700

    New pattern matching keywords in Collections
    For issue #1724

--- utest/utils/test_match.py
+++ utest/utils/test_match.py
@@ -24,6 +24,14 @@ class TestMatcher(unittest.TestCase):
         assert matcher.match('--Foo')
         assert not matcher.match('foo')
 
+    def test_regexp_matcher(self):
+        matcher = Matcher('F .*', ignore=['-'], caseless=False, spaceless=True,
+                          regexp=True)
+        assert matcher.pattern == 'F .*'
+        assert matcher.match('Foo')
+        assert matcher.match('--Foo')
+        assert not matcher.match('foo')
+
     def test_matches_with_string(self):
         for pattern in ['abc', 'ABC', '*', 'a*', '*C', 'a*c', '*a*b*c*', 'AB?',
                         '???', '?b*', '*abc', 'abc*', '*abc*']:
@@ -31,13 +39,30 @@ class TestMatcher(unittest.TestCase):
         for pattern in ['def', '?abc', '????', '*ed', 'b*']:
             self._matches_not('abc', pattern)
 
+    def test_regexp_matches_with_string(self):
+        for pattern in ['abc', 'ABC', '.*', 'a.*', '.*C', 'a.*c', '.*a.*b.*c.*',
+                        'AB.',
+                        '...', '.b.*', '.*abc', 'abc.*', '.*abc.*']:
+            self._matches('abc', pattern, regexp=True)
+        for pattern in ['def', '.abc', '....', '.*ed', 'b.*']:
+            self._matches_not('abc', pattern, regexp=True)
+
     def test_matches_with_multiline_string(self):
         for pattern in ['*', 'multi*string', 'multi?line?string', '*\n*']:
             self._matches('multi\nline\nstring', pattern, spaceless=False)
 
+    def test_regexp_matches_with_multiline_string(self):
+        for pattern in ['.*', 'multi.*string', 'multi.line.string', '.*\n.*']:
+            self._matches('multi\nline\nstring', pattern, spaceless=False,
+                          regexp=True)
+
     def test_matches_with_slashes(self):
         for pattern in ['a*','aa?b*','*c','?a?b?c']:
             self._matches('aa/b\\c', pattern)
+
+    def test_regexp_matches_with_slashes(self):
+        for pattern in ['a.*', 'aa.b.*', '.*c', '.a.b.c']:
+            self._matches('aa/b\\c', pattern, regexp=True)
 
     def test_matches_no_pattern(self):
         for string in ['foo', '', ' ', '      ', 'what ever',
@@ -45,8 +70,19 @@ class TestMatcher(unittest.TestCase):
                        'forw/slash/and\\back\\slash']:
             self._matches(string, string), string
 
+    def test_regexp_matches_no_pattern(self):
+        for string in ['foo', '', ' ', '      ', 'what ever']:
+            self._matches(string, string, regexp=True), string
+
     def test_match_any(self):
         matcher = Matcher('H?llo')
+        assert matcher.match_any(('Hello', 'world'))
+        assert matcher.match_any(['jam', 'is', 'hillo'])
+        assert not matcher.match_any(('no', 'match', 'here'))
+        assert not matcher.match_any(())
+
+    def test_regexp_match_any(self):
+        matcher = Matcher('H.llo', regexp=True)
         assert matcher.match_any(('Hello', 'world'))
         assert matcher.match_any(['jam', 'is', 'hillo'])
         assert not matcher.match_any(('no', 'match', 'here'))
@@ -68,20 +104,34 @@ class TestMultiMatcher(unittest.TestCase):
         assert matcher.match('..::FOO::..')
         assert not matcher.match('bar')
 
+    def test_match_regexp_pattern(self):
+        matcher = MultiMatcher(['xxx', 'f.*'], ignore='_:', regexp=True)
+        assert matcher.match('xxx')
+        assert matcher.match('foo')
+        assert matcher.match('__::FOO::__')
+        assert not matcher.match('bar')
+
     def test_do_not_match_when_no_patterns_by_default(self):
         assert not MultiMatcher().match('xxx')
 
     def test_configure_to_match_when_no_patterns(self):
         assert MultiMatcher(match_if_no_patterns=True).match('xxx')
+        assert MultiMatcher(match_if_no_patterns=True, regexp=True).match('xxx')
 
     def test_len(self):
         assert_equals(len(MultiMatcher()), 0)
         assert_equals(len(MultiMatcher([])), 0)
         assert_equals(len(MultiMatcher(['one', 'two'])), 2)
+        assert_equals(len(MultiMatcher(regexp=True)), 0)
+        assert_equals(len(MultiMatcher([], regexp=True)), 0)
+        assert_equals(len(MultiMatcher(['one', 'two'], regexp=True)), 2)
 
     def test_iter(self):
         assert_equals(tuple(MultiMatcher()), ())
         assert_equals(list(MultiMatcher(['1', 'xxx', '3'])), ['1', 'xxx', '3'])
+        assert_equals(tuple(MultiMatcher(regexp=True)), ())
+        assert_equals(list(MultiMatcher(['1', 'xxx', '3'], regexp=True)),
+                      ['1', 'xxx', '3'])
 
     def test_single_string_is_converted_to_list(self):
         matcher = MultiMatcher('one string')
@@ -89,8 +139,21 @@ class TestMultiMatcher(unittest.TestCase):
         assert not matcher.match('o')
         assert_equals(len(matcher), 1)
 
+    def test_regexp_single_string_is_converted_to_list(self):
+        matcher = MultiMatcher('one string', regexp=True)
+        assert matcher.match('one string')
+        assert not matcher.match('o')
+        assert_equals(len(matcher), 1)
+
     def test_match_any(self):
         matcher = MultiMatcher(['H?llo', 'w*'])
+        assert matcher.match_any(('Hi', 'world'))
+        assert matcher.match_any(['jam', 'is', 'hillo'])
+        assert not matcher.match_any(('no', 'match', 'here'))
+        assert not matcher.match_any(())
+
+    def test_regexp_match_any(self):
+        matcher = MultiMatcher(['H.llo', 'w.*'], regexp=True)
         assert matcher.match_any(('Hi', 'world'))
         assert matcher.match_any(['jam', 'is', 'hillo'])
         assert not matcher.match_any(('no', 'match', 'here'))
