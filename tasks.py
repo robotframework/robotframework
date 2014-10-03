58bc1ab61d73a99e2023787e319bc1a153c3add7
Jussi Malinen <jussi.ao.malinen@gmail.com>
2014-10-03 15:47:07 +0300

    Implemented the userguide updating

--- tasks.py
+++ tasks.py
@@ -6,6 +6,9 @@ and run `invoke --help` and `invode --list` for details how to execute tasks.
 
 import os
 import os.path
+import shutil
+import zipfile
+import re
 
 from invoke import task, run
 
@@ -13,7 +16,6 @@ from invoke import task, run
 def dryrun(command):
     print command
 
-run = dryrun
 
 
 assert os.getcwd() == os.path.dirname(__file__)
@@ -39,30 +41,62 @@ def add_docs(version):
     extract_ug(version)
     update_latest(version)
     update_index(version)
-    push_changes('Updated to version {}'.format(version))
+    push_changes(version)
 
 
 @task
 def copy_ug(version):
     """Copy User Guide from dist to correct location."""
+    target = 'robotframework-userguide-{}.zip'.format(version)
+    source = os.path.join('dist', target)
+    print "Copying {} to {} ".format(source, target)
+    shutil.copy(source, target)
 
 
 @task
 def extract_ug(version):
     """Extract User Guide package to 'x.y(.z)' directory."""
+    basename = 'robotframework-userguide-{}'.format(version)
+    source = '{}.zip'.format(basename)
+    if os.path.isdir(version):
+        print "Removing directory '{}'".format(version)
+        shutil.rmtree(version)
+    print "Extracting {}".format(source)
+    zipfile.ZipFile(source).extractall()
+    print "Renaming {} to {}".format(basename, version)
+    os.rename(basename, version)
 
 
 @task
 def update_latest(version):
     """Update 'latest' directory to point to now latest 'x.y(.z)' directory."""
+    print "Removing 'latest'"
+    shutil.rmtree('latest')
+    print "Copying '{}' to 'latest'".format(version)
+    shutil.copytree(version, 'latest')
 
 
 def update_index(version):
     """Add information about the new release index.html."""
+    contents = []
+    matcher = re.compile(r'(\s+)<option value="latest".+')
+    with open('index.html', 'r') as original:
+        contents = original.readlines()
+    with open('index.html', 'w') as out:
+        for row in contents:
+            match = matcher.match(row)
+            if match:
+                out.write(row)
+                out.write('{indent}<option value="{version}">{version}</option>\n'.format(indent=match.groups()[0], version=version))
+            else:
+                out.write(row)
+    print "Updated 'index.html' with links to {}".format(version)
 
 
 @task
-def push_changes(message, path='.'):
+def push_changes(version):
     """Commit and push changes."""
-    run("git commit -m '{}' {}".format(message, path))
-    run("git push")
+    print "\nRun the following commands:\n"
+    print "git add {} index.html latest".format(version)
+    print "git commit -m 'Updated to {}'".format(version)
+    print "git push"
