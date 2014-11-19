90c65c246bcc9cfb359d50c9fbaba3d63d097db7
Pekka Klärck <peke@iki.fi>
2014-11-20 01:05:22 +0200

    DateTime: Fixed epoch related tests that were incorrectly DST dependent

--- atest/testdata/standard_libraries/datetime/datesandtimes.py
+++ atest/testdata/standard_libraries/datetime/datesandtimes.py
@@ -2,4 +2,4 @@ from datetime import timedelta, datetime
 import time
 
 TIMEZONE = time.altzone if time.localtime().tm_isdst else time.timezone
-EPOCH = 1398375912.0 + TIMEZONE
+EPOCH = 1398375912.0 + time.altzone
