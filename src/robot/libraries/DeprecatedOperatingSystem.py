64ae3c9c07020226eea466110ab7288568f2b1f5
Pekka Klärck <peke@iki.fi>
2015-02-16 17:25:28 +0200

    Rename

--- src/robot/libraries/DeprecatedOperatingSystem.py
+++ src/robot/libraries/DeprecatedOperatingSystem.py
@@ -16,7 +16,7 @@ from .DeprecatedBuiltIn import deprecator
 from .OperatingSystem import OperatingSystem
 
 
-OPSYS = OperatingSystem()
+OS = OperatingSystem()
 
 
 class DeprecatedOperatingSystem(object):
@@ -24,29 +24,29 @@ class DeprecatedOperatingSystem(object):
 
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
 
-    delete_environment_variable = OPSYS.remove_environment_variable
-    environment_variable_is_set = OPSYS.environment_variable_should_be_set
-    environment_variable_is_not_set = OPSYS.environment_variable_should_not_be_set
+    delete_environment_variable = OS.remove_environment_variable
+    environment_variable_is_set = OS.environment_variable_should_be_set
+    environment_variable_is_not_set = OS.environment_variable_should_not_be_set
 
-    fail_unless_exists = OPSYS.should_exist
-    fail_if_exists = OPSYS.should_not_exist
-    fail_unless_file_exists = OPSYS.file_should_exist
-    fail_if_file_exists = OPSYS.file_should_not_exist
-    fail_unless_dir_exists = OPSYS.directory_should_exist
-    fail_if_dir_exists = OPSYS.directory_should_not_exist
-    fail_unless_dir_empty = OPSYS.directory_should_be_empty
-    fail_if_dir_empty = OPSYS.directory_should_not_be_empty
-    fail_unless_file_empty = OPSYS.file_should_be_empty
-    fail_if_file_empty = OPSYS.file_should_not_be_empty
+    fail_unless_exists = OS.should_exist
+    fail_if_exists = OS.should_not_exist
+    fail_unless_file_exists = OS.file_should_exist
+    fail_if_file_exists = OS.file_should_not_exist
+    fail_unless_dir_exists = OS.directory_should_exist
+    fail_if_dir_exists = OS.directory_should_not_exist
+    fail_unless_dir_empty = OS.directory_should_be_empty
+    fail_if_dir_empty = OS.directory_should_not_be_empty
+    fail_unless_file_empty = OS.file_should_be_empty
+    fail_if_file_empty = OS.file_should_not_be_empty
 
-    empty_dir = OPSYS.empty_directory
-    remove_dir = OPSYS.remove_directory
-    copy_dir = OPSYS.copy_directory
-    move_dir = OPSYS.move_directory
-    create_dir = OPSYS.create_directory
-    list_dir = OPSYS.list_directory
-    list_files_in_dir = OPSYS.list_files_in_directory
-    list_dirs_in_dir = OPSYS.list_directories_in_directory
-    count_items_in_dir = OPSYS.count_items_in_directory
-    count_files_in_dir = OPSYS.count_files_in_directory
-    count_dirs_in_dir = OPSYS.count_directories_in_directory
+    empty_dir = OS.empty_directory
+    remove_dir = OS.remove_directory
+    copy_dir = OS.copy_directory
+    move_dir = OS.move_directory
+    create_dir = OS.create_directory
+    list_dir = OS.list_directory
+    list_files_in_dir = OS.list_files_in_directory
+    list_dirs_in_dir = OS.list_directories_in_directory
+    count_items_in_dir = OS.count_items_in_directory
+    count_files_in_dir = OS.count_files_in_directory
+    count_dirs_in_dir = OS.count_directories_in_directory
