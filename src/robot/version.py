f8fd7f85d6bcf62871019ca3e179bf6ff666c513
Jussi Malinen <jussi.ao.malinen@gmail.com>
2015-04-10 15:13:29 +0300

    Updated version to 2.9a1

--- src/robot/version.py
+++ src/robot/version.py
@@ -17,7 +17,7 @@ import sys
 
 # Version number typically updated by running `invoke set_version <version>`.
 # Run `invoke --help set_version` or see tasks.py for details.
-VERSION = '2.9.dev20150202'
+VERSION = '2.9a1'
 
 
 def get_version(naked=False):
