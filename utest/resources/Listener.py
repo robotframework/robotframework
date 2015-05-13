e947c493671e7c87f8fa3b787abe5ae670aa8bef
Wojciech Kowaluk <wojciech.kowaluk@nokia.com>
2015-05-13 12:30:54 +0200

    Unregister listeners on Output.close

---
+++ utest/resources/Listener.py
@@ -0,0 +1,14 @@
+import sys
+
+
+class Listener:
+    ROBOT_LISTENER_API_VERSION = 2
+
+    def __init__(self, name):
+        self.name = name
+
+    def start_suite(self, name, attrs):
+        sys.__stdout__.write("[from listener {}]\n".format(self.name))
+
+    def close(self):
+        sys.__stdout__.write("[listener close]\n")
