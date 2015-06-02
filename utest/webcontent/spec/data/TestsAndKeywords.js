f2436bff91ac2330364bc7e5aab83d866ee8bb32
Jussi Malinen <jussi.ao.malinen@gmail.com>
2015-06-02 13:35:47 +0300

    regen

--- utest/webcontent/spec/data/TestsAndKeywords.js
+++ utest/webcontent/spec/data/TestsAndKeywords.js
@@ -1,20 +1,20 @@
 window.testsAndKeywordsOutput = {};
 
-window.testsAndKeywordsOutput["suite"] = [1,2,3,0,[],[1,0,37],[],[[4,0,1,0,[],[1,25,4],[[0,5,0,0,0,0,0,0,[1,25,1],[[0,6,7,0,8,0,0,0,[1,25,1],[],[]]],[]],[0,9,0,0,0,0,0,0,[1,26,1],[[0,6,7,0,8,0,0,0,[1,26,1],[],[]]],[]],[0,10,0,0,0,0,0,0,[1,27,1],[[0,6,7,0,8,0,0,0,[1,27,1],[],[]]],[]],[0,11,0,0,0,0,0,0,[1,28,0],[[0,6,7,0,8,0,0,0,[1,28,0],[],[]]],[]]]],[12,0,1,0,[],[1,29,1],[[0,6,7,0,8,0,0,0,[1,29,1],[],[]]]],[13,0,1,0,[],[1,30,1],[[0,6,7,0,8,0,0,0,[1,31,0],[],[]]]],[14,0,1,0,[],[1,35,1],[[0,6,7,0,8,0,0,0,[1,36,0],[],[]]]]],[],[4,4,4,4]];
+window.testsAndKeywordsOutput["suite"] = [1,2,3,0,[],[1,0,41],[],[[4,0,1,0,[],[1,26,4],[[0,5,0,0,0,0,0,0,[1,27,1],[[0,6,7,0,8,0,0,0,[1,27,1],[],[]]],[]],[0,9,0,0,0,0,0,0,[1,28,1],[[0,6,7,0,8,0,0,0,[1,28,0],[],[]]],[]],[0,10,0,0,0,0,0,0,[1,29,0],[[0,6,7,0,8,0,0,0,[1,29,0],[],[]]],[]],[0,11,0,0,0,0,0,0,[1,30,0],[[0,6,7,0,8,0,0,0,[1,30,0],[],[]]],[]]]],[12,0,1,0,[],[1,31,0],[[0,6,7,0,8,0,0,0,[1,31,0],[],[]]]],[13,0,1,0,[],[1,32,0],[[0,6,7,0,8,0,0,0,[1,32,0],[],[]]]],[14,0,1,0,[],[1,39,1],[[0,6,7,0,8,0,0,0,[1,39,1],[],[]]]]],[],[4,4,4,4]];
 
 window.testsAndKeywordsOutput["strings"] = [];
 
 window.testsAndKeywordsOutput["strings"] = window.testsAndKeywordsOutput["strings"].concat(["*","*TestsAndKeywords","*/Users/jmalinen/Documents/workspace/robotframework/utest/webcontent/spec/data/TestsAndKeywords.txt","*utest/webcontent/spec/data/TestsAndKeywords.txt","*Test 1","*kw1","*No Operation","*BuiltIn","*<p>Does absolutely nothing.\x3c/p>","*kw2","*kw3","*kw4","*Test 2","*Test 3","*Test 4"]);
 
-window.testsAndKeywordsOutput["generatedTimestamp"] = "20150526 14:11:13 GMT +03:00";
+window.testsAndKeywordsOutput["generatedTimestamp"] = "20150602 13:21:39 GMT +03:00";
 
 window.testsAndKeywordsOutput["errors"] = [];
 
 window.testsAndKeywordsOutput["stats"] = [[{"elapsed":"00:00:00","fail":0,"label":"Critical Tests","pass":4},{"elapsed":"00:00:00","fail":0,"label":"All Tests","pass":4}],[],[{"elapsed":"00:00:00","fail":0,"id":"s1","label":"TestsAndKeywords","name":"TestsAndKeywords","pass":4}]];
 
-window.testsAndKeywordsOutput["generatedMillis"] = -580;
+window.testsAndKeywordsOutput["generatedMillis"] = -863;
 
-window.testsAndKeywordsOutput["baseMillis"] = 1432638673580;
+window.testsAndKeywordsOutput["baseMillis"] = 1433240499863;
 
 window.settings = {"background":{"fail":"DeepPink"},"logURL":"log.html","reportURL":"report.html"};
 
