885e70a4e0f875312b9d7efc8b58a7b6865c4e5b
Jussi Malinen <jussi.ao.malinen@gmail.com>
2015-06-22 19:49:41 +0300

    Use setuptools always when available. Fixes #2033
    Fixes a deprecation warning when uninstalling with pip an installation done with "python setup.py install".

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ import os
 from os.path import abspath, join, dirname
 from distutils.core import setup
 
-if 'develop' in sys.argv or 'bdist_wheel' in sys.argv:
-    import setuptools    # support setuptools development mode and wheels
+try:
+    import setuptools    # use setuptools when available
+except ImportError:
+    pass
 
 CURDIR = dirname(abspath(__file__))
 
