bb0b6c106936178c2e72d2094b9793aab5f60c77
Pekka Klärck <peke@iki.fi>
2015-06-29 12:46:43 +0300

    BuiltIn: Automatic variables in expression require $ prefix.
    Related to #2040. Documentation still needs to be updated.

--- src/robot/variables/__init__.py
+++ src/robot/variables/__init__.py
@@ -19,6 +19,7 @@ This package is mainly for internal usage.
 
 from .assigner import VariableAssigner
 from .isvar import contains_var, is_var, is_scalar_var, is_list_var, is_dict_var
+from .notfound import variable_not_found
 from .scopes import VariableScopes
 from .splitter import VariableSplitter, VariableIterator
 from .tablesetter import VariableTableValue, DictVariableTableValue
