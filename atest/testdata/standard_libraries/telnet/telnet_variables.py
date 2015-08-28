fdffc4f4860b292283806ba7e53710b19b8dd954
Jussi Malinen <jussi.ao.malinen@gmail.com>
2015-08-28 15:38:00 +0300

    change telnet to use platform.uname instead of os.uname, because that works also on jython and windows

--- atest/testdata/standard_libraries/telnet/telnet_variables.py
+++ atest/testdata/standard_libraries/telnet/telnet_variables.py
@@ -1,11 +1,10 @@
-import os
+import platform
 
 # We assume that prompt is PS1='\u@\h \W \$ '
 HOST = 'localhost'
 USERNAME = 'test'
 PASSWORD = 'test'
 PROMPT = '$ '
-# TODO: os.uname does not exist on jython. Tests should pass otherways.
-FULL_PROMPT = '%s@%s ~ $ ' % (USERNAME, os.uname()[1])
+FULL_PROMPT = '%s@%s ~ $ ' % (USERNAME, platform.uname()[1])
 PROMPT_START = '%s@' % USERNAME
 HOME = '/home/%s' % USERNAME
