2806b34d6ee6c31f55d4759671be26913eec3906
Pekka Klärck <peke@iki.fi>
2015-11-02 01:11:52 +0200

    version bump to celebrate closing py3 compatibility

--- src/robot/version.py
+++ src/robot/version.py
@@ -17,7 +17,7 @@ import sys
 
 # Version number typically updated by running `invoke set_version <version>`.
 # Run `invoke --help set_version` or see tasks.py for details.
-VERSION = '2.9.3.dev20151009'
+VERSION = '2.9.3.dev20151102'
 
 
 def get_version(naked=False):
