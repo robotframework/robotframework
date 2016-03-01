robotframework/robotframework

commit 233d9a4b8e60add49406740987f015d2c6f01ea4

Tuning utils.printable_name.
- Fixed `printable_name('42FOO', code_style=True)` to yield '42 FOO'
  and not '42FOO'. '42Foo' already yielded '42 Foo'.
- Small performance enhancements.

src/robot/utils/misc.py
@@ -67,33 +67,35 @@ def printable_name(string, code_style=False):
     parts = string.split()
     if not parts:
         return ''
-    if code_style and len(parts) == 1:
-        parts = _splitCamelCaseString(parts[0])
-    return ' '.join(part[0].upper() + part[1:] for part in parts if part != '')
+    if code_style and len(parts) == 1 \
+            and not (string.isalpha() and string.islower()):
+        parts = _camelCaseSplit(list(string))
+    return ' '.join(part[0].upper() + part[1:] for part in parts)
 
 
-def _splitCamelCaseString(string):
-    parts = []
-    current_part = []
-    string = ' ' + string + ' '  # extra spaces make going through string easier
-    for i in range(1, len(string)-1):
-        # on 1st/last round prev/next is ' ' and char is 1st/last real char
-        prev, char, next = string[i-1:i+2]
-        if _isWordBoundary(prev, char, next):
-            parts.append(''.join(current_part))
-            current_part = [char]
+def _camelCaseSplit(chars):
+    token = []
+    for prev, char, next in zip([''] + chars, chars, chars[1:] + ['']):
+        if _isCamelCaseBoundary(prev, char, next):
+            if token:
+                yield ''.join(token)
+            token = [char]
         else:
-            current_part.append(char)
-    parts.append(''.join(current_part))   # append last part
-    return parts
+            token.append(char)
+    if token:
+        yield ''.join(token)
 
 
-def _isWordBoundary(prev, char, next):
+def _isCamelCaseBoundary(prev, char, next):
+    if prev.isdigit():
+        return not char.isdigit()
+    if not prev.isalpha():
+        return False
     if char.isupper():
-        return (prev.islower() or next.islower()) and prev.isalnum()
+        return not (prev.isupper() and (not next or next.isupper()))
     if char.isdigit():
-        return prev.isalpha()
-    return prev.isdigit()
+        return not prev.isdigit()
+    return False
 
 
 def plural_or_not(item):

utest/utils/test_misc.py
@@ -75,6 +75,7 @@ class TestMiscUtils(unittest.TestCase):
                          ('camelCaseName', 'CamelCaseName'),
                          ('with89numbers', 'With89numbers'),
                          ('with 89 numbers', 'With 89 Numbers'),
+                         ('with 89_numbers', 'With 89_numbers'),
                          ('', '')]:
             assert_equal(printable_name(inp), exp)
 
@@ -84,12 +85,17 @@ class TestMiscUtils(unittest.TestCase):
                          ('under_score_name', 'Under Score Name'),
                          ('under_score and spaces', 'Under Score And Spaces'),
                          ('miXed_CAPS_nAMe', 'MiXed CAPS NAMe'),
+                         ('with 89_numbers', 'With 89 Numbers'),
                          ('camelCaseName', 'Camel Case Name'),
-                         ('camelCaseWithDigit1', 'Camel Case With Digit 1'),
-                         ('name42WithNumbers666', 'Name 42 With Numbers 666'),
-                         ('12more34numbers', '12 More 34 Numbers'),
                          ('mixedCAPSCamelName', 'Mixed CAPS Camel Name'),
+                         ('camelCaseWithDigit1', 'Camel Case With Digit 1'),
+                         ('teamX', 'Team X'),
+                         ('name42WithNumbers666', 'Name 42 With Numbers 666'),
+                         ('name42WITHNumbers666', 'Name 42 WITH Numbers 666'),
+                         ('12more34numbers', '12 More 34 Numbers'),
                          ('foo-bar', 'Foo-bar'),
+                         ('Foo-b:a;r!', 'Foo-b:a;r!'),
+                         ('Foo-B:A;R!', 'Foo-B:A;R!'),
                          ('', '')]:
             assert_equal(printable_name(inp, code_style=True), exp)
 

commit 53dd38a2fd0284b2355ab5e3ef53937262fb4307

Fixing utils.printable_name after tuning it earlier.
Changes done in revision 233d9a4b8e60add49406740987f015d2c6f01ea4
broke other tests. Fixed that and added related unit tests for
printable_name itself.

src/robot/utils/misc.py
@@ -69,7 +69,7 @@ def printable_name(string, code_style=False):
         return ''
     if code_style and len(parts) == 1 \
             and not (string.isalpha() and string.islower()):
-        parts = _camelCaseSplit(list(string))
+        parts = _camelCaseSplit(list(parts[0]))
     return ' '.join(part[0].upper() + part[1:] for part in parts)
 
 
@@ -89,10 +89,8 @@ def _camelCaseSplit(chars):
 def _isCamelCaseBoundary(prev, char, next):
     if prev.isdigit():
         return not char.isdigit()
-    if not prev.isalpha():
-        return False
     if char.isupper():
-        return not (prev.isupper() and (not next or next.isupper()))
+        return next.islower() or prev.isalpha() and not prev.isupper()
     if char.isdigit():
         return not prev.isdigit()
     return False

utest/utils/test_misc.py
@@ -70,6 +70,8 @@ class TestMiscUtils(unittest.TestCase):
                          ('ALLCAPS', 'ALLCAPS'),
                          ('name with spaces', 'Name With Spaces'),
                          ('more   spaces', 'More Spaces'),
+                         ('  leading and trailing  ', 'Leading And Trailing'),
+                         ('  12number34  ', '12number34'),
                          ('Cases AND spaces', 'Cases AND Spaces'),
                          ('under_Score_name', 'Under_Score_name'),
                          ('camelCaseName', 'CamelCaseName'),
@@ -82,8 +84,12 @@ class TestMiscUtils(unittest.TestCase):
     def test_printable_name_with_code_style(self):
         for inp, exp in [('simple', 'Simple'),
                          ('ALLCAPS', 'ALLCAPS'),
+                         ('name with spaces', 'Name With Spaces'),
+                         ('    more   spaces    ', 'More Spaces'),
                          ('under_score_name', 'Under Score Name'),
-                         ('under_score and spaces', 'Under Score And Spaces'),
+                         ('under__score and spaces', 'Under Score And Spaces'),
+                         ('__leading and trailing_ __', 'Leading And Trailing'),
+                         ('__12number34__', '12 Number 34'),
                          ('miXed_CAPS_nAMe', 'MiXed CAPS NAMe'),
                          ('with 89_numbers', 'With 89 Numbers'),
                          ('camelCaseName', 'Camel Case Name'),
@@ -93,6 +99,12 @@ class TestMiscUtils(unittest.TestCase):
                          ('name42WithNumbers666', 'Name 42 With Numbers 666'),
                          ('name42WITHNumbers666', 'Name 42 WITH Numbers 666'),
                          ('12more34numbers', '12 More 34 Numbers'),
+                         ('2KW', '2 KW'),
+                         ('KW2', 'KW 2'),
+                         ('xKW', 'X KW'),
+                         ('KWx', 'K Wx'),
+                         (':KW', ':KW'),
+                         ('KW:', 'KW:'),
                          ('foo-bar', 'Foo-bar'),
                          ('Foo-b:a;r!', 'Foo-b:a;r!'),
                          ('Foo-B:A;R!', 'Foo-B:A;R!'),