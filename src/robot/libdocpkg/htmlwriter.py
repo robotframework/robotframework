ff0d0a1b6ea2e8c0f94904554f1c9090f79f427d
Pekka Klärck <peke@iki.fi>
2016-09-23 13:14:57 +0300

    Libdoc: Syntax highlighting when using reST. #2448
    Added Pygments styles and configured docutils to be compatible with
    them.  Embedding Pygments styles to all Libdoc outputs may be a bit
    questionable, but they are small enough that I don't think it is a
    real problem. We may also later add syntax highlighting to Libdoc's
    default syntax too.
    
    Also made background color used by examples etc. in Libdoc outputs and
    also in logs and reports a little lighter. Syntax highlighting looks
    better on a little lighter background and this background color is
    used also in the User Guide.

--- src/robot/libdocpkg/htmlwriter.py
+++ src/robot/libdocpkg/htmlwriter.py
@@ -159,7 +159,8 @@ class DocToHtml(object):
             from docutils.core import publish_parts
         except ImportError:
             raise DataError("reST format requires 'docutils' module to be installed.")
-        parts = publish_parts(doc, writer_name='html')
+        parts = publish_parts(doc, writer_name='html',
+                              settings_overrides={'syntax_highlight': 'short'})
         return self._format_html(parts['html_body'])
 
     def __call__(self, doc):
