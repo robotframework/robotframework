294e9919eeb97848d2e0441c30ce9e94f0b0da48
Pekka Klärck <peke@iki.fi>
2017-01-03 11:31:14 +0200

    Fix including __init__ when using --suite #2515
    Also renamed and reorganized arguments to some of the internal methos.

--- src/robot/parsing/populators.py
+++ src/robot/parsing/populators.py
@@ -105,15 +105,16 @@ class FromDirectoryPopulator(object):
     ignored_dirs = ('CVS',)
 
     def populate(self, path, datadir, include_suites=None,
-                 warn_on_skipped=False, extensions=None, recurse=True):
+                 warn_on_skipped=False, include_extensions=None, recurse=True):
         LOGGER.info("Parsing test data directory '%s'" % path)
         include_suites = self._get_include_suites(path, include_suites or [])
-        init_file, children = self._get_children(path, extensions, include_suites)
+        init_file, children = self._get_children(path, include_extensions,
+                                                 include_suites)
         if init_file:
             self._populate_init_file(datadir, init_file)
         if recurse:
-            self._populate_children(datadir, children, include_suites,
-                                    extensions, warn_on_skipped)
+            self._populate_children(datadir, children, include_extensions,
+                                    include_suites, warn_on_skipped)
 
     def _populate_init_file(self, datadir, init_file):
         datadir.initfile = init_file
@@ -122,11 +123,11 @@ class FromDirectoryPopulator(object):
         except DataError as err:
             LOGGER.error(err.message)
 
-    def _populate_children(self, datadir, children, include_suites, extensions,
-                           warn_on_skipped):
+    def _populate_children(self, datadir, children, include_extensions,
+                           include_suites, warn_on_skipped):
         for child in children:
             try:
-                datadir.add_child(child, include_suites, extensions,
+                datadir.add_child(child, include_suites, include_extensions,
                                   warn_on_skipped)
             except DataError as err:
                 self._log_failed_parsing("Parsing data source '%s' failed: %s"
@@ -159,11 +160,12 @@ class FromDirectoryPopulator(object):
         name = os.path.basename(os.path.normpath(path))
         return self._is_in_included_suites(name, incl_suites)
 
-    def _get_children(self, dirpath, extensions, incl_suites):
+    def _get_children(self, dirpath, incl_extensions, incl_suites):
         init_file = None
         children = []
-        for path, base in self._list_dir(dirpath, extensions, incl_suites):
-            if self._is_init_file(path, base):
+        for path, is_init_file in self._list_dir(dirpath, incl_extensions,
+                                                 incl_suites):
+            if is_init_file:
                 if not init_file:
                     init_file = path
                 else:
@@ -172,36 +174,42 @@ class FromDirectoryPopulator(object):
                 children.append(path)
         return init_file, children
 
-    def _list_dir(self, dir_path, extensions, incl_suites):
+    def _list_dir(self, dir_path, incl_extensions, incl_suites):
         # os.listdir returns Unicode entries when path is Unicode
         names = os.listdir(unic(dir_path))
         for name in sorted(names, key=lambda item: item.lower()):
             name = unic(name)  # needed to handle nfc/nfd normalization on OSX
+            path = os.path.join(dir_path, name)
             base, ext = os.path.splitext(name)
             ext = ext[1:].lower()
-            path = os.path.join(dir_path, name)
-            if self._is_included(path, base, ext, extensions, incl_suites):
-                yield path, base
+            if self._is_init_file(path, base, ext, incl_extensions):
+                yield path, True
+            elif self._is_included(path, base, ext, incl_extensions, incl_suites):
+                yield path, False
             else:
                 LOGGER.info("Ignoring file or directory '%s'." % name)
 
-    def _is_included(self, path, base, ext, extensions, incl_suites):
-        if base.startswith(self.ignored_prefixes) and base.lower() != '__init__':
+    def _is_init_file(self, path, base, ext, incl_extensions):
+        return (base.lower() == '__init__' and
+                self._extension_is_accepted(ext, incl_extensions) and
+                os.path.isfile(path))
+
+    def _extension_is_accepted(self, ext, incl_extensions):
+        if incl_extensions:
+            return ext in incl_extensions
+        return ext in READERS
+
+    def _is_included(self, path, base, ext, incl_extensions, incl_suites):
+        if base.startswith(self.ignored_prefixes):
             return False
         if os.path.isdir(path):
             return base not in self.ignored_dirs or ext
-        if extensions and ext not in extensions:
-            return False
-        if ext not in READERS:
+        if not self._extension_is_accepted(ext, incl_extensions):
             return False
         return self._is_in_included_suites(base, incl_suites)
 
     def _is_in_included_suites(self, name, incl_suites):
         return not incl_suites or incl_suites.match(self._split_prefix(name))
-
-    def _is_init_file(self, path, base):
-        # TODO: Should only support `__init__`, not `__INIT__`.
-        return os.path.isfile(path) and base.lower() == '__init__'
 
     def _split_prefix(self, name):
         return name.split('__', 1)[-1]
