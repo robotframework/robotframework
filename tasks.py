cb909c315dd68e0a9c572fe88c534b68e5138c76
Pekka Klärck <peke@iki.fi>
2017-02-13 23:54:06 +0200

    tasks.py: Invoke 0.13+ style, print_function

--- tasks.py
+++ tasks.py
@@ -4,30 +4,21 @@ Executed by Invoke <http://pyinvoke.org>. Install it with `pip install invoke`
 and run `invoke --help` and `invode --list` for details how to execute tasks.
 """
 
+from __future__ import print_function
 import os
 import os.path
 import shutil
 import zipfile
 import re
 
-from invoke import task, run
+from invoke import task
 
 
 assert os.getcwd() == os.path.dirname(__file__)
 
 
-@task(default=True)
-def help():
-    """Show help, basically an alias for --help.
-
-    This task can be removed once the fix to this issue is released:
-    https://github.com/pyinvoke/invoke/issues/180
-    """
-    run('invoke --help')
-
-
 @task
-def add_docs(version, push=False):
+def add_docs(ctx, version, push=False):
     """Add documentation of given version.
 
     Uses other tasks to do everything.
@@ -41,54 +32,52 @@ def add_docs(version, push=False):
     if not is_preview(version):
         update_latest(version)
     update_index(version)
-    add_changes(version)
+    add_changes(ctx, version)
     if push:
-        push_changes(version)
+        push_changes(ctx, version)
 
 
 def is_preview(version):
     return any(c not in '1234567890.' for c in version)
 
 
-@task
 def copy_ug(version):
     """Copy User Guide from dist to correct location."""
     target = 'robotframework-userguide-{}.zip'.format(version)
     source = os.path.join('dist', target)
-    print "Copying {} to {} ".format(source, target)
+    print("Copying {} to {} ".format(source, target))
     shutil.copy(source, target)
 
 
-@task
 def extract_ug(version):
     """Extract User Guide package to 'x.y(.z)' directory."""
     basename = 'robotframework-userguide-{}'.format(version)
     source = '{}.zip'.format(basename)
     if os.path.isdir(version):
-        print "Removing directory '{}'".format(version)
+        print("Removing directory '{}'".format(version))
         shutil.rmtree(version)
-    print "Extracting {}".format(source)
+    print("Extracting {}".format(source))
     zipfile.ZipFile(source).extractall()
-    print "Renaming {} to {}".format(basename, version)
+    print("Renaming {} to {}".format(basename, version))
     os.rename(basename, version)
 
 
-@task
 def update_latest(version):
     """Update 'latest' directory to point to now latest 'x.y(.z)' directory."""
-    print "Removing 'latest'"
+    print("Removing 'latest'")
     shutil.rmtree('latest')
-    print "Copying '{}' to 'latest'".format(version)
+    print("Copying '{}' to 'latest'".format(version))
     shutil.copytree(version, 'latest')
-    print "Removing 'robotframework-userguide-latest.zip'"
+    print("Removing 'robotframework-userguide-latest.zip'")
     os.remove('robotframework-userguide-latest.zip')
-    print "Copying 'robotframework-userguide-{}.zip' to 'robotframework-userguide-latest.zip'".format(version)
-    shutil.copy('robotframework-userguide-{}.zip'.format(version), 'robotframework-userguide-latest.zip')
+    print("Copying 'robotframework-userguide-{}.zip' to "
+          "'robotframework-userguide-latest.zip'".format(version))
+    shutil.copy('robotframework-userguide-{}.zip'.format(version),
+                'robotframework-userguide-latest.zip')
 
 
 def update_index(version):
     """Add information about the new release index.html."""
-    contents = []
     preview_matcher = re.compile(r'(\s+)<option class="preview".+')
     latest_matcher = re.compile(r'(\s+)<option value="latest".+')
     preview = is_preview(version)
@@ -101,23 +90,26 @@ def update_index(version):
             match = latest_matcher.match(row)
             if match:
                 if preview:
-                    out.write('{indent}<option class="preview" value="{version}">{version}</option>\n'.format(indent=match.groups()[0], version=version))
+                    out.write('{indent}<option class="preview" value="{version}">{version}</option>\n'
+                              .format(indent=match.groups()[0], version=version))
                 out.write(row)
                 if not preview:
-                    out.write('{indent}<option value="{version}">{version}</option>\n'.format(indent=match.groups()[0], version=version))
+                    out.write('{indent}<option value="{version}">{version}</option>\n'
+                              .format(indent=match.groups()[0], version=version))
             else:
                 out.write(row)
-    print "Updated 'index.html' with links to {}".format(version)
+    print("Updated 'index.html' with links to {}".format(version))
 
-@task
-def add_changes(version):
+
+def add_changes(ctx, version):
     """Adds changes to git."""
-    print "Staging files to git."
-    run("git add {0} robotframework-userguide-{0}.zip index.html latest robotframework-userguide-latest.zip".format(version))
+    print("Staging files to git.")
+    ctx.run("git add {0} robotframework-userguide-{0}.zip index.html latest "
+            "robotframework-userguide-latest.zip".format(version))
 
-@task
-def push_changes(version):
+
+def push_changes(ctx, version):
     """Commit and push changes."""
-    print "Pushing changes to gh-pages."
-    run("git commit -m 'Updated to {}'".format(version))
-    run("git push")
+    print("Pushing changes to gh-pages.")
+    ctx.run("git commit -m 'Updated to {}'".format(version))
+    ctx.run("git push")
