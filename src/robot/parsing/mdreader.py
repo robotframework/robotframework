0dea71bf20315c237a9f6486625e1009af820bbd
vairamsvsjdo <vairam.svs@jaydevops.com>
2017-06-21 02:19:56 -0700

    class definition updated.

--- src/robot/parsing/mdreader.py
+++ src/robot/parsing/mdreader.py
@@ -19,32 +19,29 @@ from robot.utils import Utf8Reader
 from .txtreader import TxtReader
 
 
-def MarkDownReader():
-    class MarkDownReader(object):
-        def __init__(self):
-            self.robot_lines = []
-            self.robot_data = ''
+class MarkDownReader(object):
+    def __init__(self):
+        self.robot_lines = []
+        self.robot_data = ''
 
-        def robotize(self, md_file):
-            try:
-                include_line = False
-                for line in Utf8Reader(md_file).readlines():
-                    if not include_line:
-                        include_line = (line.strip().lower() ==
-                                        "```robotframework")
-                    elif line.strip() == "```":
-                        include_line = False
-                    else:
-                        self.robot_lines.append(line)
-                self.robot_data = ''.join(self.robot_lines)
-            finally:
-                return self.robot_data
+    def robotize(self, md_file):
+        try:
+            include_line = False
+            for line in Utf8Reader(md_file).readlines():
+                if not include_line:
+                    include_line = (line.strip().lower() ==
+                                    "```robotframework")
+                elif line.strip() == "```":
+                    include_line = False
+                else:
+                    self.robot_lines.append(line)
+            self.robot_data = ''.join(self.robot_lines)
+        finally:
+            return self.robot_data
 
-        def read(self, md_file, rawdata):
-            return self._read_text(self.robotize(md_file), rawdata)
+    def read(self, md_file, rawdata):
+        return self._read_text(self.robotize(md_file), rawdata)
 
-        def _read_text(self, data, rawdata):
-            txtfile = BytesIO(data.encode('UTF-8'))
-            return TxtReader().read(txtfile, rawdata)
-
-    return MarkDownReader()
+    def _read_text(self, data, rawdata):
+        txtfile = BytesIO(data.encode('UTF-8'))
+        return TxtReader().read(txtfile, rawdata)
