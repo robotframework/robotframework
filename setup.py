3d1a79107c90ee93041f7218915a49e115c82db0
Hugo <hugovk@users.noreply.github.com>
2017-10-14 12:41:52 +0300

    Update classifiers

--- setup.py
+++ setup.py
@@ -31,7 +31,11 @@ Development Status :: 5 - Production/Stable
 License :: OSI Approved :: Apache Software License
 Operating System :: OS Independent
 Programming Language :: Python :: 2
+Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
 Programming Language :: Python :: Implementation :: CPython
 Programming Language :: Python :: Implementation :: Jython
 Programming Language :: Python :: Implementation :: IronPython
