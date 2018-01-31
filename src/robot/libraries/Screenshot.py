f3c253619c42676cd14a6156e818fff6d55d46e7
teddy12090 <teddy12090@gmail.com>
2018-01-31 21:34:55 +0800

    using `wx.Bitmap` instead of `wx.EmptyBitmap` when wxpython version is greater and equal than 4

--- src/robot/libraries/Screenshot.py
+++ src/robot/libraries/Screenshot.py
@@ -346,7 +346,10 @@ class ScreenshotTaker(object):
             self._wx_app_reference = wx.App(False)
         context = wx.ScreenDC()
         width, height = context.GetSize()
-        bitmap = wx.Bitmap(width, height, -1)
+        if wx.__version__ >= '4':
+            bitmap = wx.Bitmap(width, height, -1)
+        else:
+            bitmap = wx.EmptyBitmap(width, height, -1)
         memory = wx.MemoryDC()
         memory.SelectObject(bitmap)
         memory.Blit(0, 0, width, height, context, -1, -1)
