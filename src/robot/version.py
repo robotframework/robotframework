fd0d0b9f829378269a19912f5e4ca05bbb30ad8d
Pekka Klärck <peke@iki.fi>
2018-03-30 21:14:40 +0300

    Updated version to 3.0.3rc1

--- src/robot/version.py
+++ src/robot/version.py
@@ -18,7 +18,7 @@ import sys
 
 # Version number typically updated by running `invoke set-version <version>`.
 # Run `invoke --help set-version` or see tasks.py for details.
-VERSION = '3.0.3.dev20180315'
+VERSION = '3.0.3rc1'
 
 
 def get_version(naked=False):
