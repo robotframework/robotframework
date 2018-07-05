d25e88bf1c0304d69510af1685db6116ea254179
Pekka Klärck <peke@iki.fi>
2018-07-05 21:28:48 +0300

    tidy/whitespace

--- atest/testdata/libdoc/module.py
+++ atest/testdata/libdoc/module.py
@@ -14,6 +14,7 @@ def keyword(a1='d', *a2):
     See `get hello` for details"""
     pass
 
+
 def get_hello():
     """Get the intialization variables
 
@@ -21,16 +22,20 @@ def get_hello():
     and `introduction` for introduction"""
     return 'foo'
 
+
 def non_ascii_doc():
     u"""Hyv\u00E4\u00E4 y\u00F6t\u00E4.
 
     \u0421\u043F\u0430\u0441\u0438\u0431\u043E!"""
 
+
 def non_ascii_doc_with_bytes():
     """Hyv\xE4\xE4 y\xF6t\xE4."""
 
+
 def non_ascii_doc_with_declared_utf_8():
     """Hyvää yötä."""
+
 
 @deco.keyword('Set Name Using Robot Name Attribute')
 def name_set_in_method_signature(a, b, *args, **kwargs):
@@ -39,15 +44,18 @@ def name_set_in_method_signature(a, b, *args, **kwargs):
     """
     pass
 
+
 @deco.keyword('Takes ${embedded} ${args}')
 def takes_embedded_args(a=1, b=2, c=3):
     """A keyword which uses embedded args
     """
     pass
 
+
 @deco.keyword(tags=['1', 1, 'one', 'yksi'])
 def keyword_with_tags_1():
     pass
+
 
 @deco.keyword('Keyword with tags 2', ('2', 2, 'two', 'kaksi'))
 def setting_both_name_and_tags_by_decorator():
--- atest/testresources/testlibs/classes.py
+++ atest/testresources/testlibs/classes.py
@@ -3,87 +3,122 @@ __version__ = 'N/A'  # This should be ignored when version is parsed
 
 class NameLibrary:
     handler_count = 10
+
     def simple1(self):
         """Simple 1"""
+
     def simple2___(self):
         """Simple 2"""
+
     def underscore_name(self):
         """Underscore Name"""
+
     def underscore_name2_(self):
         """Underscore Name2"""
+
     def un_der__sco_r__e_3(self):
         """Un Der Sco R E 3"""
+
     def MiXeD_CAPS(self):
         """MiXeD CAPS"""
+
     def camelCase(self):
         """Camel Case"""
+
     def camelCase2(self):
         """Camel Case 2"""
+
     def mixedCAPSCamel(self):
         """Mixed CAPS Camel"""
+
     def camelCase_and_underscores_doesNot_work(self):
         """CamelCase And Underscores DoesNot Work"""
+
     def _skipped1(self):
         """Starts with underscore"""
+
     skipped2 = "Not a function"
 
 
 class DocLibrary:
     handler_count = 3
-    def no_doc(self): pass
+
+    def no_doc(self):
+        pass
+
     no_doc.expected_doc = ''
     no_doc.expected_shortdoc = ''
+
     def one_line_doc(self):
         """One line doc"""
+
     one_line_doc.expected_doc = 'One line doc'
     one_line_doc.expected_shortdoc = 'One line doc'
+
     def multiline_doc(self):
         """Multiline doc.
 
         Spans multiple lines.
         """
+
     multiline_doc.expected_doc = 'Multiline doc.\n\nSpans multiple lines.'
     multiline_doc.expected_shortdoc = 'Multiline doc.'
 
 
 class ArgInfoLibrary:
     handler_count = 13
+
     def no_args(self):
         """[], [], None, None"""
         # Argument inspection had a bug when there was args on function body
         # so better keep some of them around here.
         a=b=c=1
+
     def required1(self, one):
         """['one',], [], None, None"""
+
     def required2(self, one, two):
         """['one','two'], [], None, None"""
+
     def required9(self, one, two, three, four, five, six, seven, eight, nine):
         """['one','two','three','four','five','six','seven','eight','nine'],\
            [], None, None"""
+
     def default1(self, one=1):
         """['one'], [1], None, None"""
+
     def default5(self, one='', two=None, three=3, four='huh', five=True):
         """['one','two','three','four','five'], ['',None,3,'huh',True], None, None"""
+
     def required1_default1(self, one, two=''):
         """['one','two'], [''], None, None"""
+
     def required2_default3(self, one, two, three=3, four=4, five=5):
         """['one','two','three','four','five'], [3,4,5], None, None"""
+
     def varargs(self,*one):
         """[], [], 'one', None"""
+
     def required2_varargs(self, one, two, *three):
         """['one','two'], [], 'three', None"""
+
     def req4_def2_varargs(self, one, two, three, four, five=5, six=6, *seven):
         """['one','two','three','four','five','six'], [5,6], 'seven', None"""
+
     def req2_def3_varargs_kwargs(self, three, four, five=5, six=6, seven=7, *eight, **nine):
         """['three','four','five','six','seven'], [5,6,7], 'eight', 'nine'"""
+
     def varargs_kwargs(self,*one, **two):
         """[], [], 'one', 'two'"""
+
 
 class GetattrLibrary:
     handler_count = 3
     keyword_names = ['foo','bar','zap']
+
     def get_keyword_names(self):
         return self.keyword_names
+
     def __getattr__(self, name):
         def handler(*args):
             return name, args
@@ -94,8 +129,10 @@ class GetattrLibrary:
 
 class SynonymLibrary:
     handler_count = 3
+
     def handler(self):
         pass
+
     synonym_handler = handler
     another_synonym = handler
 
@@ -107,11 +144,13 @@ class VersionLibrary:
 
 
 class VersionObjectLibrary:
+
     class _Version:
         def __init__(self, ver):
             self._ver = ver
         def __str__(self):
             return self._ver
+
     ROBOT_LIBRARY_VERSION = _Version('ver')
     kw = lambda x:None
 
@@ -133,6 +172,7 @@ class RecordingLibrary(object):
 
 
 class ArgDocDynamicLibrary:
+
     def __init__(self):
         kws = [('No Arg', []),
                ('One Arg', ['arg']),
@@ -141,17 +181,22 @@ class ArgDocDynamicLibrary:
                ('No Arg Spec', None)]
         self._keywords = dict((name, _KeywordInfo(name, argspec))
                               for name, argspec in kws)
+
     def get_keyword_names(self):
         return sorted(self._keywords.keys())
+
     def run_keyword(self, name, *args):
         print('*INFO* Executed keyword %s with arguments %s' % (name, args))
+
     def get_keyword_documentation(self, name):
         return self._keywords[name].doc
+
     def get_keyword_arguments(self, name):
         return self._keywords[name].argspec
 
 
 class ArgDocDynamicLibraryWithKwargsSupport(ArgDocDynamicLibrary):
+
     def __init__(self):
         ArgDocDynamicLibrary.__init__(self)
         for name, argspec in [('Kwargs', ['**kwargs']),
@@ -166,17 +211,20 @@ class ArgDocDynamicLibraryWithKwargsSupport(ArgDocDynamicLibrary):
 
 class _KeywordInfo:
     doc_template = 'Keyword documentation for %s'
+
     def __init__(self, name, argspec):
         self.doc = self.doc_template % name
         self.argspec = argspec
 
 
 class InvalidGetDocDynamicLibrary(ArgDocDynamicLibrary):
+
     def get_keyword_documentation(self, name, invalid_arg):
         pass
 
 
 class InvalidGetArgsDynamicLibrary(ArgDocDynamicLibrary):
+
     def get_keyword_arguments(self, name):
         1/0
 
