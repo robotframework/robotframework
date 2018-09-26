dd48373f84de4ec253eede0ba408baf00fdf9b5e
Pekka Klärck <peke@iki.fi>
2018-09-26 17:13:05 +0300

    We do support Python 3.7

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ Programming Language :: Python :: 3
 Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
 Programming Language :: Python :: Implementation :: CPython
 Programming Language :: Python :: Implementation :: Jython
 Programming Language :: Python :: Implementation :: IronPython
