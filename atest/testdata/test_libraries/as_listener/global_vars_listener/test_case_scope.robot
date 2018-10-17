a7c338db40d28adacde13af16761c2932311a40a
mparvu <mihaiparvu@users.noreply.github.com>
2018-10-17 19:52:25 +0300

    Added acceptance tests for Global and Test Suite library scopes

---
+++ atest/testdata/test_libraries/as_listener/global_vars_listener/test_case_scope.robot
@@ -0,0 +1,10 @@
+*** Settings ***
+Documentation    Test global variables in 'close' listener method with test case scope library
+Library          ../global_vars_listenerlibrary.py
+
+*** Test Cases ***
+Global variables test case scope test 1
+    No Operation
+
+Global variables test case scope test 2
+    No Operation
--- atest/testdata/test_libraries/as_listener/global_vars_listenerlibrary.py
+++ atest/testdata/test_libraries/as_listener/global_vars_listenerlibrary.py
@@ -1,19 +1,20 @@
 import sys
+
 from robot.libraries.BuiltIn import BuiltIn
-from collections import OrderedDict
+
 
 class global_vars_listenerlibrary():
     ROBOT_LISTENER_API_VERSION = 2
 
-    global_vars = OrderedDict([("Suite name:", "${SUITE_NAME}"),
-                               ("Suite documentation:", "${SUITE_DOCUMENTATION}"),
-                               ("Previous test name:", "${PREV_TEST_NAME}"),
-                               ("Previous test status:", "${PREV_TEST_STATUS}"),
-                               ("Log level:", "${LOG_LEVEL}")])
+    global_vars = ["${SUITE_NAME}",
+                   "${SUITE_DOCUMENTATION}",
+                   "${PREV_TEST_NAME}",
+                   "${PREV_TEST_STATUS}",
+                   "${LOG_LEVEL}"]
 
     def __init__(self):
         self.ROBOT_LIBRARY_LISTENER = self
 
     def _close(self):
-        for var, value in self.global_vars.items():
-            sys.__stderr__.write(var + (BuiltIn().get_variable_value(value)) +"\n")
+        for var in self.global_vars:
+            sys.__stderr__.write('%s: %s\n' % (var, BuiltIn().get_variable_value(var)))
