robotframework/robotframework

commit 4aa556478aa0f24f06c71c59edb892647b20855b

Parse only relevant test cases for -suite parent.child option
Fixes #2564.

atest/robot/core/filter_by_names.robot
@@ -157,6 +157,10 @@ Filter Using Suite Long Name
     Run Suites    --suite Subsuites.Sub1
     Should Contain Suites    ${SUITE}    Subsuites
     Should Contain Tests   ${SUITE}   SubSuite1 First
+    Run Suites    --suite suite_should.be.parsed    ${DATA DIR}/misc
+    Should Contain Suites    ${SUITE.suites[0]}    Be
+    Should Contain Tests    ${SUITE}    Executed
+    Should Not Contain Tests    ${SUITE}    Not Executed
 
 Filter Using Long Suite Name WIth Pattern
     Run Suites    --suite suites.*.SUB?

src/robot/parsing/populators.py
@@ -118,7 +118,7 @@ class FromDirectoryPopulator(object):
                  include_extensions=None, recurse=True):
         LOGGER.info("Parsing test data directory '%s'" % path)
         include_suites = self._get_include_suites(path, include_suites or [])
-        init_file, children = self._get_children(path, include_extensions,
+        init_file, children = self._get_children(path, datadir, include_extensions,
                                                  include_suites)
         if init_file:
             self._populate_init_file(datadir, init_file)
@@ -145,7 +145,7 @@ class FromDirectoryPopulator(object):
 
     def _get_include_suites(self, path, incl_suites):
         if not isinstance(incl_suites, SuiteNamePatterns):
-            incl_suites = SuiteNamePatterns(self._create_included_suites(incl_suites))
+            incl_suites = SuiteNamePatterns(incl_suites)
         if not incl_suites:
             return incl_suites
         # If a directory is included, also all its children should be included.
@@ -153,21 +153,14 @@ class FromDirectoryPopulator(object):
             return SuiteNamePatterns()
         return incl_suites
 
-    def _create_included_suites(self, incl_suites):
-        for suite in incl_suites:
-            yield suite
-            while '.' in suite:
-                suite = suite.split('.', 1)[1]
-                yield suite
-
     def _directory_is_included(self, path, incl_suites):
         name = os.path.basename(os.path.normpath(path))
         return self._is_in_included_suites(name, incl_suites)
 
-    def _get_children(self, dirpath, incl_extensions, incl_suites):
+    def _get_children(self, dirpath, datadir, incl_extensions, incl_suites):
         init_file = None
         children = []
-        for path, is_init_file in self._list_dir(dirpath, incl_extensions,
+        for path, is_init_file in self._list_dir(dirpath, datadir, incl_extensions,
                                                  incl_suites):
             if is_init_file:
                 if not init_file:
@@ -178,7 +171,7 @@ class FromDirectoryPopulator(object):
                 children.append(path)
         return init_file, children
 
-    def _list_dir(self, dir_path, incl_extensions, incl_suites):
+    def _list_dir(self, dir_path, datadir, incl_extensions, incl_suites):
         # os.listdir returns Unicode entries when path is Unicode
         names = os.listdir(unic(dir_path))
         for name in sorted(names, key=lambda item: item.lower()):
@@ -188,7 +181,7 @@ class FromDirectoryPopulator(object):
             ext = ext[1:].lower()
             if self._is_init_file(path, base, ext, incl_extensions):
                 yield path, True
-            elif self._is_included(path, base, ext, incl_extensions, incl_suites):
+            elif self._is_included(path, datadir, base, ext, incl_extensions, incl_suites):
                 yield path, False
             else:
                 LOGGER.info("Ignoring file or directory '%s'." % name)
@@ -203,17 +196,26 @@ class FromDirectoryPopulator(object):
             return ext in incl_extensions
         return ext in READERS
 
-    def _is_included(self, path, base, ext, incl_extensions, incl_suites):
+    def _is_included(self, path, datadir, base, ext, incl_extensions, incl_suites):
         if base.startswith(self.ignored_prefixes):
             return False
         if os.path.isdir(path):
             return base not in self.ignored_dirs or ext
         if not self._extension_is_accepted(ext, incl_extensions):
             return False
-        return self._is_in_included_suites(base, incl_suites)
+        longname = self._get_longname(base, datadir, incl_suites)
+        return self._is_in_included_suites(base, incl_suites, longname)
 
-    def _is_in_included_suites(self, name, incl_suites):
-        return not incl_suites or incl_suites.match(self._split_prefix(name))
+    def _is_in_included_suites(self, name, incl_suites, longname=None):
+        return not incl_suites or incl_suites.match(self._split_prefix(name), longname)
 
     def _split_prefix(self, name):
         return name.split('__', 1)[-1]
+
+    def _get_longname(self, base, datadir, include_suites):
+        if include_suites:
+            longname = self._split_prefix(base)
+            while datadir:
+                longname = '%s.%s' % (datadir.name, longname)
+                datadir = datadir.parent
+            return longname

atest/testdata/misc/suite_should/be/parsed.robot
@@ -1,0 +1,3 @@
+*** Test Cases ***
+Executed
+    No Operation

atest/testdata/misc/suite_should/not be/parsed.robot
@@ -1,0 +1,6 @@
+*** Settings ***
+Non-Existing    Causes error
+
+*** Test Cases ***
+Not Executed
+    No Operation

utest/parsing/test_populator.py
@@ -21,18 +21,6 @@ class _MockLogger(object):
         return self._output.getvalue()
 
 
-class FromDirectoryPopulatorTest(unittest.TestCase):
-
-    def test_included_suites_with_dot(self):
-        create_included_suites = FromDirectoryPopulator()._create_included_suites
-        for inp, exp in [([], []),
-                         (['foo'], ['foo']),
-                         (['bar.zoo'], ['bar.zoo', 'zoo']),
-                         (['1.2.3', 'x.y', 'z'],
-                          ['1.2.3', '2.3', '3', 'x.y', 'y', 'z'])]:
-            assert_equal(list(create_included_suites(inp)), exp)
-
-
 class _PopulatorTest(unittest.TestCase):
 
     def setUp(self):

utest/testdoc/test_jsonconverter.py
@@ -30,7 +30,7 @@ class TestJsonConverter(unittest.TestCase):
                      fullName='Misc',
                      doc='<p>My doc</p>',
                      metadata=[('1', '<p>2</p>'), ('abc', '<p>123</p>')],
-                     numberOfTests=182,
+                     numberOfTests=184,
                      tests=[],
                      keywords=[])
         test_convert(self.suite['suites'][0],
@@ -107,7 +107,7 @@ class TestJsonConverter(unittest.TestCase):
                      tags=['d1', 'd2', 'f1'],
                      timeout='')
         test_convert(self.suite['suites'][-2]['tests'][0],
-                     id='s1-s11-t1',
+                     id='s1-s12-t1',
                      name='Default Test Timeout',
                      fullName='Misc.Timeouts.Default Test Timeout',
                      doc='<p>I have a timeout</p>',

commit 4a922830da36f0fddce7e9c5e887ccf13875eda4

Fix regression using "--suite root.child" with nested suites.
The problem was caused by PR #2583 and explained in this commend:
https://github.com/robotframework/robotframework/pull/2583#issuecomment-441212848

Fixes #2564 that was re-opened.

atest/resources/atest_resource.robot
@@ -155,6 +155,12 @@ File Should Contain Regexp
     ${file} =    Get Output File    ${path}
     Should Match Regexp    ${file.strip()}    ${exp}
 
+File Should Not Contain Regexp
+    [Arguments]    ${path}    @{expected}
+    ${exp} =    Catenate    @{expected}
+    ${file} =    Get Output File    ${path}
+    Should Not Match Regexp    ${file.strip()}    ${exp}
+
 File Should Be Equal To
     [Arguments]    ${path}    @{expected}
     ${content} =    Get Output File    ${path}
@@ -258,6 +264,10 @@ Syslog Should Match Regexp
 Syslog Should Contain Regexp
     [Arguments]    @{expected}
     File Should Contain Regexp    ${SYSLOG_FILE}    @{expected}
+
+Syslog Should Not Contain Regexp
+    [Arguments]    @{expected}
+    File Should Not Contain Regexp    ${SYSLOG FILE}    @{expected}
 
 Check Names
     [Arguments]    ${item}    ${name}    ${longprefix}=

atest/robot/core/filter_by_names.robot
@@ -78,15 +78,15 @@ Correct Files Processed With --suite Matches Files
     [Documentation]    Test that only files matching --suite are processed.
     ...                Additionally __init__ files should never be ignored.
     ${root} =    Normalize Path    ${DATA DIR}/${SUITE DIR}
-    Check Syslog Contains    Parsing test data directory '${root}'
-    Check Syslog Contains    Ignoring file or directory 'fourth.robot'
-    Check Syslog Contains    Ignoring file or directory 'tsuite3.robot'
-    Check Syslog Contains    Parsing test data directory '${root}${/}subsuites'
-    Check Syslog Contains    Ignoring file or directory 'sub1.robot'
-    Check Syslog Contains    Ignoring file or directory 'sub2.robot'
-    Check Syslog Contains    Parsing file '${root}${/}tsuite1.robot
-    Check Syslog Contains    Parsing file '${root}${/}tsuite2.robot
-    Check Syslog Does Not Contain    Ignoring file or directory '__init__.robot'
+    Check Syslog Contains    Parsing directory '${root}'.
+    Check Syslog Contains    Ignoring file or directory '${root}${/}fourth.robot'.
+    Check Syslog Contains    Ignoring file or directory '${root}${/}tsuite3.robot'.
+    Check Syslog Contains    Parsing directory '${root}${/}subsuites'.
+    Check Syslog Contains    Ignoring file or directory '${root}${/}subsuites${/}sub1.robot'.
+    Check Syslog Contains    Ignoring file or directory '${root}${/}subsuites${/}sub2.robot'.
+    Check Syslog Contains    Parsing file '${root}${/}tsuite1.robot'.
+    Check Syslog Contains    Parsing file '${root}${/}tsuite2.robot'.
+    Syslog Should Not Contain Regexp    Ignoring file or directory '.*__init__.robot'.
 
 Non-Existing Suite
     Run Failing Test
@@ -119,16 +119,15 @@ Correct Files Processed When --suite Matches Directory
     [Documentation]    Testing that only files matching to --suite are processed.
     ...                This time --suite matches directory so all suites under it
     ...                should be processed. Using data from previous test case.
-    ${suitedir} =    Join Path    ${DATA DIR}    ${SUITE DIR}
-    ${subsuitedir} =    Join Path    ${suitedir}    subsuites
-    Check Syslog Contains    Parsing test data directory '${suitedir}'
-    Check Syslog Contains    Ignoring file or directory 'fourth.robot'
-    Check Syslog Contains    Ignoring file or directory 'tsuite1.robot'
-    Check Syslog Contains    Ignoring file or directory 'tsuite2.robot'
-    Check Syslog Contains    Ignoring file or directory 'tsuite3.robot'
-    Check Syslog Contains    Parsing test data directory '${subsuitedir}'
-    Check Syslog Contains    Parsing file '${subsuitedir}${/}sub1.robot'
-    Check Syslog Contains    Parsing file '${subsuitedir}${/}sub2.robot'
+    ${root} =    Normalize Path    ${DATA DIR}/${SUITE DIR}
+    Check Syslog Contains    Parsing directory '${root}'.
+    Check Syslog Contains    Ignoring file or directory '${root}${/}fourth.robot'.
+    Check Syslog Contains    Ignoring file or directory '${root}${/}tsuite1.robot'.
+    Check Syslog Contains    Ignoring file or directory '${root}${/}tsuite2.robot'.
+    Check Syslog Contains    Ignoring file or directory '${root}${/}tsuite3.robot'.
+    Check Syslog Contains    Parsing directory '${root}${/}subsuites'.
+    Check Syslog Contains    Parsing file '${root}${/}subsuites${/}sub1.robot'.
+    Check Syslog Contains    Parsing file '${root}${/}subsuites${/}sub2.robot'.
 
 Suite Under Subdirectory Using Pattern
     Run And Check Suites Within Subdirs    --suite sub?    Sub1    Sub2
@@ -150,17 +149,32 @@ Suite, Test Include And Exclude Together
     Should Contain Suites   ${SUITE}    Subsuites
     Should Contain Tests    ${SUITE}    SubSuite1 First
 
-Filter Using Suite Long Name
+Suite Long Name Matching File
     Run Suites    --suite suites.fourth
     Should Contain Suites    ${SUITE}    Fourth
     Should Contain Tests   ${SUITE}    Suite4 First
+
+Suite Long Name Matching Directory
+    Run Suites    --suite suites.subsuites
+    Should Contain Suites    ${SUITE}    Subsuites
+    Should Contain Suites    ${SUITE.suites[0]}    Sub1    Sub2
+    Should Contain Tests   ${SUITE}    SubSuite1 First    SubSuite2 First
+
+Suite Long Name Without Beginning
     Run Suites    --suite Subsuites.Sub1
     Should Contain Suites    ${SUITE}    Subsuites
     Should Contain Tests   ${SUITE}   SubSuite1 First
+
+Correct Files Processed When --suite Matches Directory Using Long Name
     Run Suites    --suite suite_should.be.parsed    ${DATA DIR}/misc
     Should Contain Suites    ${SUITE.suites[0]}    Be
     Should Contain Tests    ${SUITE}    Executed
     Should Not Contain Tests    ${SUITE}    Not Executed
+    ${should} =    Normalize Path    ${DATA DIR}/misc/suite_should
+    Check Syslog Contains    Parsing directory '${should}${/}be'.
+    Check Syslog Contains    Parsing file '${should}${/}be${/}parsed.robot'.
+    Check Syslog Contains    Parsing directory '${should}${/}not be'.
+    Check Syslog Contains    Ignoring file or directory '${should}${/}not be${/}parsed.robot'.
 
 Filter Using Long Suite Name WIth Pattern
     Run Suites    --suite suites.*.SUB?

atest/robot/parsing/caching_libs_and_resources.robot
@@ -32,7 +32,7 @@ Process Resource Files Only Once
     Should Contain X Times    ${SYSLOG}    Parsing file '${dir}${/}02_resource.robot'    2
     Should Contain X Times    ${SYSLOG}    Parsing file '${dir}${/}03_resource.robot'    2
     # Check that resources were parsed only once
-    Syslog File Should Contain In Order    Parsing test data directory    '${dir}'
+    Syslog File Should Contain In Order    Parsing directory '${dir}'.
     Syslog File Should Contain In Order    Parsing file '${dir}${/}01_tests.robot'.
     Syslog File Should Contain In Order    Data source '${dir}${/}02_resource.robot' has no tests or tasks.
     Syslog File Should Contain In Order    Data source '${dir}${/}03_resource.robot' has no tests or tasks.
@@ -46,8 +46,7 @@ Process Resource Files Only Once
 
 *** Keywords ***
 Syslog File Should Contain In Order
-    [Arguments]    @{parts}
-    ${text} =    Catenate    @{parts}
+    [Arguments]    ${text}
     Should Contain X Times    ${SYSLOG}    ${text}    1
     ${pre}    ${post} =    Set Variable    ${SYSLOG.split("${text.replace('\\','\\\\')}")}
     Set Suite Variable    ${SYSLOG}    ${post}

atest/robot/parsing/data_formats/formats_resource.robot
@@ -58,14 +58,12 @@ Run Suite Dir And Check Results
     Should Contain Suites    ${SUITE}    Sample    With Init
     Should Contain Suites    ${SUITE.suites[1]}    Sub Suite1    Sub Suite2
     Should Contain Tests    ${SUITE}    @{SAMPLE_TESTS}    @{SUBSUITE_TESTS}
-    ${invalid} =    Normalize Path    ${path}/invalid.${type}
-    Check Syslog Contains    | INFO \ | Data source '${invalid}' has no tests or tasks.
-    ${empty} =    Normalize Path    ${path}/empty.${type}
-    Check Syslog Contains    | INFO \ | Data source '${empty}' has no tests or tasks.
-    Check Syslog Contains    | INFO \ | Ignoring file or directory 'not_a_picture.jpg'.
-    ${invenc} =    Normalize Path    ${path}/invalid_encoding.${type}
+    ${path} =    Normalize Path    ${path}
+    Check Syslog Contains    | INFO \ | Data source '${path}${/}invalid.${type}' has no tests or tasks.
+    Check Syslog Contains    | INFO \ | Data source '${path}${/}empty.${type}' has no tests or tasks.
+    Check Syslog Contains    | INFO \ | Ignoring file or directory '${path}${/}not_a_picture.jpg'.
     Run Keyword If    "${type}" not in ("html", "rest")
-    ...    Check Syslog Contains    | ERROR | Parsing '${invenc}' failed: UnicodeDecodeError
+    ...    Check Syslog Contains    | ERROR | Parsing '${path}${/}invalid_encoding.${type}' failed: UnicodeDecodeError
 
 Check Suite With Init
     [Arguments]    ${suite}

atest/robot/parsing/data_formats/resource_extension.robot
@@ -14,4 +14,5 @@ Resource with '*.resource' extension
 '*.resource' files are not parsed for tests
     Should Contain Suites    ${SUITE}    Tests
     Should Contain Tests     ${SUITE}    Resource with '*.resource' extension
-    Check Syslog Contains    | INFO \ | Ignoring file or directory 'tests.resource'.
+    ${path} =    Normalize Path    ${DATADIR}/parsing/data_formats/resource_extension/tests.resource
+    Check Syslog Contains    | INFO \ | Ignoring file or directory '${path}'.

src/robot/parsing/populators.py
@@ -116,8 +116,8 @@ class FromDirectoryPopulator(object):
 
     def populate(self, path, datadir, include_suites=None,
                  include_extensions=None, recurse=True):
-        LOGGER.info("Parsing test data directory '%s'" % path)
-        include_suites = self._get_include_suites(path, include_suites or [])
+        LOGGER.info("Parsing directory '%s'." % path)
+        include_suites = self._get_include_suites(path, datadir, include_suites )
         init_file, children = self._get_children(path, datadir, include_extensions,
                                                  include_suites)
         if init_file:
@@ -143,19 +143,14 @@ class FromDirectoryPopulator(object):
             except DataError as err:
                 LOGGER.error("Parsing '%s' failed: %s" % (child, err.message))
 
-    def _get_include_suites(self, path, incl_suites):
+    def _get_include_suites(self, path, datadir, incl_suites):
         if not isinstance(incl_suites, SuiteNamePatterns):
             incl_suites = SuiteNamePatterns(incl_suites)
-        if not incl_suites:
-            return incl_suites
         # If a directory is included, also all its children should be included.
-        if self._directory_is_included(path, incl_suites):
+        if self._is_in_included_suites(os.path.basename(path), datadir.parent,
+                                       incl_suites):
             return SuiteNamePatterns()
         return incl_suites
-
-    def _directory_is_included(self, path, incl_suites):
-        name = os.path.basename(os.path.normpath(path))
-        return self._is_in_included_suites(name, incl_suites)
 
     def _get_children(self, dirpath, datadir, incl_extensions, incl_suites):
         init_file = None
@@ -173,7 +168,8 @@ class FromDirectoryPopulator(object):
 
     def _list_dir(self, dir_path, datadir, incl_extensions, incl_suites):
         # os.listdir returns Unicode entries when path is Unicode
-        names = os.listdir(unic(dir_path))
+        dir_path = unic(dir_path)
+        names = os.listdir(dir_path)
         for name in sorted(names, key=lambda item: item.lower()):
             name = unic(name)  # needed to handle nfc/nfd normalization on OSX
             path = os.path.join(dir_path, name)
@@ -184,7 +180,7 @@ class FromDirectoryPopulator(object):
             elif self._is_included(path, datadir, base, ext, incl_extensions, incl_suites):
                 yield path, False
             else:
-                LOGGER.info("Ignoring file or directory '%s'." % name)
+                LOGGER.info("Ignoring file or directory '%s'." % path)
 
     def _is_init_file(self, path, base, ext, incl_extensions):
         return (base.lower() == '__init__' and
@@ -203,19 +199,20 @@ class FromDirectoryPopulator(object):
             return base not in self.ignored_dirs or ext
         if not self._extension_is_accepted(ext, incl_extensions):
             return False
-        longname = self._get_longname(base, datadir, incl_suites)
-        return self._is_in_included_suites(base, incl_suites, longname)
+        return self._is_in_included_suites(base, datadir, incl_suites)
 
-    def _is_in_included_suites(self, name, incl_suites, longname=None):
-        return not incl_suites or incl_suites.match(self._split_prefix(name), longname)
+    def _is_in_included_suites(self, name, datadir, incl_suites):
+        if not incl_suites:
+            return True
+        name = self._split_prefix(name)
+        return incl_suites.match(name, self._get_longname(name, datadir))
 
     def _split_prefix(self, name):
         return name.split('__', 1)[-1]
 
-    def _get_longname(self, base, datadir, include_suites):
-        if include_suites:
-            longname = self._split_prefix(base)
-            while datadir:
-                longname = '%s.%s' % (datadir.name, longname)
-                datadir = datadir.parent
-            return longname
+    def _get_longname(self, name, datadir):
+        longname = name
+        while datadir:
+            longname = '%s.%s' % (datadir.name, longname)
+            datadir = datadir.parent
+        return longname

atest/robot/core/test_suite_dir.robot
@@ -36,13 +36,14 @@ Child Suites Not Containing Tests Not Executed
     Should Contain Suites    ${SUITE.suites[0].suites[0]}    Test Dir 3
     Should Contain Suites    ${SUITE.suites[0].suites[0].suites[0]}    Test File 3
 
-
 File Without Extension
-    Check Syslog Contains  Ignoring file or directory 'no_extension'.
+    ${path} =    Normalize Path    ${DATADIR}/core/test_suite_dir/no_extension
+    Check Syslog Contains  Ignoring file or directory '${path}'.
 
 File and Directory Starting with _
-    Check Syslog Contains  Ignoring file or directory '_ignore_this_file.robot'.
-    Check Syslog Contains  Ignoring file or directory '_ignore_this_dir'.
+    ${path} =    Normalize Path    ${DATADIR}/core/test_suite_dir
+    Check Syslog Contains  Ignoring file or directory '${path}${/}_ignore_this_file.robot'.
+    Check Syslog Contains  Ignoring file or directory '${path}${/}_ignore_this_dir'.
 
 *** Keywords ***
 Suite Passed