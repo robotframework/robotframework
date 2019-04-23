4710d41c5afced6df54e2ef66aefc29df4cc5e7b
Pekka Klärck <peke@iki.fi>
2019-04-23 15:25:40 +0300

    Post-Easter Easter cleanup.
    - Reformat writing log message.
    - Fix and cleanup test.
    
    See issue #3159 and PR #3117.

--- src/robot/libraries/Easter.py
+++ src/robot/libraries/Easter.py
@@ -19,9 +19,12 @@ from robot.api import logger
 def none_shall_pass(who):
     if who is not None:
         raise AssertionError('None shall pass!')
-    logger.info('<iframe width="560" height="315" \
-                 src="https://www.youtube-nocookie.com/embed/zKhEw7nD9C4?autoplay=1" \
-                 frameborder="0" \
-                 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" \
-                 allowfullscreen> \
-                 </iframe>', html=True)
+    logger.info(
+        '<iframe width="560" height="315" '
+        'src="https://www.youtube-nocookie.com/embed/zKhEw7nD9C4?autoplay=1" '
+        'frameborder="0" '
+        'allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" '
+        'allowfullscreen>'
+        '</iframe>',
+        html=True
+    )
