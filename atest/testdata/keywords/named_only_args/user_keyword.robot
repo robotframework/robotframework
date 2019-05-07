robotframework/robotframework

commit fb6881383537a4ecf93172bbeeb709ccf857c2a7

Avoid escaping `=` if kw accepts named-only arguments
Fixes #3047.

atest/robot/keywords/named_only_args/python.robot
@@ -28,10 +28,13 @@ Missing value
 Missing multiple values
     Check Test Case    ${TESTNAME}
 
-Unexpected keyword argumemt
+Unexpected keyword argument
     Check Test Case    ${TESTNAME}
 
-Multiple unexpected keyword argumemt
+Multiple unexpected keyword argument
+    Check Test Case    ${TESTNAME}
+
+Multiple unexpected keyword argument with inequality
     Check Test Case    ${TESTNAME}
 
 Unexpected positional argument
@@ -43,3 +46,10 @@ With varargs
 
 With other arguments
     Check Test Case    ${TESTNAME}
+
+With only one named argument
+    check test case  ${TESTNAME}
+
+With positional argument containing equal sign
+    check test case  ${TESTNAME}
+

atest/robot/keywords/named_only_args/user_keyword.robot
@@ -25,10 +25,13 @@ Missing value
 Missing multiple values
     Check Test Case    ${TESTNAME}
 
-Unexpected keyword argumemt
+Unexpected keyword argument
     Check Test Case    ${TESTNAME}
 
-Multiple unexpected keyword argumemt
+Multiple unexpected keyword argument
+    Check Test Case    ${TESTNAME}
+
+Multiple unexpected keyword argument with inequality
     Check Test Case    ${TESTNAME}
 
 Unexpected positional argument
@@ -40,3 +43,9 @@ With varargs
 
 With other arguments
     Check Test Case    ${TESTNAME}
+
+With only one named argument
+    check test case  ${TESTNAME}
+
+With positional argument containing equal sign
+    check test case  ${TESTNAME}

src/robot/running/arguments/argumentresolver.py
@@ -51,7 +51,7 @@ class NamedArgumentResolver(object):
         for arg in arguments:
             if self._is_dict_var(arg):
                 named.append(arg)
-            elif self._is_named(arg, variables):
+            elif self._is_named(arg, named, variables):
                 named.append(split_from_equals(arg))
             elif named:
                 self._raise_positional_after_named()
@@ -63,13 +63,15 @@ class NamedArgumentResolver(object):
         return (is_string(arg) and arg[:2] == '&{' and arg[-1] == '}' and
                 VariableSplitter(arg).is_dict_variable())
 
-    def _is_named(self, arg, variables=None):
+    def _is_named(self, arg, previous_named, variables=None):
         if not (is_string(arg) and '=' in arg):
             return False
         name, value = split_from_equals(arg)
         if value is None:
             return False
-        if self._argspec.kwargs or self._argspec.kwonlyargs:
+        if self._argspec.kwargs or name in self._argspec.kwonlyargs:
+            return True
+        if previous_named:
             return True
         if not self._argspec.supports_named:
             return False

atest/testdata/keywords/named_only_args/python.robot
@@ -44,13 +44,17 @@ Missing multiple values
     [Documentation]    FAIL Keyword 'KwOnlyArgs.Many Kw Only Args' missing named-only arguments 'first' and 'third'.
     Many Kw Only Args    second=xxx
 
-Unexpected keyword argumemt
+Unexpected keyword argument
     [Documentation]    FAIL Keyword 'KwOnlyArgs.Kw Only Arg' got unexpected named argument 'invalid'.
     Kw Only Arg    kwo=value    invalid=ooops
 
-Multiple unexpected keyword argumemt
+Multiple unexpected keyword argument
     [Documentation]    FAIL Keyword 'KwOnlyArgs.Kw Only Arg' got unexpected named arguments 'invalid' and 'ooops'.
     Kw Only Arg    kwo=value    invalid=ooops    ooops=invalid
+
+Multiple unexpected keyword argument with inequality
+    [Documentation]    FAIL Keyword 'KwOnlyArgs.Kw Only Arg' got unexpected named arguments '<' and 'ooops'.
+    Kw Only Arg    kwo=value    <=ooops    ooops=invalid
 
 Unexpected positional argument 1
     [Documentation]    FAIL Keyword 'KwOnlyArgs.Kw Only Arg' expected 0 non-named arguments, got 1.
@@ -75,3 +79,9 @@ With other arguments
     Should Be Equal    ${result}    p1-p2-p3-p4-k1-k2-k3=3-k4=4
     ${result} =    All Arg Types    k4=!!!    kwo_def=k2    k3=!    pos_req=p1    pos_def=p2    kwo_req=k1
     Should Be Equal    ${result}    p1-p2-k1-k2-k3=!-k4=!!!
+
+With only one named argument
+    Kw Only Arg With Varargs    First arg    Second arg    Maybe a third    kwo=Actually, correct!
+
+With positional argument containing equal sign
+    Kw Only Arg With Varargs    One more time    a=1    <=    2    kwo=Actually, correct!

atest/testdata/keywords/named_only_args/user_keyword.robot
@@ -48,13 +48,17 @@ Missing multiple values
     [Documentation]    FAIL Keyword 'Many Kw Only Args' missing named-only arguments 'first' and 'third'.
     Many Kw Only Args    second=xxx
 
-Unexpected keyword argumemt
+Unexpected keyword argument
     [Documentation]    FAIL Keyword 'Kw Only Arg' got unexpected named argument 'invalid'.
     Kw Only Arg    kwo=value    invalid=ooops
 
-Multiple unexpected keyword argumemt
+Multiple unexpected keyword argument
     [Documentation]    FAIL Keyword 'Kw Only Arg' got unexpected named arguments 'invalid' and 'ooops'.
     Kw Only Arg    kwo=value    invalid=ooops    ooops=invalid
+
+Multiple unexpected keyword argument with inequality
+    [Documentation]    FAIL Keyword 'Kw Only Arg' got unexpected named arguments '<' and 'ooops'.
+    Kw Only Arg    kwo=value    <=ooops    ooops=invalid
 
 Unexpected positional argument 1
     [Documentation]    FAIL Keyword 'Kw Only Arg' expected 0 non-named arguments, got 1.
@@ -79,6 +83,12 @@ With other arguments
     Should Be Equal    ${result}    p1-p2-p3-p4-k1-k2-k3=3-k4=4
     ${result} =    All Arg Types    k3=!    kwo_def=k2    k4=!!!    pos_req=p1    pos_def=p2    kwo_req=k1
     Should Be Equal    ${result}    p1-p2-k1-k2-k3=!-k4=!!!
+
+With only one named argument
+    Kw Only Arg With Varargs    First arg    Second arg    Maybe a third    kwo=Actually, correct!
+
+With positional argument containing equal sign
+    Kw Only Arg With Varargs    One more time    a=1    <=    2    kwo=Actually, correct!
 
 *** Keywords ***
 Kw Only Arg

atest/testdata/cli/dryrun/args.robot
@@ -38,10 +38,10 @@ Valid named args
 
 Invalid named args
     [Documentation]  FAIL Several failures occurred:\n\n
-    ...  1) Keyword 'Library.Normal Args' got positional argument after named arguments.\n\n
+    ...  1) Keyword 'Library.Normal Args' got unexpected named argument 'c'.\n\n
     ...  2) Keyword 'Library.Normal Args' got positional argument after named arguments.\n\n
-    ...  3) Keyword 'Library.Normal Args' got positional argument after named arguments.\n\n
-    ...  4) Keyword 'Library.Normal And Varargs' got positional argument after named arguments.\n\n
+    ...  3) Keyword 'Library.Normal Args' got unexpected named argument 'c'.\n\n
+    ...  4) Keyword 'Library.Normal And Varargs' got unexpected named argument 'c'.\n\n
     ...  5) Keyword 'Library.Normal And Varargs And Kwargs' got positional argument after named arguments.
     Normal args    a=1    c=3
     Normal args    a=1    2

atest/testdata/keywords/named_args/with_python_keywords.robot
@@ -122,6 +122,7 @@ Non working named combinations with varargs
     missing value for argument 'a'.                   b=b
     got positional argument after named arguments.    b=b    b
     got positional argument after named arguments.    a      b=b    c
+    got unexpected named argument 'c'.    a      b=b    c=c
     got multiple values for argument 'a'.             a      b      a=a
     got multiple values for argument 'b'.             a      b      b=b
     got multiple values for argument 'a'.             a      a=a

atest/testdata/keywords/named_args/with_user_keywords.robot
@@ -86,7 +86,7 @@ Non working named combinations with varargs
     missing value for argument 'a'.    b=b
     got positional argument after named arguments.    b=b    b
     got positional argument after named arguments.    a      b=b    c
-    got positional argument after named arguments.    a      b=b    c=c
+    got unexpected named argument 'c'.    a      b=b    c=c
     got multiple values for argument 'a'.             a      b      a=a
     got multiple values for argument 'b'.             a      b      b=b
     got multiple values for argument 'a'.             a      a=a

commit b121c6c0452e7c64f6b4136bc7d8f60421a5d32c

Fix named-only args with variable in name.
This was a regression caused by #3116. The main problem was that there
were no tests for this kind of usage earlier.

atest/robot/keywords/named_only_args/python.robot
@@ -44,5 +44,11 @@ With varargs
 With other arguments
     Check Test Case    ${TESTNAME}
 
+Argument name as variable
+    Check Test Case    ${TESTNAME}
+
+Argument name as non-existing variable
+    Check Test Case    ${TESTNAME}
+
 With positional argument containing equal sign
     Check Test Case    ${TESTNAME}

atest/robot/keywords/named_only_args/user_keyword.robot
@@ -41,5 +41,11 @@ With varargs
 With other arguments
     Check Test Case    ${TESTNAME}
 
+Argument name as variable
+    Check Test Case    ${TESTNAME}
+
+Argument name as non-existing variable
+    Check Test Case    ${TESTNAME}
+
 With positional argument containing equal sign
     Check Test Case    ${TESTNAME}

src/robot/running/arguments/argumentresolver.py
@@ -64,20 +64,15 @@ class NamedArgumentResolver(object):
                 VariableSplitter(arg).is_dict_variable())
 
     def _is_named(self, arg, previous_named, variables=None):
-        if not (is_string(arg) and '=' in arg):
-            return False
         name, value = split_from_equals(arg)
         if value is None:
             return False
-        if self._argspec.kwargs or name in self._argspec.kwonlyargs:
-            return True
-        if previous_named:
-            return True
-        if not self._argspec.supports_named:
-            return False
         if variables:
             name = variables.replace_scalar(name)
-        return name in self._argspec.positional
+        argspec = self._argspec
+        if previous_named or name in argspec.kwonlyargs or argspec.kwargs:
+            return True
+        return argspec.supports_named and name in argspec.positional
 
     def _raise_positional_after_named(self):
         raise DataError("%s '%s' got positional argument after named arguments."

src/robot/utils/escaping.py
@@ -72,6 +72,7 @@ class Unescaper(object):
             return unescaper(text[1:])
 
     def _unescaper_for_n(self, text):
+        # TODO: Deprecate ignoring space after newline in RF 3.2.
         if text.startswith(' '):
             text = text[1:]
         return '\n' + text
@@ -126,6 +127,8 @@ class EscapeFinder(object):
 
 
 def split_from_equals(string):
+    if not is_string(string) or '=' not in string:
+        return string, None
     index = _get_split_index(string)
     if index == -1:
         return string, None

atest/testdata/keywords/named_only_args/python.robot
@@ -76,6 +76,19 @@ With other arguments
     ${result} =    All Arg Types    k4=!!!    kwo_def=k2    k3=!    pos_req=p1    pos_def=p2    kwo_req=k1
     Should Be Equal    ${result}    p1-p2-k1-k2-k3=!-k4=!!!
 
+Argument name as variable
+    ${name} =    Set Variable    kwo
+    ${result} =    Kw Only Arg    ${name}=value
+    Should Be Equal    ${result}    value
+    ${result} =    Kw Only Arg    k${name[1]}o=xxx
+    Should Be Equal    ${result}    xxx
+    ${result} =    Kw Only Arg With Default    another=${name}    ${name}=${EMPTY}
+    Should Be Equal    ${result}    -kwo
+
+Argument name as non-existing variable
+    [Documentation]    FAIL Variable '${i do not exist}' not found.
+    Kw Only Arg    ${i do not exist}=value
+
 With positional argument containing equal sign
     ${result} =    Kw Only Arg With Varargs    One more time    a=1    <=    2    kwo=No escaping needed!
     Should Be Equal    ${result}    One more time-a=1-<=-2-No escaping needed!

atest/testdata/keywords/named_only_args/user_keyword.robot
@@ -80,6 +80,19 @@ With other arguments
     ${result} =    All Arg Types    k3=!    kwo_def=k2    k4=!!!    pos_req=p1    pos_def=p2    kwo_req=k1
     Should Be Equal    ${result}    p1-p2-k1-k2-k3=!-k4=!!!
 
+Argument name as variable
+    ${name} =    Set Variable    kwo
+    ${result} =    Kw Only Arg    ${name}=value
+    Should Be Equal    ${result}    value
+    ${result} =    Kw Only Arg    k${name[1]}o=xxx
+    Should Be Equal    ${result}    xxx
+    ${result} =    Kw Only Arg With Default    another=${name}    ${name}=${EMPTY}
+    Should Be Equal    ${result}    -kwo
+
+Argument name as non-existing variable
+    [Documentation]    FAIL Variable '${i do not exist}' not found.
+    Kw Only Arg    ${i do not exist}=value
+
 With positional argument containing equal sign
     ${result} =    Kw Only Arg With Varargs    One more time    a=1    <=    2    kwo=No escaping needed!
     Should Be Equal    ${result}    One more time-a=1-<=-2-No escaping needed!