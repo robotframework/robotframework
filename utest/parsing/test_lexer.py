3a4570b1e43025429ed4c42de8d4a28ed3e97b76
Pekka Klärck <peke@iki.fi>
2019-12-31 01:11:00 +0200

    Enhance lexer tests

--- utest/parsing/test_lexer.py
+++ utest/parsing/test_lexer.py
@@ -10,15 +10,105 @@ from robot.parsing import get_tokens, get_resource_tokens, Token
 
 T = Token
 
-
-def assert_tokens(tokens, expected):
-    assert_equal(len(tokens), len(expected))
+def assert_tokens(source, expected, get_tokens=get_tokens, data_only=False):
+    tokens = list(get_tokens(source, data_only))
+    assert_equal(len(tokens), len(expected),
+                 'Expected %d tokens:\n%s\n\nGot %d tokens:\n%s'
+                 % (len(expected), expected, len(tokens), tokens),
+                 values=False)
     for act, exp in zip(tokens, expected):
         exp = Token(*exp)
         assert_equal(act.type, exp.type)
-        assert_equal(act.value, exp.value)
+        assert_equal(act.value, exp.value, formatter=repr)
         assert_equal(act.lineno, exp.lineno)
         assert_equal(act.columnno, exp.columnno)
+
+
+class TestName(unittest.TestCase):
+
+    def test_name_on_own_row(self):
+        self._verify('My Name',
+                     [(T.NAME, 'My Name', 2, 1), (T.EOS, '', 2, 8)])
+        self._verify('My Name    ',
+                     [(T.NAME, 'My Name', 2, 1), (T.EOL, '    ', 2, 8), (T.EOS, '', 2, 12)])
+        self._verify('My Name\n    Keyword',
+                     [(T.NAME, 'My Name', 2, 1), (T.EOL, '\n', 2, 8), (T.EOS, '', 2, 9),
+                      (T.SEPARATOR, '    ', 3, 1), (T.KEYWORD, 'Keyword', 3, 5), (T.EOS, '', 3, 12)])
+        self._verify('My Name  \n    Keyword',
+                     [(T.NAME, 'My Name', 2, 1), (T.EOL, '  \n', 2, 8), (T.EOS, '', 2, 11),
+                      (T.SEPARATOR, '    ', 3, 1), (T.KEYWORD, 'Keyword', 3, 5), (T.EOS, '', 3, 12)])
+
+    def test_name_and_keyword_on_same_row(self):
+        self._verify('Name    Keyword',
+                     [(T.NAME, 'Name', 2, 1), (T.SEPARATOR, '    ', 2, 5), (T.EOS, '', 2, 9),
+                      (T.KEYWORD, 'Keyword', 2, 9), (T.EOS, '', 2, 16)])
+        self._verify('N  K  A',
+                     [(T.NAME, 'N', 2, 1), (T.SEPARATOR, '  ', 2, 2), (T.EOS, '', 2, 4),
+                      (T.KEYWORD, 'K', 2, 4), (T.SEPARATOR, '  ', 2, 5),
+                      (T.ARGUMENT, 'A', 2, 7), (T.EOS, '', 2, 8)])
+        self._verify('N  ${v}=  K',
+                     [(T.NAME, 'N', 2, 1), (T.SEPARATOR, '  ', 2, 2), (T.EOS, '', 2, 4),
+                      (T.ASSIGN, '${v}=', 2, 4), (T.SEPARATOR, '  ', 2, 9),
+                      (T.KEYWORD, 'K', 2, 11), (T.EOS, '', 2, 12)])
+
+    def test_name_and_setting_on_same_row(self):
+        self._verify('Name    [Documentation]    The doc.',
+                     [(T.NAME, 'Name', 2, 1), (T.SEPARATOR, '    ', 2, 5), (T.EOS, '', 2, 9),
+                      (T.DOCUMENTATION, '[Documentation]', 2, 9), (T.SEPARATOR, '    ', 2, 24),
+                      (T.ARGUMENT, 'The doc.', 2, 28), (T.EOS, '', 2, 36)])
+
+    def _verify(self, data, tokens):
+        assert_tokens('*** Test Cases ***\n' + data,
+                      [(T.TESTCASE_HEADER, '*** Test Cases ***', 1, 1),
+                       (T.EOL, '\n', 1, 19),
+                       (T.EOS, '', 1, 20)] + tokens)
+        assert_tokens('*** Keywords ***\n' + data,
+                      [(T.KEYWORD_HEADER, '*** Keywords ***', 1, 1),
+                       (T.EOL, '\n', 1, 17),
+                       (T.EOS, '', 1, 18)] + tokens,
+                      get_tokens=get_resource_tokens)
+
+
+class TestNameWithPipes(unittest.TestCase):
+
+    def test_name_on_own_row(self):
+        self._verify('| My Name',
+                     [(T.SEPARATOR, '| ', 2, 1), (T.NAME, 'My Name', 2, 3), (T.EOS, '', 2, 10)])
+        self._verify('| My Name |',
+                     [(T.SEPARATOR, '| ', 2, 1), (T.NAME, 'My Name', 2, 3), (T.SEPARATOR, ' |', 2, 10), (T.EOS, '', 2, 12)])
+        self._verify('| My Name | ',
+                     [(T.SEPARATOR, '| ', 2, 1), (T.NAME, 'My Name', 2, 3), (T.SEPARATOR, ' |', 2, 10), (T.EOL, ' ', 2, 12), (T.EOS, '', 2, 13)])
+
+    def test_name_and_keyword_on_same_row(self):
+        self._verify('| Name | Keyword',
+                     [(T.SEPARATOR, '| ', 2, 1), (T.NAME, 'Name', 2, 3), (T.SEPARATOR, ' | ', 2, 7), (T.EOS, '', 2, 10),
+                      (T.KEYWORD, 'Keyword', 2, 10), (T.EOS, '', 2, 17)])
+        self._verify('| N | K | A |',
+                     [(T.SEPARATOR, '| ', 2, 1), (T.NAME, 'N', 2, 3), (T.SEPARATOR, ' | ', 2, 4), (T.EOS, '', 2, 7),
+                      (T.KEYWORD, 'K', 2, 7), (T.SEPARATOR, ' | ', 2, 8),
+                      (T.ARGUMENT, 'A', 2, 11), (T.SEPARATOR, ' |', 2, 12), (T.EOS, '', 2, 14)])
+        self._verify('|    N  |  ${v} =    |    K    ',
+                     [(T.SEPARATOR, '|    ', 2, 1), (T.NAME, 'N', 2, 6), (T.SEPARATOR, '  |  ', 2, 7), (T.EOS, '', 2, 12),
+                      (T.ASSIGN, '${v} =', 2, 12), (T.SEPARATOR, '    |    ', 2, 18),
+                      (T.KEYWORD, 'K', 2, 27), (T.EOL, '    ', 2, 28), (T.EOS, '', 2, 32)])
+
+    def test_name_and_setting_on_same_row(self):
+        self._verify('| Name | [Documentation] | The doc.',
+                     [(T.SEPARATOR, '| ', 2, 1), (T.NAME, 'Name', 2, 3), (T.SEPARATOR, ' | ', 2, 7), (T.EOS, '', 2, 10),
+                      (T.DOCUMENTATION, '[Documentation]', 2, 10), (T.SEPARATOR, ' | ', 2, 25),
+                      (T.ARGUMENT, 'The doc.', 2, 28), (T.EOS, '', 2, 36)])
+
+    def _verify(self, data, tokens):
+        assert_tokens('*** Test Cases ***\n' + data,
+                      [(T.TESTCASE_HEADER, '*** Test Cases ***', 1, 1),
+                       (T.EOL, '\n', 1, 19),
+                       (T.EOS, '', 1, 20)] + tokens)
+        assert_tokens('*** Keywords ***\n' + data,
+                      [(T.KEYWORD_HEADER, '*** Keywords ***', 1, 1),
+                       (T.EOL, '\n', 1, 17),
+                       (T.EOS, '', 1, 18)] + tokens,
+                      get_tokens=get_resource_tokens)
+
 
 
 class SourceFormatsTestBase(unittest.TestCase):
@@ -104,9 +194,8 @@ Example
         self._verify(self.data, data_only=True)
 
     def _verify(self, source, data_only=False):
-        tokens = get_tokens(source, data_only)
         expected = self.data_tokens if data_only else self.tokens
-        assert_tokens(list(tokens), expected)
+        assert_tokens(source, expected, data_only=data_only)
 
 
 class TestGetResourceTokensSourceFormats(SourceFormatsTestBase):
@@ -172,9 +261,9 @@ NOOP    No Operation
         self._verify(self.data, data_only=True)
 
     def _verify(self, source, data_only=False):
-        tokens = get_resource_tokens(source, data_only)
         expected = self.data_tokens if data_only else self.tokens
-        assert_tokens(list(tokens), expected)
+        assert_tokens(source, expected, get_tokens=get_resource_tokens,
+                      data_only=data_only)
 
 
 if __name__ == '__main__':
