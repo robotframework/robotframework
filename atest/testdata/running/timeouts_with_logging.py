e5cd42f967714ffdd5e71eba8aa332177586abbb
Pekka Klärck <peke@iki.fi>
2020-01-02 13:14:10 +0200

    Try to avoid hanging tests on CI

--- atest/testdata/running/timeouts_with_logging.py
+++ atest/testdata/running/timeouts_with_logging.py
@@ -1,4 +1,6 @@
 import logging
+import time
+
 from robot.api import logger
 
 
@@ -8,8 +10,10 @@ MSG = 'A rather long message that is slow to write on the disk. ' * 10000
 def rf_logger():
     while True:
         logger.info(MSG)
+        time.sleep(0)    # give time for other threads
 
 
 def python_logging():
     while True:
         logging.info(MSG)
+        time.sleep(0)    # give time for other threads
