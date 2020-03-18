c2eeaed36c8cbeb0ba88f6d69796aa7326575f5a
Mikhail Kulinich <tysonite@gmail.com>
2020-03-18 13:07:15 +0300

    Disable warnings from Java >= 9 when using Jython
    This is done by setting JAVA_OPTS to `--add-opens java.base/java.io=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED`.
    
    For details see https://github.com/jythontools/jython/issues/171

--- atest/interpreter.py
+++ atest/interpreter.py
@@ -31,6 +31,7 @@ class Interpreter(object):
         self.name = name
         self.version = version
         self.version_info = tuple(int(item) for item in version.split('.'))
+        self.java_version_info = self._get_java_version_info()
 
     def _get_interpreter(self, path):
         path = path.replace('/', os.sep)
@@ -47,6 +48,21 @@ class Interpreter(object):
         name = name if 'PyPy' not in output else 'PyPy'
         version = re.match(r'\d+\.\d+\.\d+', version).group()
         return name, version
+
+    def _get_java_version_info(self):
+        if not self.is_jython:
+            return -1, -1
+        try:
+            # platform.java_ver() returns Java version in a format:
+            # ('9.0.7.1', ...) or ('11.0.6', ...) or ('1.8.0_121', ...)
+            script = 'import platform; print(platform.java_ver()[0])'
+            output = subprocess.check_output(self.interpreter + ['-c', script],
+                                             stderr=subprocess.STDOUT,
+                                             encoding='UTF-8')
+        except (subprocess.CalledProcessError, FileNotFoundError):
+            raise ValueError('Invalid interpreter: %s' % self.path)
+        major, minor = output.strip().split('.', 2)[:2]
+        return int(major), int(minor)
 
     @property
     def os(self):
@@ -117,6 +133,17 @@ class Interpreter(object):
         return tools_jar
 
     @property
+    def java_opts(self):
+        if not self.is_jython:
+            return None
+        java_opts = os.environ.get('JAVA_OPTS', '')
+        if self.version_info[:3] >= (2, 7, 2) and self.java_version_info[0] >= 9:
+            # https://github.com/jythontools/jython/issues/171
+            if '--add-opens' not in java_opts:
+                java_opts += ' --add-opens java.base/java.io=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED'
+        return java_opts
+
+    @property
     def is_python(self):
         return self.name == 'Python'
 
@@ -173,7 +200,10 @@ class Interpreter(object):
         return self.interpreter + [join(ROBOT_PATH, 'tidy.py')]
 
     def __str__(self):
-        return '%s %s on %s' % (self.name, self.version, self.os)
+        java = ''
+        if self.is_jython:
+            java = '(Java %s) ' % '.'.join(str(ver_part) for ver_part in self.java_version_info)
+        return '%s %s %son %s' % (self.name, self.version, java, self.os)
 
 
 class StandaloneInterpreter(Interpreter):
