45da62bc65f35c1e687504132bdc03df040480a3
Pekka Klärck <peke@iki.fi>
2020-04-20 09:16:50 +0300

    Fix importing BuiltIn when PYTHONCASEOK is set.
    The problem was that since 6a2155a5b68fd97172d2e492e1db8d0d3c0a86a9
    (#3540) BuiltIn imports `collections` and when PYTHONCASEOK is set
    (like it's on our CI) that ends importing `Collections` library from
    the same directory and not the globally available module.
    
    The issue obviously affected only platforms where PYTHONCASEOK had an
    effect. It also didn't affect Python 3 because there absolute imports
    are on automatically.

--- src/robot/libraries/BuiltIn.py
+++ src/robot/libraries/BuiltIn.py
@@ -13,6 +13,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+from __future__ import absolute_import
+
 from collections import OrderedDict
 import difflib
 import re
