740554c605c2a69730dd59b2232143fd8a76ce4d
Pekka Klärck <peke@iki.fi>
2020-08-25 12:41:21 +0300

    Libdoc: Show enum members in HTML output.
    Only show max four member, if there are more, replace them with `...`.
    This can still be fine-tuned before 3.2.2 and enhanced even more in 4.0.
    
    Also separate possible default value from type information using ` = `,
    not just with `=` like when there's no type info. This is what PEP-8
    recommends with Python code and looks better in our case as well.
    
    See #3637 for futher discussion.

--- atest/testdata/libdoc/Annotations.py
+++ atest/testdata/libdoc/Annotations.py
@@ -1,27 +1,51 @@
+from enum import Enum
+
+
 class UnknownType(object):
     pass
+
+
+class Small(Enum):
+    one = 1
+    two = 2
+    three = 3
+    four = 4
+
+
+class Big(Enum):
+    A = 1
+    B = 2
+    C = 3
+    D = 4
+    E = 5
+    F = 6
+    G = 7
 
 
 def A_type_annotation(integer: int, boolean: bool, string: str):
     pass
 
 
-def B_annotation_and_default(integer: int=42, list_: list=None):
+def B_enum(small: Small, big: Big):
     pass
 
 
-def C_annotated_kw_only_args(*, kwo: int, with_default: str='value'):
+def C_annotation_and_default(integer: int = 42, list_: list = None, enum: Small = None):
     pass
 
 
-def D_annotated_varags_and_kwargs(*varargs: int, **kwargs: bool):
+def D_annotated_kw_only_args(*, kwo: int, with_default: str='value'):
     pass
 
 
-def E_unknown_types(unknown: UnknownType, unrecognized: Ellipsis):
+def E_annotated_varags_and_kwargs(*varargs: int, **kwargs: bool):
     pass
 
 
-def F_non_type_annotations(arg: 'One of the usages in PEP-3107',
+def F_unknown_types(unknown: UnknownType, unrecognized: Ellipsis):
+    pass
+
+
+def G_non_type_annotations(arg: 'One of the usages in PEP-3107',
                            *varargs: 'But surely feels odd...'):
     pass
