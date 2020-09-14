fa78485b8d1a3a6447b08643260f757adb4ca91b
Pekka Klärck <peke@iki.fi>
2020-09-14 13:56:57 +0300

    RF 4.0 development has officially started!

--- src/robot/version.py
+++ src/robot/version.py
@@ -18,7 +18,7 @@ import sys
 
 # Version number typically updated by running `invoke set-version <version>`.
 # Run `invoke --help set-version` or see tasks.py for details.
-VERSION = '3.2.3.dev1'
+VERSION = '4.0.dev1'
 
 
 def get_version(naked=False):
