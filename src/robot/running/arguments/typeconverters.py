078fb43447c0e0c2cb9603e17eb5f433d1f28f5c
Mikko Korpela <mikko.korpela@gmail.com>
2020-10-22 16:09:18 +0300

    int float conversions to bytes booleans, bytearrays

--- src/robot/running/arguments/typeconverters.py
+++ src/robot/running/arguments/typeconverters.py
@@ -12,7 +12,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-
+import sys
 from ast import literal_eval
 from collections import OrderedDict
 try:
@@ -128,8 +128,11 @@ class BooleanConverter(TypeConverter):
     type = bool
     type_name = 'boolean'
     aliases = ('bool',)
+    value_types = (unicode, float, int)
 
     def _convert(self, value, explicit_type=True):
+        if isinstance(value, (float, int)):
+            return bool(value)
         upper = value.upper()
         if upper in TRUE_STRINGS:
             return True
@@ -190,15 +193,23 @@ class DecimalConverter(TypeConverter):
             raise ValueError
 
 
+
+def _int_to_bytes(value):
+    return value.to_bytes((value.bit_length() // 8) + 1, byteorder=sys.byteorder)
+
+
 @TypeConverter.register
 class BytesConverter(TypeConverter):
     type = bytes
     abc = getattr(abc, 'ByteString', None)    # ByteString is new in Python 3
     type_name = 'bytes'                       # Needed on Python 2
+    value_types = (unicode, int)
 
     def _convert(self, value, explicit_type=True):
         if PY2 and not explicit_type:
             return value
+        if not PY2 and isinstance(value, int):
+            return _int_to_bytes(value)
         try:
             value = value.encode('latin-1')
         except UnicodeEncodeError as err:
@@ -210,8 +221,11 @@ class BytesConverter(TypeConverter):
 @TypeConverter.register
 class ByteArrayConverter(TypeConverter):
     type = bytearray
+    value_types = (unicode, int)
 
     def _convert(self, value, explicit_type=True):
+        if not PY2 and isinstance(value, int):
+            return bytearray(_int_to_bytes(value))
         try:
             return bytearray(value, 'latin-1')
         except UnicodeEncodeError as err:
