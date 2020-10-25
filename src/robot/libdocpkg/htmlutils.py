bcfc4c76312d5513e3060b5b5bb39c414385dd86
René <snooz@posteo.de>
2020-10-25 15:41:25 +0100

    mod copyright

--- src/robot/libdocpkg/htmlutils.py
+++ src/robot/libdocpkg/htmlutils.py
@@ -1,4 +1,5 @@
-#  Copyright 2020-     Robot Framework Foundation
+#  Copyright 2008-2015 Nokia Networks
+#  Copyright 2016-     Robot Framework Foundation
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
