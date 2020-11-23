237aa6e2f8a567670e0480f33841f44728252cc8
Pekka Klärck <peke@iki.fi>
2020-11-23 19:08:26 +0200

    Little code cleanup

--- src/robot/result/model.py
+++ src/robot/result/model.py
@@ -36,8 +36,9 @@ from itertools import chain
 from operator import attrgetter
 import warnings
 
+from robot import model
 from robot.model import TotalStatisticsBuilder, Messages, Keywords
-from robot import model, utils
+from robot.utils import get_elapsed_time, setter
 
 from .configurer import SuiteConfigurer
 from .messagefilter import MessageFilter
@@ -63,26 +64,23 @@ class Keyword(model.Keyword):
 
     See the base class for documentation of attributes not documented here.
     """
-    __slots__ = ['kwname', 'libname', 'status', 'starttime', 'endtime', 'message', 'lineno', 'source']
+    __slots__ = ['kwname', 'libname', 'status', 'starttime', 'endtime', 'message',
+                 'lineno', 'source']
     keyword_class = None        #: Internal usage only.
     message_class = Message     #: Internal usage only.
 
-    def __init__(self, kwname='', libname='', doc='', args=(), assign=(),
-                 tags=(), timeout=None, type='kw',  status='FAIL',
-                 starttime=None, endtime=None, parent=None,
-                 lineno=None, source=None):
-        model.Keyword.__init__(self, '', doc, args, assign, tags, timeout,
-                               type, parent)
-        self.parent = parent
+    def __init__(self, kwname='', libname='', doc='', args=(), assign=(), tags=(),
+                 timeout=None, type='kw', status='FAIL', starttime=None, endtime=None,
+                 parent=None, lineno=None, source=None):
+        model.Keyword.__init__(self, '', doc, args, assign, tags, timeout, type, parent)
         self.messages = None
         self.keywords = None
         #: Name of the keyword without library or resource name.
         self.kwname = kwname or ''
         #: Name of the library or resource containing this keyword.
         self.libname = libname or ''
-        #: Execution status as a string. Typically ``PASS`` or ``FAIL``, but
-        #: library keywords have status ``NOT_RUN`` in the dry-ryn mode.
-        #: See also :attr:`passed`.
+        #: Execution status as a string. Typically ``PASS``, ``FAIL`` or ``SKIP``,
+        #: but library keywords have status ``NOT_RUN`` in the dry-ryn mode.
         self.status = status
         #: Keyword execution start time in format ``%Y%m%d %H:%M:%S.%f``.
         self.starttime = starttime
@@ -93,12 +91,12 @@ class Keyword(model.Keyword):
         self.lineno = lineno
         self.source = source
 
-    @utils.setter
+    @setter
     def keywords(self, keywords):
         """Child keywords as a :class:`~.Keywords` object."""
         return Keywords(self.keyword_class or self.__class__, self, keywords)
 
-    @utils.setter
+    @setter
     def messages(self, messages):
         """Messages as a :class:`~.model.message.Messages` object."""
         return Messages(self.message_class, self, messages)
@@ -115,7 +113,7 @@ class Keyword(model.Keyword):
     @property
     def elapsedtime(self):
         """Total execution time in milliseconds."""
-        return utils.get_elapsed_time(self.starttime, self.endtime)
+        return get_elapsed_time(self.starttime, self.endtime)
 
     @property
     def name(self):
@@ -190,7 +188,7 @@ class TestCase(model.TestCase):
     @property
     def elapsedtime(self):
         """Total execution time in milliseconds."""
-        return utils.get_elapsed_time(self.starttime, self.endtime)
+        return get_elapsed_time(self.starttime, self.endtime)
 
     @property
     def passed(self):
@@ -313,7 +311,7 @@ class TestSuite(model.TestSuite):
     def elapsedtime(self):
         """Total execution time in milliseconds."""
         if self.starttime and self.endtime:
-            return utils.get_elapsed_time(self.starttime, self.endtime)
+            return get_elapsed_time(self.starttime, self.endtime)
         return sum(child.elapsedtime for child in
                    chain(self.suites, self.tests, (self.setup, self.teardown)))
 
