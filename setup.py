b7b6852e0c1bae0e5d4f4845bea49f7b263c1f0a
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-10-19 17:19:49 +0300

    Add 'Programming Language :: Python :: 3 :: Only' Trove classifier

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ Development Status :: 5 - Production/Stable
 License :: OSI Approved :: Apache Software License
 Operating System :: OS Independent
 Programming Language :: Python :: 3
+Programming Language :: Python :: 3 :: Only
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
