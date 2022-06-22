robotframework/robotframework

commit b72a399a33b241161597c5a3c55091d5707526bf

Support localized BDD-prefixes (#519)
Add Finnish translations for the prefixes

Also make space after BDD/prefix manadatory, fixes #4379

src/robot/running/namespace.py
@@ -43,7 +43,7 @@ class Namespace:
         self.variables = variables
         self.languages = languages
         self._imports = resource.imports
-        self._kw_store = KeywordStore(resource)
+        self._kw_store = KeywordStore(resource, languages)
         self._imported_variable_files = ImportCache()
         self._suite_name = suite.longname
         self._running_test = False
@@ -223,11 +223,12 @@ class Namespace:
 
 class KeywordStore:
 
-    def __init__(self, resource):
+    def __init__(self, resource, languages):
         self.user_keywords = UserLibrary(resource, UserLibrary.TEST_CASE_FILE_TYPE)
         self.libraries = OrderedDict()
         self.resources = ImportCache()
         self.search_order = ()
+        self.languages = languages
 
     def get_library(self, name_or_instance):
         if name_or_instance is None:
@@ -294,10 +295,13 @@ class KeywordStore:
         return runner
 
     def _get_bdd_style_runner(self, name):
-        lower = name.lower()
-        for prefix in ['given ', 'when ', 'then ', 'and ', 'but ']:
-            if lower.startswith(prefix):
-                runner = self._get_runner(name[len(prefix):])
+        parts = name.split(maxsplit=1)
+        if len(parts) == 1:
+            return None
+        prefix, keyword = parts
+        for lang in self.languages:
+            if prefix.title() in lang.bdd_prefixes:
+                runner = self._get_runner(keyword)
                 if runner:
                     runner = copy.copy(runner)
                     runner.name = name

atest/robot/keywords/optional_given_when_then.robot
@@ -1,5 +1,5 @@
 *** Settings ***
-Suite Setup     Run Tests  ${EMPTY}  keywords/optional_given_when_then.robot
+Suite Setup     Run Tests  --lang fi  keywords/optional_given_when_then.robot
 Resource        atest_resource.robot
 
 *** Test Cases ***
@@ -46,3 +46,14 @@ Keyword can be used with and without prefix
     Should Be Equal  ${tc.kws[5].name}  Then we are in Berlin city
     Should Be Equal  ${tc.kws[6].name}  we are in Berlin city
 
+In user keyword name with normal arguments and localized prefixes
+    ${tc} =  Check Test Case  ${TEST NAME}
+    Should Be Equal  ${tc.kws[0].name}  Oletetaan we don't drink too many beers
+    Should Be Equal  ${tc.kws[1].name}  Kun we are in
+    Should Be Equal  ${tc.kws[2].name}  mutta we don't drink too many beers
+    Should Be Equal  ${tc.kws[3].name}  Ja time
+    Should Be Equal  ${tc.kws[4].name}  Niin we get this feature ready today
+    Should Be Equal  ${tc.kws[5].name}  ja we don't drink too many beers
+
+Prefix must be followed by space
+    Check Test Case  ${TEST NAME}

atest/testdata/keywords/optional_given_when_then.robot
@@ -42,6 +42,21 @@ Keyword can be used with and without prefix
     Then we are in Berlin city
     we are in Berlin city
 
+In user keyword name with normal arguments and localized prefixes
+    Oletetaan we don't drink too many beers
+    Kun we are in  museum  cafe
+    mutta we don't drink too many beers
+    Ja time  does  not  run  out
+    Niin we get this feature ready today
+    ja we don't drink too many beers
+
+Prefix must be followed by space
+    [Documentation]  FAIL
+    ...    No keyword with name 'Givenwe don't drink too many beers' found. Did you mean:
+    ...    ${SPACE*4}We Don't Drink Too Many Beers
+    Givenwe don't drink too many beers
+
+
 *** Keywords ***
 We don't drink too many beers
     No Operation
@@ -68,4 +83,3 @@ We ${x} This ${thing} Implemented
 
 We Go To ${somewhere}
     Should Be Equal  ${somewhere}  walking tour
-

src/robot/conf/languages.py
@@ -175,4 +175,4 @@ class Fi(Language):
     timeout = 'Aikaraja'
     arguments = 'Argumentit'
     return_ = 'Paluuarvo'
-    bdd_prefixes = {}
+    bdd_prefixes = {'Oletetaan', 'Kun', 'Niin', 'Ja', 'Mutta'}

src/robot/parsing/lexer/markers.py
@@ -17,6 +17,7 @@ from robot.conf import Languages
 
 
 class Markers:
+    # FIXME: should this be merged with conf.Languages
 
     def __init__(self, languages):
         if not isinstance(languages, Languages):

commit a8b980d78f198d7d5eaa782260364853b1f6f6ae

Fix bug introduced in b72a399a33b241161597c5a3c55091d5707526bf

src/robot/running/namespace.py
@@ -296,7 +296,7 @@ class KeywordStore:
 
     def _get_bdd_style_runner(self, name):
         parts = name.split(maxsplit=1)
-        if len(parts) == 1:
+        if len(parts) < 2:
             return None
         prefix, keyword = parts
         for lang in self.languages: