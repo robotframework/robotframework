robotframework/robotframework

commit 09ca68f431c513bded1077d0dcbd94976bc0297b

Enhancements to public `robot.api.Languages´ API (#4496) #4494

atest/robot/parsing/translations.robot
@@ -20,8 +20,12 @@ Custom
     Validate Translations
 
 Custom task aliases
-    Run Tests    --lang ${DATADIR}/parsing/translations/custom/custom.py --rpa    parsing/translations/custom
+    Run Tests    --lang ${DATADIR}/parsing/translations/custom/custom.py --rpa    parsing/translations/custom/tasks.robot
     Validate Task Translations
+
+Custom Per file configuration
+    Run Tests    -P ${DATADIR}/parsing/translations/custom    parsing/translations/custom/custom_per_file.robot
+    Validate Translations
 
 Invalid
     ${result} =    Run Tests Without Processing Output    --lang bad    parsing/finnish.robot
@@ -44,8 +48,9 @@ Per file configuration with multiple languages
     Should Be Equal    ${tc.doc}    приклад
 
 Invalid per file configuration
+    Run Tests    ${EMPTY}    parsing/translations/per_file_config/many.robot
     Error in file    0    parsing/translations/per_file_config/many.robot    4
-    ...    Invalid language configuration: No language with name 'invalid' found.
+    ...    Invalid language configuration: Language "invalid" not found nor importable as a module.
 
 Per file configuration bleeds to other files
     [Documentation]    This is a technical limitation and will hopefully change!

src/robot/conf/languages.py
@@ -16,34 +16,49 @@
 import inspect
 import os.path
 
+from robot.errors import DataError
 from robot.utils import is_list_like, Importer, normalize
 
 
 class Languages:
+    """Keeps a list of languages and unifies the translations in the properties.
 
-    def __init__(self, languages=None):
+    :param languages: a language or a list of languages.
+        Can be the name, the code or an instance.
+    :type languages: str, class: Language, list[str, class: Language], optional
+    :param add_default: if True the default language (En) and some aliases is (Default: True)
+    :type add_default: bool, optional
+
+    Example::
+
+        languages = Languages(["de"])
+        print(languages.settings)
+    """
+
+    def __init__(self, languages=None, add_english=True):
         self.languages = []
-        # The English singular forms are added for backwards compatibility
-        self.headers = {
-            'Setting': 'Settings',
-            'Variable': 'Variables',
-            'Test Case': 'Test Cases',
-            'Task': 'Tasks',
-            'Keyword': 'Keywords',
-            'Comment': 'Comments'
-        }
+        self.headers = {}
         self.settings = {}
         self.bdd_prefixes = set()
         self.true_strings = {'True', '1'}
         self.false_strings = {'False', '0', 'None', ''}
-        for lang in self._get_languages(languages):
+        for lang in self._get_languages(languages, add_english):
             self._add_language(lang)
 
-    def reset(self, languages=None):
-        self.__init__(languages)
+    def reset(self, languages=None, add_english=True):
+        """Resets the instance to the given languages."""
+        self.__init__(languages, add_english)
 
     def add_language(self, name):
-        self._add_language(Language.from_name(name))
+        try:
+            languages = [Language.from_name(name)]
+        except ValueError:
+            try:
+                languages = self._import_languages(name)
+            except DataError:
+                raise ValueError(f'Language "{name}" not found nor importable as a module.')
+        for lang in languages:
+            self._add_language(lang)
 
     def _add_language(self, lang):
         if lang in self.languages:
@@ -55,8 +70,8 @@ class Languages:
         self.true_strings |= {s.title() for s in lang.true_strings}
         self.false_strings |= {s.title() for s in lang.false_strings}
 
-    def _get_languages(self, languages):
-        languages = self._resolve_languages(languages)
+    def _get_languages(self, languages, add_english=True):
+        languages = self._resolve_languages(languages, add_english)
         available = self._get_available_languages()
         returned = []
         for lang in languages:
@@ -70,14 +85,24 @@ class Languages:
                     returned.extend(self._import_languages(lang))
         return returned
 
-    def _resolve_languages(self, languages):
+    def _resolve_languages(self, languages, add_english=True):
         if not languages:
             languages = []
         elif is_list_like(languages):
             languages = list(languages)
         else:
             languages = [languages]
-        languages.append(En())
+        if add_english:
+            languages.append(En())
+            # The English singular forms are added for backwards compatibility
+            self.headers = {
+                'Setting': 'Settings',
+                'Variable': 'Variables',
+                'Test Case': 'Test Cases',
+                'Task': 'Tasks',
+                'Keyword': 'Keywords',
+                'Comment': 'Comments'
+            }
         return languages
 
     def _get_available_languages(self):
@@ -97,6 +122,7 @@ class Languages:
             lang = os.path.abspath(lang)
         module = Importer('language file').import_module(lang)
         return [value() for _, value in inspect.getmembers(module, is_language)]
+
 
     def __iter__(self):
         return iter(self.languages)

utest/api/test_languages.py
@@ -1,5 +1,7 @@
 import unittest
 
+from os.path import abspath, dirname, join
+
 from robot.api import Language, Languages
 from robot.conf.languages import En, Fi, PtBr, Th
 from robot.utils.asserts import assert_equal, assert_not_equal, assert_raises_with_msg
@@ -85,6 +87,12 @@ class TestLanguages(unittest.TestCase):
         assert_equal(list(Languages(['fi'])), [Fi(), En()])
         assert_equal(list(Languages(['fi', PtBr()])), [Fi(), PtBr(), En()])
 
+    def test_init_without_default(self):
+        assert_equal(list(Languages(add_english=False)), [])
+        assert_equal(list(Languages('fi', add_english=False)), [Fi()])
+        assert_equal(list(Languages(['fi'], add_english=False)), [Fi()])
+        assert_equal(list(Languages(['fi', PtBr()], add_english=False)), [Fi(), PtBr()])
+
     def test_reset(self):
         langs = Languages(['fi'])
         langs.reset()
@@ -94,6 +102,15 @@ class TestLanguages(unittest.TestCase):
         langs.reset(['fi', PtBr()])
         assert_equal(list(langs), [Fi(), PtBr(), En()])
 
+    def test_reset_with_default(self):
+        langs = Languages(['fi'])
+        langs.reset(add_english=False)
+        assert_equal(list(langs), [])
+        langs.reset('fi', add_english=False)
+        assert_equal(list(langs), [Fi()])
+        langs.reset(['fi', PtBr()], add_english=False)
+        assert_equal(list(langs), [Fi(), PtBr()])
+
     def test_duplicates_are_not_added(self):
         langs = Languages(['Finnish', 'en', Fi(), 'pt-br'])
         assert_equal(list(langs), [Fi(), En(), PtBr()])
@@ -101,6 +118,20 @@ class TestLanguages(unittest.TestCase):
         assert_equal(list(langs), [Fi(), En(), PtBr()])
         langs.add_language('th')
         assert_equal(list(langs), [Fi(), En(), PtBr(), Th()])
+
+    def test_add_language_with_custom_module(self):
+        data = join(abspath(dirname(__file__)), 'orcish_languages.py')
+        langs = Languages()
+        langs.add_language(data)
+        self.assertIn(("Orcish Loud", "or-CLOU"), [(v.name, v.code) for v in langs])
+        self.assertIn(("Orcish Quiet", "or-CQUI"), [(v.name, v.code) for v in langs])
+
+    def test_add_language_with_invalid_custom_module(self):
+        langs = Languages()
+        with self.assertRaises(ValueError) as context:
+            langs.add_language("invalid")
+        self.assertTrue('Language "invalid" not found nor importable as a module.' in context.exception.args)
+
 
 
 if __name__ == '__main__':

utest/parsing/test_lexer.py
@@ -2290,7 +2290,7 @@ Dokumentaatio    Documentation
 '''
         expected = [
             (T.ERROR, 'language: in:va:lid', 1, 0,
-             "Invalid language configuration: No language with name 'in:va:lid' found."),
+             'Invalid language configuration: Language "in:va:lid" not found nor importable as a module.'),
             (T.EOL, '\n', 1, 19),
             (T.EOS, '', 1, 20),
             (T.COMMENT, 'language: bad again', 2, 0),

utest/parsing/test_model.py
@@ -1287,7 +1287,7 @@ Dokumentaatio    Header is de and setting is fi.
                     ]),
                     Error([
                         Token('ERROR', 'language: bad', 2, 0,
-                              "Invalid language configuration: No language with name 'bad' found."),
+                              'Invalid language configuration: Language "bad" not found nor importable as a module.'),
                         Token('EOL', '\n', 2, 13)
                     ]),
                     Comment([

atest/testdata/parsing/translations/custom/custom_per_file.robot
@@ -1,0 +1,61 @@
+language: custom
+*** H S ***
+D                  Suite documentation.
+M                  Metadata    Value
+S S                Suite Setup
+S T                Suite Teardown
+T S                Test Setup
+T Tea              Test Teardown
+t tem              Test Template
+T ti               1 minute
+t Ta               test    tags
+k T                keyword    tags
+L                  OperatingSystem
+R                  resource.resource
+V                  ../../variables.py
+
+*** h v ***
+${VARIABLE}        variable value
+
+*** H TE ***
+Test without settings
+    Nothing to see here
+
+Test with settings
+    [D]            Test documentation.
+    [Ta]           own tag
+    [S]            NONE
+    [tea]          NONE
+    [tEm]          NONE
+    [ti]           NONE
+    Keyword        ${VARIABLE}
+
+*** h K ***
+Suite Setup
+    Directory Should Exist    ${CURDIR}
+
+Suite Teardown
+    Keyword In Resource
+
+Test Setup
+    Should Be Equal    ${VARIABLE}         variable value
+    Should Be Equal    ${RESOURCE FILE}    variable in resource file
+    Should Be Equal    ${VARIABLE FILE}    variable in variable file
+
+Test Teardown
+    No Operation
+
+Test Template
+    [A]    ${message}
+    Log    ${message}
+
+Keyword
+    [d]      Keyword documentation.
+    [a]      ${arg}
+    [ta]     own tag
+    [tI]     1h
+    Should Be Equal    ${arg}    ${VARIABLE}
+    [TEA]    No Operation
+
+*** H C ***
+Ignored comments.

utest/api/orcish_languages.py
@@ -1,0 +1,9 @@
+from robot.api import Language
+
+class OrcQui(Language):
+    """Orcish Quiet"""
+    settings_header="Jiivo"
+
+class OrcLou(Language):
+    """Orcish Loud"""
+    settings_header="JIIVA"

commit 7b6d8197600862e142a9cafa499bcc75e7027772

Some further enhancements to public robot.api.Languages API.
- More doc strings.
- Change exception used by `Languages.add_language` on error to match
  exception used when initializing `Languages`.
- Make `Language.code/name` propertys accessible also from class.
  This required implementing new `@classproperty` decorator.

This is part of issue #4494 and the work was started in #4496.

atest/robot/parsing/translations.robot
@@ -50,7 +50,8 @@ Per file configuration with multiple languages
 Invalid per file configuration
     Run Tests    ${EMPTY}    parsing/translations/per_file_config/many.robot
     Error in file    0    parsing/translations/per_file_config/many.robot    4
-    ...    Invalid language configuration: Language "invalid" not found nor importable as a module.
+    ...    Invalid language configuration:
+    ...    Language 'invalid' not found nor importable as a language module.
 
 Per file configuration bleeds to other files
     [Documentation]    This is a technical limitation and will hopefully change!

src/robot/conf/languages.py
@@ -17,25 +17,30 @@ import inspect
 import os.path
 
 from robot.errors import DataError
-from robot.utils import is_list_like, Importer, normalize
+from robot.utils import classproperty, is_list_like, Importer, normalize
 
 
 class Languages:
     """Keeps a list of languages and unifies the translations in the properties.
 
-    :param languages: a language or a list of languages.
-        Can be the name, the code or an instance.
-    :type languages: str, class: Language, list[str, class: Language], optional
-    :param add_default: if True the default language (En) and some aliases is (Default: True)
-    :type add_default: bool, optional
-
     Example::
 
-        languages = Languages(["de"])
+        languages = Languages('de', add_english=False)
         print(languages.settings)
+        languages = Languages(['pt-BR', 'Finnish', 'MyLang.py'])
+        print(list(languages))
     """
 
     def __init__(self, languages=None, add_english=True):
+        """
+        :param languages: Initial language or list of languages.
+            Languages can be given as language codes or names, paths or names of
+            language modules to load, or as :class:`Language` instances.
+        :param add_english: If True, English is added automatically.
+        :raises :class:`~robot.errors.DataError` if a given language is not found.
+
+        :meth:`add.language` can be used to add languages after initialization.
+        """
         self.languages = []
         self.headers = {}
         self.settings = {}
@@ -50,13 +55,23 @@ class Languages:
         self.__init__(languages, add_english)
 
     def add_language(self, name):
+        """Add new language.
+
+        :param name: Name or code of a language to add, or name or path of
+            a language module to load.
+        :raises: :class:`~robot.errors.DataError` if the language is not found.
+
+        Language codes and names are passed to by :meth:`Language.from_name`.
+        Language modules are imported and :class:`Language` subclasses in them
+        loaded.
+        """
         try:
             languages = [Language.from_name(name)]
-        except ValueError:
+        except ValueError as err1:
             try:
                 languages = self._import_languages(name)
-            except DataError:
-                raise ValueError(f'Language "{name}" not found nor importable as a module.')
+            except DataError as err2:
+                raise DataError(f'{err1} {err2}')
         for lang in languages:
             self._add_language(lang)
 
@@ -108,9 +123,10 @@ class Languages:
     def _get_available_languages(self):
         available = {}
         for lang in Language.__subclasses__():
-            available[normalize(lang.__name__)] = lang
-            if lang.__doc__:
-                available[normalize(lang.__doc__.splitlines()[0])] = lang
+            available[normalize(lang.code, ignore='-')] = lang
+            available[normalize(lang.name)] = lang
+        if '' in available:
+            available.pop('')
         return available
 
     def _import_languages(self, lang):
@@ -122,7 +138,6 @@ class Languages:
             lang = os.path.abspath(lang)
         module = Importer('language file').import_module(lang)
         return [value() for _, value in inspect.getmembers(module, is_language)]
-
 
     def __iter__(self):
         return iter(self.languages)
@@ -193,26 +208,26 @@ class Language:
                 return lang()
         raise ValueError(f"No language with name '{name}' found.")
 
-    @property
-    def code(self):
+    @classproperty
+    def code(cls):
         """Language code like 'fi' or 'pt-BR'.
 
         Got based on the class name. If the class name is two characters (or less),
         the code is just the name in lower case. If it is longer, a hyphen is added
         remainder of the class name is upper-cased.
         """
-        code = type(self).__name__.lower()
+        code = cls.__name__.lower()
         if len(code) < 3:
             return code
         return f'{code[:2]}-{code[2:].upper()}'
 
-    @property
-    def name(self):
+    @classproperty
+    def name(cls):
         """Language name like 'Finnish' or 'Brazilian Portuguese'.
 
         Got from the first line of the class docstring.
         """
-        return self.__doc__.splitlines()[0] if self.__doc__ else ''
+        return cls.__doc__.splitlines()[0] if cls.__doc__ else ''
 
     @property
     def headers(self):
@@ -926,7 +941,7 @@ class Tr(Language):
     documentation_setting = 'Dokümantasyon'
     metadata_setting = 'Üstveri'
     suite_setup_setting = 'Takım Kurulumu'
-    suite_teardown_setting =  'Takım Bitişi'
+    suite_teardown_setting = 'Takım Bitişi'
     test_setup_setting = 'Test Kurulumu'
     task_setup_setting = 'Görev Kurulumu'
     test_teardown_setting = 'Test Bitişi'

src/robot/parsing/lexer/statementlexers.py
@@ -13,8 +13,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import re
-
+from robot.errors import DataError
 from robot.utils import normalize_whitespace
 from robot.variables import is_assign
 
@@ -122,8 +121,11 @@ class ImplicitCommentLexer(CommentLexer):
             lang = statement[0].value.split(':', 1)[1].strip()
             try:
                 self.ctx.add_language(lang)
-            except ValueError as err:
-                statement[0].set_error(f'Invalid language configuration: {err}')
+            except DataError:
+                statement[0].set_error(
+                    f"Invalid language configuration: "
+                    f"Language '{lang}' not found nor importable as a language module."
+                )
             else:
                 statement[0].type = Token.CONFIG
 

src/robot/utils/__init__.py
@@ -49,8 +49,8 @@ from .markuputils import html_format, html_escape, xml_escape, attribute_escape
 from .markupwriters import HtmlWriter, XmlWriter, NullMarkupWriter
 from .importer import Importer
 from .match import eq, Matcher, MultiMatcher
-from .misc import (isatty, parse_re_flags, plural_or_not, printable_name,seq2str,
-                   seq2str2, test_or_task)
+from .misc import (classproperty, isatty, parse_re_flags, plural_or_not,
+                   printable_name, seq2str, seq2str2, test_or_task)
 from .normalizing import normalize, normalize_whitespace, NormalizedDict
 from .platform import PY_VERSION, PYPY, UNIXY, WINDOWS, RERAISED_EXCEPTIONS
 from .recommendations import RecommendationFinder

src/robot/utils/misc.py
@@ -13,7 +13,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from operator import add, sub
 import re
 
 from .robottypes import is_integer
@@ -125,6 +124,7 @@ def isatty(stream):
     except ValueError:    # Occurs if file is closed.
         return False
 
+
 def parse_re_flags(flags=None):
     result = 0
     if not flags:
@@ -140,3 +140,30 @@ def parse_re_flags(flags=None):
             else:
                 raise ValueError(f'Unknown regexp flag: {flag}')
     return result
+
+
+class classproperty(property):
+    """Property that works with classes in addition to instances.
+
+    Only supports getters. Setters and deleters cannot work with classes due
+    to how the descriptor protocol works, and they are thus explicitly disabled.
+    Metaclasses must be used if they are needed.
+    """
+
+    def __init__(self, fget, fset=None, fdel=None, doc=None):
+        if fset:
+            self.setter(fset)
+        if fdel:
+            self.deleter(fset)
+        super().__init__(fget)
+        if doc:
+            self.__doc__ = doc
+
+    def __get__(self, instance, owner):
+        return self.fget(owner)
+
+    def setter(self, fset):
+        raise TypeError('Setters are not supported.')
+
+    def deleter(self, fset):
+        raise TypeError('Deleters are not supported.')

utest/api/test_languages.py
@@ -4,20 +4,26 @@ from os.path import abspath, dirname, join
 
 from robot.api import Language, Languages
 from robot.conf.languages import En, Fi, PtBr, Th
-from robot.utils.asserts import assert_equal, assert_not_equal, assert_raises_with_msg
+from robot.errors import DataError
+from robot.utils.asserts import (assert_equal, assert_not_equal, assert_true,
+                                 assert_raises_with_msg)
 
 
 class TestLanguage(unittest.TestCase):
 
     def test_one_part_code(self):
         assert_equal(Fi().code, 'fi')
+        assert_equal(Fi.code, 'fi')
 
     def test_two_part_code(self):
         assert_equal(PtBr().code, 'pt-BR')
+        assert_equal(PtBr.code, 'pt-BR')
 
     def test_name(self):
         assert_equal(Fi().name, 'Finnish')
+        assert_equal(Fi.name, 'Finnish')
         assert_equal(PtBr().name, 'Brazilian Portuguese')
+        assert_equal(PtBr.name, 'Brazilian Portuguese')
 
     def test_name_with_multiline_docstring(self):
         class X(Language):
@@ -26,18 +32,21 @@ class TestLanguage(unittest.TestCase):
             Other lines are ignored.
             """
         assert_equal(X().name, 'Language Name')
+        assert_equal(X.name, 'Language Name')
 
     def test_name_without_docstring(self):
         class X(Language):
             pass
         X.__doc__ = None
         assert_equal(X().name, '')
+        assert_equal(X.name, '')
 
     def test_all_standard_languages_have_code_and_name(self):
         for cls in Language.__subclasses__():
-            lang = cls()
-            assert lang.code
-            assert lang.name
+            assert cls().code
+            assert cls.code
+            assert cls().name
+            assert cls.name
 
     def test_eq(self):
         assert_equal(Fi(), Fi())
@@ -127,11 +136,12 @@ class TestLanguages(unittest.TestCase):
         self.assertIn(("Orcish Quiet", "or-CQUI"), [(v.name, v.code) for v in langs])
 
     def test_add_language_with_invalid_custom_module(self):
-        langs = Languages()
-        with self.assertRaises(ValueError) as context:
-            langs.add_language("invalid")
-        self.assertTrue('Language "invalid" not found nor importable as a module.' in context.exception.args)
-
+        with self.assertRaises(DataError) as context:
+            Languages().add_language('invalid')
+        assert_true(context.exception.args[0].startswith(
+            "No language with name 'invalid' found. "
+            "Importing language file 'invalid' failed: "
+        ))
 
 
 if __name__ == '__main__':

utest/parsing/test_lexer.py
@@ -2290,7 +2290,8 @@ Dokumentaatio    Documentation
 '''
         expected = [
             (T.ERROR, 'language: in:va:lid', 1, 0,
-             'Invalid language configuration: Language "in:va:lid" not found nor importable as a module.'),
+             "Invalid language configuration: Language 'in:va:lid' not found "
+             "nor importable as a language module."),
             (T.EOL, '\n', 1, 19),
             (T.EOS, '', 1, 20),
             (T.COMMENT, 'language: bad again', 2, 0),
@@ -2315,6 +2316,7 @@ Dokumentaatio    Documentation
         assert_tokens(data, expected, get_init_tokens, lang=lang)
         assert_equal(lang.languages,
                      [Language.from_name(lang) for lang in ('en', 'fi')])
+
 
 if __name__ == '__main__':
     unittest.main()

utest/parsing/test_model.py
@@ -1268,7 +1268,7 @@ Example
 
 class TestLanguageConfig(unittest.TestCase):
 
-    def test_valid(self):
+    def test_config(self):
         model = get_model('''\
 language: fi
 language: bad
@@ -1287,7 +1287,8 @@ Dokumentaatio    Header is de and setting is fi.
                     ]),
                     Error([
                         Token('ERROR', 'language: bad', 2, 0,
-                              'Invalid language configuration: Language "bad" not found nor importable as a module.'),
+                              "Invalid language configuration: Language 'bad' "
+                              "not found nor importable as a language module."),
                         Token('EOL', '\n', 2, 13)
                     ]),
                     Comment([

utest/utils/test_misc.py
@@ -1,9 +1,9 @@
 import re
 import unittest
 
-from robot.utils import (parse_re_flags, plural_or_not, printable_name,
+from robot.utils import (classproperty, parse_re_flags, plural_or_not, printable_name,
                          seq2str, test_or_task)
-from robot.utils.asserts import assert_equal, assert_raises_with_msg
+from robot.utils.asserts import assert_equal, assert_raises, assert_raises_with_msg
 
 
 class TestSeg2Str(unittest.TestCase):
@@ -96,7 +96,6 @@ class TestPluralOrNot(unittest.TestCase):
             assert_equal(plural_or_not(plural), 's')
 
 
-
 class TestTestOrTask(unittest.TestCase):
 
     def test_no_match(self):
@@ -146,6 +145,71 @@ class TestParseReFlags(unittest.TestCase):
                              ('IGNORECASE|foo', 'Unknown regexp flag: foo'),
                              ('compile', 'Unknown regexp flag: compile')]:
             assert_raises_with_msg(ValueError, exp_msg, parse_re_flags, inp)
+
+
+class TestClassProperty(unittest.TestCase):
+
+    def setUp(self):
+        class Class:
+            @classproperty
+            def p(cls):
+                assert cls is Class
+                return 1
+        self.cls = Class
+
+    def test_get_from_class(self):
+        assert self.cls.p == 1
+
+    def test_get_from_instance(self):
+        assert self.cls().p == 1
+
+    def test_set_in_class_overrides(self):
+        # This cannot be avoided without using metaclasses.
+        self.cls.p = 2
+        assert self.cls.p == 2
+        assert self.cls().p == 2
+
+    def test_set_in_instance_fails(self):
+        assert_raises(AttributeError, setattr, self.cls(), 'p', 2)
+
+    def test_cannot_have_setter(self):
+        code = '''
+class Class:
+    @classproperty
+    def p(cls):
+        pass
+    @p.setter
+    def p(cls):
+        pass
+'''
+        assert_raises_with_msg(TypeError, 'Setters are not supported.',
+                               exec, code, globals())
+        assert_raises_with_msg(TypeError, 'Setters are not supported.',
+                               classproperty, lambda c: None, lambda c, v: None)
+
+    def test_cannot_have_deleter(self):
+        code = '''
+class Class:
+    @classproperty
+    def p(cls):
+        pass
+    @p.deleter
+    def p(cls):
+        pass
+'''
+        assert_raises_with_msg(TypeError, 'Deleters are not supported.',
+                               exec, code, globals())
+        assert_raises_with_msg(TypeError, 'Deleters are not supported.',
+                               classproperty, lambda c: None, None, lambda c, v: None)
+
+    def test_doc(self):
+        class Class(self.cls):
+            @classproperty
+            def p(cls):
+                """Doc for p."""
+            q = classproperty(lambda cls: None, doc='Doc for q.')
+        assert_equal(Class.__dict__['p'].__doc__, 'Doc for p.')
+        assert_equal(Class.__dict__['q'].__doc__, 'Doc for q.')
 
 
 if __name__ == "__main__":