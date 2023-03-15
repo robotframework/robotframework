69c880890ae80d2ac80bb51a812aae92c4c8bc65
Pekka Klärck <peke@iki.fi>
2023-03-15 02:30:07 +0200

    Increase output.xml schema version after recent changes

--- src/robot/output/xmllogger.py
+++ src/robot/output/xmllogger.py
@@ -35,7 +35,7 @@ class XmlLogger(ResultVisitor):
         writer.start('robot', {'generator': get_full_version(generator),
                                'generated': get_timestamp(),
                                'rpa': 'true' if rpa else 'false',
-                               'schemaversion': '3'})
+                               'schemaversion': '4'})
         return writer
 
     def close(self):
