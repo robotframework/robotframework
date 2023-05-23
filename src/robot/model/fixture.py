f9bfb542ff5c4101518bc882f3d20f7ab526dae2
Pekka Klärck <peke@iki.fi>
2023-05-24 01:23:40 +0300

    Update fixture.py

--- src/robot/model/fixture.py
+++ src/robot/model/fixture.py
@@ -20,6 +20,7 @@ if TYPE_CHECKING:
     from robot.model import DataDict, Keyword, TestCase, TestSuite
     from robot.running.model import UserKeyword
 
+
 T = TypeVar('T', bound='Keyword')
 
 
@@ -27,7 +28,7 @@ def create_fixture(fixture_class: Type[T],
                    fixture: 'T|DataDict|None',
                    parent: 'TestCase|TestSuite|Keyword|UserKeyword',
                    fixture_type: str) -> T:
-    """ Configures or creates a `fixture_class`instance """
+    """Create or configure a `fixture_class` instance."""
     # If a fixture instance has been passed in update the config
     if isinstance(fixture, fixture_class):
         return fixture.config(parent=parent, type=fixture_type)
