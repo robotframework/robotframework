c52b257066d77f924072afaf03267d90c3f0b620
Daniel Biehl <dbiehl@live.de>
2023-10-24 22:38:56 +0200

    Cache initialization in VisitorFinder moved to __init_subclass__ and handle generic_visit in VisitorFinder, but be safe from Liskov substitution principle

--- src/robot/parsing/model/visitor.py
+++ src/robot/parsing/model/visitor.py
@@ -13,24 +13,29 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import ast
-from typing import Any, Callable, Iterator
+from abc import ABC
+from ast import AST
+from collections import defaultdict
+from typing import Any, Callable, Generic, Iterator, TypeVar
 
 from ...utils.notset import NOT_SET, NotSet
 from .statements import Node
 
+TVisitorResult = TypeVar("TVisitorResult")
 
-class VisitorFinder:
-    __cls_finder_cache__: "dict[type[Any], Callable[..., Any]|None|NotSet]"
 
-    def __new__(cls, *_args: Any, **_kwargs: Any):  # type: ignore[no-untyped-def]
-        if not hasattr(cls, "__cls_finder_cache__"):
-            cls.__cls_finder_cache__ = {}
-        return super().__new__(cls)
+class VisitorFinder(ABC, Generic[TVisitorResult]):
+    __visitor_finder_cache: "dict[type[Any], Callable[[Node], Any]|None|NotSet]"
+    __default_visitor_method: "Callable[..., TVisitorResult]"
+
+    def __init_subclass__(cls, default_visitor_name: str = "generic_visit", **kwargs: Any) -> None:
+        super().__init_subclass__(**kwargs)
+        cls.__visitor_finder_cache = defaultdict(lambda: NOT_SET)
+        cls.__default_visitor_method = getattr(cls, default_visitor_name)
 
     @classmethod
-    def __find_visitor(cls, node_cls: "type[Any]") -> "Callable[..., Any]|None":
-        if node_cls is ast.AST:
+    def _find_visitor_class_method(cls, node_cls: "type[Any]") -> "Callable[..., TVisitorResult]|None":
+        if node_cls is AST:
             return None
         method_name = "visit_" + node_cls.__name__
         method = getattr(cls, method_name, None)
@@ -41,17 +46,17 @@ class VisitorFinder:
             if callable(method):
                 return method  # type: ignore[no-any-return]
         for base in node_cls.__bases__:
-            method = cls._find_visitor_cached(base)
+            method = cls._find_visitor_class_method(base)
             if method:
                 return method
         return None
 
     @classmethod
-    def _find_visitor_cached(cls, node_cls: "type[Any]") -> "Callable[..., Any]|None":
-        result = cls.__cls_finder_cache__.get(node_cls, NOT_SET)
+    def _find_visitor(cls, node_cls: "type[Any]") -> Callable[..., TVisitorResult]:
+        result = cls.__visitor_finder_cache[node_cls]
         if result is NOT_SET:
-            result = cls.__cls_finder_cache__[node_cls] = cls.__find_visitor(node_cls)
-        return result  # type: ignore[return-value]
+            result = cls.__visitor_finder_cache[node_cls] = cls._find_visitor_class_method(node_cls)
+        return result or cls.__default_visitor_method  # type: ignore[return-value]
 
 
 def _iter_field_values(node: Node) -> "Iterator[Node|list[Node]|None]":
@@ -70,7 +75,7 @@ def iter_fields(node: Node) -> "Iterator[tuple[str, Node|list[Node]|None]]":
             pass
 
 
-class ModelVisitor(VisitorFinder):
+class ModelVisitor(VisitorFinder[None]):
     """NodeVisitor that supports matching nodes based on their base classes.
 
     In other ways identical to the standard `ast.NodeVisitor
@@ -84,7 +89,7 @@ class ModelVisitor(VisitorFinder):
     """
 
     def visit(self, node: Node) -> None:
-        visitor = self._find_visitor_cached(type(node)) or self.__class__.generic_visit
+        visitor = self._find_visitor(type(node))
         visitor(self, node)
 
     def generic_visit(self, node: Node) -> None:
@@ -98,7 +103,7 @@ class ModelVisitor(VisitorFinder):
                 self.visit(value)
 
 
-class ModelTransformer(VisitorFinder):
+class ModelTransformer(VisitorFinder["Node|list[Node]|None"]):
     """NodeTransformer that supports matching nodes based on their base classes.
 
     See :class:`ModelVisitor` for explanation how this is different compared
@@ -107,7 +112,7 @@ class ModelTransformer(VisitorFinder):
     """
 
     def visit(self, node: Node) -> "Node|list[Node]|None":
-        visitor = self._find_visitor_cached(type(node)) or self.__class__.generic_visit
+        visitor = self._find_visitor(type(node))
         return visitor(self, node)
 
     def generic_visit(self, node: Node) -> "Node|list[Node]|None":
