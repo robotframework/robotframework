469dfe2473a52677416bc61a2701a9b17d886fa0
Daniel Biehl <dbiehl@live.de>
2023-10-25 20:58:14 +0200

    cleanup, remove unwanted typehints and some small cosmetic changes in VisitorFinder

--- src/robot/parsing/model/visitor.py
+++ src/robot/parsing/model/visitor.py
@@ -13,38 +13,29 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from abc import ABC
-from ast import AST
-from collections import defaultdict
-from typing import Any, Callable, Generic, Iterator, TypeVar
+from abc import ABC, abstractmethod
+from ast import AST, NodeTransformer, NodeVisitor
 
-from ...utils.notset import NOT_SET, NotSet
 from .statements import Node
 
-TVisitorResult = TypeVar("TVisitorResult")
 
-
-class VisitorFinder(ABC, Generic[TVisitorResult]):
-    __visitor_finder_cache: "dict[type[Any], Callable[[Node], Any]|None|NotSet]"
-    __default_visitor_method: "Callable[..., TVisitorResult]"
-
-    def __init_subclass__(cls, default_visitor_name: str = "generic_visit", **kwargs: Any) -> None:
+class VisitorFinder(ABC):
+    def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
-        cls.__visitor_finder_cache = defaultdict(lambda: NOT_SET)
-        cls.__default_visitor_method = getattr(cls, default_visitor_name)
+        cls.__visitor_finder_cache = {}
 
     @classmethod
-    def _find_visitor_class_method(cls, node_cls: "type[Any]") -> "Callable[..., TVisitorResult]|None":
+    def _find_visitor_class_method(cls, node_cls):
         if node_cls is AST:
             return None
         method_name = "visit_" + node_cls.__name__
         method = getattr(cls, method_name, None)
         if callable(method):
-            return method  # type: ignore[no-any-return]
+            return method
         if method_name == "visit_Return":
             method = getattr(cls, "visit_ReturnSetting", None)
             if callable(method):
-                return method  # type: ignore[no-any-return]
+                return method
         for base in node_cls.__bases__:
             method = cls._find_visitor_class_method(base)
             if method:
@@ -52,30 +43,18 @@ class VisitorFinder(ABC, Generic[TVisitorResult]):
         return None
 
     @classmethod
-    def _find_visitor(cls, node_cls: "type[Any]") -> Callable[..., TVisitorResult]:
-        result = cls.__visitor_finder_cache[node_cls]
-        if result is NOT_SET:
-            result = cls.__visitor_finder_cache[node_cls] = cls._find_visitor_class_method(node_cls)
-        return result or cls.__default_visitor_method  # type: ignore[return-value]
+    def _find_visitor(cls, node_cls):
+        if node_cls in cls.__visitor_finder_cache:
+            return cls.__visitor_finder_cache[node_cls]
+        result = cls.__visitor_finder_cache[node_cls] = cls._find_visitor_class_method(node_cls) or cls.generic_visit
+        return result
+
+    @abstractmethod
+    def generic_visit(self, node):
+        ...
 
 
-def _iter_field_values(node: Node) -> "Iterator[Node|list[Node]|None]":
-    for field in node._fields:
-        try:
-            yield getattr(node, field)
-        except AttributeError:
-            pass
-
-
-def iter_fields(node: Node) -> "Iterator[tuple[str, Node|list[Node]|None]]":
-    for field in node._fields:
-        try:
-            yield field, getattr(node, field)
-        except AttributeError:
-            pass
-
-
-class ModelVisitor(VisitorFinder[None]):
+class ModelVisitor(NodeVisitor, VisitorFinder):
     """NodeVisitor that supports matching nodes based on their base classes.
 
     In other ways identical to the standard `ast.NodeVisitor
@@ -92,18 +71,8 @@ class ModelVisitor(VisitorFinder[None]):
         visitor = self._find_visitor(type(node))
         visitor(self, node)
 
-    def generic_visit(self, node: Node) -> None:
-        for value in _iter_field_values(node):
-            if value is None:
-                continue
-            if isinstance(value, list):
-                for item in value:
-                    self.visit(item)
-            else:
-                self.visit(value)
 
-
-class ModelTransformer(VisitorFinder["Node|list[Node]|None"]):
+class ModelTransformer(NodeTransformer, VisitorFinder):
     """NodeTransformer that supports matching nodes based on their base classes.
 
     See :class:`ModelVisitor` for explanation how this is different compared
@@ -114,27 +83,3 @@ class ModelTransformer(VisitorFinder["Node|list[Node]|None"]):
     def visit(self, node: Node) -> "Node|list[Node]|None":
         visitor = self._find_visitor(type(node))
         return visitor(self, node)
-
-    def generic_visit(self, node: Node) -> "Node|list[Node]|None":
-        for field, old_value in iter_fields(node):
-            if old_value is None:
-                continue
-            if isinstance(old_value, list):
-                new_values = []
-                for value in old_value:
-                    new_value = self.visit(value)
-                    if new_value is None:
-                        continue
-                    if isinstance(new_value, list):
-                        new_values.extend(new_value)
-                        continue
-                    new_values.append(new_value)
-                old_value[:] = new_values
-            else:
-                new_node = self.visit(old_value)
-                if new_node is None:
-                    delattr(node, field)
-                else:
-                    setattr(node, field, new_node)
-
-        return node
