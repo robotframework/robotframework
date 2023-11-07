8ebb92e214c5008a3084161e16da2bbb7ef8aeb4
Pekka Klärck <peke@iki.fi>
2023-11-07 17:25:04 +0200

    ModelVisitor: Add note about caching, refactor
    Enhance ModelVisitor after changes in PR #4911:
    - Add note about visitor method caching to documentation. Fixes #4934.
    - Type hints. I know I objected them in my PR review, but I think
      these aren't too distractive.
    - Some refactoring.

--- src/robot/parsing/model/visitor.py
+++ src/robot/parsing/model/visitor.py
@@ -13,21 +13,32 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from abc import ABC, abstractmethod
-from ast import AST, NodeTransformer, NodeVisitor
+from ast import NodeTransformer, NodeVisitor
+from typing import Callable
 
 from .statements import Node
 
 
-class VisitorFinder(ABC):
+# Unbound method and thus needs `NodeVisitor` as `self`.
+VisitorMethod = Callable[[NodeVisitor, Node], 'None|Node|list[Node]']
+
+
+class VisitorFinder:
+    __visitor_cache: 'dict[type[Node], VisitorMethod]'
+
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
-        cls.__visitor_finder_cache = {}
+        cls.__visitor_cache = {}
 
     @classmethod
-    def _find_visitor_class_method(cls, node_cls):
-        if node_cls is AST:
-            return None
+    def _find_visitor(cls, node_cls: 'type[Node]') -> VisitorMethod:
+        if node_cls not in cls.__visitor_cache:
+            visitor = cls._find_visitor_from_class(node_cls)
+            cls.__visitor_cache[node_cls] = visitor or cls.generic_visit
+        return cls.__visitor_cache[node_cls]
+
+    @classmethod
+    def _find_visitor_from_class(cls, node_cls: 'type[Node]') -> 'VisitorMethod|None':
         method_name = "visit_" + node_cls.__name__
         method = getattr(cls, method_name, None)
         if callable(method):
@@ -37,39 +48,37 @@ class VisitorFinder(ABC):
             if callable(method):
                 return method
         for base in node_cls.__bases__:
-            method = cls._find_visitor_class_method(base)
-            if method:
-                return method
+            if issubclass(base, Node):
+                method = cls._find_visitor_from_class(base)
+                if method:
+                    return method
         return None
 
-    @classmethod
-    def _find_visitor(cls, node_cls):
-        if node_cls in cls.__visitor_finder_cache:
-            return cls.__visitor_finder_cache[node_cls]
-        result = cls.__visitor_finder_cache[node_cls] = cls._find_visitor_class_method(node_cls) or cls.generic_visit
-        return result
-
-    @abstractmethod
-    def generic_visit(self, node):
-        ...
+    def generic_visit(self, node: Node) -> 'None|Node|list[Node]':
+        raise NotImplementedError
 
 
 class ModelVisitor(NodeVisitor, VisitorFinder):
     """NodeVisitor that supports matching nodes based on their base classes.
 
-    In other ways identical to the standard `ast.NodeVisitor
+    The biggest difference compared to the standard `ast.NodeVisitor
     <https://docs.python.org/library/ast.html#ast.NodeVisitor>`__,
-    but allows creating ``visit_ClassName`` methods so that the ``ClassName``
-    is one of the base classes of the node. For example, this visitor method
-    matches all ``Statement`` nodes::
+    is that this class allows creating ``visit_ClassName`` methods so that
+    the ``ClassName`` is one of the base classes of the node. For example,
+    the following visitor method matches all node classes that extend
+    ``Statement``::
 
         def visit_Statement(self, node):
             ...
+
+    Another difference is that visitor methods are cached for performance
+    reasons. This means that dynamically adding ``visit_Something`` methods
+    does not work.
     """
 
     def visit(self, node: Node) -> None:
-        visitor = self._find_visitor(type(node))
-        visitor(self, node)
+        visitor_method = self._find_visitor(type(node))
+        visitor_method(self, node)
 
 
 class ModelTransformer(NodeTransformer, VisitorFinder):
@@ -80,6 +89,6 @@ class ModelTransformer(NodeTransformer, VisitorFinder):
     <https://docs.python.org/library/ast.html#ast.NodeTransformer>`__.
     """
 
-    def visit(self, node: Node) -> "Node|list[Node]|None":
-        visitor = self._find_visitor(type(node))
-        return visitor(self, node)
+    def visit(self, node: Node) -> 'None|Node|list[Node]':
+        visitor_method = self._find_visitor(type(node))
+        return visitor_method(self, node)
