b3ce554f2c389fba27d0989767b5f3aa4660a335
René <41592183+Snooz82@users.noreply.github.com>
2023-12-21 12:52:00 +0100

    implemented dark_light theme toggle button (#4990)
    Part of #3725.
    
    Signed-off-by: René <snooz@posteo.de>

--- src/robot/htmldata/rebot/view.js
+++ src/robot/htmldata/rebot/view.js
@@ -1,3 +1,20 @@
+const lightModeIcon = `
+<svg class="light-mode-icon" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24"
+    viewBox="0 0 24 24" fill="#000000">
+    <rect fill="none" height="24" width="24" />
+    <path
+        d="M12,7c-2.76,0-5,2.24-5,5s2.24,5,5,5s5-2.24,5-5S14.76,7,12,7L12,7z M2,13l2,0c0.55,0,1-0.45,1-1s-0.45-1-1-1l-2,0 c-0.55,0-1,0.45-1,1S1.45,13,2,13z M20,13l2,0c0.55,0,1-0.45,1-1s-0.45-1-1-1l-2,0c-0.55,0-1,0.45-1,1S19.45,13,20,13z M11,2v2 c0,0.55,0.45,1,1,1s1-0.45,1-1V2c0-0.55-0.45-1-1-1S11,1.45,11,2z M11,20v2c0,0.55,0.45,1,1,1s1-0.45,1-1v-2c0-0.55-0.45-1-1-1 C11.45,19,11,19.45,11,20z M5.99,4.58c-0.39-0.39-1.03-0.39-1.41,0c-0.39,0.39-0.39,1.03,0,1.41l1.06,1.06 c0.39,0.39,1.03,0.39,1.41,0s0.39-1.03,0-1.41L5.99,4.58z M18.36,16.95c-0.39-0.39-1.03-0.39-1.41,0c-0.39,0.39-0.39,1.03,0,1.41 l1.06,1.06c0.39,0.39,1.03,0.39,1.41,0c0.39-0.39,0.39-1.03,0-1.41L18.36,16.95z M19.42,5.99c0.39-0.39,0.39-1.03,0-1.41 c-0.39-0.39-1.03-0.39-1.41,0l-1.06,1.06c-0.39,0.39-0.39,1.03,0,1.41s1.03,0.39,1.41,0L19.42,5.99z M7.05,18.36 c0.39-0.39,0.39-1.03,0-1.41c-0.39-0.39-1.03-0.39-1.41,0l-1.06,1.06c-0.39,0.39-0.39,1.03,0,1.41s1.03,0.39,1.41,0L7.05,18.36z" />
+</svg>`
+
+const darkModeIcon = `
+<svg class="dark-mode-icon" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24"
+    viewBox="0 0 24 24" fill="#FFFFFF">
+    <rect fill="none" height="24" width="24" />
+    <path
+        d="M11.01,3.05C6.51,3.54,3,7.36,3,12c0,4.97,4.03,9,9,9c4.63,0,8.45-3.5,8.95-8c0.09-0.79-0.78-1.42-1.54-0.95 c-0.84,0.54-1.84,0.85-2.91,0.85c-2.98,0-5.4-2.42-5.4-5.4c0-1.06,0.31-2.06,0.84-2.89C12.39,3.94,11.9,2.98,11.01,3.05z" />
+</svg>`
+
+
 function removeJavaScriptDisabledWarning() {
     // Not using jQuery here for maximum speed
     document.getElementById('javascript-disabled').style.display = 'none';
@@ -39,6 +56,10 @@ function setTitle(suiteName, type) {
 function addHeader() {
     var generated = util.timestamp(window.output.generated);
     $.tmpl('<h1>${title}</h1>' +
+           '<button id=theme-toggle>' +
+             lightModeIcon +
+             darkModeIcon +
+           '</button>' +
            '<div id="generated">' +
              '<span>Generated<br>${generated}</span><br>' +
              '<span id="generated-ago">${ago} ago</span>' +
@@ -50,6 +71,8 @@ function addHeader() {
         ago: util.createGeneratedAgoString(generated),
         title: document.title
     }).appendTo($('#header'));
+    document.getElementById('theme-toggle')?.addEventListener('click', onClick);
+    reflectThemePreference();
 }
 
 function addReportOrLogLink(myType) {
@@ -188,3 +211,50 @@ function stopPropagation(event) {
     if (event.stopPropagation)
         event.stopPropagation();
 }
+
+const storageKey = 'theme-preference';
+const urlParams = new URLSearchParams(window.location.search);
+const theme = { value: getThemePreference() };
+
+window.matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', ({matches:isDark}) => {
+        theme.value = isDark ? 'dark' : 'light';
+        setThemePreference();
+    });
+
+window.addEventListener('storage', ({key, newValue}) => {
+    if (key === storageKey) {
+        theme.value = newValue === 'dark' ? 'dark' : 'light';
+        setThemePreference();
+    }
+})
+
+function getThemePreference() {
+    if (urlParams.has('theme')) {
+        const urlTheme = urlParams.get('theme') === 'dark' ? 'dark' : 'light';
+        localStorage.setItem(storageKey, urlTheme);
+        urlParams.delete('theme');
+        return urlTheme;
+    }
+    if (localStorage.getItem(storageKey))
+        return localStorage.getItem(storageKey) === 'dark' ? 'dark' : 'light';
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+}
+    
+function setThemePreference() {
+    localStorage.setItem(storageKey, theme.value);
+    reflectThemePreference();
+}
+
+function reflectThemePreference() {
+    document.body.setAttribute('data-theme', theme.value);
+    document.querySelector('#theme-toggle')?.setAttribute('aria-label', theme.value);
+    const event = new Event('theme-change', {value: theme.value});
+    document.dispatchEvent(event);
+}
+    
+function onClick() {
+    theme.value = theme.value === 'light' ? 'dark' : 'light';
+    setThemePreference();
+}
+
