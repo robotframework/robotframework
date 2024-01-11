robotframework/robotframework

commit 43fa40d78b2dcbebeb516fe148febec5ba027b60

Pass paths to listener v3 methods to `pathlib.Path` instances
Also use `Path` internally. Fixes #4988.

src/robot/reporting/logreportwriters.py
@@ -13,10 +13,11 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+from pathlib import Path
 from os.path import basename, splitext
 
 from robot.htmldata import HtmlFileWriter, ModelWriter, LOG, REPORT
-from robot.utils import file_writer, is_string
+from robot.utils import file_writer
 
 from .jswriter import JsResultWriter, SplitLogWriter
 
@@ -27,9 +28,9 @@ class _LogReportWriter:
     def __init__(self, js_model):
         self._js_model = js_model
 
-    def _write_file(self, path, config, template):
+    def _write_file(self, path: Path, config, template):
         outfile = file_writer(path, usage=self.usage) \
-            if is_string(path) else path  # unit test hook
+            if isinstance(path, Path) else path  # unit test hook
         with outfile:
             model_writer = RobotModelWriter(outfile, self._js_model, config)
             writer = HtmlFileWriter(outfile, model_writer)
@@ -39,26 +40,31 @@ class _LogReportWriter:
 class LogWriter(_LogReportWriter):
     usage = 'log'
 
-    def write(self, path, config):
+    def write(self, path: 'Path|str', config):
+        if isinstance(path, str):
+            path = Path(path)
         self._write_file(path, config, LOG)
         if self._js_model.split_results:
-            self._write_split_logs(splitext(path)[0])
+            self._write_split_logs(path)
 
-    def _write_split_logs(self, base):
+    def _write_split_logs(self, path: Path):
         for index, (keywords, strings) in enumerate(self._js_model.split_results,
                                                     start=1):
-            self._write_split_log(index, keywords, strings, '%s-%d.js' % (base, index))
+            name = f'{path.stem}-{index}.js'
+            self._write_split_log(index, keywords, strings, path.with_name(name))
 
-    def _write_split_log(self, index, keywords, strings, path):
+    def _write_split_log(self, index, keywords, strings, path: Path):
         with file_writer(path, usage=self.usage) as outfile:
             writer = SplitLogWriter(outfile)
-            writer.write(keywords, strings, index, basename(path))
+            writer.write(keywords, strings, index, path.stem)
 
 
 class ReportWriter(_LogReportWriter):
     usage = 'report'
 
-    def write(self, path, config):
+    def write(self, path: 'Path|str', config):
+        if isinstance(path, str):
+            path = Path(path)
         self._write_file(path, config, REPORT)
 
 

atest/testdata/output/listener_interface/v3.py
@@ -1,4 +1,3 @@
-import os
 import sys
 
 from robot.api import SuiteVisitor
@@ -74,23 +73,23 @@ message = log_message
 
 
 def output_file(path):
-    print("Output: %s" % os.path.basename(path), file=sys.__stderr__)
+    print(f"Output: {path.name}", file=sys.__stderr__)
 
 
 def log_file(path):
-    print("Log: %s" % os.path.basename(path), file=sys.__stderr__)
+    print(f"Log: {path.name}", file=sys.__stderr__)
 
 
 def report_file(path):
-    print("Report: %s" % os.path.basename(path), file=sys.__stderr__)
+    print(f"Report: {path.name}", file=sys.__stderr__)
 
 
 def debug_file(path):
-    print("Debug: %s" % os.path.basename(path), file=sys.__stderr__)
+    print(f"Debug: {path.name}", file=sys.__stderr__)
 
 
 def xunit_file(path):
-    print("Xunit: %s" % os.path.basename(path), file=sys.__stderr__)
+    print(f"Xunit: {path.name}", file=sys.__stderr__)
 
 
 def close():

atest/testresources/listeners/ListenAll.py
@@ -94,7 +94,7 @@ class ListenAll:
         self._out_file('Debug', path)
 
     def _out_file(self, name, path):
-        assert os.path.isabs(path)
+        assert isinstance(path, str) and os.path.isabs(path)
         self.outfile.write('%s: %s\n' % (name, os.path.basename(path)))
 
     def close(self):

src/robot/api/interfaces.py
@@ -879,22 +879,22 @@ class ListenerV3:
         if it is enabled.
         """
 
-    def output_file(self, path: str):
+    def output_file(self, path: Path):
         """Called after the output file has been created.
 
         At this point the file is guaranteed to be closed.
         """
 
-    def log_file(self, path: str):
+    def log_file(self, path: Path):
         """Called after the log file has been created."""
 
-    def report_file(self, path: str):
+    def report_file(self, path: Path):
         """Called after the report file has been created."""
 
-    def xunit_file(self, path: str):
+    def xunit_file(self, path: Path):
         """Called after the xunit compatible output file has been created."""
 
-    def debug_file(self, path: str):
+    def debug_file(self, path: Path):
         """Called after the debug file has been created."""
 
     def close(self):

src/robot/conf/settings.py
@@ -18,7 +18,6 @@ import os
 import random
 import string
 import sys
-import time
 import warnings
 from datetime import datetime
 from pathlib import Path
@@ -114,7 +113,7 @@ class _BaseSettings:
                 return str(value)
             return value if value and value.upper() != 'NONE' else None
         if name == 'OutputDir':
-            return abspath(value)
+            return Path(value).absolute()
         if name in ['SuiteStatLevel', 'ConsoleWidth']:
             return self._convert_to_positive_integer_or_default(name, value)
         if name == 'VariableFiles':
@@ -225,7 +224,7 @@ class _BaseSettings:
             LOGGER.error('Log file cannot be created if output.xml is disabled.')
             return None
         name = self._process_output_name(option, name)
-        path = abspath(os.path.join(self['OutputDir'], name))
+        path = self.output_directory / name
         create_destination_directory(path, f'{option.lower()} file')
         return path
 
@@ -366,27 +365,27 @@ class _BaseSettings:
         return '\n'.join(f'{name}: {self._opts[name]}' for name in sorted(self._opts))
 
     @property
-    def output_directory(self):
-        return self['OutputDir']
+    def output_directory(self) -> Path:
+        return Path(self['OutputDir'])
 
     @property
-    def output(self):
+    def output(self) -> 'Path|None':
         return self['Output']
 
     @property
-    def legacy_output(self):
+    def legacy_output(self) -> bool:
         return self['LegacyOutput']
 
     @property
-    def log(self):
+    def log(self) -> 'Path|None':
         return self['Log']
 
     @property
-    def report(self):
+    def report(self) -> 'Path|None':
         return self['Report']
 
     @property
-    def xunit(self):
+    def xunit(self) -> 'Path|None':
         return self['XUnit']
 
     @property

src/robot/output/debugfile.py
@@ -13,6 +13,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+from pathlib import Path
+
 from robot.errors import DataError
 from robot.utils import file_writer, seq2str2
 
@@ -55,7 +57,7 @@ class _DebugFileWriter(LoggerApi):
         self._end('SUITE', data.full_name, result.end_time, result.elapsed_time)
         self._separator('SUITE')
         if self._indent == 0:
-            LOGGER.debug_file(self._outfile.name)
+            LOGGER.debug_file(Path(self._outfile.name))
             self.close()
 
     def start_test(self, data, result):

src/robot/output/listeners.py
@@ -15,6 +15,7 @@
 
 import os.path
 from abc import ABC
+from pathlib import Path
 
 from robot.errors import DataError, TimeoutError
 from robot.model import BodyItem
@@ -314,11 +315,6 @@ class ListenerFacade(LoggerApi, ABC):
         self.listener = listener
         self.name = name
         self.library = library
-        self.output_file = self._get_method('output_file')
-        self.report_file = self._get_method('report_file')
-        self.log_file = self._get_method('log_file')
-        self.xunit_file = self._get_method('xunit_file')
-        self.debug_file = self._get_method('debug_file')
 
     def _get_method(self, name, fallback=None):
         for method_name in self._get_method_names(name):
@@ -399,6 +395,12 @@ class ListenerV3Facade(ListenerFacade):
         # Messages
         self.log_message = get('log_message')
         self.message = get('message')
+        # Result files
+        self.output_file = self._get_method('output_file')
+        self.report_file = self._get_method('report_file')
+        self.log_file = self._get_method('log_file')
+        self.xunit_file = self._get_method('xunit_file')
+        self.debug_file = self._get_method('debug_file')
         # Close
         self.close = get('close')
 
@@ -455,6 +457,12 @@ class ListenerV2Facade(ListenerFacade):
         # Messages
         self._log_message = self._get_method('log_message')
         self._message = self._get_method('message')
+        # Result files
+        self._output_file = self._get_method('output_file')
+        self._report_file = self._get_method('report_file')
+        self._log_file = self._get_method('log_file')
+        self._xunit_file = self._get_method('xunit_file')
+        self._debug_file = self._get_method('debug_file')
         # Close
         self._close = self._get_method('close')
 
@@ -598,6 +606,21 @@ class ListenerV2Facade(ListenerFacade):
 
     def message(self, message):
         self._message(self._message_attributes(message))
+
+    def output_file(self, path: Path):
+        self._output_file(str(path))
+
+    def report_file(self, path: Path):
+        self._report_file(str(path))
+
+    def log_file(self, path: Path):
+        self._log_file(str(path))
+
+    def xunit_file(self, path: Path):
+        self._xunit_file(str(path))
+
+    def debug_file(self, path: Path):
+        self._debug_file(str(path))
 
     def _suite_attrs(self, data, result, end=False):
         attrs = {

src/robot/output/loggerapi.py
@@ -13,6 +13,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+from pathlib import Path
 from typing import Literal, TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -163,35 +164,35 @@ class LoggerApi:
     def message(self, message: 'model.Message'):
         pass
 
-    def output_file(self, path: str):
+    def output_file(self, path: Path):
         """Called when XML output file is closed.
 
         Calls :meth:`result_file` by default.
         """
         self.result_file('Output', path)
 
-    def report_file(self, path: str):
+    def report_file(self, path: Path):
         """Called when report file is closed.
 
         Calls :meth:`result_file` by default.
         """
         self.result_file('Report', path)
 
-    def log_file(self, path: str):
+    def log_file(self, path: Path):
         """Called when log file is closed.
 
         Calls :meth:`result_file` by default.
         """
         self.result_file('Log', path)
 
-    def xunit_file(self, path: str):
+    def xunit_file(self, path: Path):
         """Called when xunit file is closed.
 
         Calls :meth:`result_file` by default.
         """
         self.result_file('XUnit', path)
 
-    def debug_file(self, path: str):
+    def debug_file(self, path: Path):
         """Called when debug file is closed.
 
         Calls :meth:`result_file` by default.
@@ -199,7 +200,7 @@ class LoggerApi:
         self.result_file('Debug', path)
 
     def result_file(self, kind: Literal['Output', 'Report', 'Log', 'XUnit', 'Debug'],
-                    path: str):
+                    path: Path):
         """Called when any result file is closed by default.
 
         ``kind`` specifies the file type. This method is not called if a result

commit 800eb5d4f5e560ae6a53dbc47a715abc96b1b0cd

Fix --splitlog.
The regression was introduced in
43fa40d78b2dcbebeb516fe148febec5ba027b60.

Thanks to Emile Zwiggelaar for reporting this on our Slack.

atest/robot/cli/runner/output_files.robot
@@ -49,6 +49,9 @@ Outputs Into Different Dirs
 Split Log
     Run Tests Without Processing Output    --outputdir ${CLI OUTDIR} --output o.xml --report r.html --log l.html --splitlog    ${TESTFILE}
     Directory Should Contain    ${CLI OUTDIR}    l-1.js    l-2.js    l.html    o.xml    r.html
+    FOR    ${name}    IN    l-1.js    l-2.js
+        File Should Contain    ${CLI OUTDIR}/${name}    window.fileLoading.notify("${name}");
+    END
 
 Non-writable Output File
     Create Directory    ${CLI OUTDIR}/diréctöry.xml

src/robot/reporting/logreportwriters.py
@@ -56,7 +56,7 @@ class LogWriter(_LogReportWriter):
     def _write_split_log(self, index, keywords, strings, path: Path):
         with file_writer(path, usage=self.usage) as outfile:
             writer = SplitLogWriter(outfile)
-            writer.write(keywords, strings, index, path.stem)
+            writer.write(keywords, strings, index, path.name)
 
 
 class ReportWriter(_LogReportWriter):