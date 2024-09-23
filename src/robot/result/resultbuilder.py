e88bba7cbfa70618e615372e9809790dc7ad4cf7
Pekka Klärck <peke@iki.fi>
2024-09-23 18:04:37 +0300

    Clarify that ExecutionResultBuilder only supports XML, not JSON

--- src/robot/result/resultbuilder.py
+++ src/robot/result/resultbuilder.py
@@ -21,7 +21,6 @@ from .executionresult import CombinedResult, is_json_source, Result
 from .flattenkeywordmatcher import (create_flatten_message, FlattenByNameMatcher,
                                     FlattenByTypeMatcher, FlattenByTags)
 from .merger import Merger
-from .model import TestSuite
 from .xmlelementhandlers import XmlElementHandler
 
 
@@ -104,7 +103,7 @@ def _xml_result(source, options):
 
 
 class ExecutionResultBuilder:
-    """Builds :class:`~.executionresult.Result` objects based on output files.
+    """Builds :class:`~.executionresult.Result` objects based on XML output files.
 
     Instead of using this builder directly, it is recommended to use the
     :func:`ExecutionResult` factory method.
