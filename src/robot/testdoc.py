bb21cafad7625a8d15244e5fa7fd236b48b47b59
Franz Haas <franz.dominik.haas@gmail.com>
2025-03-27 21:44:39 +0100

    - call function...

--- src/robot/testdoc.py
+++ src/robot/testdoc.py
@@ -43,7 +43,7 @@ if __name__ == '__main__' and 'robot' not in sys.modules:
         logging.warning("depricated running without having python path setup proactively, please either install or configure python path before running testdoc.py")
 
 import robot as __ignore
-assert Path(__file__).absolute.parent() == Path(__ignore.__file__).absolute.parent(), "you run run.py using a robot package from a different path... this is not suported"
+assert Path(__file__).absolute().parent() == Path(__ignore.__file__).absolute().parent(), "you run run.py using a robot package from a different path... this is not suported"
 
 
 from robot.conf import RobotSettings
