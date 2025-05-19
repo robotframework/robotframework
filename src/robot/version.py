0f5cf25344d5df43cbf2bb90fbdc42a60994f455
Pekka Klärck <peke@iki.fi>
2025-05-19 23:43:57 +0300

    Updated version to 7.3rc2

--- src/robot/version.py
+++ src/robot/version.py
@@ -18,7 +18,7 @@ import sys
 
 # Version number typically updated by running `invoke set-version <version>`.
 # Run `invoke --help set-version` or see tasks.py for details.
-VERSION = "7.3rc2.dev1"
+VERSION = "7.3rc2"
 
 
 def get_version(naked=False):
