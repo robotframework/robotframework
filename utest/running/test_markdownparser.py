007bc0ccbb5cf6726072af225c3cc1aa3eac67f9
Pekka Klärck <peke@iki.fi>
2026-04-22 02:06:07 +0300

    Enhance MarkdownParser unit tests
    - Use helper methods for verification to avoid duplication.
    - Use triple quoted strings to make data easier to read.
    - Remove comments made redundant by the above change.
    - Add some more tests.
    
    Part of #5603.

--- utest/running/test_markdownparser.py
+++ utest/running/test_markdownparser.py
@@ -5,6 +5,7 @@ from robot.utils.asserts import assert_equal
 
 
 class FakeFileReader:
+
     def __init__(self, text):
         self._lines = text.splitlines(keepends=True)
 
@@ -12,193 +13,229 @@ class FakeFileReader:
         return self._lines
 
 
-def _read(text):
-    parser = MarkdownParser()
-    return parser._read_markdown_data(FakeFileReader(text))
-
-
 class TestReadMarkdownData(unittest.TestCase):
 
-    def test_empty_file(self):
-        # (empty file)
-        assert_equal(_read(""), "")
+    def assert_no_data(self, md):
+        self._assert(md, "")
 
-    def test_no_robot_blocks(self):
-        # # Title
-        #
-        # Some text.
-        assert_equal(_read("# Title\n\nSome text.\n"), "")
+    def assert_data(self, md, expected="data\n"):
+        self._assert(md, expected)
+
+    def _assert(self, md, expected):
+        parser = MarkdownParser()
+        actual = parser._read_markdown_data(FakeFileReader(md))
+        assert_equal(actual, expected)
+
+    def test_empty(self):
+        self.assert_no_data("")
+
+    def test_no_blocks(self):
+        self.assert_no_data(
+            """
+# Title
+
+Some text.
+"""
+        )
 
     def test_single_robotframework_block(self):
-        # ```robotframework
-        # *** Test Cases ***
-        # My Test    Log    Hello, RF!
-        # ```
-        md = (
-            "```robotframework\n*** Test Cases ***\nMy Test    Log    Hello, RF!\n```\n"
+        self.assert_data(
+            """
+```robotframework
+data
+```
+"""
         )
-        assert_equal(_read(md), "*** Test Cases ***\nMy Test    Log    Hello, RF!\n")
 
     def test_single_robot_block(self):
-        # ```robot
-        # *** Test Cases ***
-        # My Test    Log    Hello, Robot!
-        # ```
-        md = "```robot\n*** Test Cases ***\nMy Test    Log    Hello, Robot!\n```\n"
-        assert_equal(_read(md), "*** Test Cases ***\nMy Test    Log    Hello, Robot!\n")
-
-    def test_non_robot_code_block_ignored(self):
-        # ```python
-        # def foo(): pass
-        # ```
-        assert_equal(_read("```python\ndef foo(): pass\n```\n"), "")
-
-    def test_multiple_blocks_separated_by_empty_line(self):
-        # ```robotframework
-        # *** Settings ***
-        # Selenium Library
-        # ```
-        #
-        # ```robotframework
-        # *** Test Cases ***
-        # T    Log    x
-        # ```
-        md = (
-            "```robotframework\n*** Settings ***\nSelenium Library\n```\n"
-            ""
-            "```robotframework\n*** Test Cases ***\nT    Log    x\n```\n"
+        self.assert_data(
+            """
+```robot
+data
+```
+"""
         )
-        result = _read(md)
-        assert_equal(
-            result,
-            "*** Settings ***\nSelenium Library\n"
-            "\n"
-            "*** Test Cases ***\nT    Log    x\n",
+
+    def test_unrecognized_block(self):
+        self.assert_no_data(
+            """
+```python
+def foo():
+    pass
+```
+
+```
+no data
+```
+"""
+        )
+
+    def test_multiple_blocks(self):
+        self.assert_data(
+            """
+```robotframework
+data 1
+```
+
+```
+no data
+```
+
+```robot
+data 2
+```
+""",
+            "data 1\n\ndata 2\n",
         )
 
     def test_text_outside_blocks_ignored(self):
-        # Ignored
-        # ```robotframework
-        # *** Test Cases ***
-        # ```
-        # Also ignored
-        md = "Ignored\n```robotframework\n*** Test Cases ***\n```\nAlso ignored\n"
-        assert_equal(_read(md), "*** Test Cases ***\n")
+        self.assert_data(
+            """
+Ignored
+```robotframework
+data
+```
+Also ignored
+"""
+        )
 
-    def test_block_fence_with_leading_whitespace_recognized(self):
-        #   ```robotframework
-        # *** Test Cases ***
-        #   ```
-        md = "  ```robotframework\n*** Test Cases ***\n  ```\n"
-        assert_equal(_read(md), "*** Test Cases ***\n")
+    def test_fence_with_leading_whitespace(self):
+        self.assert_data(
+            """
+  ```robotframework
+data
+  ```
+"""
+        )
 
-    def test_wrong_fence_not_recognized(self):
-        # ```robot-extension
-        # *** Test Cases ***
-        # ```
-        md = "```robot-extension\n*** Test Cases ***\n```\n"
-        assert_equal(_read(md), "")
+    def test_leading_whitespace_must_not_match(self):
+        self.assert_data(
+            """
+                 ```robotframework
+           data
+     ```
+"""
+        )
 
-    def test_no_trailing_newline_after_last_block(self):
-        # ```robotframework
-        # *** Test Cases ***
-        # ``` (no trailing newline)
-        md = "```robotframework\n*** Test Cases ***\n```"
-        assert_equal(_read(md), "*** Test Cases ***\n")
+    def test_whitespace_before_language(self):
+        self.assert_data(
+            """
+```       robotframework
+data
+```
+"""
+        )
+
+    def test_content_after_language(self):
+        self.assert_data(
+            """
+~~~robot start=3
+data
+~~~
+"""
+        )
+
+    def test_no_trailing_newline(self):
+        self.assert_data(
+            """
+```robotframework
+data
+```"""
+        )
 
     def test_empty_block(self):
-        # ```robotframework
-        # ```
-        md = "```robotframework\n```\n"
-        assert_equal(_read(md), "")
-
-    def test_unclosed_block_includes_content_until_eof(self):
-        # ```robotframework
-        # *** Test Cases ***
-        # My Test    Log    Hello, Robot!  (no closing fence)
-        md = "```robotframework\n*** Test Cases ***\nMy Test    Log    Hello, Robot!"
-        assert_equal(_read(md), "*** Test Cases ***\nMy Test    Log    Hello, Robot!")
-
-    def test_indented_block_content_is_dedented(self):
-        # - A list item:
-        #   ```robotframework
-        #   *** Test Cases ***
-        #   My Test    Log    Hello
-        #   ```
-        md = (
-            "- A list item:\n"
-            "  ```robotframework\n"
-            "  *** Test Cases ***\n"
-            "  My Test    Log    Hello\n"
-            "  ```\n"
-        )
-        assert_equal(_read(md), "*** Test Cases ***\nMy Test    Log    Hello\n")
-
-    def test_two_indented_blocks_content_is_dedented(self):
-        # - First item:
-        #   ```robotframework
-        #   *** Settings ***
-        #   Library    Collections
-        #   ```
-        #
-        # - Second item:
-        #   ```robotframework
-        #   *** Test Cases ***
-        #   My Test    Log    Hello
-        #   ```
-        md = (
-            "- First item:\n"
-            "  ```robotframework\n"
-            "  *** Settings ***\n"
-            "  Library    Collections\n"
-            "  ```\n"
-            "\n"
-            "- Second item:\n"
-            "  ```robotframework\n"
-            "  *** Test Cases ***\n"
-            "  My Test    Log    Hello\n"
-            "  ```\n"
-        )
-        assert_equal(
-            _read(md),
-            "*** Settings ***\nLibrary    Collections\n"
-            "\n"
-            "*** Test Cases ***\nMy Test    Log    Hello\n",
+        self.assert_no_data(
+            """
+```robotframework
+```
+""",
         )
 
-    def test_tilde_fence_blocks(self):
-        # ~~~robotframework
-        # *** Test Cases ***
-        # My Test    Log    Tilde Fence
-        # ~~~
-        md = "~~~robotframework\n*** Test Cases ***\nMy Test    Log    Tilde Fence\n~~~\n"
-        assert_equal(_read(md), "*** Test Cases ***\nMy Test    Log    Tilde Fence\n")
+    def test_unclosed_block(self):
+        self.assert_data(
+            """
+```robotframework
+data
+"""
+        )
+
+    def test_dedent(self):
+        self.assert_data(
+            """
+- An example:
+  ```robotframework
+  left
+      indent
+          more indent
+  ```
+""",
+            """\
+left
+    indent
+        more indent
+""",
+        )
+
+    def test_tilde_fence(self):
+        self.assert_data(
+            """
+~~~robotframework
+data
+~~~
+"""
+        )
+
+    def test_fence_styles_must_match(self):
+        self.assert_data(
+            """
+~~~robotframework
+```
+~~~
+
+```robot
+~~~
+```
+""",
+            "```\n\n~~~\n",
+        )
 
     def test_longer_fence(self):
-        # ``````robot
-        # *** Test Cases ***
-        # My Test    Log    Longer Fence
-        # ``````
-        md = "``````robot\n*** Test Cases ***\nMy Test    Log    Longer Fence\n``````\n"
-        assert_equal(_read(md), "*** Test Cases ***\nMy Test    Log    Longer Fence\n")
-
-    def test_longer_closure_fence(self):
-        # ```robot
-        # *** Test Cases ***
-        # My Test    Log    Longer Fence
-        # ````
-        md = "```robot\n*** Test Cases ***\nMy Test    Log    Longer Fence\n````\n"
-        assert_equal(_read(md), "*** Test Cases ***\nMy Test    Log    Longer Fence\n")
-
-    def test_fence_with_info_string(self):
-        # ~~~robot start=3
-        # *** Test Cases ***
-        # My Test    Log    Info String
-        # ~~~
-        md = (
-            "~~~robot start=3\n*** Test Cases ***\nMy Test    Log    Info String\n~~~\n"
+        self.assert_data(
+            """
+``````robot
+data
+``````
+"""
         )
-        assert_equal(_read(md), "*** Test Cases ***\nMy Test    Log    Info String\n")
+
+    def test_longer_close_fence(self):
+        self.assert_data(
+            """
+```robot
+data
+``````````
+"""
+        )
+
+    def test_too_short_open_fence(self):
+        self.assert_no_data(
+            """
+``robot
+no data
+```
+"""
+        )
+
+    def test_too_short_close_fence(self):
+        self.assert_data(
+            """
+```robot
+data
+``
+more?
+""",
+            "data\n``\nmore?\n",
+        )
 
 
 if __name__ == "__main__":
