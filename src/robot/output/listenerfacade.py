40311165ab5d05045172ad54b2ce0e9ab3ec5001
Oliver Boehmer <oli@spine.de>
2026-05-10 14:52:16 +0200

    add future import

--- src/robot/output/listenerfacade.py
+++ src/robot/output/listenerfacade.py
@@ -13,6 +13,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+from __future__ import annotations
+
 from abc import ABC
 from collections.abc import Callable
 
