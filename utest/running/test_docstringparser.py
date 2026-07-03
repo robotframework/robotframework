befc4bb311f4598e41244ebb23500b6961b907a1
Tatu Aalto <2665023+aaltat@users.noreply.github.com>
2026-07-03 15:01:57 +0300

    feat: Google style docstring parsing (#5684)
    Implements (and tests) the parser component of #5604. Still needs to be integrated with rest of the code..

---
+++ utest/running/test_docstringparser.py
@@ -0,0 +1,442 @@
+import unittest
+from dataclasses import dataclass
+
+import yaml
+
+from robot.running.docstringparser import parse_docstring
+from robot.utils.asserts import assert_equal
+
+
+@dataclass
+class Expected:
+    doc: str
+    args: "dict[str, str]"
+    returns: str = ""
+
+
+class DocstringParsing(unittest.TestCase):
+
+    def verify(self, data):
+        tests_run = False
+        for name, docstring, expected in self.get_data(data):
+            with self.subTest(msg=name):
+                parsed = parse_docstring(docstring)
+                assert_equal(parsed.doc, expected.doc, formatter=repr)
+                assert_equal(parsed.returns, expected.returns, formatter=repr)
+                assert_equal(list(parsed.args), list(expected.args))
+                for key in parsed.args:
+                    assert_equal(parsed.args[key], expected.args[key], formatter=repr)
+            tests_run = True
+        if not tests_run:
+            raise ValueError("No tests found in data")
+
+    def get_data(self, data):
+        for test in data.split("\n## ")[1:]:
+            name, test = test.lstrip().split("\n", 1)
+            docstring, expected = test.split("- - -\n")
+            expected = yaml.safe_load(expected) or {}
+            expected = Expected(
+                expected.get("doc", "").rstrip(),
+                {k: v.rstrip() for k, v in expected.get("args", {}).items()},
+                expected.get("returns", "").rstrip(),
+            )
+            yield name, docstring, expected
+
+    def test_sections_and_boundaries(self):
+        self.verify(
+            """
+## Empty
+- - -
+
+## Documentation only
+Doc
+
+Some more documentation
+- - -
+doc: |
+    Doc
+
+    Some more documentation
+
+## Arguments only
+Args:
+    name: Documentation
+- - -
+args:
+    name: Documentation
+
+## Returns only
+Returns:
+    The result value.
+- - -
+returns: The result value.
+
+## All sections
+Doc
+
+Args:
+    name: Documentation
+
+Bit more documentation
+
+Returns:
+    The result value.
+
+Even more documentation
+- - -
+doc: |
+    Doc
+
+    Bit more documentation
+
+    Even more documentation
+args:
+    name: Documentation
+returns:
+    The result value.
+
+## Empty sections
+Args:
+
+Returns:
+- - -
+
+## Single space indentation ends section
+Args:
+    name: Documentation
+ Part of overall documentation.
+
+Returns:
+ This is also part of overall doc.
+- - -
+doc:
+    "Part of overall documentation.\\n\\n This is also part of overall doc."
+args:
+    name: Documentation
+
+## Section must be separated by blank line
+Doc
+Args:
+    name: Part of Doc
+- - -
+doc: |
+    Doc
+    Args:
+        name: Part of Doc
+
+## Headers are case insensitive
+args:
+    name: Documentation
+
+RETURNS: The result value.
+- - -
+args:
+    name: Documentation
+returns: The result value.
+
+## "Arguments" header
+Arguments:
+    name: Documentation
+- - -
+args:
+    name: Documentation
+
+## "Parameters" header
+PaRaMeTeRS:
+    name: Documentation
+- - -
+args:
+    name: Documentation
+
+## "Return" header
+Return:
+    The result value.
+- - -
+returns: The result value.
+
+## "Yields" header
+Yields:
+    The result value.
+- - -
+returns: The result value.
+
+## Unrecognized sections
+Doc
+
+Args:
+    name: Documentation
+
+Raises:
+    ValueError: If bad.
+- - -
+doc: |
+    Doc
+
+    Raises:
+        ValueError: If bad.
+args:
+    name: Documentation
+"""
+        )
+
+    def test_documentation(self):
+        self.verify(
+            """
+## One line doc
+Doc
+- - -
+doc:
+    Doc
+
+## Multiline doc
+First line
+
+Second paragraph
+on multiple lines.
+- - -
+doc: |
+    First line
+
+    Second paragraph
+    on multiple lines.
+
+## Whitespace handling
+First line
+
+
+    def example():
+        pass
+
+The end
+- - -
+doc: |
+    First line
+
+
+        def example():
+            pass
+
+    The end
+
+## Leading whitespace stripped
+
+    Doc
+- - -
+doc:
+    Doc
+"""
+        )
+
+    def test_arguments(self):
+        self.verify(
+            """
+## Basics
+Args:
+    name: Documentation
+    name2: https://example.com
+
+    name3: Documentation 3
+- - -
+args:
+    name: Documentation
+    name2: https://example.com
+    name3: Documentation 3
+
+## Indentation handling
+Args:
+    empty:
+    inline: doc
+     off_by_one: parsed as own arg
+      off_by_two: part of off_by_one
+    own_line:
+      own line
+    multiline: Start.
+
+      Doc continues.
+    indentation1:
+      Starts on own line.
+
+            def example():
+                pass
+
+      The end
+    indentation2: Starts on same line.
+      Indentation is
+        preserved.
+
+    indentation3:
+            Indentation
+        is preserved.
+- - -
+args:
+    empty: ""
+    inline: doc
+    off_by_one: |
+        parsed as own arg
+        off_by_two: part of off_by_one
+    own_line: own line
+    multiline: |
+        Start.
+
+        Doc continues.
+    indentation1: |
+        Starts on own line.
+
+              def example():
+                  pass
+
+        The end
+    indentation2: |
+        Starts on same line.
+        Indentation is
+          preserved.
+    indentation3:
+        "    Indentation\\nis preserved."
+
+## Multiple args sections
+Args:
+    name1: Documentation 1
+    name2: Documentation 2
+
+ARGS:
+  name1: Overwrite 1
+  name3: Documentation 3
+- - -
+args:
+    name1: Overwrite 1
+    name2: Documentation 2
+    name3: Documentation 3
+
+## Invalid indentation
+Args:
+    name: Not found
+  This sets indentation level
+- - -
+args:
+    <no-name>: "  name: Not found\\nThis sets indentation level"
+
+## Invalid content
+Args:
+    invalid content
+    name: found
+    part of name
+- - -
+args:
+    <no-name>:
+        invalid content
+    name: |
+        found
+        part of name
+"""
+        )
+
+    def test_normalize_name(self):
+        self.verify(
+            """
+## Remove types
+Args:
+    name (str): The name
+    other (list[str]): Parameterized
+    union (int | str): The union argument
+- - -
+args:
+    name: The name
+    other: Parameterized
+    union: The union argument
+
+## Normalize varargs and kwargs
+Args:
+    *args: varargs
+    **kws: kwargs
+- - -
+args:
+    args: varargs
+    kws: kwargs
+
+## Normalize Robot args
+Args:
+    ${a  r  g}: argument
+    @{args}: varargs
+    &{kws}: kwargs
+- - -
+args:
+    a  r  g: argument
+    args: varargs
+    kws: kwargs
+
+## Keyword-only marker is removed
+Args:
+    arg: normal argument
+    *: kw only marker
+    kwo: kw only argument
+- - -
+args:
+    arg: normal argument
+    kwo: kw only argument
+
+## Empty name after normalization
+Doc
+
+Args:
+    ${  }: Documentation
+- - -
+doc:
+    Doc
+args:
+    <no-name>: Documentation
+"""
+        )
+
+    def test_returns(self):
+        self.verify(
+            """
+## Basics
+Returns:
+    The result value.
+- - -
+returns: The result value.
+
+## Multiline
+Returns:
+    First line of return description
+      that continues here.
+
+        Indentation is
+          preserved.
+
+Args:
+    name: Documentation
+- - -
+args:
+    name: Documentation
+returns: |
+    First line of return description
+      that continues here.
+
+        Indentation is
+          preserved.
+
+## Inline
+Returns: The result value.
+- - -
+returns: The result value.
+
+## Inline and multiline
+Returns: First line of return description
+    that continues here.
+
+        Indentation is
+          preserved.
+- - -
+returns: |
+    First line of return description
+    that continues here.
+
+        Indentation is
+          preserved.
+"""
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
