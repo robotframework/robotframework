41148db0aba130c42dc360dfc4b7034e14ed5156
Pekka Klärck <peke@iki.fi>
2026-08-10 17:03:21 +0300

    Switch from listener API 2 to 3

--- atest/robot/standard_libraries/builtin/listener_using_builtin.py
+++ atest/robot/standard_libraries/builtin/listener_using_builtin.py
@@ -1,9 +1,6 @@
 from robot.libraries.BuiltIn import BuiltIn
 
-BIN = BuiltIn()
-ROBOT_LISTENER_API_VERSION = 2
 
-
-def start_keyword(*args):
-    if BIN.get_variables()["${TESTNAME}"] == "Listener Using BuiltIn":
-        BIN.set_test_variable("${SET BY LISTENER}", "quux")
+def start_keyword(data, result):
+    if BuiltIn().get_variables()["${TESTNAME}"] == "Listener Using BuiltIn":
+        BuiltIn().set_test_variable("${SET BY LISTENER}", "quux")
