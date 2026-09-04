0ffeabcd07cd6bf7ca4399984d8c6bf36d236a7c
Pekka Klärck <peke@iki.fi>
2026-09-04 11:23:08 +0300

    Back to dev version

--- src/robot/version.py
+++ src/robot/version.py
@@ -18,7 +18,7 @@ import sys
 
 # Version number typically updated by running `invoke set-version <version>`.
 # Run `invoke --help set-version` or see tasks.py for details.
-VERSION = "7.5rc1"
+VERSION = "7.5rc2.dev1"
 
 
 def get_version(naked=False):
